e627efbc038275466b9ca78f994c8d9f1431076e
Mahmood Akhtar <mahmood233@hotmail.com>
2018-10-23 15:40:00 +0100

    First commit of a sample .Net Core Web App and an xUint Tests project.

---
+++ src/Dfc.CourseDirectory.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dfc.CourseDirectory.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
