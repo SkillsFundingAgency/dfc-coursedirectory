00ead4389efc97e6aa2302007f1368446fe04f6f
Mahmood Akhtar <mahmood233@hotmail.com>
2018-10-29 16:43:24 +0000

    Added GovukPhaseBanner view component. Tried to get the tag helper to invoke view component via the mark-up does not currently when rendering the page. However invoking the view component via the razor syntax works fine.

---
+++ src/Dfc.CourseDirectory.Web/Components/GovukPhaseBanner/GovukPhaseBannerModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.Web.Components.GovukPhaseBanner
+{
+    public class GovukPhaseBannerModel
+    {
+        private const string TAG = "alpha";
+        private const string LINK_URL = "#";
+        private const string LINK_TEXT = "feedback";
+
+        public bool IsVisible { get; set; }
+        public string Tag { get; set; }
+        public string LinkUrl { get; set; }
+        public string LinkText { get; set; }
+
+        public GovukPhaseBannerModel(bool isVisible = false) 
+            : this(isVisible, TAG, LINK_URL, LINK_TEXT) { }
+
+        public GovukPhaseBannerModel(
+            bool isVisible,
+            string tag,
+            string linkUrl,
+            string linkText)
+        {
+            if (string.IsNullOrWhiteSpace(tag)) throw new ArgumentException("Cannot be null, empty or whitespace only.", nameof(tag));
+            if (string.IsNullOrWhiteSpace(linkUrl)) throw new ArgumentException("Cannot be null, empty or whitespace only.", nameof(linkUrl));
+            if (string.IsNullOrWhiteSpace(linkText)) throw new ArgumentException("Cannot be null, empty or whitespace only.", nameof(linkText));
+
+            IsVisible = isVisible;
+            Tag = tag;
+            LinkUrl = linkUrl;
+            LinkText = linkText;
+        }
+    }
+}
