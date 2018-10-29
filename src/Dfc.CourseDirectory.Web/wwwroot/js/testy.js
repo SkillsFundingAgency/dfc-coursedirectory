96c13fc2e3d16439b50f0e3692d9341ae87b2b97
Mahmood Akhtar <mahmood233@hotmail.com>
2018-10-29 13:13:37 +0000

    Some house-keeping with the package.josn file and the Views/Shared fofder. Added jquery.validate.hooks.js. Separated display concerns into partials and layout files.

--- src/Dfc.CourseDirectory.Web/wwwroot/js/testy.js
+++
@@ -1,9 +0,0 @@
-﻿//testy.js
-function myfunction() {
-    var someVar = "some value";
-    var someOther = "some other var";
-    //var notused = 123;
-    return someVar + someOther;
-}
-
-myfunction();
