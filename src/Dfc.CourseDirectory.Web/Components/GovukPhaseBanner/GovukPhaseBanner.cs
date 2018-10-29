9dac926084c6842586698003cf23d9f42aeaa633
Mahmood Akhtar <mahmood233@hotmail.com>
2018-10-29 16:43:24 +0000

    Added GovukPhaseBanner view component. Tried to get the tag helper to invoke view component via the mark-up does not currently when rendering the page. However invoking the view component via the razor syntax works fine.

---
+++ src/Dfc.CourseDirectory.Web/Components/GovukPhaseBanner/GovukPhaseBanner.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.Web.Components.GovukPhaseBanner
+{
+    public class GovukPhaseBanner : ViewComponent
+    {
+        public GovukPhaseBanner() { }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+            bool isVisible,
+            string tag,
+            string linkUrl,
+            string linkText)
+        {
+            return View("~/Components/GovukPhaseBanner/Default.cshtml", new GovukPhaseBannerModel(isVisible));
+        }
+    }
+}
