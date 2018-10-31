33276aea36d2732473fb2f8517d15a42147a48ef
Mahmood Akhtar <mahmood233@hotmail.com>
2018-10-31 12:11:16 +0000

    Starting to intergrate Lars Search Api as a service and added a empty *.Services.Tests project.

---
+++ src/Dfc.CourseDirectory.Services/Interfaces/ILarsSearchFilterBuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace Dfc.CourseDirectory.Services.Interfaces
+{
+    public interface ILarsSearchFilterBuilder
+    {
+        ILarsSearchFilterBuilder And(string value);
+        ILarsSearchFilterBuilder Or(string value);
+        ILarsSearchFilterBuilder Not(string value);
+        ILarsSearchFilterBuilder StartsWith(string value);
+        ILarsSearchFilterBuilder EqualTo(string value);
+        ILarsSearchFilterBuilder NotEqualTo(string value);
+        ILarsSearchFilterBuilder GreaterThan(string value);
+        ILarsSearchFilterBuilder LessThan(string value);
+        ILarsSearchFilterBuilder GreatherThanOrEqualTo(string value);
+        ILarsSearchFilterBuilder LessThanOrEqualTo(string value);
+        string Build();
+    }
+}
