66c3bc232dfd4d1946f524772b067e0360250d44
Mahmood Akhtar <mahmood233@hotmail.com>
2018-11-13 17:05:34 +0000

    Working on the Pagination view component and commenting out sample unit tests.

--- src/Dfc.CourseDirectory.Web/ViewComponents/Pagination/Pagination.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Pagination/Pagination.cs
@@ -1,18 +1,120 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Dfc.CourseDirectory.Web.ViewComponents.Pagination
 {
     public class Pagination : ViewComponent
     {
-        public IViewComponentResult Invoke(PaginationModel model)
+        public IViewComponentResult Invoke(
+            string url,
+            string paramName,
+            int currentPage,
+            int totalItems,
+            int itemsPerPage,
+            int pagesToDisplay,
+            bool isSliding)
         {
-            var actualModel = model ?? new PaginationModel();
+            var actualModel = new PaginationModel();
 
             return View("~/ViewComponents/Pagination/Default.cshtml", actualModel);
         }
+
+        internal int GetTotalNoOfPages(
+            int totalItems, 
+            int itemsPerPage)
+        {
+            if (totalItems <= itemsPerPage || totalItems == 0 || itemsPerPage == 0) return 1;
+
+            return (int)Math.Ceiling((decimal)totalItems / itemsPerPage);
+        }
+
+        internal bool TryGetPrevious(
+            string url, 
+            string paramName, 
+            int currentPage, 
+            out PaginationItemModel paginationItemModel)
+        {
+            paginationItemModel = null;
+            if (currentPage <= 1) return false;
+
+            var pageNo = currentPage - 1;
+            var ariaLabel = $"Goto the previous page, page {pageNo}";
+            var urlWithPageNo = GetUrlWithPageNo(url, paramName, pageNo);
+
+            paginationItemModel = new PaginationItemModel(
+                urlWithPageNo,
+                "Previous",
+                ariaLabel);
+
+            return true;
+        }
+
+        internal IEnumerable<PaginationItemModel> GetPages(
+            string url, 
+            string paramName, 
+            PageBoundary pageBoundary)
+        {
+            var pages = new List<PaginationItemModel>();
+
+            for (var i = pageBoundary.StartAt; i <= pageBoundary.PagesToDisplay; i++)
+            {
+                var urlWithPageNo = GetUrlWithPageNo(url, paramName, i);
+            }
+
+            return pages;
+        }
+
+        internal bool TryGetNext(
+            string url, 
+            string paramName, 
+            int currentPage, 
+            int totalNoOfPages,
+            out PaginationItemModel paginationItemModel)
+        {
+            paginationItemModel = null;
+            if (currentPage >= totalNoOfPages) return false;
+
+            var pageNo = currentPage + 1;
+            var ariaLabel = $"Goto the next page, page {pageNo}";
+            var urlWithPageNo = GetUrlWithPageNo(url, paramName, pageNo);
+
+            paginationItemModel = new PaginationItemModel(
+                urlWithPageNo,
+                "Next",
+                ariaLabel);
+
+            return true;
+        }
+
+        internal string GetUrlWithPageNo(
+            string url, 
+            string paramName, 
+            int pageNo)
+        {
+            var ub = new UriBuilder(url);
+            var query = QueryHelpers.ParseQuery(ub.Query);
+            var items = query
+                .SelectMany(
+                    x => x.Value,
+                    (col, value) => new KeyValuePair<string, string>(col.Key, value))
+                .ToList();
+
+            items.RemoveAll(x => x.Key == paramName);
+
+            var qb = new QueryBuilder(items)
+            {
+                { paramName, pageNo.ToString() }
+            };
+
+            ub.Query = qb.ToQueryString().ToString();
+
+            return ub.ToString();
+        }
     }
+
+
 }
--- tests/Dfc.CourseDirectory.Services.Tests/LarsSearchServiceTests.cs
+++ tests/Dfc.CourseDirectory.Services.Tests/LarsSearchServiceTests.cs
@@ -12,34 +12,34 @@ namespace Dfc.CourseDirectory.Services.Tests
         [Fact]
         public async void SearchAsync_Just_to_help_me_dev()
         {
-            // arrange
-            var criteria = new LarsSearchCriteria(
-                "business Management",
-                true,
-                "NotionalNVQLevelv2 eq '4' and AwardOrgCode eq 'NONE'",
-                new LarsSearchFacet[]
-                {
-                    LarsSearchFacet.NotionalNVQLevelv2,
-                    LarsSearchFacet.AwardOrgCode
-                });
+            //// arrange
+            //var criteria = new LarsSearchCriteria(
+            //    "business Management",
+            //    true,
+            //    "NotionalNVQLevelv2 eq '4' and AwardOrgCode eq 'NONE'",
+            //    new LarsSearchFacet[]
+            //    {
+            //        LarsSearchFacet.NotionalNVQLevelv2,
+            //        LarsSearchFacet.AwardOrgCode
+            //    });
 
-            var mockLogger = new Mock<ILogger<LarsSearchService>>();
+            //var mockLogger = new Mock<ILogger<LarsSearchService>>();
 
-            var settings = new LarsSearchSettings()
-            {
-                ApiUrl = "https://dfc-larsearch.search.windows.net/indexes/index-lars-awardorg/docs/search",
-                ApiKey = "B60E6C1EB35B630F22F471375AF2FD23",
-                ApiVersion = "2017-11-11",
-                Indexes = "index-lars-awardorg"
-            };
+            //var settings = new LarsSearchSettings()
+            //{
+            //    ApiUrl = "https://dfc-larsearch.search.windows.net/indexes/index-lars-awardorg/docs/search",
+            //    ApiKey = "B60E6C1EB35B630F22F471375AF2FD23",
+            //    ApiVersion = "2017-11-11",
+            //    Indexes = "index-lars-awardorg"
+            //};
 
-            var service = new LarsSearchService(mockLogger.Object, new HttpClient(), Options.Create(settings));
+            //var service = new LarsSearchService(mockLogger.Object, new HttpClient(), Options.Create(settings));
 
-            // act
-            var actual = await service.SearchAsync(criteria);
+            //// act
+            //var actual = await service.SearchAsync(criteria);
 
-            // assert
-            Assert.Equal(1, 0);
+            //// assert
+            //Assert.Equal(1, 0);
         }
     }
 }
