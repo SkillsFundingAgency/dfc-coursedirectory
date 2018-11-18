da951e7f2a0f6b7823c162499be5ffbf164ef1c5
Mahmood Akhtar <mahmood233@hotmail.com>
2018-11-18 16:54:08 +0000

    Thinned out LarsSearchController.Index method. Removed RequestModelExtensions. Finished ILarsSearchHelper implementation for now.

--- src/Dfc.CourseDirectory.Web/Controllers/LarsSearchController.cs
+++ src/Dfc.CourseDirectory.Web/Controllers/LarsSearchController.cs
@@ -1,4 +1,5 @@
-﻿using Dfc.CourseDirectory.Services;
+﻿using Dfc.CourseDirectory.Common;
+using Dfc.CourseDirectory.Services;
 using Dfc.CourseDirectory.Services.Enums;
 using Dfc.CourseDirectory.Services.Interfaces;
 using Dfc.CourseDirectory.Web.Helpers;
@@ -8,7 +9,7 @@ using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System.Collections.Generic;
+using System;
 using System.Threading.Tasks;
 
 namespace Dfc.CourseDirectory.Web.Controllers
@@ -16,19 +17,28 @@ namespace Dfc.CourseDirectory.Web.Controllers
     public class LarsSearchController : Controller
     {
         private readonly ILogger<LarsSearchController> _logger;
-        private readonly ILarsSearchService _larsSearchService;
         private readonly ILarsSearchSettings _larsSearchSettings;
+        private readonly ILarsSearchService _larsSearchService;
+        private readonly ILarsSearchHelper _larsSearchHelper;
         private readonly IPaginationHelper _paginationHelper;
 
         public LarsSearchController(
             ILogger<LarsSearchController> logger,
-            ILarsSearchService larsSearchService,
             IOptions<LarsSearchSettings> larsSearchSettings,
+            ILarsSearchService larsSearchService,
+            ILarsSearchHelper larsSearchHelper,
             IPaginationHelper paginationHelper)
         {
+            Throw.IfNull(logger, nameof(logger));
+            Throw.IfNull(larsSearchSettings, nameof(larsSearchSettings));
+            Throw.IfNull(larsSearchService, nameof(larsSearchService));
+            Throw.IfNull(larsSearchHelper, nameof(larsSearchHelper));
+            Throw.IfNull(paginationHelper, nameof(paginationHelper));
+
             _logger = logger;
-            _larsSearchService = larsSearchService;
             _larsSearchSettings = larsSearchSettings.Value;
+            _larsSearchService = larsSearchService;
+            _larsSearchHelper = larsSearchHelper;
             _paginationHelper = paginationHelper;
         }
 
@@ -42,155 +52,18 @@ namespace Dfc.CourseDirectory.Web.Controllers
             }
             else
             {
-                var criteria = requestModel.ToLarsSearchCriteria(
-                    _paginationHelper.GetCurrentPageNo(
-                        Request.GetDisplayUrl(),
-                        _larsSearchSettings.PageParamName),
+                var criteria = _larsSearchHelper.GetLarsSearchCriteria(
+                    requestModel,
+                    _paginationHelper.GetCurrentPageNo(Request.GetDisplayUrl(), _larsSearchSettings.PageParamName),
                     _larsSearchSettings.ItemsPerPage,
-                    new LarsSearchFacet[]
-                    {
-                        LarsSearchFacet.AwardOrgCode,
-                        LarsSearchFacet.NotionalNVQLevelv2,
-                        LarsSearchFacet.SectorSubjectAreaTier1,
-                        LarsSearchFacet.SectorSubjectAreaTier2
-                    });
+                    (LarsSearchFacet[])Enum.GetValues(typeof(LarsSearchFacet)));
 
                 var result = await _larsSearchService.SearchAsync(criteria);
-                var items = new List<LarsSearchResultItemModel>();
 
                 if (result.IsSuccess && result.HasValue)
                 {
-                    var totalCount = result.Value.ODataCount ?? 0;
-                    var filters = new List<LarsSearchFilterModel>();
-
-                    if (result.Value.SearchFacets != null)
-                    {
-                        var notionalNVQLevelv2Facets = new List<LarsSearchFilterItemModel>();
-                        var notionalNVQLevelv2FacetName = "NotionalNVQLevelv2Filter";
-                        var notionalNVQLevelv2IdCount = 0;
-
-                        foreach (var item in result.Value.SearchFacets.NotionalNVQLevelv2)
-                        {
-                            notionalNVQLevelv2Facets.Add(new LarsSearchFilterItemModel
-                            {
-                                Id = $"{notionalNVQLevelv2FacetName}-{notionalNVQLevelv2IdCount++}",
-                                Name = notionalNVQLevelv2FacetName,
-                                Text = $"Level {item.Value}",
-                                Value = item.Value,
-                                Count = item.Count,
-                                IsSelected = requestModel.IsFilterSelected(
-                                    nameof(requestModel.NotionalNVQLevelv2Filter),
-                                    item.Value)
-                            });
-                        }
-
-                        var notionalNVQLevelv2Filter = new LarsSearchFilterModel
-                        {
-                            Title = "Notional NVQ Level v2",
-                            Items = notionalNVQLevelv2Facets
-                        };
-
-                        filters.Add(notionalNVQLevelv2Filter);
-
-                        var awardOrgCodeFacets = new List<LarsSearchFilterItemModel>();
-                        var awardOrgCodeFacetName = "AwardOrgCodeFilter";
-                        var awardOrgCodeIdCount = 0;
-
-                        foreach (var item in result.Value.SearchFacets.AwardOrgCode)
-                        {
-                            awardOrgCodeFacets.Add(new LarsSearchFilterItemModel
-                            {
-                                Id = $"{awardOrgCodeFacetName}-{awardOrgCodeIdCount++}",
-                                Name = awardOrgCodeFacetName,
-                                Text = item.Value,
-                                Value = item.Value,
-                                Count = item.Count,
-                                IsSelected = requestModel.IsFilterSelected(
-                                    nameof(requestModel.AwardOrgCodeFilter),
-                                    item.Value)
-                            });
-                        }
-
-                        var awardOrgCodeFilter = new LarsSearchFilterModel
-                        {
-                            Title = "Award Org Code",
-                            Items = awardOrgCodeFacets
-                        };
-
-                        filters.Add(awardOrgCodeFilter);
-
-                        var sectorSubjectAreaTier1Facets = new List<LarsSearchFilterItemModel>();
-                        var sectorSubjectAreaTier1FacetName = "SectorSubjectAreaTier1Filter";
-                        var sectorSubjectAreaTier1IdCount = 0;
-
-                        foreach (var item in result.Value.SearchFacets.SectorSubjectAreaTier1)
-                        {
-                            sectorSubjectAreaTier1Facets.Add(new LarsSearchFilterItemModel
-                            {
-                                Id = $"{sectorSubjectAreaTier1FacetName}-{sectorSubjectAreaTier1IdCount++}",
-                                Name = sectorSubjectAreaTier1FacetName,
-                                Text = item.Value,
-                                Value = item.Value,
-                                Count = item.Count,
-                                IsSelected = requestModel.IsFilterSelected(
-                                    nameof(requestModel.SectorSubjectAreaTier1Filter),
-                                    item.Value)
-                            });
-                        }
-
-                        var sectorSubjectAreaTier1Filter = new LarsSearchFilterModel
-                        {
-                            Title = "Sector Subject Area Tier 1",
-                            Items = sectorSubjectAreaTier1Facets
-                        };
-
-                        filters.Add(sectorSubjectAreaTier1Filter);
-
-                        var sectorSubjectAreaTier2Facets = new List<LarsSearchFilterItemModel>();
-                        var sectorSubjectAreaTier2FacetName = "SectorSubjectAreaTier2Filter";
-                        var sectorSubjectAreaTier2IdCount = 0;
-
-                        foreach (var item in result.Value.SearchFacets.SectorSubjectAreaTier2)
-                        {
-                            sectorSubjectAreaTier2Facets.Add(new LarsSearchFilterItemModel
-                            {
-                                Id = $"{sectorSubjectAreaTier2FacetName}-{sectorSubjectAreaTier2IdCount++}",
-                                Name = sectorSubjectAreaTier2FacetName,
-                                Text = item.Value,
-                                Value = item.Value,
-                                Count = item.Count,
-                                IsSelected = requestModel.IsFilterSelected(
-                                    nameof(requestModel.SectorSubjectAreaTier2Filter),
-                                    item.Value)
-                            });
-                        }
-
-                        var sectorSubjectAreaTier2Filter = new LarsSearchFilterModel
-                        {
-                            Title = "Sector Subject Area Tier 2",
-                            Items = sectorSubjectAreaTier2Facets
-                        };
-
-                        filters.Add(sectorSubjectAreaTier2Filter);
-                    }
-
-                    foreach (var item in result.Value.Value)
-                    {
-                        items.Add(new LarsSearchResultItemModel(
-                            item.SearchScore,
-                            item.LearnAimRef,
-                            item.LearnAimRefTitle,
-                            item.NotionalNVQLevelv2,
-                            item.AwardOrgCode,
-                            item.LearnDirectClassSystemCode1,
-                            item.LearnDirectClassSystemCode2,
-                            item.SectorSubjectAreaTier1,
-                            item.SectorSubjectAreaTier2,
-                            item.GuidedLearningHours,
-                            item.TotalQualificationTime,
-                            item.UnitType,
-                            item.AwardOrgName));
-                    }
+                    var filters = _larsSearchHelper.GetLarsSearchFilterModels(result.Value.SearchFacets, requestModel);
+                    var items = _larsSearchHelper.GetLarsSearchResultItemModels(result.Value.Value);
 
                     model = new LarsSearchResultModel(
                         requestModel.SearchTerm,
@@ -198,7 +71,7 @@ namespace Dfc.CourseDirectory.Web.Controllers
                         Request.GetDisplayUrl(),
                         _larsSearchSettings.PageParamName,
                         _larsSearchSettings.ItemsPerPage,
-                        totalCount,
+                        result.Value.ODataCount ?? 0,
                         filters);
                 }
                 else
