e19b60fb257b16df4a5d4e9973a7c8197d8c260f
Mahmood Akhtar <mahmood233@hotmail.com>
2018-11-18 23:59:08 +0000

    ValueObject pattern for LarsSearchFilterModel and LarsSearchFilterItemModel.

--- src/Dfc.CourseDirectory.Web/ViewComponents/LarsSearchResult/LarsSearchFilterItemModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/LarsSearchResult/LarsSearchFilterItemModel.cs
@@ -1,12 +1,47 @@
-﻿namespace Dfc.CourseDirectory.Web.ViewComponents.LarsSearchResult
+﻿using Dfc.CourseDirectory.Common;
+using System.Collections.Generic;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.LarsSearchResult
 {
-    public class LarsSearchFilterItemModel
+    public class LarsSearchFilterItemModel : ValueObject<LarsSearchFilterItemModel>
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Text { get; set; }
-        public string Value { get; set; }
-        public int Count { get; set; }
-        public bool IsSelected { get; set; }
+        public string Id { get; }
+        public string Name { get; }
+        public string Text { get; }
+        public string Value { get; }
+        public int Count { get; }
+        public bool IsSelected { get; }
+
+        public LarsSearchFilterItemModel(
+            string id,
+            string name,
+            string text,
+            string value,
+            int count,
+            bool isSelected)
+        {
+            Throw.IfNullOrWhiteSpace(id, nameof(id));
+            Throw.IfNullOrWhiteSpace(name, nameof(name));
+            Throw.IfNullOrWhiteSpace(text, nameof(text));
+            Throw.IfNullOrWhiteSpace(value, nameof(value));
+            Throw.IfLessThan(0, count, nameof(count));
+
+            Id = id;
+            Name = name;
+            Text = text;
+            Value = value;
+            Count = count;
+            IsSelected = isSelected;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Id;
+            yield return Name;
+            yield return Text;
+            yield return Value;
+            yield return Count;
+            yield return IsSelected;
+        }
     }
 }
