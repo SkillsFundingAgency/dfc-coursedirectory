becd168655a68bd66bad030336e692bec198045b
Mahmood Akhtar <mahmood233@hotmail.com>
2018-11-18 15:32:29 +0000

    Refactoring ILarsSearchHelper implementation.

--- src/Dfc.CourseDirectory.Web/Helpers/ILarsSearchHelper.cs
+++ src/Dfc.CourseDirectory.Web/Helpers/ILarsSearchHelper.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Dfc.CourseDirectory.Services;
 using Dfc.CourseDirectory.Services.Enums;
 using Dfc.CourseDirectory.Services.Interfaces;
 using Dfc.CourseDirectory.Web.RequestModels;
+using Dfc.CourseDirectory.Web.ViewComponents.LarsSearchResult;
 
 namespace Dfc.CourseDirectory.Web.Helpers
 {
     public interface ILarsSearchHelper
     {
-        ILarsSearchCriteria GetLarsSearchCriteria(LarsSearchRequestModel larsSearchRequestModel, int currentPageNo, int itemsPerPage, IEnumerable<LarsSearchFacet> facets);
+        ILarsSearchCriteria GetLarsSearchCriteria(LarsSearchRequestModel larsSearchRequestModel, int currentPageNo, int itemsPerPage, IEnumerable<LarsSearchFacet> facets = null);
+        LarsSearchFilterModel GetLarsSearchFilterModel(string title, string facetName, Func<string, string> textStrategy, IEnumerable<SearchFacet> searchFacets, IEnumerable<string> selectedValues);
+        IEnumerable<LarsSearchResultItemModel> GetLarsSearchResultItemModel(IEnumerable<LarsSearchResultItem> larsSearchResultItems);
     }
 }
