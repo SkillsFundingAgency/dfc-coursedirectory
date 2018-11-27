6628e500265bb8a16108c8c3b5fad0b9dbb10be4
bradleysimon <bradley_simon@hotmail.com>
2018-11-27 14:30:06 +0000

    done

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/VenueList/VenueListModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dfc.CourseDirectory.Common;
+using Dfc.CourseDirectory.Web.ViewComponents.Interfaces;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.Venue
+{
+    public class VenueListModel
+    {
+        public IEnumerable<VenueItemModel> Venues{ get; set; }
+    }
+}
