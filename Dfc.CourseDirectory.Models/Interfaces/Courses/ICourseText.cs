cc938c3e7c436c74ffdc7081cb8ef52115ed21f8
Liam Wright <mail@liam-wright.co.uk>
2018-12-03 12:47:22 +0000

    Implemented Model Project

---
+++ Dfc.CourseDirectory.Models/Interfaces/Courses/ICourseText.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dfc.CourseDirectory.Models.Interfaces.Courses
+{
+    public interface ICourseText
+    {
+        string CourseTitle { get;}
+        string Learn { get; }
+        string How { get; }
+        string Why { get; }
+    }
+}
--- Dfc.CourseDirectory.Models/Models/Courses/Course.cs
+++ Dfc.CourseDirectory.Models/Models/Courses/Course.cs
@@ -2,29 +2,40 @@
 using Dfc.CourseDirectory.Models.Interfaces.Courses;
 using Dfc.CourseDirectory.Models.Models.Providers;
 using Dfc.CourseDirectory.Models.Models.Qualifications;
-using Dfc.CourseDirectory.Models.Models.Venues;
 using System.Collections.Generic;
 
 namespace Dfc.CourseDirectory.Models.Models.Courses
 {
     public class Course : ValueObject<Course>, ICourse
     {
-        public Provider Provider { get; set; }
+        public Provider Provider { get; }
         public Qualification Qualification { get; }
-        public Venue Venue { get; }
+        public CourseData CourseData { get; }
+        public CourseText CourseText { get; }
 
         public Course(
             Provider provider,
             Qualification qualification,
-            Venue venue)
+            CourseData data,
+            CourseText text)
         {
+            Throw.IfNull(provider, nameof(provider));
+            Throw.IfNull(qualification, nameof(qualification));
+            Throw.IfNull(data, nameof(data));
+            Throw.IfNull(text, nameof(text));
 
+            Provider = provider;
+            Qualification = qualification;
+            CourseData = data;
+            CourseText = text;
         }
+
         protected override IEnumerable<object> GetEqualityComponents()
         {
             yield return Provider;
             yield return Qualification;
-            yield return Venue;
+            yield return CourseData;
+            yield return CourseText;
         }
     }
 }
