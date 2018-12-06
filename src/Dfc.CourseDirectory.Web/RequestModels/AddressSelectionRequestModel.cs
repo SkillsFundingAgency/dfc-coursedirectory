c6ea3ead853c42a3c104d299e97487372df21019
bradleysimon <bradley_simon@hotmail.com>
2018-12-06 09:16:56 +0000

    retrieve address

---
+++ src/Dfc.CourseDirectory.Web/RequestModels/AddressSelectionRequestModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Dfc.CourseDirectory.Web.RequestModels
+{
+    public class AddressSelectionRequestModel
+    {
+        public string Id { get; set; }
+
+        public AddressSelectionRequestModel()
+        {
+
+        }
+    }
+}
---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/AddressSelectionConfirmation/AddressSelectionConfirmationModel.cs
@@ -0,0 +1,15 @@
+﻿using Dfc.CourseDirectory.Common;
+using Dfc.CourseDirectory.Web.ViewComponents.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.AddressSelectionConfirmation
+{
+    public class AddressSelectionConfirmationModel 
+    {
+        public string Id { get; set; }
+        public string AddressLine1 { get; set; }
+        public string PostCode { get; set; }
+    }
+}
