77e2646620acfc48a658e380733f51622734c280
Liam Wright <mail@liam-wright.co.uk>
2018-12-07 09:12:10 +0000

    Interim check-in to share unit tests

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/EditVenueName/EditVenueName.cs
@@ -0,0 +1,15 @@
+﻿using Dfc.CourseDirectory.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.EditVenueName
+{
+    public class EditVenueName : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var model = new EditAddressViewModel();
+
+            return View("~/ViewComponents/EditVenueName/Default.cshtml", model);
+        }
+    }
+}
---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/EditVenueName/VenueNameModel.cs
@@ -0,0 +1,27 @@
+﻿using Dfc.CourseDirectory.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.EditVenueName
+{
+    public class VenueNameModel : ValueObject<VenueNameModel>
+    {
+        public string VenueName { get; }
+
+        public VenueNameModel(
+            string venueName)
+        {
+            Throw.IfNullOrWhiteSpace(venueName, nameof(venueName));
+            Throw.IfGreaterThan(255, venueName.Length, nameof(venueName));
+
+            VenueName = venueName;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return VenueName;
+        }
+    }
+}
