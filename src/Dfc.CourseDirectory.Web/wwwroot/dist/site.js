bab7a259360d70fcff5020221c0edc99a4309a4b
Mahmood Akhtar <mahmood233@hotmail.com>
2018-12-07 10:00:43 +0000

    venue search component js with the jquery document ready function now the events fire.

--- src/Dfc.CourseDirectory.Web/wwwroot/dist/site.js
+++ src/Dfc.CourseDirectory.Web/wwwroot/dist/site.js
@@ -257,65 +257,69 @@
 "use strict";
 
 (function ($) {
-    var debounce = function (cb, delay) {
-        var inDebounce;
-        return function () {
-            var context = this;
-            var args = arguments;
-            clearTimeout(inDebounce);
-            inDebounce = setTimeout(function () {
-                cb.apply(context, args);
-            }, delay);
+
+    $(function () {
+        var debounce = function (cb, delay) {
+            var inDebounce;
+            return function () {
+                var context = this;
+                var args = arguments;
+                clearTimeout(inDebounce);
+                inDebounce = setTimeout(function () {
+                    cb.apply(context, args);
+                }, delay);
+            };
         };
-    };
 
-    var isNullOrWhitespace = function (input) {
-        if (typeof input === 'undefined' || input == null) return true;
-        return input.replace(/\s/g, '').length < 1;
-    }
-
-    var replaceAll = function (search, find, replace) {
-        return search.split(find).join(replace);
-    };
-
-    var makeRequestWithPayload = function (payload, success) {
-        console.log(payload);
-        var qs = $.param(payload);
-        qs = replaceAll(qs, "%5B%5D", "");
-        $.get("/VenueSearch?" + qs, success);
-    };
-
-    var removeSearchResults = function () {
-        var $venueSearchResultContainer = $("#VenueSearchResultContainer");
-        $venueSearchResultContainer.html("");
-    };
-
-    var replaceSearchResult = function (searchResults) {
-        var $venueSearchResultContainer = $("#VenueSearchResultContainer");
-        $venueSearchResultContainer.html("");
-        $venueSearchResultContainer.html(searchResults);
-    };
-
-    var $UKPrn = $("#UKPrn");
-    var $SearchButton = $("#search");
-
-    var doSearch = function () {
-        if (isNullOrWhitespace($UKPrn.val())) {
-            removeSearchResults();
-        
-        } else {
-            makeRequestWithPayload({
-                SearchTerm: $UKPrn.val()
-            }, onSucess);
+        var isNullOrWhitespace = function (input) {
+            if (typeof input === 'undefined' || input == null) return true;
+            return input.replace(/\s/g, '').length < 1;
         }
-    };
 
-    var onSucess = function (data) {
-        replaceSearchResult(data);
+        var replaceAll = function (search, find, replace) {
+            return search.split(find).join(replace);
+        };
 
-        //TODO MUST!!! RUN THIS AFTER CHANGING THE DOM
-        window.GOVUKFrontend.initAll();
-    };
+        var makeRequestWithPayload = function (payload, success) {
+            console.log(payload);
+            var qs = $.param(payload);
+            qs = replaceAll(qs, "%5B%5D", "");
+            $.get("/VenueSearch?" + qs, success);
+        };
 
-    $SearchButton.on("click", debounce(doSearch, 400));
+        var removeSearchResults = function () {
+            var $venueSearchResultContainer = $("#VenueSearchResultContainer");
+            $venueSearchResultContainer.html("");
+        };
+
+        var replaceSearchResult = function (searchResults) {
+            var $venueSearchResultContainer = $("#VenueSearchResultContainer");
+            $venueSearchResultContainer.html("");
+            $venueSearchResultContainer.html(searchResults);
+        };
+
+        var $UKPrn = $("#UKPrn");
+        var $SearchButton = $("#search");
+
+        var doSearch = function () {
+            if (isNullOrWhitespace($UKPrn.val())) {
+                removeSearchResults();
+        
+            } else {
+                makeRequestWithPayload({
+                    SearchTerm: $UKPrn.val()
+                }, onSucess);
+            }
+        };
+
+        var onSucess = function (data) {
+            replaceSearchResult(data);
+
+            //TODO MUST!!! RUN THIS AFTER CHANGING THE DOM
+            window.GOVUKFrontend.initAll();
+        };
+
+        $SearchButton.on("click", debounce(doSearch, 400));
+    });
+
 })(jQuery);
