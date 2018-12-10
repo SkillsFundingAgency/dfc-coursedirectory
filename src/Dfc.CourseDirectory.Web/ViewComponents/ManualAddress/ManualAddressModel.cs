d230fb14ee0dcc809a65574cba5dee6245c2c4ee
suniara <35219355+suniara@users.noreply.github.com>
2018-12-10 13:24:48 +0000

    Add venue manually (#11)
    * Enter address manually - controller, viewcomponents & views
    
    * Removed comments
    
    * Changed AddVenueManualModel to ManualAddressModel and moved to ManualAddress view component folder
    
    * Validation
    
    * Changed manual address link
    
    * Null check before trim on Manual Venue/address entry
    
    * Change Building/Street to AddressLine1/2

--- src/Dfc.CourseDirectory.Web/ViewComponents/ManualAddress/ManualAddressModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/ManualAddress/ManualAddressModel.cs
@@ -6,11 +6,13 @@ namespace Dfc.CourseDirectory.Web.ViewComponents.ManualAddress
     {
         public string Id { get; set; }
 
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
         [Required(ErrorMessage = "Venue name is required", AllowEmptyStrings = false)]
         [StringLength(255, ErrorMessage = "The maximum length of Venue Name is 255 characters")]
         [RegularExpression(@"^[\x00-\x7F]+$", ErrorMessage = "Only english based characters allowed")]
         public string VenueName { get; set; }
 
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
         [Required(ErrorMessage = "Address Line 1 is required", AllowEmptyStrings = false)]
         [StringLength(100, ErrorMessage = "The maximum length of Address Line 1 is 100 characters")]
         [RegularExpression(@"^[\x00-\x7F]+$")]
@@ -20,6 +22,7 @@ namespace Dfc.CourseDirectory.Web.ViewComponents.ManualAddress
         [RegularExpression(@"^[\x00-\x7F]+$")]
         public string AddressLine2 { get; set; }
 
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
         [Required(ErrorMessage = "Town or City is required", AllowEmptyStrings = false)]
         [StringLength(75, ErrorMessage = "The maximum length of Town or City is 75 characters")]
         [RegularExpression(@"^[\x00-\x7F]+$")]
@@ -29,6 +32,7 @@ namespace Dfc.CourseDirectory.Web.ViewComponents.ManualAddress
         [RegularExpression(@"^[\x00-\x7F]+$")]
         public string County { get; set; }
 
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
         [Required(ErrorMessage = "Postcode is required", AllowEmptyStrings = false)]
         [RegularExpression(@"([a-zA-Z][0-9]|[a-zA-Z][0-9][0-9]|[a-zA-Z][a-zA-Z][0-9]|[a-zA-Z][a-zA-Z][0-9][0-9]|[a-zA-Z][0-9][a-zA-Z]|[a-zA-Z][a-zA-Z][0-9][a-zA-Z]) ([0-9][abdefghjklmnpqrstuwxyzABDEFGHJLMNPQRSTUWXYZ][abdefghjklmnpqrstuwxyzABDEFGHJLMNPQRSTUWXYZ])", ErrorMessage = "Enter a valid postcode")]
         public string Postcode { get; set; }
