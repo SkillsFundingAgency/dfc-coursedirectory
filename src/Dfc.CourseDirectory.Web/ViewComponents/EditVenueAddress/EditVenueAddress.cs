fede02009417b61ab502821e00bce7fb809ec31d
Liam Wright <mail@liam-wright.co.uk>
2018-12-11 14:28:54 +0000

    Edit venue address view, and controller flow implemented

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/EditVenueAddress/EditVenueAddress.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.EditVenueAddress
+{
+    public class EditVenueAddress : ViewComponent
+    {
+        public IViewComponentResult Invoke(EditVenueAddressModel model)
+        {
+            return View("~/ViewComponents/EditVenueAddress/Default.cshtml", model);
+        }
+    }
+}
