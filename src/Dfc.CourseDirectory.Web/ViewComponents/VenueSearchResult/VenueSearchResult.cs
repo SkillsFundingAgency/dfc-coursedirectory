bcc05ff1ed807faaaa65b5493966e9b01c8a784a
bradleysimon <bradley_simon@hotmail.com>
2018-12-12 12:56:22 +0000

    wire up landing page

--- src/Dfc.CourseDirectory.Web/ViewComponents/VenueSearchResult/VenueSearchResult.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/VenueSearchResult/VenueSearchResult.cs
@@ -10,37 +10,8 @@ namespace Dfc.CourseDirectory.Web.ViewComponents.VenueSearchResult
 {
     public class VenueSearchResult : ViewComponent
     {
-
-        private readonly IHttpContextAccessor _contextAccessor;
-        private ISession _session => _contextAccessor.HttpContext.Session;
-
-
-
-        public VenueSearchResult(IHttpContextAccessor contextAccessor)
-        {
-            _contextAccessor = contextAccessor;
-
-            _contextAccessor.HttpContext.Session.SetString("UKPRN", "MyStore");
-            _session.SetString("Test", "Ben Rules!");
-        }
-
         public IViewComponentResult Invoke(VenueSearchResultModel model)
         {
-            _session.SetString("Test", "Ben Rules!");
-            if (model != null)
-            {
-                TempData["UKPRN"] = model.SearchTerm;
-                TempData.Keep();
-
-                _contextAccessor.HttpContext.Session.SetString("UKPRN", model.SearchTerm);
-
-                var a= _contextAccessor.HttpContext.Session.GetString("UKPRN");
-            }
-
-            var b = _contextAccessor.HttpContext.Session.GetString("UKPRN");
-
-            _session.GetString("Test");
-
             var actualModel = model ?? new VenueSearchResultModel();
 
             return View("~/ViewComponents/VenueSearchResult/Default.cshtml", actualModel);
