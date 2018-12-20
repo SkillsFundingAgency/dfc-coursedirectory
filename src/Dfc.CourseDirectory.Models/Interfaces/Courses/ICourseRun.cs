ce572b3e3966a1bd17a606a8a6d7309a833c41dc
Ian <44667988+ian-findacourse@users.noreply.github.com>
2018-12-20 16:42:06 +0000

    DFC-6076 Course model changes (#48)

--- src/Dfc.CourseDirectory.Models/Interfaces/Courses/ICourseRun.cs
+++ src/Dfc.CourseDirectory.Models/Interfaces/Courses/ICourseRun.cs
@@ -1,20 +1,44 @@
-﻿using Dfc.CourseDirectory.Models.Models.Courses;
-using Dfc.CourseDirectory.Models.Models.Venues;
+﻿
 using System;
+using Dfc.CourseDirectory.Models.Models.Courses;
+using Dfc.CourseDirectory.Models.Interfaces.Providers;
+using Dfc.CourseDirectory.Models.Interfaces.Qualifications;
+using Dfc.CourseDirectory.Models.Interfaces.Venues;
+
 
 namespace Dfc.CourseDirectory.Models.Interfaces.Courses
 {
     public interface ICourseRun
     {
-        Venue Venue { get; }
-        string Price { get; }
-        string Duration { get; }
-        string StudyMode { get; }
-        string Attendance { get; }
-        Guid CourseID { get; }
+        //Guid id { get; }
+        string CourseDescription { get; }
+        string EntryRequirments { get; } //Requirements { get; }
+        string WhatYoullLearn { get; }
+        string HowYoullLearn { get; }
+        string WhatYoullNeed { get; }
+        string WhatYoullNeedToBring { get; }
+        string HowYoullBeAssessed { get; }
+        string WhereNext { get; }
+        string CourseName { get; }
+        string ProviderCourseID { get; } //string CourseID { get; }
+        string DeliveryMode { get; }
+        bool FlexibleStartDate { get; }
+        DateTime StartDate { get; }
         string CourseURL { get; }
-        string Pattern { get; }
-        string Requirements { get; }
-
+        decimal Cost { get; } //string Price { get; }
+        string CostDescription { get; }
+        bool AdvancedLearnerLoan { get; }
+        DurationUnit DurationUnit { get; }
+        int DurationValue { get; }
+        StudyMode StudyMode { get; } //string StudyMode { get; }
+        AttendancePattern AttendancePattern { get; } ////string Attendance { get; }
+        //string Pattern { get; }
+        IVenue Venue { get; }
+        IProvider Provider { get; }
+        IQualification Qualification { get; }
+        DateTime CreatedDate { get; }
+        string CreatedBy { get; }
+        DateTime UpdatedDate { get; }
+        string UpdatedBy { get; }
     }
 }
