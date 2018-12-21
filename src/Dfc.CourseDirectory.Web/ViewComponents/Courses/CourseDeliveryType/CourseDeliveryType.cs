6c383171e772ed93e83a28dd082c1ee72ba4cc9d
bradleysimon <bradley_simon@hotmail.com>
2018-12-21 15:40:24 +0000

    done

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/CourseDeliveryType/CourseDeliveryType.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Web.ViewComponents.Courses.CourseDeliveryType;
+using Dfc.CourseDirectory.Web.ViewComponents.Courses.CourseFor;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.CourseDelivery
+{
+    public class CourseDeliveryType : ViewComponent
+    {
+        public IViewComponentResult Invoke(CourseDeliveryTypeModel model)
+        {
+            return View("~/ViewComponents/Courses/CourseDeliveryType/Default.cshtml", model);
+        }
+    }
+}
---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/CourseDeliveryType/CourseDeliveryTypeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Web.ViewComponents.Courses.CourseDelivery;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.CourseDeliveryType
+{
+    public class CourseDeliveryTypeModel
+    {
+        public List<DeliveryTypeModel> DeliveryTypes { get; set; }
+        public string LabelText { get; set; }
+        public string HintText { get; set; }
+        public string AriaDescribedBy { get; set; }
+    }
+}
