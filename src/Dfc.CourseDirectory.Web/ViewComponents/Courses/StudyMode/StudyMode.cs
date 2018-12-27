a18bce6e8dc21541f254d820605b1fbc4c88cfdf
Anatoli Malev <32457442+MakCom@users.noreply.github.com>
2018-12-27 17:58:18 +0000

    select study mode - fulltime, partime, flexible (#70)

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/StudyMode/StudyMode.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.StudyMode
+{
+    public class StudyMode : ViewComponent
+    {
+        public IViewComponentResult Invoke(StudyModeModel model)
+        {
+            return View("~/ViewComponents/Courses/StudyMode/Default.cshtml", model);
+        }
+    }
+}
