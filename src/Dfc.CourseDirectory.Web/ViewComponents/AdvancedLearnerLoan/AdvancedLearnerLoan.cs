a6ae62fd312bde33c00f1ae6143961de720641b7
Liam Wright <mail@liam-wright.co.uk>
2018-12-31 12:26:59 +0000

    Converted component into View Component

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/AdvancedLearnerLoan/AdvancedLearnerLoan.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.AdvancedLearnerLoan
+{
+    public class AdvancedLearnerLoan : ViewComponent 
+    {
+        public IViewComponentResult Invoke(AdvancedLearnerLoanModel model)
+        {
+            return View("~/ViewComponents/AdvancedLearnerLoan/Default.cshtml", model);
+        }
+    }
+}
