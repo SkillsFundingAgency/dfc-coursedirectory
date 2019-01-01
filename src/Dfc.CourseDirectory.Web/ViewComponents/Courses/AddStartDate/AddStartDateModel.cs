824eca6d37d356bae97c0c35962140b4d03111c8
bradleysimon <bradley_simon@hotmail.com>
2019-01-01 20:51:30 +0000

    done

--- src/Dfc.CourseDirectory.Web/ViewComponents/Courses/AddStartDate/AddStartDateModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/AddStartDate/AddStartDateModel.cs
@@ -16,23 +16,23 @@ namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.AddStartDate
 
         public StartDateType StartDateType { get; set; }
 
-        [Range(0, 31, ErrorMessage = "Day must be a number between 1 and 31")]
+        //[Range(0, 31, ErrorMessage = "Day must be a number between 1 and 31")]
         public string Day { get; set; }
         public string DayAriaDescribedBy { get; set; }
         public string DayLabelText { get; set; }
 
-        [Range(0, 12, ErrorMessage = "Month must be a number between 1 and 12")]
+        //[Range(0, 12, ErrorMessage = "Month must be a number between 1 and 12")]
         public string Month { get; set; }
         public string MonthAriaDescribedBy { get; set; }
         public string MonthLabelText { get; set; }
 
-        [MaxLength(4, ErrorMessage = "Year must be a valid 4 digit year")]
-        [MinLength(4, ErrorMessage = "Year must be a valid 4 digit year")]
+        //[MaxLength(4, ErrorMessage = "Year must be a valid 4 digit year")]
+        //[MinLength(4, ErrorMessage = "Year must be a valid 4 digit year")]
         public string Year { get; set; }
         public string YearAriaDescribedBy { get; set; }
         public string YearLabelText { get; set; }
 
-        public DateTime StartDate { get; set; }
+        //public DateTime StartDate { get; set; }
 
        
     }
