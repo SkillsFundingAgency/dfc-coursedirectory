65e2a014a4a38db2a4b35fa4f2c6c35bdd005717
Liam Wright <mail@liam-wright.co.uk>
2019-01-02 09:56:13 +0000

    Added necessary headers in Startup.cs to resolve OWASP fixes.

--- src/Dfc.CourseDirectory.Web/Startup.cs
+++ src/Dfc.CourseDirectory.Web/Startup.cs
@@ -125,6 +125,7 @@ namespace Dfc.CourseDirectory.Web
             {
                 context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
                 context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Add("X-Xss-Protection", "1");
 
                 context.Response.GetTypedHeaders().CacheControl =
                   new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
