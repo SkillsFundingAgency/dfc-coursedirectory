76f1c6ac92bec3681e4a19a1931f23179c5b582a
Liam Wright <mail@liam-wright.co.uk>
2019-01-03 12:30:23 +0000

    Stub data, and view model created for "Your Courses" view for development purposes.

---
+++ src/Dfc.CourseDirectory.Web/ViewModels/YourCoursesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Dfc.CourseDirectory.Models.Models.Courses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.Web.ViewModels
+{
+    public class YourCoursesViewModel
+    {
+        public int? UKPRN { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
