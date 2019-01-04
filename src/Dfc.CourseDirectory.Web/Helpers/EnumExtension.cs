414c48267faf7c1a1e9e3256d4a88aad4ee4cdf4
simon bradley <bradley_simon@outlook.com>
2019-01-04 15:40:25 +0000

    Dfc 6323 your courses line 2 view component (#90)
    * WIP
    
    * WIP
    
    * WIP
    
    * wip
    
    * WIP
    
    * line 2 complete

---
+++ src/Dfc.CourseDirectory.Web/Helpers/EnumExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Dfc.CourseDirectory.Web.Helpers
+{
+    public static class EnumExtension
+    {
+        public static string ToDescription(this System.Enum value)
+        {
+            var attributes = (DescriptionAttribute[])value.GetType().GetField(value.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : value.ToString();
+        }
+
+        public static IEnumerable<SelectListItem> ToSelectList<T>(this System.Enum enumValue)
+        {
+            return
+                System.Enum.GetValues(enumValue.GetType()).Cast<T>()
+                    .Select(
+                        x =>
+                            new SelectListItem
+                            {
+                                Text = ((System.Enum)(object)x).ToDescription(),
+                                Value = x.ToString(),
+                                Selected = (enumValue.Equals(x))
+                            });
+        }
+    }
+}
