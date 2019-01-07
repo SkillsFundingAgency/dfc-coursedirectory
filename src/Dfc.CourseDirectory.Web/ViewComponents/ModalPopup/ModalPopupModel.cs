3ff35387537a31491bdb08020cb2d0e1dcd4e82d
Liam Wright <mail@liam-wright.co.uk>
2019-01-07 16:12:35 +0000

    Completed modal pop-up for single course only

--- src/Dfc.CourseDirectory.Web/ViewComponents/ModalPopup/ModalPopupModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/ModalPopup/ModalPopupModel.cs
@@ -7,5 +7,13 @@ namespace Dfc.CourseDirectory.Web.ViewComponents.ModalPopup
 {
     public class ModalPopupModel
     {
+        public string CourseTitle { get; set; }
+        public string CourseDescription { get; set; }
+        public string EntryRequirements { get; set; }
+        public string WhatYoullLearn { get; set; }
+        public string HowYoullLearn { get; set; }
+        public string WhatYoullNeed { get; set; }
+        public string HowYoullBeAssessed { get; set; }
+        public string WhereNext { get; set; }
     }
 }
