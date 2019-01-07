d0b225fd2df09d1141c2057429c5287742fdc2cf
Liam Wright <mail@liam-wright.co.uk>
2019-01-07 12:16:05 +0000

    Popup function working now

--- src/Dfc.CourseDirectory.Web/ViewComponents/ModalPopup/ModalPopup.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/ModalPopup/ModalPopup.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Dfc.CourseDirectory.Web.ViewComponents.ModalPopup
 {
-    public class ModalPopup
+    public class ModalPopup : ViewComponent
     {
+        public IViewComponentResult Invoke(ModalPopupModel model)
+        {
+            return View("~/ViewComponents/ModalPopup/Default.cshtml", model);
+        }
     }
 }
