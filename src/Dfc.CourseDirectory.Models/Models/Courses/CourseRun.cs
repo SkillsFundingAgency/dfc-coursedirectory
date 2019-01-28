a29fd1eec6402c1c970957ceccffe1100b71e5d8
suniara <35219355+suniara@users.noreply.github.com>
2019-01-28 13:40:50 +0000

    Edit Duration validation (#154)

--- src/Dfc.CourseDirectory.Models/Models/Courses/CourseRun.cs
+++ src/Dfc.CourseDirectory.Models/Models/Courses/CourseRun.cs
@@ -88,6 +88,8 @@ namespace Dfc.CourseDirectory.Models.Models.Courses
         public decimal? Cost { get; set; }
         public string CostDescription { get; set; }       
         public DurationUnit DurationUnit { get; set; }
+        [Required(ErrorMessage = "Enter Duration")]
+        [RegularExpression("^([0-9]|[0-9][0-9]|[0-9][0-9][0-9])$", ErrorMessage = "Duration must be numeric and maximum length is 3 digits")]
         public int? DurationValue { get; set; }
         public StudyMode StudyMode { get; set; }
         public AttendancePattern AttendancePattern { get; set; }
