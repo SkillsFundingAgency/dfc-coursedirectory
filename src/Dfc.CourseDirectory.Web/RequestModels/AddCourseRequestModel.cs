413bbabc04dacada764f5c4e857d5401c61df191
bradleysimon <bradley_simon@hotmail.com>
2019-02-04 12:37:50 +0000

    done

--- src/Dfc.CourseDirectory.Web/RequestModels/AddCourseRequestModel.cs
+++ src/Dfc.CourseDirectory.Web/RequestModels/AddCourseRequestModel.cs
@@ -20,7 +20,7 @@ namespace Dfc.CourseDirectory.Web.RequestModels
         public decimal Cost { get; set; }
         public string CostDescription { get; set; }
         public bool AdvancedLearnerLoan { get; set; }
-        public DurationUnit Id { get; set; }
+        public DurationUnit DurationUnit { get; set; }
         public int DurationLength { get; set; }
         public StudyMode StudyMode { get; set; }
         public AttendancePattern AttendanceMode { get; set; }
--- src/Dfc.CourseDirectory.Web/ViewModels/AddCourseDetailsViewModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewModels/AddCourseDetailsViewModel.cs
@@ -62,7 +62,7 @@ namespace Dfc.CourseDirectory.Web.ViewModels
         public string CourseProviderReference { get; set; }
         public string Url { get; set; }
         public bool AdvancedLearnerLoan { get; set; }
-        public int DurationUnitId { get; set; }
+        public DurationUnit DurationUnit { get; set; }
         public StudyMode StudyMode { get; set; }
         public AttendancePattern AttendanceMode { get; set; }
 
