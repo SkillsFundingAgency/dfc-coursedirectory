8c4d0b057618fd9c46893b342952f35fac8418a1
simon bradley <bradley_simon@outlook.com>
2019-02-05 15:09:57 +0000

    Your courses v2 (#174)
    * wip
    
    * done
    
    * done
    
    * DFC-???? Ading qualifications list screen.
    
    * DFC-???? Adding gds summary list controol to the frontend to display the qualification type links
    
    * DFC-???? Adding Your Course 2 button on search provider screen.
    
    * done

---
+++ src/Dfc.CourseDirectory.Web/ViewModels/CoursesForQualificationAndCountViewModel.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Dfc.CourseDirectory.Models.Models.Courses;
+
+namespace Dfc.CourseDirectory.Web.ViewModels
+{
+    public class CoursesForQualificationAndCountViewModel
+    {
+        public Course Course { get; set; }
+
+        public int CourseRunCount { get; set; }
+
+        public IEnumerable<CourseRun> CourseRuns { get; set; }
+
+        public string QualificationType { get; set; }
+    }
+}
