88ba0c842b97d56255a3629661b880913d211a35
suniara <35219355+suniara@users.noreply.github.com>
2019-02-14 14:15:37 +0000

    Funding options view component (#197)

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/FundingOptions/FundingOptionsModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.FundingOptions
+{
+    public class FundingOptionsModel
+    {
+        public string FundingOptionsHintText { get; set; }
+        public string FundingOptionsLabelText { get; set; }
+        public string FundingOptionsAriaDescribedBy { get; set; }
+
+        public bool AdultEducationBudget { get; set; }
+        public string AdultEducationBudgetLabelText { get; set; }
+        public string AdultEducationBudgetAriaDescribedBy { get; set; }
+
+        public bool AdvancedLearnerLoan { get; set; }
+        public string AdvancedLearnerLoanLabelText { get; set; }
+        public string AdvancedLearnerLoanAriaDescribedBy { get; set; }
+
+    }
+}
