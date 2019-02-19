285b7b571700c5b1a3a14d2eb3775590be9679df
Liam Wright <mail@liam-wright.co.uk>
2019-02-19 09:59:45 +0000

    Added additional permissions for providers

--- src/Dfc.CourseDirectory.Web/Startup.cs
+++ src/Dfc.CourseDirectory.Web/Startup.cs
@@ -169,6 +169,7 @@ namespace Dfc.CourseDirectory.Web
                 options.AddPolicy("Admin", policy => policy.RequireRole("Developer"));
                 options.AddPolicy("ElevatedUserRole", policy => policy.RequireRole("Developer", "Helpdesk"));
                 options.AddPolicy("Helpdesk", policy => policy.RequireRole("Helpdesk"));
+                options.AddPolicy("Provider", policy => policy.RequireRole("Provider User", "Provider Superuser"));
             });
             services.AddDistributedMemoryCache();
 
