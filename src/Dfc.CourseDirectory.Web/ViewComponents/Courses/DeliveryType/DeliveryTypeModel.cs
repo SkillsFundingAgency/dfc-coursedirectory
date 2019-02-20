58a41b7da9f9211782fc419bd8fddba0a6fdaa56
bradleysimon <bradley_simon@hotmail.com>
2019-02-20 11:30:15 +0000

    WIP

--- src/Dfc.CourseDirectory.Web/ViewComponents/Courses/DeliveryType/DeliveryTypeModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/DeliveryType/DeliveryTypeModel.cs
@@ -9,7 +9,7 @@ namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.DeliveryType
 {
     public class DeliveryTypeModel
     {
-        [Required(ErrorMessage = "Select Delivery Mode")]
+        //[Required(ErrorMessage = "Select Delivery Mode")]
         public DeliveryMode DeliveryMode { get; set; }
         public string LabelText { get; set; }
         public string HintText { get; set; }
