5d9daf90a6b58c85013444c69ac5d51fec148ba1
Mahinder Suniara <mahinder.suniara@btinternet.com>
2019-02-21 13:48:22 +0000

    DFC-7492-Validation for AddCourse

--- src/Dfc.CourseDirectory.Web/ViewComponents/Courses/WhereNext/WhereNextModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/WhereNext/WhereNextModel.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.WhereNext
+﻿namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.WhereNext
 {
     public class WhereNextModel
     {
-        [MaxLength(500, ErrorMessage = "'Where next' must be 500 characters or less")]
-        [RegularExpression(@"[a-zA-Z0-9 /\r\n?|\n\¬\!\£\$\%\^\&\*\(\)_\+\-\=\{\}\[\]\;\:\@\'\#\~\,\<\>\.\?\/\|\`\" + "\"" + @"\\]+", ErrorMessage = "Where next contains invalid characters")]
         public string WhereNext { get; set; }
         public string LabelText { get; set; }
         public string HintText { get; set; }
