2f8d6e106502b47e3ffc806a4167f20269cc33af
suniara <35219355+suniara@users.noreply.github.com>
2019-02-21 13:54:57 +0000

    Dfc 7221 add validation for add course (#216)
    * CourseFor validation changes
    
    * DFC-7492-Validation for AddCourse

--- src/Dfc.CourseDirectory.Web/ViewComponents/Courses/CourseFor/CourseForModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/CourseFor/CourseForModel.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-
-namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.CourseFor
+﻿namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.CourseFor
 {
     public class CourseForModel
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Enter who is the course for")]
-        [MaxLength(2000, ErrorMessage = "Who is this course for must be 2000 characters or less")]
-        [RegularExpression(@"[a-zA-Z0-9 /\r\n?|\n\¬\!\£\$\%\^\&\*\(\)_\+\-\=\{\}\[\]\;\:\@\'\#\~\,\<\>\.\?\/\|\`\" + "\"" + @"\\]+", ErrorMessage = "Who is this course for contains invalid characters")]
         public string CourseFor { get; set; }
         public string LabelText { get; set; }
         public string HintText { get; set; }
--- src/Dfc.CourseDirectory.Web/ViewComponents/Courses/HowYouWillLearn/HowYouWillLearnModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/HowYouWillLearn/HowYouWillLearnModel.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.HowYouWillLearn
+﻿namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.HowYouWillLearn
 {
     public class HowYouWillLearnModel
     {
-        [MaxLength(500, ErrorMessage = "How you’ll learn must be 500 characters or less")]
-        [RegularExpression(@"[a-zA-Z0-9 /\r\n?|\n\¬\!\£\$\%\^\&\*\(\)_\+\-\=\{\}\[\]\;\:\@\'\#\~\,\<\>\.\?\/\|\`\" + "\"" + @"\\]+", ErrorMessage = "How you’ll learn contains invalid characters")]
         public string HowYouWillLearn { get; set; }
         public string LabelText { get; set; }
         public string HintText { get; set; }
