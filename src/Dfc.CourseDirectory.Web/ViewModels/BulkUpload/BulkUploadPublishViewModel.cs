4129e9a159a7dede90957bdc7a3670a01b33e723
bradleysimon <bradley_simon@hotmail.com>
2019-02-27 10:29:04 +0000

    wip

---
+++ src/Dfc.CourseDirectory.Web/ViewModels/BulkUpload/BulkUploadPublishViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Models.Models.Courses;
+using Dfc.CourseDirectory.Services.Interfaces.CourseService;
+
+namespace Dfc.CourseDirectory.Web.ViewModels.BulkUpload
+{
+    public class BulkUploadPublishViewModel
+    {
+       public Guid? CourseId { get; set; }
+
+        public IEnumerable<Course> Courses { get; set; }
+    }
+}
