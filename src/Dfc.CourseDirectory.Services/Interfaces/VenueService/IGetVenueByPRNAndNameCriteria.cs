3d7f22931d5b32e3102c040d4f7993f587d06f08
Ian Rose <ian.findacourse@gmail.com>
2019-02-27 15:47:19 +0000

    Add service wrapper for endpoint to get venue by name

---
+++ src/Dfc.CourseDirectory.Services/Interfaces/VenueService/IGetVenueByPRNAndNameCriteria.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Dfc.CourseDirectory.Services.Interfaces.VenueService
+{
+    public interface IGetVenuesByPRNAndNameCriteria
+    {
+        string PRN { get; }
+        string Name { get; }
+    }
+}
