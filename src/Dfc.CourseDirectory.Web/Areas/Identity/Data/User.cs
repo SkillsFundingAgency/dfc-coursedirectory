56281b97920f390490289904bb25b0a1ee05a1c7
Liam Wright <mail@liam-wright.co.uk>
2019-03-14 11:48:26 +0000

    If PasswordResetRequired = 1 in DB on account, user will need to reset password on login

--- src/Dfc.CourseDirectory.Web/Areas/Identity/Data/User.cs
+++ src/Dfc.CourseDirectory.Web/Areas/Identity/Data/User.cs
@@ -9,5 +9,6 @@ namespace Dfc.CourseDirectory.Web.Areas.Identity.Data
     // Add profile data for application users by adding properties to the User class
     public class User : IdentityUser
     {
+        public bool PasswordResetRequired { get; set; }
     }
 }
