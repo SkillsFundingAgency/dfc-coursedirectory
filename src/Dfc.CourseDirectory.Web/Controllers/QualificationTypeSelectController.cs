be3f07b0e19d3b9ff26ba9c9f7cd6eb796e60c7a
Mahinder Suniara <mahinder.suniara@btinternet.com>
2019-03-15 13:17:03 +0000

    qualification selector page

---
+++ src/Dfc.CourseDirectory.Web/Controllers/QualificationTypeSelectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.Web.Controllers
+{
+    public class QualificationTypeSelectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
