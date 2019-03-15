a15ed16ff86d1ec4f294767de5fd86ab34e98245
bradleysimon <bradley_simon@hotmail.com>
2019-03-15 11:04:25 +0000

    done

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/ChooseRegion/ChooseRegionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Dfc.CourseDirectory.Models.Models;
+using Dfc.CourseDirectory.Models.Models.Courses;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.ChooseRegion
+{
+    public class ChooseRegionModel
+    {
+        public SelectRegionModel Regions { get; set; }
+
+      
+    }
+}
