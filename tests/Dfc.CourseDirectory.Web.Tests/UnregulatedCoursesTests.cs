e0c475717987aa672156031f1429ba0b5316cf9a
Liam Wright <28269309+LiamWright@users.noreply.github.com>
2019-03-18 13:46:41 +0000

    Fixes issues with logging in, and UKPRN assigning (#310)

---
+++ tests/Dfc.CourseDirectory.Web.Tests/UnregulatedCoursesTests.cs
@@ -0,0 +1,49 @@
+﻿using Dfc.CourseDirectory.Services;
+using Dfc.CourseDirectory.Web.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Dfc.CourseDirectory.Web.Tests
+{
+    public class UnregulatedCoursesTests
+    {
+        Mock serviceLogger = new Mock<ILogger<LarsSearchService>>();
+        Mock mockLogger = new Mock<ILogger<UnregulatedCoursesController>>();
+        HttpContextAccessor contextAccessor = new HttpContextAccessor();
+        LarsSearchSettings settings = new LarsSearchSettings()
+        {
+            ApiUrl = "",
+            ApiKey = ""
+        };
+
+        //    CourseService service = new CourseService(serviceLogger.Object, new HttpClient(), Options.Create(settings));
+        //    CourseSearchCriteria criteria = new CourseSearchCriteria(UKPRNvalue: 10000654);
+
+        //    var resultCourses = service.GetYourCoursesByUKPRNAsync(criteria).Result.Value;
+
+        //    vm.Courses = resultCourses.Value.SelectMany(o => o.Value).SelectMany(i => i.Value).ToList();
+        //    vm.NumberOfCoursesInFiles = vm.Courses.Count();
+        //    //var course = courses2.SingleOrDefault(x => x.id == model.CourseId);
+
+        //    // var courserun = course.CourseRuns.SingleOrDefault(x => x.id == model.courseRun.id);
+
+        //    //Arrange
+        //    var controller = new PublishCoursesController(
+        //        logger: mockLogger.Object,
+        //        contextAccessor: contextAccessor,
+        //        courseService: service
+        //        );
+
+
+        [Fact]
+        public void ServiceWrapper()
+        {
+            
+        }
+    }
+}
