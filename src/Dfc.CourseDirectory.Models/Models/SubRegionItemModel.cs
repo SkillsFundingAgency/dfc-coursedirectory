0076a1fe07d6fbc5e2d9178acc1fe4fb53c883ed
suniara <35219355+suniara@users.noreply.github.com>
2019-03-25 12:00:45 +0000

    Changed to SubRegionItemModel (#348)

---
+++ src/Dfc.CourseDirectory.Models/Models/SubRegionItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Dfc.CourseDirectory.Models.Models
+{
+    public class SubRegionItemModel
+    {
+        public string Id { get; set; }
+        public string SubRegionName { get; set; }
+        public bool? Checked { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
