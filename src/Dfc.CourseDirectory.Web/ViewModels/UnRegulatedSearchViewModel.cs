04cb04580bd53b6b0e85d63378555f482e6d620b
bradleysimon <bradley_simon@hotmail.com>
2019-04-01 16:00:38 +0100

    done

--- src/Dfc.CourseDirectory.Web/ViewModels/UnRegulatedSearchViewModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewModels/UnRegulatedSearchViewModel.cs
@@ -5,7 +5,7 @@ namespace Dfc.CourseDirectory.Web.ViewModels
 {
     public class UnRegulatedSearchViewModel
     {
-        [RegularExpression("^[zZ]{1}[a-zA-Z0-9]{7}$", ErrorMessage = "Code must start with Z followed by 8 numbers or letters")]
+        [RegularExpression("^[zZ]{1}[a-zA-Z0-9]{7}$", ErrorMessage = "Code must start with Z followed by 7 numbers or letters")]
         [Required(ErrorMessage = "Enter a z code to search")]
         public string Search { get; set; }
 
