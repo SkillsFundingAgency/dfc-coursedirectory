5f9bdd66e4ae655559cd8fb782d5fc7609ef59aa
simon bradley <bradley_simon@outlook.com>
2019-04-11 11:17:36 +0100

    done (#420)

--- src/Dfc.CourseDirectory.Web/ViewComponents/GoogleAnalyticsTagHelperComponent.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/GoogleAnalyticsTagHelperComponent.cs
@@ -27,18 +27,35 @@ namespace Dfc.CourseDirectory.Web.ViewComponents
             if (string.Equals(output.TagName, "head", StringComparison.OrdinalIgnoreCase))
             {
                 // Get the tracking code from the configuration
-                var trackingCode = "UA-137823248-1"; 
-               //var trackingCode = _googleAnalyticsOptions.TrackingCode;
-                if (!string.IsNullOrEmpty(trackingCode))
-                {
-                    // PostContent correspond to the text just before closing tag
-                    output.PostContent
-                        .AppendHtml("<script async src='https://www.googletagmanager.com/gtag/js?id=")
-                        .AppendHtml(trackingCode)
-                        .AppendHtml("'></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date);gtag('config','")
-                        .AppendHtml(trackingCode)
-                        .AppendHtml("',{displayFeaturesTask:'null'});</script>");
-                }
+                //var trackingCode = "UA-137823248-1"; 
+                //var trackingCode = _googleAnalyticsOptions.TrackingCode;
+                //if (!string.IsNullOrEmpty(trackingCode))
+                //// {
+                // PostContent correspond to the text just before closing tag
+                //output.PostContent
+                //    .AppendHtml("<script async src='https://www.googletagmanager.com/gtag/js?id=")
+                //    .AppendHtml(trackingCode)
+                //    .AppendHtml("'></script><script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date);gtag('config','")
+                //    .AppendHtml(trackingCode)
+                //    .AppendHtml("',{displayFeaturesTask:'null'});</script>");
+
+                output.PostContent
+                    .AppendHtml("<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':")
+                    .AppendHtml("new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],")
+                    .AppendHtml("j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=")
+                    .AppendHtml("'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);")
+                    .AppendHtml("})(window,document,'script','dataLayer','GTM-P768LJ5');</script>");
+
+               // output.PostContent.AppendHtml("<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f = d.getElementsByTagName(s)[0],j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async=true;j.src=https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j, f);})(window, document,'script','dataLayer','GTM-5R9DDR9');</script>");
+              //  }
+            }
+
+            if (string.Equals(output.TagName, "body", StringComparison.OrdinalIgnoreCase))
+            {
+
+                output.PostContent
+                    .AppendHtml("<noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-P768LJ5'")
+                    .AppendHtml("height='0' width='0' style='display: none; visibility: hidden'></iframe></noscript>");
             }
         }
     }
