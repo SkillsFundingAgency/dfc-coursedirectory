fe82dbd1c28b9bff2fb780ad9e2b2aa9bcb3e1a0
Liam Wright <mail@liam-wright.co.uk>
2019-04-23 16:26:06 +0100

    New EnumExtension function added, Display names in courserun model added

--- src/Dfc.CourseDirectory.Web/Controllers/CourseSummaryController.cs
+++ src/Dfc.CourseDirectory.Web/Controllers/CourseSummaryController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Dfc.CourseDirectory.Common;
 using Dfc.CourseDirectory.Models.Interfaces.Courses;
