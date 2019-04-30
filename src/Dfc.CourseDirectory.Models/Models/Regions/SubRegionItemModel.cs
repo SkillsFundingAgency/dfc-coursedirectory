158f7877cfc9bf923159047795ae201ad5539cfa
Ian <44667988+ian-findacourse@users.noreply.github.com>
2019-04-30 16:20:37 +0100

    COUR-132-FAC-search-region-weighting (#483)

--- src/Dfc.CourseDirectory.Models/Models/Regions/SubRegionItemModel.cs
+++ src/Dfc.CourseDirectory.Models/Models/Regions/SubRegionItemModel.cs
@@ -1,5 +1,16 @@
-﻿namespace Dfc.CourseDirectory.Models.Models.Regions
+﻿
+using System.Linq;
+
+
+namespace Dfc.CourseDirectory.Models.Models.Regions
 {
+    public enum SearchResultWeightings
+    {
+        Low,
+        Medium,
+        High
+    }
+
     public class SubRegionItemModel
     {
         public string Id { get; set; }
@@ -7,5 +18,16 @@
         public bool? Checked { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public SearchResultWeightings Weighting { get { return CalcWeighting(); } }
+
+        // Subregions also used to store regions for azure search index, so calculate search weighting based on whether it's a region or subregion
+        private SearchResultWeightings CalcWeighting()
+        {
+            if (new SelectRegionModel().RegionItems.Any(r => r.Id == Id))
+                return SearchResultWeightings.Medium;
+            if (new SelectRegionModel().RegionItems.SelectMany(r => r.SubRegion).Any(r => r.Id == Id))
+                return SearchResultWeightings.High;
+            return SearchResultWeightings.Low;
+        }
     }
 }
