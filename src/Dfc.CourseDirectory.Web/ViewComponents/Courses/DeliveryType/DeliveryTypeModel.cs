3f4fa99c8f47b9ff41a1edeaa03a0afaed9d9c16
Liam Wright <mail@liam-wright.co.uk>
2019-05-08 09:39:09 +0100

    Updated DeliveryMode view component to contain second line of hint text as per story

--- src/Dfc.CourseDirectory.Web/ViewComponents/Courses/DeliveryType/DeliveryTypeModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/DeliveryType/DeliveryTypeModel.cs
@@ -13,6 +13,7 @@ namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.DeliveryType
         public DeliveryMode DeliveryMode { get; set; }
         public string LabelText { get; set; }
         public string HintText { get; set; }
+        public string SecondHintText { get; set; }
         public string AriaDescribedBy { get; set; }
     }
 }
