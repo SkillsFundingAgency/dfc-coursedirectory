ad77f7b4495a8d17e8663acafbb335c44249d58c
Liam Wright <28269309+LiamWright@users.noreply.github.com>
2019-05-22 11:01:06 +0100

    Logging level change (#549)
    * Bumped up Logging level to fit in line with Prod.
    
    * Increased Application insights debugging
    
    * Undoing Main function edits

--- src/Dfc.CourseDirectory.Web/Program.cs
+++ src/Dfc.CourseDirectory.Web/Program.cs
@@ -9,24 +9,8 @@ namespace Dfc.CourseDirectory.Web
     {
         public static void Main(string[] args)
         {
-            var host = CreateWebHostBuilder(args).Build();
-            var logger = host.Services.GetRequiredService<ILogger<Program>>();
-            // This will be picked up by AI
-            logger.LogInformation("From Program. Running the host now..");
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
-
-        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        //    WebHost.CreateDefaultBuilder(args)
-        //        .UseStartup<Startup>()
-        //        .ConfigureLogging((context, builder) =>
-        //        {
-        //            builder.AddConfiguration(context.Configuration.GetSection("Logging"));
-        //            builder.addApplicationInsi
-
-        //        })
-        //        .UseStartup<Startup>()
-        //        .UseApplicationInsights();
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
@@ -37,15 +21,6 @@ namespace Dfc.CourseDirectory.Web
                     var ikey = context.Configuration.GetSection("ApplicationInsights:InstrumentationKey");
                     builder.AddApplicationInsights(ikey.Value);
 
-                    // Adding the filter below to ensure logs of all severity from Program.cs
-                    // is sent to ApplicationInsights.
-                    builder.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>
-                                     (typeof(Program).FullName, LogLevel.Trace);
-
-                    // Adding the filter below to ensure logs of all severity from Startup.cs
-                    // is sent to ApplicationInsights.
-                    builder.AddFilter<Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider>
-                                     (typeof(Startup).FullName, LogLevel.Trace);
                 }
         );
     }
