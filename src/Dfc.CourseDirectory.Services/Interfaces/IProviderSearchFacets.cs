826866dab3113653df4c5cc87e90d3f54363d7df
Ian Rose <ian.findacourse@gmail.com>
2019-05-24 09:02:54 +0100

    temp check-in

---
+++ src/Dfc.CourseDirectory.Services/Interfaces/IProviderSearchFacets.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace Dfc.CourseDirectory.Services.Interfaces
+{
+    public interface IProviderSearchFacets
+    {
+        IEnumerable<SearchFacet> Town { get; }
+        string TownODataType { get; }
+        //IEnumerable<SearchFacet> Region { get; }
+        //string RegionODataType { get; }
+    }
+}
