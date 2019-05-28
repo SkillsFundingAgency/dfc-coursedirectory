3fdf7071d9d7e8fb9bd5fe5923f34c7b19bf3d64
Ian Rose <ian.findacourse@gmail.com>
2019-05-28 14:57:44 +0100

    wip

---
+++ src/Dfc.CourseDirectory.Models/Models/Providers/ProviderAzureSearchResults.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Dfc.CourseDirectory.Common.Interfaces;
+
+
+namespace Dfc.CourseDirectory.Models.Models.Providers
+{
+    public class ProviderAzureSearchResults //: IProviderAzureSearchResultModel
+    {
+        public string ODataContext { get; set; }
+        public int? ODataCount { get; set; }
+        public dynamic SearchFacets { get; set; } //ProviderSearchFacets SearchFacets { get; set; }
+        public IEnumerable<ProviderAzureSearchResultItem> Value { get; set; }
+    }
+}
