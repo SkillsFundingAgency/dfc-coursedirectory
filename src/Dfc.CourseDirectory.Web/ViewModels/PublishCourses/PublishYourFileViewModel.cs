1dfd98806e21e5d8a7df4afb96d22dea1b08a60e
bradleysimon <bradley_simon@hotmail.com>
2019-06-06 15:26:24 +0100

    done

---
+++ src/Dfc.CourseDirectory.Web/ViewModels/PublishCourses/PublishYourFileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Dfc.CourseDirectory.Models.Enums;
+using Dfc.CourseDirectory.Models.Models.Courses;
+
+namespace Dfc.CourseDirectory.Web.ViewModels.PublishCourses
+{
+    public class PublishYourFileViewModel
+    {
+        public int NumberOfCourses { get; set; }
+
+       
+    }
+}
