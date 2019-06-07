605fe80167fcbb67fb85b040af3af3fd6da911d0
Ian Rose <ian.findacourse@gmail.com>
2019-06-07 12:14:20 +0100

    wip

---
+++ src/Dfc.CourseDirectory.Services/Interfaces/IBlobStorageService.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Common.Interfaces;
+
+
+namespace Dfc.CourseDirectory.Services.Interfaces.BlobStorageService
+{
+    public interface IBlobStorageService
+    {
+        Task DownloadFileAsync(string filePath, Stream stream);
+        Task UploadFileAsync(string filePath, Stream stream);
+    }
+}
