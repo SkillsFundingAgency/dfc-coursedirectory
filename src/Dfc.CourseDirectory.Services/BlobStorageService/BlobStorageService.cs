f6c19110bd3d2f0407bb73448ee86d35b159804d
Ian Rose <ian.findacourse@gmail.com>
2019-06-07 12:12:13 +0100

    wip

---
+++ src/Dfc.CourseDirectory.Services/BlobStorageService/BlobStorageService.cs
@@ -0,0 +1,99 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Auth;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Dfc.CourseDirectory.Common;
+using Dfc.CourseDirectory.Models.Interfaces;
+using Dfc.CourseDirectory.Services.Interfaces.BlobStorageService;
+
+
+namespace Dfc.CourseDirectory.Services.BlobStorageService
+{
+    public class BlobStorageService : IBlobStorageService
+    {
+        private readonly ILogger<BlobStorageService> _log;
+        private readonly HttpClient _httpClient;
+
+        private readonly string _accountName;
+        private readonly string _accountKey;
+        private readonly string _container;
+        //private readonly Uri _getSomethingByIdUri;
+
+        private readonly CloudStorageAccount _account;
+        private readonly CloudBlobClient _blobClient;
+
+        public BlobStorageService(
+            ILogger<BlobStorageService> logger,
+            HttpClient httpClient,
+            IOptions<BlobStorageSettings> settings)
+        {
+            Throw.IfNull(logger, nameof(logger));
+            Throw.IfNull(httpClient, nameof(httpClient));
+            Throw.IfNull(settings, nameof(settings));
+
+            _log = logger;
+            _httpClient = httpClient;
+
+            //_getSomethingByIdUri = settings.Value.ToGetSomethingByIdUri();
+            _accountName = settings.Value.AccountName;
+            _accountKey = settings.Value.AccountKey;
+            _container = settings.Value.Container;
+
+            //Set up the client
+            _account = new CloudStorageAccount(new StorageCredentials(_accountName, _accountKey), true);
+            _blobClient = _account.CreateCloudBlobClient();
+        }
+
+        public Task DownloadFileAsync(string filePath, Stream stream)
+        {
+            try {
+                CloudBlobContainer container = _blobClient.GetContainerReference(_container);
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(filePath);
+
+                if (blockBlob.ExistsAsync().Result) {
+                    _log.LogInformation($"Downloading {filePath} from blob storage");
+                    return blockBlob.DownloadToStreamAsync(stream);
+                } else {
+                    return null;
+                }
+
+            //} catch (StorageException stex) {
+            //    _log.LogException($"Exception downloading {filePath}", stex);
+            //    return null;
+
+            } catch (Exception ex) {
+                _log.LogException($"Exception downloading {filePath}", ex);
+                return null;
+            }
+        }
+
+        public Task UploadFileAsync(string filePath, Stream stream)
+        {
+            try {
+                CloudBlobContainer container = _blobClient.GetContainerReference(_container);
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(filePath);
+
+                return blockBlob.UploadFromStreamAsync(stream);
+
+            //} catch (StorageException stex) {
+            //    _log.LogException($"Exception downloading {filePath}", stex);
+            //    return null;
+
+            } catch (Exception ex) {
+                _log.LogException($"Exception uploading {filePath}", ex);
+                return null;
+            }
+        }
+    }
+}
