622e1cecca7824dfee179743ee23bedb466ad908
Ian Rose <ian.findacourse@gmail.com>
2019-06-07 12:12:56 +0100

    wip

---
+++ src/Dfc.CourseDirectory.Services/BlobStorageService/BlobStorageSettings.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Dfc.CourseDirectory.Services.Interfaces.BlobStorageService;
+
+
+namespace Dfc.CourseDirectory.Services.BlobStorageService
+{
+    public class BlobStorageSettings : IBlobStorageSettings
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string Container { get; set; }
+    }
+}
