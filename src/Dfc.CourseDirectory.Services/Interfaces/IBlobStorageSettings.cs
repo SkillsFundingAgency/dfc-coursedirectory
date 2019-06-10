148c9600e9fb7338d1eea98a981f43e6d2232dfc
Ian Rose <ian.findacourse@gmail.com>
2019-06-10 09:21:33 +0100

    BU template path

--- src/Dfc.CourseDirectory.Services/Interfaces/IBlobStorageSettings.cs
+++ src/Dfc.CourseDirectory.Services/Interfaces/IBlobStorageSettings.cs
@@ -6,5 +6,8 @@ namespace Dfc.CourseDirectory.Services.Interfaces.BlobStorageService
         string AccountName { get; }
         string AccountKey { get; }
         string Container { get; }
+        //string BulkUploadPathFormat { get; }
+        string TemplatePath { get; }
+
     }
 }
