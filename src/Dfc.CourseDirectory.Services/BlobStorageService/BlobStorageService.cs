697906ae2016a9c345bb8eac533e441e36a243f4
Ian Rose <ian.findacourse@gmail.com>
2019-06-11 11:43:39 +0100

    COUR-941 Add archiving and file listing

--- src/Dfc.CourseDirectory.Services/BlobStorageService/BlobStorageService.cs
+++ src/Dfc.CourseDirectory.Services/BlobStorageService/BlobStorageService.cs
@@ -16,6 +16,13 @@ using Dfc.CourseDirectory.Services.Interfaces.BlobStorageService;
 
 namespace Dfc.CourseDirectory.Services.BlobStorageService
 {
+    public class BlobFileInfo
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public DateTimeOffset? DateUploaded { get; set; }
+    }
+
     public class BlobStorageService : IBlobStorageService
     {
         private readonly ILogger<BlobStorageService> _log;
@@ -23,12 +30,13 @@ namespace Dfc.CourseDirectory.Services.BlobStorageService
 
         private readonly string _accountName;
         private readonly string _accountKey;
-        private readonly string _container;
+        //private readonly string _containerName;
         //private readonly string _bulkUploadPathFormat;
         private readonly string _templatePath;
 
         private readonly CloudStorageAccount _account;
         private readonly CloudBlobClient _blobClient;
+        private readonly CloudBlobContainer _container;
 
         public BlobStorageService(
             ILogger<BlobStorageService> logger,
@@ -45,21 +53,21 @@ namespace Dfc.CourseDirectory.Services.BlobStorageService
             //_getSomethingByIdUri = settings.Value.ToGetSomethingByIdUri();
             _accountName = settings.Value.AccountName;
             _accountKey = settings.Value.AccountKey;
-            _container = settings.Value.Container;
+            //_containerName = settings.Value.Container;
             //_bulkUploadPathFormat = settings.Value.BulkUploadPathFormat;
             _templatePath = settings.Value.TemplatePath;
 
             //Set up the client
             _account = new CloudStorageAccount(new StorageCredentials(_accountName, _accountKey), true);
-            _blobClient = _account.CreateCloudBlobClient();
+            //_blobClient = _account.CreateCloudBlobClient();
+            _container = _account.CreateCloudBlobClient()
+                                 .GetContainerReference(settings.Value.Container);
         }
 
         public Task DownloadFileAsync(string filePath, Stream stream)
         {
             try {
-                //CloudBlobContainer container = _blobClient.GetContainerReference(_container);
-                CloudBlockBlob blockBlob = _blobClient.GetContainerReference(_container)
-                                                      .GetBlockBlobReference(filePath);
+                CloudBlockBlob blockBlob = _container.GetBlockBlobReference(filePath);
 
                 if (blockBlob.ExistsAsync().Result) {
                     _log.LogInformation($"Downloading {filePath} from blob storage");
@@ -81,9 +89,7 @@ namespace Dfc.CourseDirectory.Services.BlobStorageService
         public Task UploadFileAsync(string filePath, Stream stream)
         {
             try {
-                //CloudBlobContainer container = _blobClient.GetContainerReference(_container);
-                CloudBlockBlob blockBlob = _blobClient.GetContainerReference(_container)
-                                                      .GetBlockBlobReference(filePath);
+                CloudBlockBlob blockBlob = _container.GetBlockBlobReference(filePath);
 
                 stream.Position = 0;
                 return blockBlob.UploadFromStreamAsync(stream);
@@ -94,6 +100,53 @@ namespace Dfc.CourseDirectory.Services.BlobStorageService
 
             } catch (Exception ex) {
                 _log.LogException($"Exception uploading {filePath}", ex);
+                return null;
+            }
+        }
+
+        public IEnumerable<BlobFileInfo> GetFileList(string filePath)
+        {
+            try {
+                return _container.GetDirectoryReference(filePath)
+                                ?.ListBlobs()
+                                ?.OfType<CloudBlockBlob>()
+                                ?.Select(b => new BlobFileInfo() { Name = b.Name, Size = b.Properties.Length, DateUploaded = b.Properties.Created });
+
+                //} catch (StorageException stex) {
+                //    _log.LogException($"Exception listing files at {filePath}", stex);
+                //    return null;
+
+            } catch (Exception ex) {
+                _log.LogException($"Exception listing files at {filePath}", ex);
+                return null;
+            }
+        }
+
+        public IEnumerable<CloudBlockBlob> ArchiveFiles(string filePath)
+        {
+            try
+            {                
+                IEnumerable<CloudBlockBlob> blobs = _container?.GetDirectoryReference(filePath)
+                                                              ?.ListBlobs()
+                                                              ?.OfType<CloudBlockBlob>()
+                                                    ?? new CloudBlockBlob[] { };
+                foreach(CloudBlockBlob b in blobs)
+                {
+                    _container.GetBlobReference("aaaa/aaa")
+                              .StartCopy(b.Uri);
+                    b.DeleteIfExists();
+                }
+
+                return blobs;
+
+                //} catch (StorageException stex) {
+                //    _log.LogException($"Exception listing files at {filePath}", stex);
+                //    return null;
+
+            }
+            catch (Exception ex)
+            {
+                _log.LogException($"Exception listing files at {filePath}", ex);
                 return null;
             }
         }
