6daee374e58595b98a92cc9eaf11c5079a9deecf
Ian <44667988+ian-findacourse@users.noreply.github.com>
2019-06-11 14:59:46 +0100

    COUR-941 Archive and list blob storage files (#605)
    * COUR-941 Changed to stream to and from blob storage
    
    * COUR-941 Changed to stream to and from blob storage
    
    * COUR-941 Changed to stream to and from blob storage
    
    * COUR-941 Changed to stream to and from blob storage, files no longer stored on web server
    
    * COUR-941 Add archiving and file listing
    
    * COUR-941 Archive and list blob storage files
    
    * COUR-941 Archive and list blob storage files
    
    * COUR-941 Archive and list blob storage files

--- src/Dfc.CourseDirectory.Services/Interfaces/IBlobStorageService.cs
+++ src/Dfc.CourseDirectory.Services/Interfaces/IBlobStorageService.cs
@@ -2,7 +2,10 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Azure.Storage.Blob;
 using Dfc.CourseDirectory.Common.Interfaces;
+using Dfc.CourseDirectory.Services.BlobStorageService;
 
 
 namespace Dfc.CourseDirectory.Services.Interfaces.BlobStorageService
@@ -11,6 +14,8 @@ namespace Dfc.CourseDirectory.Services.Interfaces.BlobStorageService
     {
         Task DownloadFileAsync(string filePath, Stream stream);
         Task UploadFileAsync(string filePath, Stream stream);
+        IEnumerable<BlobFileInfo> GetFileList(string filePath);
+        IEnumerable<CloudBlockBlob> ArchiveFiles(string filePath);
         Task GetBulkUploadTemplateFileAsync(Stream stream);
     }
 }
