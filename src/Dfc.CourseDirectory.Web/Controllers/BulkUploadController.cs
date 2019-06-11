f3e3a94f9016bea990c8d346c0b49f23a80ec166
Ian <44667988+ian-findacourse@users.noreply.github.com>
2019-06-11 10:28:38 +0100

    COUR-941 Changed to stream to and from blob storage, files no longer stored on web server (#599)
    * COUR-941 Changed to stream to and from blob storage
    
    * COUR-941 Changed to stream to and from blob storage
    
    * COUR-941 Changed to stream to and from blob storage
    
    * COUR-941 Changed to stream to and from blob storage, files no longer stored on web server

--- src/Dfc.CourseDirectory.Web/Controllers/BulkUploadController.cs
+++ src/Dfc.CourseDirectory.Web/Controllers/BulkUploadController.cs
@@ -1,18 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+using System;
 using System.IO;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
-using Dfc.CourseDirectory.Common;
-using Dfc.CourseDirectory.Services.Interfaces.BulkUploadService;
-using Dfc.CourseDirectory.Web.ViewModels.BulkUpload;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Security.Claims;
+using Dfc.CourseDirectory.Common;
 using Dfc.CourseDirectory.Models.Enums;
+using Dfc.CourseDirectory.Services.Interfaces.BlobStorageService;
+using Dfc.CourseDirectory.Services.Interfaces.BulkUploadService;
+using Dfc.CourseDirectory.Web.ViewModels.BulkUpload;
+
 
 namespace Dfc.CourseDirectory.Web.Controllers
 {
@@ -22,6 +25,7 @@ namespace Dfc.CourseDirectory.Web.Controllers
         private readonly ILogger<BulkUploadController> _logger;
         private readonly IHttpContextAccessor _contextAccessor;
         private readonly IBulkUploadService _bulkUploadService;
+        private readonly IBlobStorageService _blobService;
 
         private IHostingEnvironment _env;
         private ISession _session => _contextAccessor.HttpContext.Session;
@@ -30,16 +34,19 @@ namespace Dfc.CourseDirectory.Web.Controllers
                 ILogger<BulkUploadController> logger,
                 IHttpContextAccessor contextAccessor,
                 IBulkUploadService bulkUploadService,
+                IBlobStorageService blobService,
                 IHostingEnvironment env)
         {
             Throw.IfNull(logger, nameof(logger));
             Throw.IfNull(contextAccessor, nameof(contextAccessor));
             Throw.IfNull(bulkUploadService, nameof(bulkUploadService));
+            Throw.IfNull(blobService, nameof(blobService));
             Throw.IfNull(env, nameof(env));
 
             _logger = logger;
             _contextAccessor = contextAccessor;
             _bulkUploadService = bulkUploadService;
+            _blobService = blobService;
             _env = env;
         }
 
@@ -58,15 +65,11 @@ namespace Dfc.CourseDirectory.Web.Controllers
         public async Task<IActionResult> Index(IFormFile bulkUploadFile)
         {
             int? UKPRN;
-
             if (_session.GetInt32("UKPRN") != null)
-            {
                 UKPRN = _session.GetInt32("UKPRN").Value;
-            }
             else
-            {
                 return RedirectToAction("Index", "Home", new { errmsg = "Please select a Provider." });
-            }
+
 
             BulkUploadViewModel vm = new BulkUploadViewModel();
             string errorMessage;
@@ -75,53 +78,29 @@ namespace Dfc.CourseDirectory.Web.Controllers
             {
                 int providerUKPRN = UKPRN.Value;
                 string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-
-                string webRoot = _env.WebRootPath;
-                if (!Directory.Exists(webRoot))
-                    Directory.CreateDirectory(webRoot);
                 string bulkUploadFileNewName = string.Format(@"{0}-{1}", DateTime.Now.ToString("yyMMdd-HHmmss"), bulkUploadFile.FileName);
-                string savedCsvFilePath = string.Format(@"{0}\BulkUploads\{1}", webRoot, bulkUploadFileNewName);
 
-                using (var stream = new FileStream(savedCsvFilePath, FileMode.Create))
-                {
-                    await bulkUploadFile.CopyToAsync(stream);
-                }
+                MemoryStream ms = new MemoryStream();
+                bulkUploadFile.CopyTo(ms);
+                Task task = _blobService.UploadFileAsync($"{UKPRN.ToString()}/Bulk Upload/Files/{bulkUploadFileNewName}", ms);
+                task.Wait();
+                var errors = _bulkUploadService.ProcessBulkUpload(ms, providerUKPRN, userId);
 
-                var errors = _bulkUploadService.ProcessBulkUpload(savedCsvFilePath, providerUKPRN, userId);
-
-                if (System.IO.File.Exists(savedCsvFilePath))
-                {
-                    System.IO.File.Delete(savedCsvFilePath);
-                }
-
-                if (errors.Any())
-                {
+                if (errors.Any()) {
                     vm.errors = errors;
-
                     return View(vm);
-                }
-                else
-                {
-                    // All good => redirect to BulkCourses action
-                    return RedirectToAction("Index", "PublishCourses", new
-                    {
-                        publishMode = PublishMode.BulkUpload
-                    });
-                }
-            }
-            else
-            {
 
-                var noFileError = new List<string>
-                {
-                    errorMessage
-                };
-                vm.errors = noFileError;
+                } else {
+                    // All good => redirect to BulkCourses action
+                    return RedirectToAction("Index", "PublishCourses", new { publishMode = PublishMode.BulkUpload });
+                }
+
+            } else {
+                vm.errors = new string[] { errorMessage };
             }
             return View(vm);
-  
-
         }
+
         /// <summary>
         /// Server side validation to match and extend the client-side validation
         /// </summary>
