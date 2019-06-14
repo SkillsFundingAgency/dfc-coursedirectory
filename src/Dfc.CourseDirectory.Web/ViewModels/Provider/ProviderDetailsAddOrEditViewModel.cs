a6f9f8ae67ddbd5d8e614841c064a818dfdcb8ff
Stephen Yates <SYatesSoftware@gmail.com>
2019-06-14 11:18:58 +0100

    COUR-347 Update ProviderType added to provider details controller

--- src/Dfc.CourseDirectory.Web/ViewModels/Provider/ProviderDetailsAddOrEditViewModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewModels/Provider/ProviderDetailsAddOrEditViewModel.cs
@@ -1,27 +1,16 @@
-﻿
-using Dfc.CourseDirectory.Models.Models.Courses;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Dfc.CourseDirectory.Services.Interfaces.CourseService;
-
+﻿using Dfc.CourseDirectory.Models.Models.Providers;
 
 namespace Dfc.CourseDirectory.Web.ViewModels
 {
     public class ProviderDetailsAddOrEditViewModel
     {
 
-
         public string UKPRN { get; set; }
-
-
+        
         public string AliasName { get; set; }
 
         public string BriefOverview { get; set; }
 
-
-
+        public ProviderType ProviderType { get; set; }
     }
 }
