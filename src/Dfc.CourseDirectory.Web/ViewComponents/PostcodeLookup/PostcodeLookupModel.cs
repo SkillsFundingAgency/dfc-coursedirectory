4aa9008f00d80bf1402bd397a7e6b8eb3ec698d4
bradleysimon <bradley_simon@hotmail.com>
2019-06-17 15:53:40 +0100

    wip

--- src/Dfc.CourseDirectory.Web/ViewComponents/PostcodeLookup/PostcodeLookupModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/PostcodeLookup/PostcodeLookupModel.cs
@@ -9,10 +9,10 @@ namespace Dfc.CourseDirectory.Web.ViewComponents.PostcodeLookup
 {
     public class PostcodeLookupModel
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Enter a postcode")]
-        [MaxLength(8, ErrorMessage = "Postcode must be 8 characters or less")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "Enter a postcode")]
+        //[MaxLength(8, ErrorMessage = "Postcode must be 8 characters or less")]
 
-        [RegularExpression("([abcdefghijklmnoprstuwyzABCDEFGHIJKLMNOPRSTUWYZ][0-9]|[abcdefghjkstuwABCDEFGHJKSTUW][0-9][0-9]|[abcdefghijklmnoprstuwyzABCDEFGHIJKLMNOPRSTUWYZ][abcdefghklmnopqrstuvwxyABCDEFGHKLMNOPQRSTUVWXY][0-9]|[abcdefghijklmnoprstuwyzABCDEFGHIJKLMNOPRSTUWYZ][abcdefghklmnopqrstuvwxyABCDEFGHKLMNOPQRSTUVWXY][0-9][0-9]|[abcdefghijklmnoprstuwyzABCDEFGHIJKLMNOPRSTUWYZ][0-9][abcdefghjkstuwABCDEFGHJKSTUW]|[abcdefghijklmnoprstuwyzABCDEFGHIJKLMNOPRSTUWYZ][abcdefghklmnopqrstuvwxyABCDEFGHKLMNOPQRSTUVWXY][0-9][abehmnprvwxyABEHMNPRVWXY]) ([0-9][abdefghjlnpqrstuwxyzABDEFGHJLNPQRSTUWXYZ][abdefghjlnpqrstuwxyzABDEFGHJLNPQRSTUWXYZ])", ErrorMessage = "Postcode must be a valid format and only include letters a to z, numbers and spaces")]
+        //[RegularExpression("([abcdefghijklmnoprstuwyzABCDEFGHIJKLMNOPRSTUWYZ][0-9]|[abcdefghjkstuwABCDEFGHJKSTUW][0-9][0-9]|[abcdefghijklmnoprstuwyzABCDEFGHIJKLMNOPRSTUWYZ][abcdefghklmnopqrstuvwxyABCDEFGHKLMNOPQRSTUVWXY][0-9]|[abcdefghijklmnoprstuwyzABCDEFGHIJKLMNOPRSTUWYZ][abcdefghklmnopqrstuvwxyABCDEFGHKLMNOPQRSTUVWXY][0-9][0-9]|[abcdefghijklmnoprstuwyzABCDEFGHIJKLMNOPRSTUWYZ][0-9][abcdefghjkstuwABCDEFGHJKSTUW]|[abcdefghijklmnoprstuwyzABCDEFGHIJKLMNOPRSTUWYZ][abcdefghklmnopqrstuvwxyABCDEFGHKLMNOPQRSTUVWXY][0-9][abehmnprvwxyABEHMNPRVWXY]) ([0-9][abdefghjlnpqrstuwxyzABDEFGHJLNPQRSTUWXYZ][abdefghjlnpqrstuwxyzABDEFGHJLNPQRSTUWXYZ])", ErrorMessage = "Postcode must be a valid format and only include letters a to z, numbers and spaces")]
         public string Postcode { get; set; }
         public string PostcodeLabelText { get; set; }
         public string PostcodeHintText { get; set; }
