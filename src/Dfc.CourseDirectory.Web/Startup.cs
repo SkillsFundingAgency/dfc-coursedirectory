7d588ed14b633a59a59becacb7828f2e426594bb
Liam Wright <mail@liam-wright.co.uk>
2019-07-12 09:48:33 +0100

    Increased error handling for null values and stripping out of illegal characters

--- src/Dfc.CourseDirectory.Web/Startup.cs
+++ src/Dfc.CourseDirectory.Web/Startup.cs
@@ -413,9 +413,6 @@ namespace Dfc.CourseDirectory.Web
 
                         HttpClient client = new HttpClient();
 
-                        _logger.LogError(token);
-                        _logger.LogError($"{apiUri}/organisations/{organisation.Id}/users/{userClaims.UserId}");
-
                         client.SetBearerToken(token);
                         var response = client.GetAsync($"{apiUri}/organisations/{organisation.Id}/users/{userClaims.UserId}").Result;
 
