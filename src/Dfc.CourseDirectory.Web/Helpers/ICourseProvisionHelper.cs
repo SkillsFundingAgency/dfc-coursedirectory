4b27bfa9dcb08f5877a7af0a6ee8520ec356c3e3
Liam Wright <mail@liam-wright.co.uk>
2019-07-15 12:17:50 +0100

    Regions issue resolved

---
+++ src/Dfc.CourseDirectory.Web/Helpers/ICourseProvisionHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.Web.Helpers
+{
+    public interface ICourseProvisionHelper
+    {
+        FileStreamResult DownloadCurrentCourseProvisions();
+    }
+}
