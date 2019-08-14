41f75c01ed1f98c054ef76358441b1453049b554
bradleysimon <bradley_simon@hotmail.com>
2019-08-14 09:56:36 +0100

    wip

--- src/Dfc.CourseDirectory.Web/Controllers/LocationsController.cs
+++ src/Dfc.CourseDirectory.Web/Controllers/LocationsController.cs
@@ -23,6 +23,7 @@ using Dfc.CourseDirectory.Web.ViewComponents.EditVenueName;
 using Dfc.CourseDirectory.Web.ViewComponents.Shared;
 using Dfc.CourseDirectory.Web.ViewComponents.VenueSearchResult;
 using Dfc.CourseDirectory.Web.ViewModels;
+using Dfc.CourseDirectory.Web.ViewModels.Locations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -74,8 +75,83 @@ namespace Dfc.CourseDirectory.Web.Controllers
         }
 
 
-       
-        
-      
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> DeleteLocation(Guid VenueId)
+        {
+            int? UKPRN = _session.GetInt32("UKPRN");
+
+            if (!UKPRN.HasValue)
+            {
+                return RedirectToAction("Index", "Home", new { errmsg = "Please select a Provider." });
+            }
+
+            LocationDeleteViewModel locationDeleteViewModel = new LocationDeleteViewModel();
+            locationDeleteViewModel.VenueId = VenueId;
+
+            return View("../Venues/locationdelete/index", locationDeleteViewModel);
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> DeleteLocation(LocationDeleteViewModel locationDeleteViewModel)
+        {
+            int? UKPRN = _session.GetInt32("UKPRN");
+
+            if (!UKPRN.HasValue)
+            {
+                return RedirectToAction("Index", "Home", new { errmsg = "Please select a Provider." });
+            }
+
+            IVenue updatedVenue = _venueService.GetVenueByIdAsync(new GetVenueByIdCriteria(locationDeleteViewModel.VenueId.ToString())).Result.Value;
+            updatedVenue.Status = VenueStatus.Deleted;
+
+            updatedVenue = _venueService.UpdateAsync(updatedVenue).Result.Value;
+
+            VenueSearchResultItemModel deletedVenue = new VenueSearchResultItemModel(HttpUtility.HtmlEncode(updatedVenue.VenueName), updatedVenue.Address1, updatedVenue.Address2, updatedVenue.Town, updatedVenue.County, updatedVenue.PostCode, updatedVenue.ID);
+
+            return RedirectToAction("Index", "Venues");
+        }
+
+        private async Task<VenueSearchResultsViewModel> GetVenues(int ukprn)
+        {
+            return await GetVenues(ukprn, null, false);
+        }
+        private async Task<VenueSearchResultsViewModel> GetVenues(int ukprn, VenueSearchResultItemModel newVenue, bool updated)
+        {
+            VenueSearchRequestModel requestModel = new VenueSearchRequestModel
+            {
+                SearchTerm = ukprn.ToString()
+            };
+            if (null != newVenue) requestModel.NewAddressId = newVenue.Id;
+
+
+            VenueSearchResultModel model;
+            var criteria = _venueSearchHelper.GetVenueSearchCriteria(requestModel);
+            var result = await _venueService.SearchAsync(criteria);
+            if (result.IsSuccess && result.HasValue)
+            {
+                var items = _venueSearchHelper.GetVenueSearchResultItemModels(result.Value.Value);
+                model = new VenueSearchResultModel(
+                    requestModel.SearchTerm,
+                    items, newVenue, updated);
+            }
+            else
+            {
+                model = new VenueSearchResultModel(result.Error);
+            }
+
+            var viewModel = new VenueSearchResultsViewModel
+            {
+                Result = model
+            };
+            return viewModel;
+        }
+
+
+
+
+
     }
 }
