17b9e6ef38af3435c80a2638cac47ea6e1aee075
Matt Daniels <matt@techtoniq.com>
2019-08-20 07:13:26 +0100

    COUR-1665 backlink view component accepts controller+action params to override default "back" behaviour

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/BackLink/BackLinkModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Dfc.CourseDirectory.Web.ViewComponents.BackLink
+{
+    public class BackLinkModel
+    {
+        public string Controller { get; set; }
+        public string Action { get; set; }
+
+        public BackLinkModel() { }
+        public BackLinkModel(string controller, string action)
+        {
+            this.Controller = controller;
+            this.Action = action;
+        }
+    }
+}
