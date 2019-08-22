e43a1f3c1d859efc757701545f46bd68e7fe3fd5
techtoniq <matt@techtoniq.com>
2019-08-22 11:26:47 +0100

    Helpdesk user viewing FE provider shouldn't see Apprenticeships panel… (#888)
    * Helpdesk user viewing FE provider shouldn't see Apprenticeships panel on the dashboard
    
    * Another commit trying to shunt the pipeline along

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/DashboardCard/DashboardCard.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.DashboardCard
+{
+    public class DashboardCard : ViewComponent
+    {
+        public IViewComponentResult Invoke(DashboardCardModel model)
+        {
+            return View("~/ViewComponents/DashboardCard/Default.cshtml", model);
+        }
+    }
+}
