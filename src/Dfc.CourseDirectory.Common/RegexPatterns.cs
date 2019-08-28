9792f479c9c4fe062acb02ad85fac0d4a64feabb
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2019-08-28 09:21:40 +0100

    Moved File validation to common and added checks for binary file upload

--- src/Dfc.CourseDirectory.Common/RegexPatterns.cs
+++ src/Dfc.CourseDirectory.Common/RegexPatterns.cs
@@ -13,5 +13,7 @@ namespace Dfc.CourseDirectory.Common
             @"^[a-zA-Z0-9/\n/\r/\\u/\¬\!\£\$\%\^\&\*\\é\\è\\ﬁ\(\)_\+\-\=\{\}\[\]\;\:\@\'\#\~\,\<\>\.\?\/\|\`\•\·\●\\’\‘\“\”\—\-\–\‐\‐\…\:/\°\®\\â\\ç\\ñ\\ü\\ø\♦\™\\t/\s\¼\¾\½\" +"\"" + "\\\\]+$";
             
         public const string DisallowHTMLTags = @"^(\<.*?\>).*?(\<\/.*?\>)"; //Not tested.
+
+        public const string Base64Encoded = @"^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$";
     }
 }
