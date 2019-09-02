7a8f3c6d0d171f3ef097439582ae7ab157a5735d
Liam Wright <28269309+LiamWright@users.noreply.github.com>
2019-09-02 09:05:35 +0100

    Alternative name attribute added (#913)

---
+++ src/Dfc.CourseDirectory.Models/Helpers/Attributes/AlternativeNameAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dfc.CourseDirectory.Models.Helpers.Attributes
+{
+    public class AlternativeName
+    {
+        public interface IAttribute<T>
+        {
+            T Value { get; }
+        }
+        public sealed class AlternativeNameAttribute : Attribute, IAttribute<string>
+        {
+            private readonly string value;
+
+            public AlternativeNameAttribute(string value)
+            {
+                this.value = value;
+            }
+
+            public string Value
+            {
+                get { return this.value; }
+            }
+        }
+    }
+}
