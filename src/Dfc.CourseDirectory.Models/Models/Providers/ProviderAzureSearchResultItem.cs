10fdae240d6ade33a90d7526a29e90ab4b75c5b5
Ian Rose <ian.findacourse@gmail.com>
2019-09-05 11:50:28 +0100

    COUR-1794 CourseDirectoryName not being returned from provider searches

--- src/Dfc.CourseDirectory.Models/Models/Providers/ProviderAzureSearchResultItem.cs
+++ src/Dfc.CourseDirectory.Models/Models/Providers/ProviderAzureSearchResultItem.cs
@@ -8,6 +8,7 @@ namespace Dfc.CourseDirectory.Models.Models.Providers
     {
         public Guid id { get; set; }
         public string Name { get; set; }
+        public string CourseDirectoryName { get; set; }
         public string Postcode { get; set; }
         public string Town { get; set; }
         public string Region { get; set; }
