8214a51342f68f5d1e8c419f2b2418a4ff114969
simon bradley <bradley_simon@outlook.com>
2019-09-06 14:53:25 +0100

    Cour 1324 course page2 ui (#928)
    * cour-1313 remove cancel option
    
    * cour-1791 remove placeholder and amended page copy
    
    * change regulated search to keep design styles consistent
    
    * add course page 2 changes
    
    * course page 2 ui updates
    
    * done

--- src/Dfc.CourseDirectory.Web/Controllers/CopyCourse/CopyCourseRunController.cs
+++ src/Dfc.CourseDirectory.Web/Controllers/CopyCourse/CopyCourseRunController.cs
@@ -368,7 +368,7 @@ namespace Dfc.CourseDirectory.Web.Controllers.CopyCourse
 
         [HttpPost]
         [Authorize]
-        public async Task<IActionResult> Index(CopyCourseRunSaveViewModel model)
+        public async Task<IActionResult> Index([FromBody] CopyCourseRunSaveViewModel model)
         {
             int? UKPRN;
 
