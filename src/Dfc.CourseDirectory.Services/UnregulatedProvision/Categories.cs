67edf33d07361dc61c2e8ad067ec22802ae41ca4
bradleysimon <bradley_simon@hotmail.com>
2019-09-10 09:46:51 +0100

    done

--- src/Dfc.CourseDirectory.Services/UnregulatedProvision/Categories.cs
+++ src/Dfc.CourseDirectory.Services/UnregulatedProvision/Categories.cs
@@ -12,20 +12,20 @@ namespace Dfc.CourseDirectory.Services.UnregulatedProvision
             {
                 List<CategoryModel> _categoriesModel = new List<CategoryModel>();
                 _categoriesModel = new List<CategoryModel>();
-                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT A", Category = "A: SFA funded NR provision" });
-                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT B", Category = "B: SFA funded English, maths, ESOL" });
-                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT C", Category = "C: SFA funded NQF Adult Basic Skills" });
+                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT A", Category = "A: Non-regulated (ESFA funded)" });
+                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT B", Category = "B: English, maths, and ESOL (ESFA funded)" });
+                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT C", Category = "C: NQF adult basic skills (ESFA funded)" });
                 _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT D", Category = "D: Innovation code" });
-                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT E", Category = "E: Not Community Learning or SFA funded" });
-                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT F", Category = "F: Community learning" });
-                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT G", Category = "G: Non-SFA funded English, maths, ESOL" });
-                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT H", Category = "H: Units of NQF qualifications" });
-                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT I", Category = "I: Work experience / placement" });
+                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT E", Category = "E: Not Community Learning and not ESFA funded" });
+                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT F", Category = "F: Community Learning" });
+                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT G", Category = "G: English, maths, and ESOL (not ESFA funded)" });
+                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT H", Category = "H: NQF qualification units" });
+                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT I", Category = "I: Work experience and work placement" });
                 _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT J", Category = "J: Supported internship" });
                 _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT K", Category = "K: Programme aim" });
-                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT L", Category = "L: ESF co financed" });
+                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT L", Category = "L: ESFA co-financed" });
                 _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT M", Category = "M: Conversion codes between HNQs" });
-                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT O", Category = "O: Education assessments" });
+                _categoriesModel.Add(new CategoryModel() { Id = "APP H CAT O", Category = "O: Education assessment" });
                 return _categoriesModel;
             }
         }
