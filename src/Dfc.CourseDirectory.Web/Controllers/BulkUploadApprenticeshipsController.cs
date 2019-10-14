3d843ced9ea4e8660798d57d2869f0bb845a7c0a
Liam Wright <mail@liam-wright.co.uk>
2019-10-14 15:23:46 +0100

    Added secondary error handling per error type

--- src/Dfc.CourseDirectory.Web/Controllers/BulkUploadApprenticeshipsController.cs
+++ src/Dfc.CourseDirectory.Web/Controllers/BulkUploadApprenticeshipsController.cs
@@ -177,9 +177,15 @@ namespace Dfc.CourseDirectory.Web.Controllers
                         errors = _apprenticeshipBulkUploadService.ValidateAndUploadCSV(ms,
                             _userHelper.GetUserDetailsFromClaims(this.HttpContext.User.Claims, UKPRN));
                     }
+                    catch(HeaderValidationException he)
+                    {
+                        errors.Add(he.Message.FirstSentence());
+                        vm.errors = errors;
+                        return View(vm);
+                    }
                     catch (Exception e)
                     {
-                        errors.Add(e.Message.FirstSentence());
+                        errors.Add(e.Message);
                         vm.errors = errors;
                         return View(vm);
                     }
