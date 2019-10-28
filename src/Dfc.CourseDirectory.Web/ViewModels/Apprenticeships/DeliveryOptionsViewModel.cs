bdedc2e7b8c6d460f8a760553464fad8dee612d9
Stephen Yates <SYatesSoftware@gmail.com>
2019-10-28 09:25:02 +0000

    changed view models

--- src/Dfc.CourseDirectory.Web/ViewModels/Apprenticeships/DeliveryOptionsViewModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewModels/Apprenticeships/DeliveryOptionsViewModel.cs
@@ -1,24 +1,12 @@
-﻿
-using Dfc.CourseDirectory.Models.Models.Courses;
+﻿using Dfc.CourseDirectory.Models.Models.Apprenticeships;
+using Dfc.CourseDirectory.Web.ViewComponents.Apprenticeships;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Dfc.CourseDirectory.Services.Interfaces.CourseService;
-using Dfc.CourseDirectory.Web.ViewModels.YourCourses;
-using System.ComponentModel.DataAnnotations;
-using Dfc.CourseDirectory.Models.Enums;
-using Dfc.CourseDirectory.Models.Models.Apprenticeships;
-using Dfc.CourseDirectory.Web.ViewComponents.Courses.ChooseRegion;
-using Dfc.CourseDirectory.Web.ViewComponents.Apprenticeships;
 
 namespace Dfc.CourseDirectory.Web.ViewModels.Apprenticeships
 {
     public class DeliveryOptionsViewModel
     {
-
-        public List<SelectListItem> Locations { get; set; }
         public Guid? LocationId { get; set; }
 
         public bool DayRelease { get; set; }
@@ -32,6 +20,8 @@ namespace Dfc.CourseDirectory.Web.ViewModels.Apprenticeships
         public bool? National { get; set; }
 
         public ApprenticeshipMode Mode { get; set; }
+
+        public List<ApprenticeshipLocation> locations { get; set; }
 
 
     }
