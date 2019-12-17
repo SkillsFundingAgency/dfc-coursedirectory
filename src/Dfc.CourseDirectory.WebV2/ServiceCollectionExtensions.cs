2cf027bce509b6f9747ff0ea206414502d9c9dff
James Gunn <james@gunn.io>
2019-12-17 13:14:52 +0000

    Add feature folder conventions
    Also ensure that controllers & views etc. from V2 are exposed in main
    project.

--- src/Dfc.CourseDirectory.WebV2/ServiceCollectionExtensions.cs
+++ src/Dfc.CourseDirectory.WebV2/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace Dfc.CourseDirectory.WebV2
 {
@@ -6,6 +7,20 @@ namespace Dfc.CourseDirectory.WebV2
     {
         public static IServiceCollection AddCourseDirectory(this IServiceCollection services)
         {
+            var thisAssembly = typeof(ServiceCollectionExtensions).Assembly;
+            
+            services
+                .AddMvc(options =>
+                {
+                    options.Conventions.Add(new AddFeaturePropertyModelConvention());
+                })
+                .AddApplicationPart(thisAssembly)
+                .AddRazorOptions(options =>
+                {
+                    options.FileProviders.Add(new ManifestEmbeddedFileProvider(thisAssembly));
+                    options.ViewLocationExpanders.Add(new FeatureViewLocationExpander());
+                });
+
             return services;
         }
     }
