c8f94500e6b781e4ec07aeb9ce07750c5c5c5b89
James Gunn <james@gunn.io>
2019-12-17 14:37:38 +0000

    Add implementation of WebApplicationFactory for tests

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/CourseDirectoryApplicationFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Dfc.CourseDirectory.WebV2.Tests
+{
+    public class CourseDirectoryApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder) => builder.UseContentRoot(".");
+
+        protected override IWebHostBuilder CreateWebHostBuilder() => WebHost
+            .CreateDefaultBuilder()
+            .UseStartup<Startup>();
+    }
+}
---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dfc.CourseDirectory.WebV2.Tests
+{
+    public class Startup : IStartup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddCourseDirectory();
+
+            return services.BuildServiceProvider(validateScopes: true);
+        }
+    }
+}
