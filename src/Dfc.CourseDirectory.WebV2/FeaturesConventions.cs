7cef82315a18706ecfbc27b07803c24817c3e17e
James Gunn <james@gunn.io>
2019-12-17 13:14:52 +0000

    Add feature folder conventions
    Also ensure that controllers & views etc. from V2 are exposed in main
    project.

---
+++ src/Dfc.CourseDirectory.WebV2/FeaturesConventions.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Dfc.CourseDirectory.WebV2
+{
+    public class AddFeaturePropertyModelConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            var assmNamespace = GetType().Namespace;
+            var featuresNamespace = assmNamespace + ".Features";
+
+            var controllerFullName = controller.ControllerType.FullName;
+
+            if (controllerFullName.StartsWith(featuresNamespace))
+            {
+                var featureName = controllerFullName.Substring(featuresNamespace.Length + 1).Split('.')[0];
+
+                controller.Properties.Add("Feature", featureName);
+
+                controller.ControllerName = featureName;
+            }
+        }
+    }
+
+    public class FeatureViewLocationExpander : IViewLocationExpander
+    {
+        private static readonly string[] _featureViewLocations = new[]
+        {
+            "/Features/{3}/{0}.cshtml",
+            "/Features/{3}/View.cshtml"
+        };
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            if (context.Values.ContainsKey("Feature") && !context.ViewName.StartsWith("Components/"))
+            {
+                var featureName = context.Values["Feature"];
+
+                return viewLocations.Concat(_featureViewLocations.Select(l => l.Replace("{3}", featureName)));
+            }
+            else
+            {
+                return viewLocations;
+            }
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            var controllerActionDescriptor = context.ActionContext.ActionDescriptor as ControllerActionDescriptor;
+
+            if (controllerActionDescriptor != null
+                && controllerActionDescriptor.Properties.ContainsKey("Feature"))
+            {
+                var featureName = (string)controllerActionDescriptor.Properties["Feature"];
+
+                context.Values.Add("Feature", featureName);
+            }
+        }
+    }
+}
