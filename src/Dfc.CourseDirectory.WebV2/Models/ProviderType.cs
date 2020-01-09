4dfeef5070c91440175b512d69a1a4da4b6c13ec
James Gunn <james@gunn.io>
2020-01-09 15:14:45 +0000

    Add additional claims and user types for testing

---
+++ src/Dfc.CourseDirectory.WebV2/Models/ProviderType.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfc.CourseDirectory.WebV2.Models
+{
+    public enum ProviderType
+    {
+        FE = 1,
+        Apprenticeships = 2,
+        Both = FE | Apprenticeships
+    }
+}
--- tests/Dfc.CourseDirectory.WebV2.Tests/AuthenticatedUserInfo.cs
+++ tests/Dfc.CourseDirectory.WebV2.Tests/AuthenticatedUserInfo.cs
@@ -1,13 +1,63 @@
-﻿namespace Dfc.CourseDirectory.WebV2.Tests
+﻿using System;
+using Dfc.CourseDirectory.WebV2.Models;
+
+namespace Dfc.CourseDirectory.WebV2.Tests
 {
     public class AuthenticatedUserInfo
     {
-        public bool IsAuthenticated { get; private set; }
+        public const string DefaultEmail = "test.user@place.com";
 
-        public void Reset()
+        public static readonly Guid DefaultUserId = new Guid("9b8adb2a-5a26-44b9-b6a0-52846f7a4555");  // Dummy ID
+
+        public string Email { get; private set; }
+        public bool IsAuthenticated { get; private set; }
+        public Guid UserId { get; private set; }  // GUID to mirror DfE Sign In
+        public string Role { get; private set; }
+
+        public int? UKPRN { get; private set; }
+        public ProviderType? ProviderType { get; private set; }
+
+        public void AsDeveloper(string email, Guid userId)
         {
             IsAuthenticated = true;
+            Email = email;
+            UserId = userId;
+            Role = "Developer";
+            UKPRN = null;
+            ProviderType = null;
         }
+
+        public void AsHelpdesk(string email, Guid userId)
+        {
+            IsAuthenticated = true;
+            Email = email;
+            UserId = userId;
+            Role = "Helpdesk";
+            UKPRN = null;
+            ProviderType = null;
+        }
+
+        public void AsProviderUser(string email, Guid userId, int ukprn, ProviderType providerType)
+        {
+            IsAuthenticated = true;
+            Email = email;
+            UserId = userId;
+            Role = "Provider User";
+            UKPRN = ukprn;
+            ProviderType = providerType;
+        }
+
+        public void AsProviderSuperUser(string email, Guid userId, int ukprn, ProviderType providerType)
+        {
+            IsAuthenticated = true;
+            Email = email;
+            UserId = userId;
+            Role = "Provider Superuser";
+            UKPRN = ukprn;
+            ProviderType = providerType;
+        }
+
+        public void Reset() => AsDeveloper(DefaultEmail, DefaultUserId);
 
         public void SetNotAuthenticated() => IsAuthenticated = false;
     }
