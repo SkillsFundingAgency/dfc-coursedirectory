8818d3386ec42e018a84e92cd2805830e56c15b8
James Gunn <james@gunn.io>
2020-01-09 13:56:45 +0000

    Add base class for tests

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/TestBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using Xunit;
+
+namespace Dfc.CourseDirectory.WebV2.Tests
+{
+    public abstract class TestBase : IClassFixture<CourseDirectoryApplicationFactory>
+    {
+        public TestBase(CourseDirectoryApplicationFactory factory)
+        {
+            Factory = factory;
+        }
+
+        protected CourseDirectoryApplicationFactory Factory { get; }
+
+        protected HttpClient CreateClient => Factory.CreateClient();
+    }
+}
