4463c3f75a765091b0706b7b02f6dcf64e837842
James Gunn <james@gunn.io>
2020-01-09 14:03:42 +0000

    Add a mechanism for testing user authentication

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/AuthenticatedUserInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Dfc.CourseDirectory.WebV2.Tests
+{
+    public class AuthenticatedUserInfo
+    {
+        public bool IsAuthenticated { get; private set; }
+
+        public void Reset()
+        {
+            IsAuthenticated = true;
+        }
+
+        public void SetNotAuthenticated() => IsAuthenticated = false;
+    }
+}
---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/TestAuthentication.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Dfc.CourseDirectory.WebV2.Tests
+{
+    public class TestAuthenticationOptions : AuthenticationSchemeOptions
+    {
+    }
+
+    public class TestAuthenticationHandler : AuthenticationHandler<TestAuthenticationOptions>
+    {
+        private readonly AuthenticatedUserInfo _authenticatedUserInfo;
+
+        public TestAuthenticationHandler(
+            AuthenticatedUserInfo authenticatedUserInfo,
+            IOptionsMonitor<TestAuthenticationOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+            _authenticatedUserInfo = authenticatedUserInfo;
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            AuthenticateResult result;
+
+            if (_authenticatedUserInfo.IsAuthenticated)
+            {
+                var claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.NameIdentifier, "Test User"),
+                    new Claim("name", "Test User")
+                };
+
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+
+                result = AuthenticateResult.Success(new AuthenticationTicket(principal, Scheme.Name));
+            }
+            else
+            {
+                result = AuthenticateResult.NoResult();
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
