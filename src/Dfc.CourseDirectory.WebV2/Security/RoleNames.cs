7902998353c05b67668fbfb953168ee611c7d168
James Gunn <james@gunn.io>
2020-01-10 11:09:16 +0000

    Centralise role names

---
+++ src/Dfc.CourseDirectory.WebV2/Security/RoleNames.cs
@@ -0,0 +1,10 @@
+﻿namespace Dfc.CourseDirectory.WebV2.Security
+{
+    public static class RoleNames
+    {
+        public const string Developer = "Developer";
+        public const string Helpdesk = "Helpdesk";
+        public const string ProviderUser = "Provider User";
+        public const string ProviderSuperUser = "Provider Superuser";
+    }
+}
