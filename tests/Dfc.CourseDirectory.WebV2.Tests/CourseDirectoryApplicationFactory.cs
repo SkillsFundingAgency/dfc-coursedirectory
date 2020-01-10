6396ba6361fc74a9f4e8ad974ff17c1265b624be
James Gunn <james@gunn.io>
2020-01-10 11:33:17 +0000

    Add filter for checking that an apprenticeship exists

--- tests/Dfc.CourseDirectory.WebV2.Tests/CourseDirectoryApplicationFactory.cs
+++ tests/Dfc.CourseDirectory.WebV2.Tests/CourseDirectoryApplicationFactory.cs
@@ -1,16 +1,29 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 
 namespace Dfc.CourseDirectory.WebV2.Tests
 {
     public class CourseDirectoryApplicationFactory : WebApplicationFactory<Startup>
     {
+        public Mock<IProviderOwnershipCache> ProviderOwnershipCache { get; } = new Mock<IProviderOwnershipCache>();
+
+        public void ResetMocks()
+        {
+            ProviderOwnershipCache.Reset();
+        }
+
         protected override void ConfigureWebHost(IWebHostBuilder builder) => builder.UseContentRoot(".");
 
         protected override IWebHostBuilder CreateWebHostBuilder() => WebHost
             .CreateDefaultBuilder()
             .UseEnvironment("Testing")
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton(ProviderOwnershipCache.Object);
+            })
             .UseStartup<Startup>();
     }
 }
