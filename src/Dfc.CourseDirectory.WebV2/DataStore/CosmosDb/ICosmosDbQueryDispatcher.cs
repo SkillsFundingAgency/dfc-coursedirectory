0e369310a9c5cb3fe6e71d68224e7ec50fbe404d
James Gunn <james@gunn.io>
2020-01-16 09:31:05 +0000

    Add abstractions for querying CosmosDb

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/CosmosDb/ICosmosDbQueryDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.CosmosDb
+{
+    public interface ICosmosDbQueryDispatcher
+    {
+        Task<T> ExecuteQuery<T>(ICosmosDbQuery<T> query);
+    }
+}
