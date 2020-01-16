1f4aa43877f19af6be863d392f3d02434b315286
James Gunn <james@gunn.io>
2020-01-16 10:51:20 +0000

    Implement IProviderOwnershipCache

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/CosmosDb/QueryHandlers/GetProviderUKPRNForApprenticeshipHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Queries;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.QueryHandlers
+{
+    public class GetProviderUKPRNForApprenticeshipHandler : ICosmosDbQueryHandler<GetProviderUKPRNForApprenticeship, int?>
+    {
+        public async Task<int?> Execute(
+            DocumentClient client,
+            Configuration configuration,
+            GetProviderUKPRNForApprenticeship request)
+        {
+            var collectionUri = UriFactory.CreateDocumentCollectionUri(
+                configuration.DatabaseId,
+                configuration.ApprenticeshipCollectionName);
+
+            var query = new SqlQuerySpec(
+                "SELECT c.ProviderUKPRN FROM c WHERE c.id = @apprenticeshipId",
+                new SqlParameterCollection()
+                {
+                    new SqlParameter("@apprenticeshipId", request.ApprenticeshipId)
+                });
+
+            var response = await client.CreateDocumentQuery(collectionUri, query, new FeedOptions() { EnableCrossPartitionQuery = true })
+                .AsDocumentQuery()
+                .ExecuteNextAsync();
+
+            return response.Count > 0 ? (int?)response.Single().ProviderUKPRN : null;
+        }
+    }
+}
