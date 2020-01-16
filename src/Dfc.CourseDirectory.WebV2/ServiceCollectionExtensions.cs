a7c7ce48ec78317c1a509cb4bb0afcbe64d1837f
James Gunn <james@gunn.io>
2020-01-16 09:31:05 +0000

    Add abstractions for querying CosmosDb

--- src/Dfc.CourseDirectory.WebV2/ServiceCollectionExtensions.cs
+++ src/Dfc.CourseDirectory.WebV2/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace Dfc.CourseDirectory.WebV2
 {
@@ -10,6 +12,17 @@ namespace Dfc.CourseDirectory.WebV2
             IHostingEnvironment environment)
         {
             var thisAssembly = typeof(ServiceCollectionExtensions).Assembly;
+
+            if (!environment.IsTesting())
+            {
+                services.AddTransient<ICosmosDbQueryDispatcher, CosmosDbQueryDispatcher>();
+
+                services.Scan(scan => scan
+                    .FromAssembliesOf(typeof(ICosmosDbQuery<>))
+                    .AddClasses(classes => classes.AssignableTo(typeof(ICosmosDbQueryHandler<,>)))
+                        .AsImplementedInterfaces()
+                        .WithTransientLifetime());
+            }
             
             services
                 .AddMvc(options =>
