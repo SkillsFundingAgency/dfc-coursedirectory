50d2e492ca89d038d8eadd902e4383ef3da47477
James Gunn <james@gunn.io>
2020-02-18 08:34:39 +0000

    Add minimal Provider model type

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/CosmosDb/Models/Provider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Models
+{
+    public class Provider
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        public string UnitedKingdomProviderReferenceNumber { get; set; }
+        [JsonExtensionData]
+        public IDictionary<string, JToken> AdditionalData { get; set; }
+    }
+}
