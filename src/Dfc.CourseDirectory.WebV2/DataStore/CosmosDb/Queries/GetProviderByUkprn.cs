0371542cdb099a4635fa3da16f2ac9bc2e52c14e
James Gunn <james@gunn.io>
2020-02-18 10:36:41 +0000

    Implement ProviderInfoCache

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/CosmosDb/Queries/GetProviderByUkprn.cs
@@ -0,0 +1,9 @@
+﻿using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Models;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Queries
+{
+    public class GetProviderByUkprn : ICosmosDbQuery<Provider>
+    {
+        public int Ukprn { get; set; }
+    }
+}
--- src/Dfc.CourseDirectory.WebV2/ProviderInfo.cs
+++ src/Dfc.CourseDirectory.WebV2/ProviderInfo.cs
@@ -5,6 +5,6 @@ namespace Dfc.CourseDirectory.WebV2
     public class ProviderInfo
     {
         public Guid ProviderId { get; set; }
-        public int UKPRN { get; set; }
+        public int Ukprn { get; set; }
     }
 }
