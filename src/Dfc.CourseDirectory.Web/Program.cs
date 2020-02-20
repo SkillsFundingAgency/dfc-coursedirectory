702133b41a384274cc656d3dfd9ae1ed5492634d
James Gunn <james@gunn.io>
2020-02-20 11:56:26 +0000

    Use UserSecrets for sensitive config values

--- src/Dfc.CourseDirectory.Web/Program.cs
+++ src/Dfc.CourseDirectory.Web/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Dfc.CourseDirectory.Web
@@ -11,17 +10,15 @@ namespace Dfc.CourseDirectory.Web
         {
             CreateWebHostBuilder(args).Build().Run();
         }
+
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .ConfigureLogging((context,
-                builder) =>
+                .ConfigureLogging((context, builder) =>
                 {
                     builder.AddConfiguration(context.Configuration.GetSection("Logging"));
-                    var ikey = context.Configuration.GetSection("APPINSIGHTS_INSTRUMENTATIONKEY");
-                    builder.AddApplicationInsights(ikey.Value);
-
-                }
-        );
+                    var appInsightsKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                    builder.AddApplicationInsights(appInsightsKey);
+                });
     }
 }
