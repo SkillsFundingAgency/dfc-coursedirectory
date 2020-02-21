255f35350378e921df7d7c61fd7673ef6d2d735e
James Gunn <james@gunn.io>
2020-02-21 19:42:50 +0000

    Add a method for returning an error view from a ModelWithErrors<T>

---
+++ src/Dfc.CourseDirectory.WebV2/ControllerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Dfc.CourseDirectory.WebV2.Validation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2
+{
+    public static class ControllerExtensions
+    {
+        public static ViewResult ViewFromErrors<T>(this Controller controller, ModelWithErrors<T> failed)
+        {
+            failed.ValidationResult.AddToModelState(controller.ModelState, null);
+
+            var result = controller.View(failed.Model);
+            result.StatusCode = 400;
+            return result;
+        }
+    }
+}
