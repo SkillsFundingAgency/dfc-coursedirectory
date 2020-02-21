fb22705225d7737371b5d2591f2fb2be7f4d660d
James Gunn <james@gunn.io>
2020-02-21 19:01:46 +0000

    Add a mechanism for retrieving info about the current user

---
+++ src/Dfc.CourseDirectory.WebV2/Security/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Dfc.CourseDirectory.WebV2.Security
+{
+    public class UserInfo
+    {
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+        public bool IsDeveloper => Role == RoleNames.Developer;
+        public bool IsHelpdesk => Role == RoleNames.Helpdesk;
+        public bool IsProvider => Role == RoleNames.ProviderSuperUser || Role == RoleNames.ProviderUser;
+    }
+}
