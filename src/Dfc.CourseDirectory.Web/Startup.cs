c3baf18cc3c00de6d06c618d8dfb3e34f1c4299e
James Gunn <james@gunn.io>
2020-02-24 13:50:23 +0000

    Add DocumentClient to Services

--- src/Dfc.CourseDirectory.Web/Startup.cs
+++ src/Dfc.CourseDirectory.Web/Startup.cs
@@ -42,6 +42,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -158,6 +159,13 @@ namespace Dfc.CourseDirectory.Web
             services.AddScoped<IApprenticeshipProvisionHelper, ApprenticeshipProvisionHelper>();
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            {
+                var endpoint = new Uri(Configuration["CosmosDbSettings:EndpointUri"]);
+                var key = Configuration["CosmosDbSettings:PrimaryKey"];
+                var documentClient = new DocumentClient(endpoint, key);
+                services.AddSingleton(documentClient);
+            }
 
             services.AddCourseDirectory(_env, Configuration);
 
