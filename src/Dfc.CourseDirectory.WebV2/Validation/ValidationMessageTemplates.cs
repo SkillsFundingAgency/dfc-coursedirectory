d0c981e4216ec03ba34de0d0c934e65da65c3c2c
James Gunn <james@gunn.io>
2020-02-24 14:43:51 +0000

    Begin to define validation message templates

---
+++ src/Dfc.CourseDirectory.WebV2/Validation/ValidationMessageTemplates.cs
@@ -0,0 +1,8 @@
+﻿namespace Dfc.CourseDirectory.WebV2.Validation
+{
+    public static class ValidationMessageTemplates
+    {
+        public static string MessageForMaxStringLength(string label, int maxLength) =>
+            $"{label} must be {maxLength} characters or less";
+    }
+}
