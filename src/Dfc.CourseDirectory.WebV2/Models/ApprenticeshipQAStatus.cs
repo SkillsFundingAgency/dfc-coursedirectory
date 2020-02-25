18062b0861acb269f9b4783e18d4fa89daa99857
James Gunn <james@gunn.io>
2020-02-25 11:47:17 +0000

    Add ApprenticeshipQAStatus enum & queries for getting & setting it

---
+++ src/Dfc.CourseDirectory.WebV2/Models/ApprenticeshipQAStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Dfc.CourseDirectory.WebV2.Models
+{
+    public enum ApprenticeshipQAStatus
+    {
+        NotStarted = 0,
+        Submitted = 1,
+        InProgress = 2,
+        Failed = 3,
+        Passed = 4
+    }
+}
