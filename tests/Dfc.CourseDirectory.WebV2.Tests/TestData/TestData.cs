e7fcaf2fd1cd21972516af6480c4ca6decdb46a9
James Gunn <james@gunn.io>
2020-02-25 11:46:33 +0000

    Add methods on TestData for executing SQL queries

--- tests/Dfc.CourseDirectory.WebV2.Tests/TestData/TestData.cs
+++ tests/Dfc.CourseDirectory.WebV2.Tests/TestData/TestData.cs
@@ -1,14 +1,47 @@
-﻿using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb;
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Dfc.CourseDirectory.WebV2.Tests
 {
     public partial class TestData
     {
         private readonly ICosmosDbQueryDispatcher _cosmosDbQueryDispatcher;
+        private readonly IServiceProvider _serviceProvider;
 
-        public TestData(ICosmosDbQueryDispatcher cosmosDbQueryDispatcher)
+        public TestData(
+            ICosmosDbQueryDispatcher cosmosDbQueryDispatcher,
+            IServiceProvider serviceProvider)
         {
             _cosmosDbQueryDispatcher = cosmosDbQueryDispatcher;
+            _serviceProvider = serviceProvider;
+        }
+
+        protected Task WithSqlQueryDispatcher(Func<ISqlQueryDispatcher, Task> action) =>
+            WithSqlQueryDispatcher(async dispatcher =>
+            {
+                await action(dispatcher);
+                return 0;
+            });
+
+        protected async Task<TResult> WithSqlQueryDispatcher<TResult>(
+            Func<ISqlQueryDispatcher, Task<TResult>> action)
+        {
+            var serviceScopeFactory = _serviceProvider.GetRequiredService<IServiceScopeFactory>();
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var transaction = scope.ServiceProvider.GetRequiredService<SqlTransaction>();
+                var queryDispatcher = scope.ServiceProvider.GetRequiredService<ISqlQueryDispatcher>();
+
+                var result = await action(queryDispatcher);
+
+                transaction.Commit();
+
+                return result;
+            }
         }
     }
 }
