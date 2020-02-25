24acc15266def55d175d123f5785596c8b7814ff
James Gunn <james@gunn.io>
2020-02-25 10:34:08 +0000

    Add As() extension method on IElement for simpler casting in tests

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/ElementExtensions.cs
@@ -0,0 +1,13 @@
+﻿using AngleSharp.Dom;
+
+namespace Dfc.CourseDirectory.WebV2.Tests
+{
+    public static class ElementExtensions
+    {
+        public static T As<T>(this IElement element)
+            where T : class, IElement
+        {
+            return element as T;
+        }
+    }
+}
