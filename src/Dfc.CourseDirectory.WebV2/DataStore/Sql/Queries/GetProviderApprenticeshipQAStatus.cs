1d9a4e6c51acc19fb4e5336210188e74ec375bfa
James Gunn <james@gunn.io>
2020-02-25 11:47:17 +0000

    Add ApprenticeshipQAStatus enum & queries for getting & setting it

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/Sql/Queries/GetProviderApprenticeshipQAStatus.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Dfc.CourseDirectory.WebV2.Models;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries
+{
+    public class GetProviderApprenticeshipQAStatus : ISqlQuery<ApprenticeshipQAStatus?>
+    {
+        public Guid ProviderId { get; set; }
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/Sql/QueryHandlers/GetProviderApprenticeshipQAStatusHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries;
+using Dfc.CourseDirectory.WebV2.Models;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.Sql.QueryHandlers
+{
+    public class GetProviderApprenticeshipQAStatusHandler :
+        ISqlQueryHandler<GetProviderApprenticeshipQAStatus, ApprenticeshipQAStatus?>
+    {
+        public Task<ApprenticeshipQAStatus?> Execute(
+            SqlTransaction transaction,
+            GetProviderApprenticeshipQAStatus query)
+        {
+            var sql = @"
+SELECT ApprenticeshipQAStatus FROM Pttcd.Providers WITH (HOLDLOCK)
+WHERE ProviderId = @ProviderId";
+
+            var paramz = new { query.ProviderId };
+
+            return transaction.Connection.QuerySingleOrDefaultAsync<ApprenticeshipQAStatus?>(sql, paramz, transaction);
+        }
+    }
+}
