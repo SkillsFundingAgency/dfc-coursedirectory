83db0c68b7f84101565da7b0a55f8685d7c69884
James Gunn <james@gunn.io>
2020-02-27 16:05:07 +0000

    Add a filter attribute that checks a given feature is configured before executing

---
+++ src/Dfc.CourseDirectory.WebV2/Filters/RequireFeatureFlagFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dfc.CourseDirectory.WebV2.Filters
+{
+    public class RequireFeatureFlagAttribute : ActionFilterAttribute
+    {
+        public RequireFeatureFlagAttribute(string feature)
+        {
+            Feature = feature;
+        }
+
+        public string Feature { get; }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var featureFlagProvider = context.HttpContext.RequestServices.GetRequiredService<IFeatureFlagProvider>();
+            if (!featureFlagProvider.HaveFeature(Feature))
+            {
+                context.Result = new NotFoundResult();
+            }
+        }
+    }
+}
