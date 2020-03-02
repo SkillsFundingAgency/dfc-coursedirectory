71317b0acb6fc0526cb2a4edd72afc19aeacce2d
James Gunn <james@gunn.io>
2020-03-02 13:32:43 +0000

    Track user sign ins

---
+++ src/Dfc.CourseDirectory.WebV2/Security/SignInTracker.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries;
+
+namespace Dfc.CourseDirectory.WebV2.Security
+{
+    public class SignInTracker
+    {
+        private readonly ISqlQueryDispatcher _sqlQueryDispatcher;
+        private readonly IClock _clock;
+
+        public SignInTracker(
+            ISqlQueryDispatcher sqlQueryDispatcher,
+            IClock clock)
+        {
+            _sqlQueryDispatcher = sqlQueryDispatcher;
+            _clock = clock;
+        }
+
+        public Task RecordSignIn(ClaimsPrincipal principal)
+        {
+            var currentUser = ClaimsPrincipalCurrentUserProvider.MapUserInfoFromPrincipal(principal);
+
+            return _sqlQueryDispatcher.ExecuteQuery(new AddUserSignIn()
+            {
+                User = currentUser,
+                SignedInUtc = _clock.UtcNow
+            });
+        }
+    }
+}
