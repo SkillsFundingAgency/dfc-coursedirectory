a199a54c30299682f253ca65d6cf787840d3527c
James Gunn <james@gunn.io>
2020-03-03 15:48:29 +0000

    Add TestData method for creating a user

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/Sql/Queries/CreateUser.cs
@@ -0,0 +1,12 @@
+﻿using OneOf.Types;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries
+{
+    public class CreateUser : ISqlQuery<None>
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
