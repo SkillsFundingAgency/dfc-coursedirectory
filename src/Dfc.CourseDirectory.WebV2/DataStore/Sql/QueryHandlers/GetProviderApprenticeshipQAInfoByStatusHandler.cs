446c537e27a34747ff807ed35bb1898079796e12
James Gunn <james@gunn.io>
2020-03-03 16:10:00 +0000

    QA Providers page

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/Sql/QueryHandlers/GetProviderApprenticeshipQAInfoByStatusHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries;
+using Dfc.CourseDirectory.WebV2.Models;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.Sql.QueryHandlers
+{
+    public class GetProviderApprenticeshipQAInfoByStatusHandler
+        : ISqlQueryHandler<GetProviderApprenticeshipQAInfoByStatus, IReadOnlyCollection<GetProviderApprenticeshipQAInfoByStatusResult>>
+    {
+        public async Task<IReadOnlyCollection<GetProviderApprenticeshipQAInfoByStatusResult>> Execute(
+            SqlTransaction transaction,
+            GetProviderApprenticeshipQAInfoByStatus query)
+        {
+            var sql = @"
+SELECT
+    s.ProviderId,
+    s.ApprenticeshipQAStatus,
+    s.SubmittedOn,
+    u.UserId,
+    u.Email,
+    u.FirstName,
+    u.LastName
+FROM Pttcd.ApprenticeshipQASubmissions s
+LEFT JOIN Pttcd.Users u ON s.LastAssessedBy = u.UserId
+WHERE s.ApprenticeshipQAStatus IN @Statuses
+ORDER BY s.SubmittedOn";
+
+            var paramz = new { query.Statuses };
+
+            return (await transaction.Connection.QueryAsync<QASubmissionResult, UserInfo, GetProviderApprenticeshipQAInfoByStatusResult>(
+                sql,
+                (r, u) => new GetProviderApprenticeshipQAInfoByStatusResult()
+                {
+                    ApprenticeshipQAStatus = r.ApprenticeshipQAStatus,
+                    LastAssessedBy = u,
+                    ProviderId = r.ProviderId,
+                    SubmittedOn = r.SubmittedOn
+                },
+                paramz,
+                transaction,
+                splitOn: "UserId")).AsList();
+        }
+
+        private class QASubmissionResult
+        {
+            public Guid ProviderId { get; set; }
+            public ApprenticeshipQAStatus ApprenticeshipQAStatus { get; set; }
+            public DateTime? SubmittedOn { get; set; }
+        }
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/Features/ApprenticeshipQA/ApprenticeshipQAController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.Filters;
+using Dfc.CourseDirectory.WebV2.Security;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.Features.ApprenticeshipQA
+{
+    [Route("apprenticeship-qa")]
+    [Authorize(Policy = AuthorizationPolicyNames.ApprenticeshipQA)]
+    [RequireFeatureFlag(FeatureFlags.ApprenticeshipQA)]
+    public class ApprenticeshipQAController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ApprenticeshipQAController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> ListProviders()
+        {
+            var result = await _mediator.Send(new ListProviders.Query());
+            return View(result);
+        }
+
+        [HttpGet("{providerId}")]
+        public IActionResult ProviderDetail(Guid providerId) =>
+            throw new System.NotImplementedException();
+    }
+}
