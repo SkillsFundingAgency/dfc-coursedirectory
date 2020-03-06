a2e212f01e25991245dacf068ded71ba6c14e88d
James Gunn <james@gunn.io>
2020-03-06 11:53:31 +0000

    Add helper method for ensuring URL is valid for anchor tag

---
+++ src/Dfc.CourseDirectory.WebV2/UrlUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dfc.CourseDirectory.WebV2
+{
+    public static class UrlUtil
+    {
+        public static string EnsureHttpPrefixed(string website)
+        {
+            if (string.IsNullOrEmpty(website))
+            {
+                throw new ArgumentException("Value cannot be empty.", nameof(website));
+            }
+
+            if (website.StartsWith("http://") || website.StartsWith("https://"))
+            {
+                return website;
+            }
+            else
+            {
+                return "http://" + website;
+            }
+        }
+    }
+}
