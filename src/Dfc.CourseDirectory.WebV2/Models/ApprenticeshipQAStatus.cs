1b3f77b7b36c883ed1b1bca74da1a1175801f89f
James Gunn <james@gunn.io>
2020-03-08 18:40:15 +0000

    Add extension method for retrieving display name for a QA status

--- src/Dfc.CourseDirectory.WebV2/Models/ApprenticeshipQAStatus.cs
+++ src/Dfc.CourseDirectory.WebV2/Models/ApprenticeshipQAStatus.cs
@@ -1,4 +1,6 @@
-﻿namespace Dfc.CourseDirectory.WebV2.Models
+﻿using System;
+
+namespace Dfc.CourseDirectory.WebV2.Models
 {
     public enum ApprenticeshipQAStatus
     {
@@ -8,5 +10,29 @@
         Failed = 3,
         Passed = 4,
         UnableToComplete = 5
+    }
+
+    public static class ApprenticeshipQAStatusExtensions
+    {
+        public static string ToDisplayName(this ApprenticeshipQAStatus status)
+        {
+            switch (status)
+            {
+                case ApprenticeshipQAStatus.NotStarted:
+                    return "Not started";
+                case ApprenticeshipQAStatus.Submitted:
+                    return "Ready for QA";
+                case ApprenticeshipQAStatus.InProgress:
+                    return "In progress";
+                case ApprenticeshipQAStatus.Failed:
+                    return "Failed";
+                case ApprenticeshipQAStatus.Passed:
+                    return "Passed";
+                case ApprenticeshipQAStatus.UnableToComplete:
+                    return "Unable to complete";
+                default:
+                    throw new NotImplementedException($"Unknown value: '{status}'.");
+            }
+        }
     }
 }
