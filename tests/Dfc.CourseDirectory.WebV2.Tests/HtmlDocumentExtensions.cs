d4758832a547f8cb51245527886c1a3e0861e7ac
James Gunn <james@gunn.io>
2020-03-09 11:01:51 +0000

    Add helper method for asserting an error was rendered

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/HtmlDocumentExtensions.cs
@@ -0,0 +1,24 @@
+﻿using AngleSharp.Html.Dom;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Dfc.CourseDirectory.WebV2.Tests
+{
+    public static class HtmlDocumentExtensions
+    {
+        public static void AssertHasError(this IHtmlDocument doc, string fieldName, string expectedMessage)
+        {
+            var errorElementId = $"{fieldName}-error";
+            var errorElement = doc.GetElementById(errorElementId);
+
+            if (errorElement == null)
+            {
+                throw new XunitException($"No error found for field '{fieldName}'.");
+            }
+
+            var vht = errorElement.GetElementsByTagName("span")[0];
+            var errorMessage = errorElement.InnerHtml.Substring(vht.OuterHtml.Length);
+            Assert.Equal(expectedMessage, errorMessage);
+        }
+    }
+}
