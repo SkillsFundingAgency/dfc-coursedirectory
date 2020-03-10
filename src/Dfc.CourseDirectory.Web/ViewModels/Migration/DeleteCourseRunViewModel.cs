1fe848b6c30cb7154075a2bf961357817e83cebf
Kevin Joy <kevin.joy@esynergy-solutions.co.uk>
2020-03-10 11:34:00 +0000

    changes

---
+++ src/Dfc.CourseDirectory.Web/ViewModels/Migration/DeleteCourseRunViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.Web.ViewModels.Migration
+{
+    public class DeleteCourseRunViewModel
+    {
+        public string CourseName { get; set; }
+    }
+}
