1edde67b1d893ba8bbc1dc9e1167176e5def685b
James Gunn <james@gunn.io>
2020-03-11 09:40:46 +0000

    Check data is valid for POSTs too

--- tests/Dfc.CourseDirectory.WebV2.Tests/Features/ApprenticeshipQA/ApprenticeshipAssessmentTests.cs
+++ tests/Dfc.CourseDirectory.WebV2.Tests/Features/ApprenticeshipQA/ApprenticeshipAssessmentTests.cs
@@ -219,6 +219,141 @@ namespace Dfc.CourseDirectory.WebV2.Tests.Features.ApprenticeshipQA
             Assert.Equal("Bad style, yo", doc.GetElementById("StyleComments").TextContent);
         }
 
+        [Theory]
+        [InlineData(TestUserType.ProviderSuperUser)]
+        [InlineData(TestUserType.ProviderUser)]
+        public async Task Post_ProviderUserCannotAccess(TestUserType userType)
+        {
+            // Arrange
+            var ukprn = 12345;
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: ukprn,
+                providerName: "Provider 1",
+                apprenticeshipQAStatus: ApprenticeshipQAStatus.Submitted);
+
+            var providerUserId = $"{ukprn}-user";
+            await TestData.CreateUser(providerUserId, "somebody@provider1.com", "Provider 1", "Person");
+
+            var apprenticeshipId = await TestData.CreateApprenticeship(ukprn);
+
+            await TestData.CreateApprenticeshipQASubmission(
+                providerId,
+                submittedOn: Clock.UtcNow,
+                submittedByUserId: providerUserId,
+                providerMarketingInformation: "The overview",
+                apprenticeshipIds: new[] { apprenticeshipId });
+
+            await User.AsTestUser(userType, providerId);
+
+            var requestContent = new FormUrlEncodedContentBuilder()
+                 .Add("CompliancePassed", true)
+                 .Add("StylePassed", true)
+                 .ToContent();
+
+            // Act
+            var response = await HttpClient.PostAsync(
+                $"apprenticeship-qa/apprenticeship-assessments/{apprenticeshipId}",
+                requestContent);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_ApprenticeshipDoesNotExistReturnsBadRequest()
+        {
+            // Arrange
+            await User.AsHelpdesk();
+
+            var apprenticeshipId = Guid.NewGuid();
+
+            var requestContent = new FormUrlEncodedContentBuilder()
+                 .Add("CompliancePassed", true)
+                 .Add("StylePassed", true)
+                 .ToContent();
+
+            // Act
+            var response = await HttpClient.PostAsync(
+                $"apprenticeship-qa/apprenticeship-assessments/{apprenticeshipId}",
+                requestContent);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_NoSubmissionReturnsBadRequest()
+        {
+            // Arrange
+            var ukprn = 12345;
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: ukprn,
+                providerName: "Provider 1",
+                apprenticeshipQAStatus: ApprenticeshipQAStatus.Submitted);
+
+            var apprenticeshipId = await TestData.CreateApprenticeship(ukprn);
+
+            await User.AsHelpdesk();
+
+            var requestContent = new FormUrlEncodedContentBuilder()
+                 .Add("CompliancePassed", true)
+                 .Add("StylePassed", true)
+                 .ToContent();
+
+            // Act
+            var response = await HttpClient.PostAsync(
+                $"apprenticeship-qa/apprenticeship-assessments/{apprenticeshipId}",
+                requestContent);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(ApprenticeshipQAStatus.Failed)]
+        [InlineData(ApprenticeshipQAStatus.NotStarted)]
+        [InlineData(ApprenticeshipQAStatus.Passed)]
+        [InlineData(ApprenticeshipQAStatus.UnableToComplete)]
+        public async Task Post_SubmissionAtInvalidStatusReturnsBadRequest(ApprenticeshipQAStatus qaStatus)
+        {
+            // Arrange
+            var ukprn = 12345;
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: ukprn,
+                providerName: "Provider 1",
+                apprenticeshipQAStatus: qaStatus);
+
+            var providerUserId = $"{ukprn}-user";
+            await TestData.CreateUser(providerUserId, "somebody@provider1.com", "Provider 1", "Person");
+
+            var apprenticeshipId = await TestData.CreateApprenticeship(ukprn);
+
+            await TestData.CreateApprenticeshipQASubmission(
+                providerId,
+                submittedOn: Clock.UtcNow,
+                submittedByUserId: providerUserId,
+                providerMarketingInformation: "The overview",
+                apprenticeshipIds: new[] { apprenticeshipId });
+
+            await User.AsHelpdesk();
+            
+            var requestContent = new FormUrlEncodedContentBuilder()
+                 .Add("CompliancePassed", true)
+                 .Add("StylePassed", true)
+                 .ToContent();
+
+            // Act
+            var response = await HttpClient.PostAsync(
+                $"apprenticeship-qa/apprenticeship-assessments/{apprenticeshipId}",
+                requestContent);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
         [Fact]
         public async Task Post_MissingCompliancePassedRendersErrorMessage()
         {
