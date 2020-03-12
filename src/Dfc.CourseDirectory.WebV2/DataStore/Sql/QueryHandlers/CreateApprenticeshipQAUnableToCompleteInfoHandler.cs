1dca21f06e338e28385d87795211af9e750bc03f
James Gunn <james@gunn.io>
2020-03-12 15:50:27 +0000

    QA change status

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/Sql/QueryHandlers/CreateApprenticeshipQAUnableToCompleteInfoHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.Sql.QueryHandlers
+{
+    public class CreateApprenticeshipQAUnableToCompleteInfoHandler
+        : ISqlQueryHandler<CreateApprenticeshipQAUnableToCompleteInfo, int>
+    {
+        public Task<int> Execute(SqlTransaction transaction, CreateApprenticeshipQAUnableToCompleteInfo query)
+        {
+            var sql = @"
+INSERT INTO Pttcd.ApprenticeshipQAUnableToCompleteInfo
+(ProviderId, UnableToCompleteReasons, Comments, AddedOn, AddedByUserId)
+VALUES (@ProviderId, @UnableToCompleteReasons, @Comments, @AddedOn, @AddedByUserId)
+
+SELECT SCOPE_IDENTITY() ApprenticeshipQAUnableToCompleteId";
+
+            return transaction.Connection.QuerySingleOrDefaultAsync<int>(sql, query, transaction);
+        }
+    }
+}
