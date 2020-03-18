83c0d1e8de8066f2e35369fca1e5ef1efc9c1503
James Gunn <james@gunn.io>
2020-03-18 18:34:08 +0000

    Port nav into v2 layout

--- tests/Dfc.CourseDirectory.WebV2.Tests/TestController.cs
+++ tests/Dfc.CourseDirectory.WebV2.Tests/TestController.cs
@@ -5,6 +5,6 @@ namespace Dfc.CourseDirectory.WebV2.Tests
     public class TestController : Controller
     {
         [Route("tests/empty")]
-        public IActionResult Empty() => View("Empty");
+        public IActionResult Empty() => View("~/TestViews/Empty.cshtml");
     }
 }
