0e6b057722cc5bc801846c573fa869946a4bc734
James Gunn <james@gunn.io>
2020-03-18 17:19:06 +0000

    Add ProviderName to ProviderInfo

--- src/Dfc.CourseDirectory.WebV2/ProviderInfo.cs
+++ src/Dfc.CourseDirectory.WebV2/ProviderInfo.cs
@@ -8,5 +8,6 @@ namespace Dfc.CourseDirectory.WebV2
         public Guid ProviderId { get; set; }
         public int Ukprn { get; set; }
         public ProviderType ProviderType { get; set; }
+        public string ProviderName { get; set; }
     }
 }
