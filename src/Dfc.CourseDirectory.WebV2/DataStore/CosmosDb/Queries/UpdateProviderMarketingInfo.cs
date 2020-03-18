ec204392284a998e992835668bb723172be20a62
James Gunn <james@gunn.io>
2020-03-18 11:16:40 +0000

    Provider QA - marketing information

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/CosmosDb/Queries/UpdateProviderMarketingInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Dfc.CourseDirectory.WebV2.Models;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Queries
+{
+    public class UpdateProviderMarketingInfo : ICosmosDbQuery<Success>
+    {
+        public Guid ProviderId { get; set; }
+        public string MarketingInformation { get; set; }
+        public UserInfo UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/Validation/ProviderValidation/AbstractValidatorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Dfc.CourseDirectory.WebV2.Validation.ProviderValidation
+{
+    public static class AbstractValidatorExtensions
+    {
+        public static void MarketingInformation<T>(this IRuleBuilderInitial<T, string> field)
+        {
+            field
+                .NotEmpty()
+                .ValidHtml(maxLength: Constants.MarketingInformationStrippedMaxLength)
+                .WithMessageForAllRules("PLACEHOLDER");
+        }
+    }
+}
