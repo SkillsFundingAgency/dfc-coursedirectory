c3ccc26ca3469fa78285bb4fbb1bdeee8bab0e29
James Gunn <james@gunn.io>
2020-03-18 08:16:30 +0000

    Remove constraint on exception handler - MediatR explodes otherwise

--- src/Dfc.CourseDirectory.WebV2/Behaviors/ErrorExceptionRequestExceptionHandler.cs
+++ src/Dfc.CourseDirectory.WebV2/Behaviors/ErrorExceptionRequestExceptionHandler.cs
@@ -7,13 +7,17 @@ namespace Dfc.CourseDirectory.WebV2.Behaviors
     public class ErrorExceptionRequestExceptionHandler<TRequest, TResponse, TException>
         : RequestExceptionHandler<TRequest, TResponse, TException>
         where TException : Exception
-        where TResponse : IOneOf
     {
         protected override void Handle(
             TRequest request,
             TException exception,
             RequestExceptionHandlerState<TResponse> state)
         {
+            if (!typeof(IOneOf).IsAssignableFrom(typeof(TResponse)))
+            {
+                return;
+            }
+
             var exceptionType = exception.GetType();
 
             if (exceptionType.IsGenericType && exceptionType.GetGenericTypeDefinition() == typeof(ErrorException<>))
