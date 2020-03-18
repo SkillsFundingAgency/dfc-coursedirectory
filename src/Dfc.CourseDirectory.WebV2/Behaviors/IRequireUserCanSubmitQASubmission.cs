823e14f75cb25e3321f806b7dddd53f2887f340d
James Gunn <james@gunn.io>
2020-03-18 08:20:44 +0000

    Add a behavior for checking user can submit QA submission

---
+++ src/Dfc.CourseDirectory.WebV2/Behaviors/IRequireUserCanSubmitQASubmission.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.WebV2.Behaviors
+{
+    public interface IRequireUserCanSubmitQASubmission<in TRequest, TResponse>
+    {
+        Task<Guid> GetProviderId(TRequest request);
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/Behaviors/RequireUserCanSubmitQASubmissionBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.Security;
+using MediatR;
+
+namespace Dfc.CourseDirectory.WebV2.Behaviors
+{
+    public class RequireUserCanSubmitQASubmissionBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IRequireUserCanSubmitQASubmission<TRequest, TResponse> _descriptor;
+        private readonly ICurrentUserProvider _currentUserProvider;
+
+        public RequireUserCanSubmitQASubmissionBehavior(
+            IRequireUserCanSubmitQASubmission<TRequest, TResponse> descriptor,
+            ICurrentUserProvider currentUserProvider)
+        {
+            _descriptor = descriptor;
+            _currentUserProvider = currentUserProvider;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            var providerId = await _descriptor.GetProviderId(request);
+            var currentUser = _currentUserProvider.GetCurrentUser();
+
+            if (!AuthorizationRules.UserCanSubmitQASubmission(currentUser, providerId))
+            {
+                throw new NotAuthorizedException();
+            }
+
+            return await next();
+        }
+    }
+}
