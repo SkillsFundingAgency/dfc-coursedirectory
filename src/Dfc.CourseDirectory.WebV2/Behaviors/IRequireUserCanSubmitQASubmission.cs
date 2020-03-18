40f0af37ab76666813125a50e5b3e67425eaa584
James Gunn <james@gunn.io>
2020-03-18 09:17:48 +0000

    Remove extraneous type parameter

--- src/Dfc.CourseDirectory.WebV2/Behaviors/IRequireUserCanSubmitQASubmission.cs
+++ src/Dfc.CourseDirectory.WebV2/Behaviors/IRequireUserCanSubmitQASubmission.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace Dfc.CourseDirectory.WebV2.Behaviors
 {
-    public interface IRequireUserCanSubmitQASubmission<in TRequest, TResponse>
+    public interface IRequireUserCanSubmitQASubmission<in TRequest>
     {
         Task<Guid> GetProviderId(TRequest request);
     }
