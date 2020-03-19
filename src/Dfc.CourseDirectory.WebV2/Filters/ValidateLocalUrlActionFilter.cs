1c7cc423f76551cba6e34dd2ab27fd4bbafdd79e
James Gunn <james@gunn.io>
2020-03-19 13:20:42 +0000

    Add an attribute for validating a local URL

---
+++ src/Dfc.CourseDirectory.WebV2/Filters/ValidateLocalUrlActionFilter.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dfc.CourseDirectory.WebV2.Filters
+{
+    public class ValidateLocalUrlActionFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+            {
+                var urlHelperFactory = context.HttpContext.RequestServices.GetRequiredService<IUrlHelperFactory>();
+                var urlHelper = urlHelperFactory.GetUrlHelper(context);
+
+                var decoratedParameters = controllerActionDescriptor.Parameters
+                    .OfType<ControllerParameterDescriptor>()
+                    .Where(p => p.ParameterInfo.GetCustomAttribute<ValidateLocalUrlAttribute>() != null)
+                    .ToList();
+
+                foreach (var param in decoratedParameters)
+                {
+                    context.ActionArguments.TryGetValue(param.Name, out var value);
+
+                    var isValid = value != null && value is string str && urlHelper.IsLocalUrl(str);
+
+                    if (!isValid)
+                    {
+                        context.Result = new BadRequestResult();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/ValidateLocalUrlAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Dfc.CourseDirectory.WebV2
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public class ValidateLocalUrlAttribute : Attribute
+    {
+    }
+}
