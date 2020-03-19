435f26c87e8bfd6f1a8e45181fdd67f4e6071a63
James Gunn <james@gunn.io>
2020-03-19 06:48:41 +0000

    Add extension method on RedirectToActionResult to append active MPTX instance ID

---
+++ src/Dfc.CourseDirectory.WebV2/MultiPageTransaction/RedirectToActionResultExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.MultiPageTransaction
+{
+    public static class RedirectToActionResultExtensions
+    {
+        public static RedirectToActionResult WithMptxInstanceId<T>(
+            this RedirectToActionResult result,
+            MptxInstanceContext<T> instanceContext)
+            where T : IMptxState, new()
+        {
+            return WithMptxInstanceId(result, instanceContext.InstanceId);
+        }
+
+        public static RedirectToActionResult WithMptxInstanceId(
+            this RedirectToActionResult result,
+            string instanceId)
+        {
+            result.RouteValues ??= new Microsoft.AspNetCore.Routing.RouteValueDictionary();
+            result.RouteValues.Add(MptxResourceFilter.InstanceIdQueryParameter, instanceId);
+            return result;
+        }
+    }
+}
