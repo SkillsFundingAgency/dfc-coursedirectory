faa078c30f8b5a4f21a17e16094f55242a0f6f76
James Gunn <james@gunn.io>
2020-03-19 12:36:29 +0000

    Add IStandardsAndFrameworksCache

---
+++ src/Dfc.CourseDirectory.WebV2/IStandardsAndFrameworksCache.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.Models;
+
+namespace Dfc.CourseDirectory.WebV2
+{
+    public interface IStandardsAndFrameworksCache
+    {
+        Task<IReadOnlyCollection<Framework>> GetAllFrameworks();
+        Task<IReadOnlyCollection<Standard>> GetAllStandards();
+        Task<Framework> GetFramework(int frameworkCode, int progType, int pathwayCode);
+        Task<Standard> GetStandard(int standardCode, int version);
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/Models/Framework.cs
@@ -0,0 +1,10 @@
+﻿namespace Dfc.CourseDirectory.WebV2.Models
+{
+    public class Framework
+    {
+        public int FrameworkCode { get; set; }
+        public int ProgType { get; set; }
+        public int PathwayCode { get; set; }
+        public string NasTitle { get; set; }
+    }
+}
