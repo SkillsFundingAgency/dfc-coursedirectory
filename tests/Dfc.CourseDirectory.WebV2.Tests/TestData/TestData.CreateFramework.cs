42b571fd160f189772f93a8d22a3d8ade65f169b
James Gunn <james@gunn.io>
2020-03-19 14:23:29 +0000

    Support for reading/writing standards & frameworks in tests

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/TestData/TestData.CreateFramework.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Query = Dfc.CourseDirectory.WebV2.Tests.DataStore.CosmosDb.Queries.CreateFramework;
+
+namespace Dfc.CourseDirectory.WebV2.Tests
+{
+    public partial class TestData
+    {
+        public async Task CreateFramework(
+            int frameworkCode,
+            int progType,
+            int pathwayCode,
+            string nasTitle)
+        {
+            var id = Guid.NewGuid().ToString();
+
+            await _cosmosDbQueryDispatcher.ExecuteQuery(
+                new Query()
+                {
+                    Id = id,
+                    FrameworkCode = frameworkCode,
+                    ProgType = progType,
+                    PathwayCode = pathwayCode,
+                    NasTitle = nasTitle
+                });
+        }
+    }
+}
