073e7a779b7512626b3a339a3ce76169cc0fff81
James Gunn <james@gunn.io>
2020-03-19 12:36:29 +0000

    Add IStandardsAndFrameworksCache

---
+++ src/Dfc.CourseDirectory.WebV2/Models/Standard.cs
@@ -0,0 +1,11 @@
+﻿namespace Dfc.CourseDirectory.WebV2.Models
+{
+    public class Standard
+    {
+        public int StandardCode { get; set; }
+        public int Version { get; set; }
+        public string StandardName { get; set; }
+        public string NotionalNVQLevelv2 { get; set; }
+        public bool OtherBodyApprovalRequired { get; set; }
+    }
+}
