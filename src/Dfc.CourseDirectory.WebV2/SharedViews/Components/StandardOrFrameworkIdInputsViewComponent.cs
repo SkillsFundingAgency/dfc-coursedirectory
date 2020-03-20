f5465d8ab23ea4a71636694c7f1f99aae22e72c4
James Gunn <james@gunn.io>
2020-03-20 13:10:06 +0000

    Add ViewComponent for rendering hidden input fields for StandardOrFramework ID

---
+++ src/Dfc.CourseDirectory.WebV2/SharedViews/Components/StandardOrFrameworkIdInputsViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Dfc.CourseDirectory.WebV2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.ViewComponents
+{
+    public class StandardOrFrameworkIdInputsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(StandardOrFramework standardOrFramework)
+        {
+            return View("~/SharedViews/Components/StandardOrFrameworkIdInputs.cshtml", standardOrFramework);
+        }
+    }
+}
