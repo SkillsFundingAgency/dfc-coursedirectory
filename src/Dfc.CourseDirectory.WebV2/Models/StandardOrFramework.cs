b158f8e701e05c3f79c5edc60a4c26e2f1dc057b
James Gunn <james@gunn.io>
2020-03-20 10:58:41 +0000

    Add StandardOrFramework model

---
+++ src/Dfc.CourseDirectory.WebV2/Models/StandardOrFramework.cs
@@ -0,0 +1,21 @@
+﻿using OneOf;
+
+namespace Dfc.CourseDirectory.WebV2.Models
+{
+    public class StandardOrFramework : OneOfBase<Standard, Framework>
+    {
+        public StandardOrFramework(Standard standard)
+            : base(0, standard, null)
+        {
+        }
+
+        public StandardOrFramework(Framework framework)
+            : base(1, null, framework)
+        {
+        }
+
+        public string StandardOrFrameworkTitle => Match(
+            s => s.StandardName,
+            f => f.NasTitle);
+    }
+}
