f9a81103648ab1d2b9e61ee61a5e6c43a6d2d010
James Gunn <james@gunn.io>
2020-03-20 13:57:05 +0000

    Add extension method on Url for appending provider ID

---
+++ src/Dfc.CourseDirectory.WebV2/UrlExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Dfc.CourseDirectory.WebV2.Filters;
+using Flurl;
+
+namespace Dfc.CourseDirectory.WebV2
+{
+    public static class UrlExtensions
+    {
+        public static Url WithProviderContext(this Url url, ProviderInfo providerInfo)
+            => url.SetQueryParam(ProviderContextResourceFilter.RouteValueKey, providerInfo.ProviderId);
+    }
+}
