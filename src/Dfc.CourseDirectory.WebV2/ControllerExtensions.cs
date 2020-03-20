52c0afcad9901570cf13f9b4b01c752c3f13ad3a
James Gunn <james@gunn.io>
2020-03-20 13:56:29 +0000

    Add overload of ViewWithErrors that takes a ViewName

--- src/Dfc.CourseDirectory.WebV2/ControllerExtensions.cs
+++ src/Dfc.CourseDirectory.WebV2/ControllerExtensions.cs
@@ -18,5 +18,18 @@ namespace Dfc.CourseDirectory.WebV2
             result.StatusCode = (int)statusCode;
             return result;
         }
+
+        public static ViewResult ViewFromErrors<T>(
+            this Controller controller,
+            string viewName,
+            ModelWithErrors<T> failed,
+            HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+        {
+            failed.ValidationResult.AddToModelState(controller.ModelState, null);
+
+            var result = controller.View(viewName, failed.Model);
+            result.StatusCode = (int)statusCode;
+            return result;
+        }
     }
 }
