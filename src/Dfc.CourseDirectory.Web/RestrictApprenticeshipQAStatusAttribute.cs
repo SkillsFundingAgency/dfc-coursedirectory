57a3bce030722e9b001c0c1706c118e637a460c4
James Gunn <james@gunn.io>
2020-03-23 10:18:03 +0000

    Move RestrictApprenticeshipQAStatusAttribute into Web project
    This should only be used on legacy actions - we have a behavior to
    enforce it differently for v2 bits.
    
    Also added an option to disable check if QA feature is not enabled.

--- src/Dfc.CourseDirectory.Web/RestrictApprenticeshipQAStatusAttribute.cs
+++ src/Dfc.CourseDirectory.Web/RestrictApprenticeshipQAStatusAttribute.cs
@@ -1,43 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2;
 using Dfc.CourseDirectory.WebV2.DataStore.Sql;
 using Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries;
+using Dfc.CourseDirectory.WebV2.HttpContextFeatures;
 using Dfc.CourseDirectory.WebV2.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Dfc.CourseDirectory.WebV2.Filters
+namespace Dfc.CourseDirectory.Web
 {
     public class RestrictApprenticeshipQAStatusAttribute : ActionFilterAttribute
     {
         public RestrictApprenticeshipQAStatusAttribute(params ApprenticeshipQAStatus[] allowedStatuses)
         {
             AllowedStatuses = new HashSet<ApprenticeshipQAStatus>(allowedStatuses);
+            RequireApprenticeshipQAFeature = true;
         }
 
         public ISet<ApprenticeshipQAStatus> AllowedStatuses { get; }
 
+        public bool RequireApprenticeshipQAFeature { get; set; }
+
         public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            var providerInfo = context.ActionArguments.Select(a => a.Value).OfType<ProviderInfo>().SingleOrDefault();
-            if (providerInfo == null)
+            var sqlQueryDispatcher = context.HttpContext.RequestServices.GetRequiredService<ISqlQueryDispatcher>();
+            var featureFlagProvider = context.HttpContext.RequestServices.GetRequiredService<IFeatureFlagProvider>();
+
+            var providerContextFeature = context.HttpContext.Features.Get<ProviderContextFeature>();
+
+            if (providerContextFeature == null)
             {
-                throw new InvalidOperationException(
-                    $"Unknown provider. " +
-                    $"This attribute can only be used when an action parameter of type {typeof(ProviderInfo).FullName} is defined.");
+                throw new InvalidOperationException("No provider context set.");
             }
 
-            var sqlQueryDispatcher = context.HttpContext.RequestServices.GetRequiredService<ISqlQueryDispatcher>();
+            var providerId = providerContextFeature.ProviderInfo.ProviderId;
+
             var currentStatus = await sqlQueryDispatcher.ExecuteQuery(
                 new GetProviderApprenticeshipQAStatus()
                 {
-                    ProviderId = providerInfo.ProviderId
+                    ProviderId = providerId
                 });
 
-            if (!AllowedStatuses.Contains(currentStatus))
+            var qaFeatureIsEnabled = featureFlagProvider.HaveFeature(FeatureFlags.ApprenticeshipQA);
+
+            if (!qaFeatureIsEnabled && RequireApprenticeshipQAFeature)
+            {
+                context.Result = new NotFoundResult();
+            }
+            else if (RequireApprenticeshipQAFeature && !AllowedStatuses.Contains(currentStatus))
             {
                 context.Result = new BadRequestResult();
             }
