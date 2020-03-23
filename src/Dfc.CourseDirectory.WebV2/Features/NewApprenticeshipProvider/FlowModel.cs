9abf20c1cac75ea2fd389f7132399142a613d7b0
James Gunn <james@gunn.io>
2020-03-23 08:35:18 +0000

    Add summary page for provider info portion of QA journey

--- src/Dfc.CourseDirectory.WebV2/Features/NewApprenticeshipProvider/FlowModel.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/NewApprenticeshipProvider/FlowModel.cs
@@ -4,5 +4,11 @@ namespace Dfc.CourseDirectory.WebV2.Features.NewApprenticeshipProvider
 {
     public class FlowModel : IMptxState
     {
+        public string ProviderMarketingInformation { get; set; }
+
+        public bool GotProviderDetail => !string.IsNullOrEmpty(ProviderMarketingInformation);
+
+        public void SetProviderDetail(string marketingInformation) =>
+            ProviderMarketingInformation = marketingInformation;
     }
 }
---
+++ src/Dfc.CourseDirectory.WebV2/Features/NewApprenticeshipProvider/FlowModelInitializer.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Queries;
+using Dfc.CourseDirectory.WebV2.HttpContextFeatures;
+using Dfc.CourseDirectory.WebV2.MultiPageTransaction;
+using Dfc.CourseDirectory.WebV2.Validation;
+using Microsoft.AspNetCore.Http;
+
+namespace Dfc.CourseDirectory.WebV2.Features.NewApprenticeshipProvider
+{
+    public class FlowModelInitializer : IInitializeMptxState<FlowModel>
+    {
+        private readonly ICosmosDbQueryDispatcher _cosmosDbQueryDispatcher;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public FlowModelInitializer(
+            ICosmosDbQueryDispatcher cosmosDbQueryDispatcher,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _cosmosDbQueryDispatcher = cosmosDbQueryDispatcher;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task Initialize(MptxInstanceContext<FlowModel> context)
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            var providerId = httpContext.Features.Get<ProviderContextFeature>().ProviderInfo.ProviderId;
+
+            var provider = await _cosmosDbQueryDispatcher.ExecuteQuery(
+                new GetProviderById()
+                {
+                    ProviderId = providerId
+                });
+
+            if (!string.IsNullOrEmpty(provider.MarketingInformation))
+            {
+                context.Update(s => s.SetProviderDetail(Html.SanitizeHtml(provider.MarketingInformation)));
+            }
+        }
+    }
+}
