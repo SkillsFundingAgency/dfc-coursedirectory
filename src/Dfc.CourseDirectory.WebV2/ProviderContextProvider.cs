6d2edaabf049a1cc1a938fae70526f92788c9f2a
James Gunn <james@gunn.io>
2020-03-24 19:37:54 +0000

    Add IProviderContextProvider and an implementation

---
+++ src/Dfc.CourseDirectory.WebV2/ProviderContextProvider.cs
@@ -0,0 +1,22 @@
+﻿using Dfc.CourseDirectory.WebV2.HttpContextFeatures;
+using Microsoft.AspNetCore.Http;
+
+namespace Dfc.CourseDirectory.WebV2
+{
+    public class ProviderContextProvider : IProviderContextProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ProviderContextProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public ProviderInfo GetProviderContext()
+        {
+            var httpContext = _httpContextAccessor.HttpContext;
+            var feature = httpContext.Features.Get<ProviderContextFeature>();
+            return feature?.ProviderInfo;
+        }
+    }
+}
