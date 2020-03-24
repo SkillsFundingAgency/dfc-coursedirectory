9cc3b5078ac6749fa4554c15a54ea772c6269785
James Gunn <james@gunn.io>
2020-03-24 20:27:50 +0000

    Add QA notifications

---
+++ src/Dfc.CourseDirectory.WebV2/Features/NewApprenticeshipProvider/QANotificationsViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.Features.NewApprenticeshipProvider
+{
+    public class QANotificationsViewComponent : ViewComponent
+    {
+        private readonly IProviderContextProvider _providerContextProvider;
+        private readonly ISqlQueryDispatcher _sqlQueryDispatcher;
+
+        public QANotificationsViewComponent(
+            IProviderContextProvider providerContextProvider,
+            ISqlQueryDispatcher sqlQueryDispatcher)
+        {
+            _providerContextProvider = providerContextProvider;
+            _sqlQueryDispatcher = sqlQueryDispatcher;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var providerId = _providerContextProvider.GetProviderContext().ProviderId;
+
+            var qaStatus = await _sqlQueryDispatcher.ExecuteQuery(
+                new GetProviderApprenticeshipQAStatus()
+                {
+                    ProviderId = providerId
+                });
+
+            return View("~/Features/NewApprenticeshipProvider/QANotifications.cshtml", qaStatus);
+        }
+    }
+}
