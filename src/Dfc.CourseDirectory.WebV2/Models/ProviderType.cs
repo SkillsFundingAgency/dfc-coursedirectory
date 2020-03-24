9392f0cdfc65eb7cdc64c197b1408882077c756f
James Gunn <james@gunn.io>
2020-03-24 10:51:14 +0000

    Add ToDisplayName extension method on ProviderType

--- src/Dfc.CourseDirectory.WebV2/Models/ProviderType.cs
+++ src/Dfc.CourseDirectory.WebV2/Models/ProviderType.cs
@@ -9,4 +9,16 @@ namespace Dfc.CourseDirectory.WebV2.Models
         Apprenticeships = 2,
         Both = FE | Apprenticeships
     }
+
+    public static class ProviderTypeExtensions
+    {
+        public static string ToDisplayName(this ProviderType providerType) =>
+            providerType switch
+            {
+                ProviderType.Apprenticeships => "Apprenticeships",
+                ProviderType.FE => "FE",
+                ProviderType.Both => "FE & Apprenticeships",
+                _ => throw new NotImplementedException($"Unknown value: '{providerType}'.")
+            };
+    }
 }
