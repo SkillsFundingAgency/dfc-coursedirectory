a73313413df4b6f851d7f6c000b9ff91fb75bbc3
James Gunn <james@gunn.io>
2020-03-25 12:13:01 +0000

    Simplify HelpdeskDashboard

--- src/Dfc.CourseDirectory.WebV2/Features/HelpdeskDashboard/HelpdeskDashboardController.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/HelpdeskDashboard/HelpdeskDashboardController.cs
@@ -1,21 +1,15 @@
-﻿using System.Threading.Tasks;
-using Dfc.CourseDirectory.WebV2.Features.HelpdeskDashboard.Dashboard;
-using MediatR;
+﻿using Dfc.CourseDirectory.WebV2.Filters;
+using Dfc.CourseDirectory.WebV2.Security;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dfc.CourseDirectory.WebV2.Features.HelpdeskDashboard
 {
+    [RequireFeatureFlag(FeatureFlags.ApprenticeshipQA)]
+    [Authorize(Policy = AuthorizationPolicyNames.ApprenticeshipQA)]
     public class HelpdeskDashboardController : Controller
     {
         [HttpGet("helpdesk-dashboard")]
-        public async Task<IActionResult> Dashboard([FromServices] IMediator mediator)
-        {
-            var result = await mediator.Send(new Query());
-            return result.Match(
-                error => error.Value == ErrorReason.NotAuthorized ?
-                    Forbid() : error.Value == ErrorReason.NothingAvailable ?
-                    (IActionResult)NotFound() : BadRequest(),
-                vm => View(vm));
-        }
+        public IActionResult Dashboard() => View();
     }
 }
