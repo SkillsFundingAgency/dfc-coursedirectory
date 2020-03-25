31803d7e4cd054b8bf38a605a13cf73530230c81
James Gunn <james@gunn.io>
2020-03-25 11:13:16 +0000

    Move admin provider nav into View Component

---
+++ src/Dfc.CourseDirectory.WebV2/SharedViews/Components/AdminProviderContextNavViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.SharedViews.Components
+{
+    public class AdminProviderContextNavViewComponent : ViewComponent
+    {
+        private readonly IProviderContextProvider _providerContextProvider;
+
+        public AdminProviderContextNavViewComponent(IProviderContextProvider providerContextProvider)
+        {
+            _providerContextProvider = providerContextProvider;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var providerContext = _providerContextProvider.GetProviderContext();
+            return View("~/SharedViews/Components/AdminProviderContextNav.cshtml", providerContext);
+        }
+    }
+}
