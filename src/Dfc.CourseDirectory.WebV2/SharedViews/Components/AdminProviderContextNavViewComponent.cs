ad043f1b441816feccefe91f4fee91f610db3811
James Gunn <james@gunn.io>
2020-03-25 12:48:43 +0000

    Hide apprenticeship options if provider has not passed QA

--- src/Dfc.CourseDirectory.WebV2/SharedViews/Components/AdminProviderContextNavViewComponent.cs
+++ src/Dfc.CourseDirectory.WebV2/SharedViews/Components/AdminProviderContextNavViewComponent.cs
@@ -1,10 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dfc.CourseDirectory.WebV2.SharedViews.Components
 {
     public class AdminProviderContextNavViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke(ProviderInfo providerInfo) =>
-            View("~/SharedViews/Components/AdminProviderContextNav.cshtml", providerInfo);
+        private readonly IFeatureFlagProvider _featureFlagProvider;
+        private readonly ISqlQueryDispatcher _sqlQueryDispatcher;
+
+        public AdminProviderContextNavViewComponent(
+            IFeatureFlagProvider featureFlagProvider,
+            ISqlQueryDispatcher sqlQueryDispatcher)
+        {
+            _featureFlagProvider = featureFlagProvider;
+            _sqlQueryDispatcher = sqlQueryDispatcher;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(ProviderInfo providerInfo)
+        {
+            var qaStatus = await _sqlQueryDispatcher.ExecuteQuery(
+                new GetProviderApprenticeshipQAStatus()
+                {
+                    ProviderId = providerInfo.ProviderId
+                });
+
+            var vm = new ProviderNavViewModel()
+            {
+                ApprenticeshipQAFeatureIsEnabled = _featureFlagProvider.HaveFeature(FeatureFlags.ApprenticeshipQA),
+                ApprenticeshipQAStatus = qaStatus,
+                ProviderContext = providerInfo
+            };
+
+            return View("~/SharedViews/Components/AdminProviderContextNav.cshtml", vm);
+        }
     }
 }
