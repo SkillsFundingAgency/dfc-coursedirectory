052f9ee49d7bd48e3ee62c0818a8e6fda1efb34b
James Gunn <james@gunn.io>
2020-03-26 09:04:19 +0000

    Remove old postcode service bits

--- src/Dfc.CourseDirectory.Services/Interfaces/IPostCodeSearchResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Dfc.CourseDirectory.Services.Interfaces
-{
-    public interface IPostCodeSearchResult
-    {
-        IEnumerable<PostCodeSearchResultItem> Value { get; }
-    }
-}
