1d4ad79849071d1aefc6666ccfe1a84e36f94f02
James Gunn <james@gunn.io>
2020-03-27 17:09:32 +0000

    Add more apprenticeship model types

--- src/Dfc.CourseDirectory.Web/Controllers/ApprenticeshipsController.cs
+++ src/Dfc.CourseDirectory.Web/Controllers/ApprenticeshipsController.cs
@@ -27,13 +27,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Dfc.CourseDirectory.Web.Helpers.Attributes;
-using Dfc.CourseDirectory.WebV2.Models;
 
 namespace Dfc.CourseDirectory.Web.Controllers
 {
     [Authorize("Apprenticeship")]
     [SelectedProviderNeeded]
-    [RestrictApprenticeshipQAStatus(ApprenticeshipQAStatus.Passed, AllowWhenApprenticeshipQAFeatureDisabled = true)]
+    [RestrictApprenticeshipQAStatus(WebV2.Models.ApprenticeshipQAStatus.Passed, AllowWhenApprenticeshipQAFeatureDisabled = true)]
     public class ApprenticeshipsController : Controller
     {
         private readonly ILogger<ApprenticeshipsController> _logger;
---
+++ src/Dfc.CourseDirectory.WebV2/Models/LocationType.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfc.CourseDirectory.WebV2.Models
+{
+    public enum LocationType
+    {
+        Venue = 1,
+        Region = 2,
+        SubRegion = 3
+    }
+}
