a100abb6a3e834d7294afcb7ba9100824adee2c1
James Gunn <james@gunn.io>
2020-03-27 08:46:39 +0000

    Reset StandardsAndFrameworksCache for each test

--- src/Dfc.CourseDirectory.WebV2/IStandardsAndFrameworksCache.cs
+++ src/Dfc.CourseDirectory.WebV2/IStandardsAndFrameworksCache.cs
@@ -6,6 +6,7 @@ namespace Dfc.CourseDirectory.WebV2
 {
     public interface IStandardsAndFrameworksCache
     {
+        void Clear();
         Task<IReadOnlyCollection<Framework>> GetAllFrameworks();
         Task<IReadOnlyCollection<Standard>> GetAllStandards();
         Task<Framework> GetFramework(int frameworkCode, int progType, int pathwayCode);
