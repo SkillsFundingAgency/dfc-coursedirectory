72927a03a7552381d2c089a93e15f934a41ab03c
James Gunn <james@gunn.io>
2020-03-27 17:09:32 +0000

    Add more apprenticeship model types

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/CosmosDb/Models/BulkUploadError.cs
@@ -0,0 +1,9 @@
+﻿namespace Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Models
+{
+    public class BulkUploadError
+    {
+        public int LineNumber { get; set; }
+        public string Header { get; set; }
+        public string Error { get; set; }
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/Models/ApprenticeshipLocationType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Dfc.CourseDirectory.WebV2.Models
+{
+    public enum ApprenticeshipLocationType
+    {
+        ClassroomBased = 1,
+        EmployerBased = 2,
+        ClassroomBasedAndEmployerBased = 3
+    }
+
+    public static class ApprenticeshipLocationTypeExtensions
+    {
+        public static string ToDisplayName(this ApprenticeshipLocationType apprenticeshipLocationType) =>
+            apprenticeshipLocationType switch
+            {
+                ApprenticeshipLocationType.EmployerBased => "Employer based locations",
+                ApprenticeshipLocationType.ClassroomBased => "Classroom based locations",
+                ApprenticeshipLocationType.ClassroomBasedAndEmployerBased => "Employer and classroom based locations",
+                _ => throw new NotImplementedException($"Unknown value: '{apprenticeshipLocationType}'.")
+            };
+    }
+}
