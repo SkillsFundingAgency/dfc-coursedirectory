5b4dc7a0ea22dbca9c586dff941cb603aaede5ee
Javeed Dutia <javeed.dutia@esynergy-solutions.co.uk>
2020-03-31 15:38:12 +0100

    dev work so far

---
+++ src/Dfc.CourseDirectory.WebV2/Helpers/Interfaces/IUkrlpSyncHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.WebV2.Helpers.Interfaces
+{
+    public interface IUkrlpSyncHelper
+    {
+        Task SyncProviderData(Guid providerId, int ukprn);
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/Security/SyncUserProviderSignInAction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Models;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Queries;
+using Dfc.CourseDirectory.WebV2.Helpers.Interfaces;
+using JWT.Algorithms;
+using JWT.Builder;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Dfc.CourseDirectory.WebV2.Security
+{
+    public class SyncUserProviderSignInAction : ISignInAction, IDisposable
+    {
+        private readonly DfeSignInSettings _settings;
+        private readonly ICosmosDbQueryDispatcher _cosmosDbQueryDispatcher;
+        private readonly IHostEnvironment _environment;
+        private readonly IUkrlpSyncHelper _ukrlpSyncHelper;
+        private readonly ICurrentUserProvider _currentUserProvider;
+
+
+        public SyncUserProviderSignInAction(
+            DfeSignInSettings settings,
+            ICosmosDbQueryDispatcher cosmosDbQueryDispatcher,
+            IHostEnvironment environment,
+            ICurrentUserProvider currentUserProvider,
+            IUkrlpSyncHelper ukrlpSyncHelper)
+        {
+            _settings = settings;
+            _cosmosDbQueryDispatcher = cosmosDbQueryDispatcher;
+            _environment = environment;
+            _currentUserProvider = currentUserProvider;
+            _ukrlpSyncHelper = ukrlpSyncHelper;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public async Task OnUserSignedIn(SignInContext context)
+        {
+            if(context.ProviderUkprn.HasValue)
+            {
+                await this._ukrlpSyncHelper.SyncProviderData(context.Provider.Id, context.Provider.Ukprn);
+            }
+        }
+    }
+}
