ced0deb0322cbc199b2d2f81552359b8107bf47c
Javeed Dutia <javeed.dutia@esynergy-solutions.co.uk>
2020-03-31 15:38:12 +0100

    dev work so far

---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/CosmosDb/Queries/UpdateProviderUkrlpData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Dfc.CourseDirectory.WebV2.Models;
+using OneOf;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Queries
+{
+    public class UpdateProviderUkrlpData : ICosmosDbQuery<Success>
+    {
+        public Guid ProviderId { get; set; }
+        public OneOf<None, string> UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/DataStore/CosmosDb/QueryHandlers/UpdateProviderUkrlpDataHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Models;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Queries;
+using Microsoft.Azure.Documents.Client;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.QueryHandlers
+{
+    public class UpdateProviderUkrlpDataHandler : ICosmosDbQueryHandler<UpdateProviderUkrlpData, Success>
+    {
+        public async Task<Success> Execute(
+            DocumentClient client,
+            Configuration configuration,
+            UpdateProviderUkrlpData request)
+        {
+            var documentUri = UriFactory.CreateDocumentUri(
+                configuration.DatabaseId,
+                configuration.ProviderCollectionName,
+                request.ProviderId.ToString());
+
+            var response = await client.ReadDocumentAsync<Provider>(documentUri);
+
+            var provider = response.Document;
+
+            provider.DateUpdated = request.UpdatedOn;
+            request.UpdatedBy.Switch(_ => { }, v => provider.UpdatedBy = v);
+
+            await client.ReplaceDocumentAsync(documentUri, provider);
+
+            return new Success();
+        }
+    }
+}
