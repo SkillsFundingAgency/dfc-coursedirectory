e49175de2a686d6fa70b67f23f05d86605a8573c
Javeed Dutia <javeed.dutia@esynergy-solutions.co.uk>
2020-03-31 15:38:12 +0100

    dev work so far

---
+++ src/Dfc.CourseDirectory.WebV2/Services/Interfaces/IUkrlpWcfService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UkrlpService;
+
+namespace Dfc.CourseDirectory.WebV2.Services.Interfaces
+{
+    public interface IUkrlpWcfService
+    {
+        ProviderRecordStructure GetProviderData(int ukprn);
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/Services/UkrlpWcfService.cs
@@ -0,0 +1,67 @@
+﻿using Dfc.CourseDirectory.WebV2.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UkrlpService;
+
+namespace Dfc.CourseDirectory.WebV2.Services
+{
+    public class UkrlpWcfService : IUkrlpWcfService
+    {
+        public ProviderRecordStructure GetProviderData(int ukprn)
+        {
+            string[] statusesToFetch =
+            {
+                    "A", // Active
+            };
+
+            List<ProviderRecordStructure> results = new List<ProviderRecordStructure>();
+            foreach (String status in statusesToFetch)
+            {
+                var request = BuildRequest(ukprn.ToString());
+                request.SelectionCriteria.ProviderStatus = status;
+                request.QueryId = GetNextQueryId();
+
+                var providerClient = new ProviderQueryPortTypeClient();
+                providerClient.InnerChannel.OperationTimeout = new TimeSpan(0, 10, 0);
+                Task<response> x = providerClient.retrieveAllProvidersAsync(request);
+                x.Wait();
+
+                results.AddRange(x.Result?.ProviderQueryResponse?.MatchingProviderRecords ?? new ProviderRecordStructure[] { });
+            }
+
+            if(results.Any())
+            {
+                return results.First();
+            }
+            else
+            {
+                return null;
+            }
+       }
+
+        private ProviderQueryStructure BuildRequest(string ukprnListToFetch)
+        {
+            SelectionCriteriaStructure scs = new SelectionCriteriaStructure
+            {
+                StakeholderId = "1",
+                UnitedKingdomProviderReferenceNumberList = new[] { ukprnListToFetch },
+                ApprovedProvidersOnly = YesNoType.No,
+                ApprovedProvidersOnlySpecified = true,
+                CriteriaCondition = QueryCriteriaConditionType.OR,
+                CriteriaConditionSpecified = true
+            };
+
+            return new ProviderQueryStructure { SelectionCriteria = scs };
+        }
+
+        private static String GetNextQueryId()
+        {
+            Int32 id = 0;
+            id++;
+            return id.ToString();
+        }
+    }
+}
