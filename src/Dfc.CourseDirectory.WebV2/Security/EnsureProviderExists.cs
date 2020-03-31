fdcd3357024b9a3c65ce512e3fe5abd9bd60b591
James Gunn <james@gunn.io>
2020-03-31 14:02:21 +0100

    Fix tracking sign ins for users who are provider-scoped

---
+++ src/Dfc.CourseDirectory.WebV2/Security/EnsureProviderExists.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.DataStore.Sql;
+using Query = Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries.EnsureProviderExists;
+
+namespace Dfc.CourseDirectory.WebV2.Security
+{
+    public class EnsureProviderExists : ISignInAction
+    {
+        private readonly ISqlQueryDispatcher _sqlQueryDispatcher;
+
+        public EnsureProviderExists(ISqlQueryDispatcher sqlQueryDispatcher)
+        {
+            _sqlQueryDispatcher = sqlQueryDispatcher;
+        }
+
+        public async Task OnUserSignedIn(SignInContext context)
+        {
+            if (context.Provider != null)
+            {
+                await _sqlQueryDispatcher.ExecuteQuery(
+                    new Query()
+                    {
+                        ProviderId = context.Provider.Id
+                    });
+            }
+        }
+    }
+}
