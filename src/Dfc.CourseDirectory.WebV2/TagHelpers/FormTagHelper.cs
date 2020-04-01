e7a037de83b61de0f8e234cb4d3595ed8cf29d77
James Gunn <james@gunn.io>
2020-04-01 09:41:48 +0100

    Enable attaching TagHelperComponents to anchor and form tags

---
+++ src/Dfc.CourseDirectory.WebV2/TagHelpers/FormTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Logging;
+
+namespace Dfc.CourseDirectory.WebV2.TagHelpers
+{
+    [HtmlTargetElement("form")]
+    public class FormTagHelper : TagHelperComponentTagHelper
+    {
+        public FormTagHelper(ITagHelperComponentManager manager, ILoggerFactory loggerFactory)
+            : base(manager, loggerFactory)
+        {
+        }
+    }
+}
