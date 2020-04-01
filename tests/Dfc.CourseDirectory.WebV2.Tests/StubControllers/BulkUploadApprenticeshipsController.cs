65fbeefda0adf24f9c317de75e0cec46f3663b17
James Gunn <james@gunn.io>
2020-04-01 13:22:57 +0100

    Add tests for Bulk Upload layout links

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/StubControllers/BulkUploadApprenticeshipsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.Tests.StubControllers
+{
+    public class BulkUploadApprenticeshipsController : Controller
+    {
+        [HttpGet("BulkUploadApprenticeships")]
+        public IActionResult Index() => Ok();
+    }
+}
