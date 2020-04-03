8d1699b2b718d6757537eeb9570f60c8e0c15684
Javeed Dutia <javeed.dutia@esynergy-solutions.co.uk>
2020-04-03 08:32:55 +0100

    added SynchUserPRoviderSignInAction to ensure provider data is synched from Ukrlp service when user signs in

--- src/Dfc.CourseDirectory.WebV2/DataStore/CosmosDb/QueryHandlers/UpdateProviderUkrlpDataHandler.cs
+++ src/Dfc.CourseDirectory.WebV2/DataStore/CosmosDb/QueryHandlers/UpdateProviderUkrlpDataHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Models;
 using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Queries;
 using Microsoft.Azure.Documents.Client;
@@ -6,26 +7,52 @@ using OneOf.Types;
 
 namespace Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.QueryHandlers
 {
-    public class UpdateProviderUkrlpDataHandler : ICosmosDbQueryHandler<UpdateProviderUkrlpData, Success>
+    public class UpsertProviderUkrlpDataHandler : ICosmosDbQueryHandler<UpsertProviderUkrlpData, Success>
     {
         public async Task<Success> Execute(
             DocumentClient client,
             Configuration configuration,
-            UpdateProviderUkrlpData request)
+            UpsertProviderUkrlpData request)
         {
             var documentUri = UriFactory.CreateDocumentUri(
                 configuration.DatabaseId,
                 configuration.ProviderCollectionName,
-                request.ProviderId.ToString());
+                request.Id.ToString());
 
-            var response = await client.ReadDocumentAsync<Provider>(documentUri);
+            if (request.Update)
+            {
+                var response = await client.ReadDocumentAsync<Provider>(documentUri);
 
-            var provider = response.Document;
+                var provider = response.Document;
 
-            provider.DateUpdated = request.UpdatedOn;
-            request.UpdatedBy.Switch(_ => { }, v => provider.UpdatedBy = v);
+                if (provider != null)
+                {
+                    provider.ProviderName = request.ProviderName;
+                    // Only update if supplied
+                    provider.ProviderContact = request.ProviderContact.Count > 0 ? request.ProviderContact : provider.ProviderContact;
+                    provider.Alias = request.Alias;
+                    provider.ProviderStatus = request.ProviderStatus;
+                    provider.DateUpdated = request.DateUpdated;
+                    request.UpdatedBy = request.UpdatedBy;
 
-            await client.ReplaceDocumentAsync(documentUri, provider);
+                    await client.ReplaceDocumentAsync(documentUri, provider);
+                }
+            }
+            else
+            {
+                var newProvider = new Provider();
+                newProvider.Id = request.Id;
+                newProvider.UnitedKingdomProviderReferenceNumber = request.UnitedKingdomProviderReferenceNumber;
+                newProvider.ProviderName = request.ProviderName;
+                newProvider.ProviderContact = request.ProviderContact;
+                newProvider.Alias = request.Alias;
+                newProvider.ProviderStatus = request.ProviderStatus;
+                newProvider.ProviderType = WebV2.Models.ProviderType.FE; //Default to FE
+                newProvider.DateUpdated = request.DateUpdated;
+                newProvider.UpdatedBy = request.UpdatedBy;
+
+                await client.CreateDocumentAsync(documentUri, newProvider);
+            }
 
             return new Success();
         }
