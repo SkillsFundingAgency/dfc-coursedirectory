4048cd487c836268adca82fac717c02e2e94ea1c
James Gunn <james@gunn.io>
2020-04-03 08:40:43 +0100

    Use distributed cache for backing ProviderInfoCache

--- src/Dfc.CourseDirectory.WebV2/ProviderInfoCache.cs
+++ src/Dfc.CourseDirectory.WebV2/ProviderInfoCache.cs
@@ -2,16 +2,17 @@
 using System.Threading.Tasks;
 using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb;
 using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Queries;
-using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 
 namespace Dfc.CourseDirectory.WebV2
 {
     public class ProviderInfoCache : IProviderInfoCache
     {
         private readonly ICosmosDbQueryDispatcher _cosmosDbQueryDispatcher;
-        private readonly IMemoryCache _cache;
+        private readonly IDistributedCache _cache;
 
-        public ProviderInfoCache(ICosmosDbQueryDispatcher cosmosDbQueryDispatcher, IMemoryCache cache)
+        public ProviderInfoCache(ICosmosDbQueryDispatcher cosmosDbQueryDispatcher, IDistributedCache cache)
         {
             _cosmosDbQueryDispatcher = cosmosDbQueryDispatcher;
             _cache = cache;
@@ -21,7 +22,24 @@ namespace Dfc.CourseDirectory.WebV2
         {
             var cacheKey = GetCacheKey(providerId);
 
-            if (!_cache.TryGetValue<ProviderInfo>(cacheKey, out var providerInfo))
+            ProviderInfo result = null;
+
+            var cacheResult = await _cache.GetStringAsync(cacheKey);
+
+            if (cacheResult != null)
+            {
+                try
+                {
+                    result = Deserialize(cacheResult);
+                }
+                catch (JsonSerializationException)
+                {
+                    // If we make a breaking change to ProviderInfo serialization could fail;
+                    // force a reload in that case
+                }
+            }
+
+            if (result == null)
             {
                 var provider = await _cosmosDbQueryDispatcher.ExecuteQuery(
                     new GetProviderById() { ProviderId = providerId });
@@ -31,18 +49,18 @@ namespace Dfc.CourseDirectory.WebV2
                     return null;
                 }
 
-                providerInfo = new ProviderInfo()
+                result = new ProviderInfo()
                 {
                     ProviderId = provider.Id,
-                    Ukprn = int.Parse(provider.UnitedKingdomProviderReferenceNumber),
+                    Ukprn = provider.Ukprn,
                     ProviderType = provider.ProviderType,
                     ProviderName = provider.ProviderName
                 };
 
-                _cache.Set(cacheKey, providerInfo);
+                await _cache.SetStringAsync(cacheKey, Serialize(result));
             }
 
-            return providerInfo;
+            return result;
         }
 
         public async Task<Guid?> GetProviderIdForUkprn(int ukprn)
@@ -60,6 +78,10 @@ namespace Dfc.CourseDirectory.WebV2
             }
         }
 
+        private static ProviderInfo Deserialize(string value) => JsonConvert.DeserializeObject<ProviderInfo>(value);
+
         private static string GetCacheKey(Guid providerId) => $"provider-info:{providerId}";
+
+        private static string Serialize(ProviderInfo value) => JsonConvert.SerializeObject(value);
     }
 }
