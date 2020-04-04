7a1bdbff0c95ddd0467c549894060258abb2a41a
James Gunn <james@gunn.io>
2020-04-04 11:14:39 +0100

    Move IStartupTask execution into HostedService

---
+++ src/Dfc.CourseDirectory.WebV2/RunStartupTasksHostedService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Dfc.CourseDirectory.WebV2
+{
+    public class RunStartupTasksHostedService : IHostedService
+    {
+        private readonly IEnumerable<IStartupTask> _startupTasks;
+
+        public RunStartupTasksHostedService(IEnumerable<IStartupTask> startupTasks)
+        {
+            _startupTasks = startupTasks;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            foreach (var t in _startupTasks)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    throw new OperationCanceledException();
+                }
+
+                await t.Execute();
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
