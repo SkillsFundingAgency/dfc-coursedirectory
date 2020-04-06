34ec9f8bfd60a2bbb92e273b137558900197aa6e
James Gunn <james@gunn.io>
2020-04-06 09:43:54 +0100

    Return UserInfo from TestData.CreateUser

--- tests/Dfc.CourseDirectory.WebV2.Tests/TestData/TestData.CreateUser.cs
+++ tests/Dfc.CourseDirectory.WebV2.Tests/TestData/TestData.CreateUser.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.Models;
 using Query = Dfc.CourseDirectory.WebV2.DataStore.Sql.Queries.CreateUser;
 
 namespace Dfc.CourseDirectory.WebV2.Tests
 {
     public partial class TestData
     {
-        public Task CreateUser(string userId, string email, string firstName, string lastName, Guid? providerId) =>
-            WithSqlQueryDispatcher(dispatcher => dispatcher.ExecuteQuery(
+        public async Task<UserInfo> CreateUser(
+            string userId,
+            string email,
+            string firstName,
+            string lastName,
+            Guid? providerId)
+        {
+            await WithSqlQueryDispatcher(dispatcher => dispatcher.ExecuteQuery(
                 new Query()
                 {
                     UserId = userId,
@@ -16,5 +23,14 @@ namespace Dfc.CourseDirectory.WebV2.Tests
                     LastName = lastName,
                     ProviderId = providerId
                 }));
+
+            return new UserInfo()
+            {
+                UserId = userId,
+                Email = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+        }
     }
 }
