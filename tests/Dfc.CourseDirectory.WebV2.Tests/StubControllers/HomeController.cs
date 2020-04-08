a37ad066ab0aa8b092163ebb581a879dc4bc1561
James Gunn <james@gunn.io>
2020-04-08 09:30:20 +0100

    Add apprenticeship & create QA submission

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/StubControllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.Tests.StubControllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index() => Content("Home");
+    }
+}
