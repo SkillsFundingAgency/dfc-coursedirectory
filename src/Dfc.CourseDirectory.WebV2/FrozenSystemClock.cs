ff890e23e4b244240eff2e202d13d30c50d38a79
James Gunn <james@gunn.io>
2020-04-08 09:04:45 +0100

    'Freeze' the current time for the duration of a request
    Ensures all references to 'now' in the scope of a request are the same.

---
+++ src/Dfc.CourseDirectory.WebV2/FrozenSystemClock.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Dfc.CourseDirectory.WebV2
+{
+    public class FrozenSystemClock : IClock
+    {
+        public DateTime UtcNow { get; } = DateTime.UtcNow;
+    }
+}
--- src/Dfc.CourseDirectory.WebV2/SystemClock.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Dfc.CourseDirectory.WebV2
-{
-    public class SystemClock : IClock
-    {
-        public DateTime UtcNow => DateTime.UtcNow;
-    }
-}
