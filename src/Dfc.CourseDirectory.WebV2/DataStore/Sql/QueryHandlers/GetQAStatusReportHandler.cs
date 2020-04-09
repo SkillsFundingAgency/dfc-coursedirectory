0c0918b81e7016556544d67cbe56f86c4d6876d7
MrKevJoy <MrKevinJoy@outlook.com>
2020-04-09 15:23:03 +0100

    tidy up query.

--- src/Dfc.CourseDirectory.WebV2/DataStore/Sql/QueryHandlers/GetQAStatusReportHandler.cs
+++ src/Dfc.CourseDirectory.WebV2/DataStore/Sql/QueryHandlers/GetQAStatusReportHandler.cs
@@ -11,19 +11,19 @@ namespace Dfc.CourseDirectory.WebV2.DataStore.Sql.QueryHandlers
         public async Task<IReadOnlyCollection<GetQAStatusReportResult>> Execute(SqlTransaction transaction, GetQAStatusReport query)
         {
             var sql = @"
-SELECT p.ProviderID, 
-CASE  assessment.Passed WHEN 1 THEN provassess.AssessedOn ELSE null end AS PassedQAOn,
-CASE  assessment.Passed WHEN 0 THEN provassess.AssessedOn ELSE null end AS FailedQAOn,
-reasons.AddedOn as UnableToCompleteOn, 
-reasons.Comments as Notes, 
-reasons.UnableToCompleteReasons, 
-users.Email, 
+SELECT p.ProviderID,
+CASE  submission.Passed WHEN 1 THEN submission.LastAssessedOn ELSE null end AS PassedQAOn,
+CASE  submission.Passed WHEN 0 THEN submission.LastAssessedOn ELSE null end AS FailedQAOn,
+reasons.AddedOn as UnableToCompleteOn,
+reasons.Comments as Notes,
+CASE (p.ApprenticeshipQAStatus & 32) WHEN 0 THEN NULL ELSE reasons.UnableToCompleteReasons END AS UnableToCompleteReasons,
+users.Email,
 p.ApprenticeshipQAStatus AS QAStatus
 FROM [pttcd].[Providers] p
 LEFT JOIN (
-    SELECT ProviderId, MAX(ApprenticeshipQASubmissionId) ApprenticeshipQASubmissionId, SubmittedByUserId
+    SELECT ProviderId, MAX(ApprenticeshipQASubmissionId) ApprenticeshipQASubmissionId
     FROM Pttcd.ApprenticeshipQASubmissions
-    GROUP BY ProviderId,SubmittedByUserId
+    GROUP BY ProviderId
 ) LatestSubmissions ON p.ProviderId = LatestSubmissions.ProviderId
 LEFT JOIN (
     SELECT ProviderId, MAX(ApprenticeshipQAUnableToCompleteId) ApprenticeshipQAUnableToCompleteId
@@ -31,14 +31,8 @@ LEFT JOIN (
     GROUP BY ProviderId
 ) LatestUnableToComplete ON p.ProviderId = LatestUnableToComplete.ProviderId
 LEFT JOIN Pttcd.ApprenticeshipQAUnableToCompleteInfo reasons on reasons.ApprenticeshipQAUnableToCompleteId = LatestUnableToComplete.ApprenticeshipQAUnableToCompleteId
-LEFT JOIN (
-    SELECT ApprenticeshipQASubmissionId, MAX(ApprenticeshipQASubmissionProviderAssessmentsId) ApprenticeshipQASubmissionProviderAssessmentsId
-    FROM Pttcd.ApprenticeshipQASubmissionProviderAssessments
-    GROUP BY ApprenticeshipQASubmissionId
-) LatestAssessment ON LatestAssessment.ApprenticeshipQASubmissionId = LatestSubmissions.ApprenticeshipQASubmissionId
-LEFT JOIN [Pttcd].ApprenticeshipQASubmissions assessment on assessment.ApprenticeshipQASubmissionId = LatestAssessment.ApprenticeshipQASubmissionId
-LEFT JOIN [Pttcd].ApprenticeshipQASubmissionProviderAssessments provassess on provassess.ApprenticeshipQASubmissionProviderAssessmentsId=LatestAssessment.ApprenticeshipQASubmissionProviderAssessmentsId
-LEFT JOIN [Pttcd].[Users] users on users.UserId=LatestSubmissions.SubmittedByUserId
+LEFT JOIN [Pttcd].ApprenticeshipQASubmissions submission on submission.ApprenticeshipQASubmissionId = LatestSubmissions.ApprenticeshipQASubmissionId
+LEFT JOIN [Pttcd].[Users] users on users.UserId=submission.SubmittedByUserId
 WHERE p.ApprenticeshipQAStatus <> 1";
 
             return (await transaction.Connection.QueryAsync<GetQAStatusReportResult>(sql, transaction: transaction)).AsList();
