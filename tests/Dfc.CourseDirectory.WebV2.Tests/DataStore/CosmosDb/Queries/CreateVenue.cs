52139eea84686d51c661eade31ef51935bb4a96b
James Gunn <james@gunn.io>
2020-04-13 18:14:00 +0100

    Add TestData method for creating a Venue

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/DataStore/CosmosDb/Queries/CreateVenue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.WebV2.Tests.DataStore.CosmosDb.Queries
+{
+    public class CreateVenue : ICosmosDbQuery<Success>
+    {
+        public Guid VenueId { get; set; }
+        public int ProviderUkprn { get; set; }
+        public string VenueName { get; set; }
+    }
+}
---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/TestData/TestData.CreateVenue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.DataStore.CosmosDb.Queries;
+using Dfc.CourseDirectory.WebV2.Tests.DataStore.CosmosDb.Queries;
+
+namespace Dfc.CourseDirectory.WebV2.Tests
+{
+    public partial class TestData
+    {
+        public async Task<Guid> CreateVenue(
+            Guid providerId,
+            string venueName = "Test Venue")
+        {
+            var provider = await _cosmosDbQueryDispatcher.ExecuteQuery(new GetProviderById()
+            {
+                ProviderId = providerId
+            });
+
+            if (provider == null)
+            {
+                throw new ArgumentException("Provider does not exist.", nameof(providerId));
+            }
+
+            var venueId = Guid.NewGuid();
+
+            await _cosmosDbQueryDispatcher.ExecuteQuery(new CreateVenue()
+            {
+                VenueId = venueId,
+                ProviderUkprn = provider.Ukprn,
+                VenueName = venueName
+            });
+
+            return venueId;
+        }
+    }
+}
