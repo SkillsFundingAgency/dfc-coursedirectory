7be13618ed0cf4986245fbf6cf0a60ce746bec1d
James Gunn <james@gunn.io>
2020-04-20 14:34:50 +0100

    Fix Database test collection - cannot be in different assembly to tests

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/DatabaseCollectionFixture.cs
@@ -0,0 +1,19 @@
+﻿using Dfc.CourseDirectory.Testing;
+using Xunit;
+
+namespace Dfc.CourseDirectory.WebV2.Tests
+{
+    [CollectionDefinition("Database")]
+    public class DatabaseCollectionFixture : ICollectionFixture<DatabaseTestBaseFixture>
+    {
+    }
+
+    [Collection("Database")]
+    public abstract class DatabaseTestBase : Testing.DatabaseTestBase
+    {
+        protected DatabaseTestBase(DatabaseTestBaseFixture fixture)
+            : base(fixture)
+        {
+        }
+    }
+}
