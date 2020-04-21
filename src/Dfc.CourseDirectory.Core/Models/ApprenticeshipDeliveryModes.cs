7090f13c7f2bbde284154d586a57a93c5e1242e9
James Gunn <james@gunn.io>
2020-04-21 09:09:59 +0100

    Add ToDisplayName extension method on ApprenticeshipDeliveryModes

--- src/Dfc.CourseDirectory.Core/Models/ApprenticeshipDeliveryModes.cs
+++ src/Dfc.CourseDirectory.Core/Models/ApprenticeshipDeliveryModes.cs
@@ -10,4 +10,17 @@ namespace Dfc.CourseDirectory.Core.Models
         DayRelease = 2,
         BlockRelease = 4
     }
+
+    public static class ApprenticeshipDeliveryModesExtensions
+    {
+        public static string ToDisplayName(this ApprenticeshipDeliveryModes apprenticeshipDeliveryModes) =>
+            apprenticeshipDeliveryModes switch
+            {
+                ApprenticeshipDeliveryModes.None => string.Empty,
+                ApprenticeshipDeliveryModes.EmployerAddress => "Employer Address",
+                ApprenticeshipDeliveryModes.DayRelease => "Day Release",
+                ApprenticeshipDeliveryModes.BlockRelease => "Block Release",
+                _ => throw new NotSupportedException($"Unknown value: '{apprenticeshipDeliveryModes}'.")
+            };
+    }
 }
