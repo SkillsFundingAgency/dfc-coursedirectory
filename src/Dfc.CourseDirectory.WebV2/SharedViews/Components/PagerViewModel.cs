00ce05748229ff6a195e8605437caf225c5871f7
MTSarwar <56112569+MTSarwar@users.noreply.github.com>
2020-05-06 12:07:13 +0100

    PTCD-295

---
+++ src/Dfc.CourseDirectory.WebV2/SharedViews/Components/PagerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dfc.CourseDirectory.WebV2.SharedViews.Components
+{
+    public class PagerViewModel
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public Func<int, string> GetPageUrl { get; set; }
+        public IEnumerable<int> PageNumbers { get; set; }
+    }
+}
