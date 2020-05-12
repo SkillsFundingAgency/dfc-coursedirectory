112c4321b123f1691c2463a96c218a4542c1c6c0
James Gunn <james@gunn.io>
2020-05-12 10:45:33 +0100

    Add Database test collection to Core.Tests

---
+++ tests/Dfc.CourseDirectory.Core.Tests/DatabaseTestBase.cs
@@ -0,0 +1,19 @@
+﻿using Dfc.CourseDirectory.Testing;
+using Xunit;
+
+namespace Dfc.CourseDirectory.Core.Tests
+{
+    [CollectionDefinition("Database")]
+    public class DatabaseTestCollection : ICollectionFixture<DatabaseTestBaseFixture>
+    {
+    }
+
+    [Collection("Database")]
+    public abstract class DatabaseTestBase : Testing.DatabaseTestBase
+    {
+        public DatabaseTestBase(DatabaseTestBaseFixture fixture)
+            : base(fixture)
+        {
+        }
+    }
+}
