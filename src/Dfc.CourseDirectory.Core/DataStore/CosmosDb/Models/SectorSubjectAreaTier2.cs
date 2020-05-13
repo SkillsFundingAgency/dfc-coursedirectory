959c630ff14f26bdb9fc1641e98b6bcd0a5601ee
James Gunn <james@gunn.io>
2020-05-13 15:00:45 +0100

    Update LARS data in Cosmos (PTCD-119)

--- src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/Models/SectorSubjectAreaTier2.cs
+++ src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/Models/SectorSubjectAreaTier2.cs
@@ -5,19 +5,15 @@ using Newtonsoft.Json.Linq;
 
 namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb.Models
 {
-    public class Framework
+    public class SectorSubjectAreaTier2
     {
         [JsonProperty("id")]
         public Guid Id { get; set; }
-        public int FrameworkCode { get; set; }
-        public int ProgType { get; set; }
-        public int PathwayCode { get; set; }
-        public string NasTitle { get; set; }
-        public int RecordStatusId { get; set; }
+        public decimal SectorSubjectAreaTier2Id { get; set; }
+        public string SectorSubjectAreaTier2Desc { get; set; }
+        public string SectorSubjectAreaTier2Desc2 { get; set; }
         public DateTime EffectiveFrom { get; set; }
-        public DateTime EffectiveTo { get; set; }
-        public decimal SectorSubjectAreaTier1 { get; set; }
-        public decimal SectorSubjectAreaTier2 { get; set; }
+        public DateTime? EffectiveTo { get; set; }
         public DateTime CreatedDateTimeUtc { get; set; }
         public DateTime ModifiedDateTimeUtc { get; set; }
         [JsonExtensionData]
---
+++ src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/Queries/UpsertStandards.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries
+{
+    public class UpsertStandards : ICosmosDbQuery<None>
+    {
+        public IEnumerable<UpsertStandardsRecord> Records { get; set; }
+    }
+
+    public class UpsertStandardsRecord
+    {
+        public int StandardCode { get; set; }
+        public int Version { get; set; }
+        public string StandardName { get; set; }
+        public string NotionalEndLevel { get; set; }
+        public string OtherBodyApprovalRequired { get; set; }
+    }
+}
