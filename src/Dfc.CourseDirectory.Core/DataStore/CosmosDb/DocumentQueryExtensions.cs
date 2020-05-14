873e674ada759b3c04d4e8fdef31d475a5d04353
James Gunn <james@gunn.io>
2020-05-14 14:48:57 +0100

    Add a method for processing every item from a Cosmos query

--- src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/DocumentQueryExtensions.cs
+++ src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/DocumentQueryExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents.Linq;
 
@@ -18,6 +19,19 @@ namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb
             }
 
             return results;
+        }
+
+        public static async Task ProcessAll<T>(this IDocumentQuery<T> query, Func<T, Task> process)
+        {
+            while (query.HasMoreResults)
+            {
+                var response = await query.ExecuteNextAsync<T>();
+
+                foreach (var doc in response)
+                {
+                    await process(doc);
+                }
+            }
         }
     }
 }
