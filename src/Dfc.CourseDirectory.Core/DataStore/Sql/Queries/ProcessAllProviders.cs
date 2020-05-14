d0fe120cd4e2a6a559f931798dbc6fd13e76bd37
James Gunn <james@gunn.io>
2020-05-14 15:26:58 +0100

    Add function to do a full provider sync from Cosmos to SQL

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/Queries/ProcessAllProviders.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Models;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.Queries
+{
+    public class ProcessAllProviders : ICosmosDbQuery<None>
+    {
+        public Func<Provider, Task> Process { get; set; }
+    }
+}
