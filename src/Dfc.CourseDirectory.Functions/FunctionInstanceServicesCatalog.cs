f95a350af69442b41bf7b0b834033a1d0a694af1
James Gunn <james@gunn.io>
2020-05-18 11:55:16 +0100

    Add a mechanism for looking up function-scoped services from filters

---
+++ src/Dfc.CourseDirectory.Functions/FunctionInstanceServicesCatalog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Host.Executors;
+
+namespace Dfc.CourseDirectory.Functions
+{
+    public class FunctionInstanceServicesCatalog : IJobActivatorEx
+    {
+        private readonly ConditionalWeakTable<IFunctionInstanceEx, IServiceProvider> _instanceServices;
+        private readonly IJobActivatorEx _innerActivator;
+
+        public FunctionInstanceServicesCatalog(IJobActivator innerActivator)
+        {
+            _instanceServices = new ConditionalWeakTable<IFunctionInstanceEx, IServiceProvider>();
+            _innerActivator = (IJobActivatorEx)innerActivator;
+        }
+
+        public IServiceProvider GetFunctionServices(Guid instanceId) =>
+            _instanceServices.SingleOrDefault(k => k.Key.Id == instanceId).Value;
+
+        T IJobActivatorEx.CreateInstance<T>(IFunctionInstanceEx functionInstance)
+        {
+            _instanceServices.Add(functionInstance, functionInstance.InstanceServices);
+            return _innerActivator.CreateInstance<T>(functionInstance);
+        }
+
+        T IJobActivator.CreateInstance<T>() => _innerActivator.CreateInstance<T>();
+    }
+}
