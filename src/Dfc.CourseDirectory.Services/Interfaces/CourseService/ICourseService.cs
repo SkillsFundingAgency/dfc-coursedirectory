845b27bfdd080ddac747fa553e38af7a66d90d5b
MTSarwar <56112569+MTSarwar@users.noreply.github.com>
2020-05-19 15:38:24 +0100

    Archive all courses and course run passing current status null so ins… (#1485)
    * Archive all courses and course run passing current status null so instead of only live courses archive all
    
    * archive courses  before bulkupload

--- src/Dfc.CourseDirectory.Services/Interfaces/CourseService/ICourseService.cs
+++ src/Dfc.CourseDirectory.Services/Interfaces/CourseService/ICourseService.cs
@@ -20,7 +20,7 @@ namespace Dfc.CourseDirectory.Services.Interfaces.CourseService
         Task<IResult<ICourse>> UpdateCourseAsync(ICourse course);
         Task<IResult<ICourse>> GetCourseByIdAsync(IGetCourseByIdCriteria criteria);
         Task<IResult> ArchiveProviderLiveCourses(int? UKPRN);
-        Task<IResult> ChangeCourseRunStatusesForUKPRNSelection(int UKPRN, int CurrentStatus, int StatusToBeChangedTo);
+        Task<IResult> ChangeCourseRunStatusesForUKPRNSelection(int UKPRN, int? CurrentStatus, int StatusToBeChangedTo);
         Task<IResult> ArchiveCourseRunsByUKPRN(int UKPRN);
         SelectRegionModel GetRegions();
         Task<IResult<ICourseSearchResult>> GetCoursesByLevelForUKPRNAsync(ICourseSearchCriteria criteria);
