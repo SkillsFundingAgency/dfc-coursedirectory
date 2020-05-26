76f1b7674c23532aa3e74ace2d40b46c9d5929f3
James Gunn <james@gunn.io>
2020-05-26 16:58:25 +0100

    Archive apprenticeships and courses for a deactivated provider (PTCD-139)

---
+++ src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/QueryHandlers/ArchiveCoursesForProviderHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries;
+using Microsoft.Azure.Documents.Client;
+
+namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb.QueryHandlers
+{
+    public class ArchiveCoursesForProviderHandler : ICosmosDbQueryHandler<ArchiveCoursesForProvider, int>
+    {
+        public async Task<int> Execute(DocumentClient client, Configuration configuration, ArchiveCoursesForProvider request)
+        {
+            var sprocUri = UriFactory.CreateStoredProcedureUri(
+                configuration.DatabaseId,
+                configuration.CoursesCollectionName,
+                "ArchiveCoursesForProvider");
+
+            var result = await client.ExecuteStoredProcedureAsync<StoredProcResponse>(
+                sprocUri,
+                new RequestOptions()
+                {
+                    PartitionKey = new Microsoft.Azure.Documents.PartitionKey(request.Ukprn)
+                },
+                request.Ukprn);
+
+            return result.Response.Updated;
+        }
+
+        private class StoredProcResponse
+        {
+            public int Updated { get; set; }
+        }
+    }
+}
---
+++ tests/Dfc.CourseDirectory.Testing/DataStore/CosmosDb/QueryHandlers/ArchiveApprenticeshipsForProviderHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries;
+
+namespace Dfc.CourseDirectory.Testing.DataStore.CosmosDb.QueryHandlers
+{
+    public class ArchiveApprenticeshipsForProviderHandler : ICosmosDbQueryHandler<ArchiveApprenticeshipsForProvider, int>
+    {
+        public int Execute(InMemoryDocumentStore inMemoryDocumentStore, ArchiveApprenticeshipsForProvider request)
+        {
+            var providerApprenticeships = inMemoryDocumentStore.Apprenticeships.All
+                .Where(a => a.ProviderUKPRN == request.Ukprn && a.RecordStatus != 4);
+
+            var updated = 0;
+
+            foreach (var app in providerApprenticeships)
+            {
+                app.RecordStatus = 4;
+                
+                foreach (var location in app.ApprenticeshipLocations)
+                {
+                    location.RecordStatus = 4;
+                }
+
+                inMemoryDocumentStore.Apprenticeships.Save(app);
+
+                updated++;
+            }
+
+            return updated;
+        }
+    }
+}
