6ea6be87d508bb5b5d6f685721db393dfef49783
James Gunn <james@gunn.io>
2020-05-26 09:06:30 +0100

    Add GetAllProviderData method to UkrlpService

--- src/Dfc.CourseDirectory.Core/ReferenceData/Ukrlp/IUkrlpService.cs
+++ src/Dfc.CourseDirectory.Core/ReferenceData/Ukrlp/IUkrlpService.cs
@@ -1,10 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using UkrlpService;
 
 namespace Dfc.CourseDirectory.Core.ReferenceData.Ukrlp
 {
     public interface IUkrlpService
     {
+       Task<IReadOnlyCollection<ProviderRecordStructure>> GetAllProviderData();
        Task<ProviderRecordStructure> GetProviderData(int ukprn);
     }
 }
