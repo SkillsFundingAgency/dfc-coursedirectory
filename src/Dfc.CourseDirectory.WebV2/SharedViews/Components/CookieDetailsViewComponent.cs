75c9e9b121a94471cfaf93cffdd29498804ab58b
MrKevJoy <MrKevinJoy@outlook.com>
2020-06-01 14:40:34 +0100

    remove view component.

--- src/Dfc.CourseDirectory.WebV2/SharedViews/Components/CookieDetailsViewComponent.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Dfc.CourseDirectory.WebV2.SharedViews.Components
-{
-    public class CookieDetailsViewComponent : ViewComponent
-    {
-        public IViewComponentResult Invoke() =>  View("~/SharedViews/Components/CookieDetails.cshtml");
-    }
-}
