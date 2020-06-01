727e2c7db774e134d499fbefb752dd6f0188c3e8
MrKevJoy <MrKevinJoy@outlook.com>
2020-06-01 14:19:53 +0100

    ptcd-325 #add cookie details page.

---
+++ src/Dfc.CourseDirectory.WebV2/SharedViews/Components/CookieDetailsViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.SharedViews.Components
+{
+    public class CookieDetailsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() =>  View("~/SharedViews/Components/CookieDetails.cshtml");
+    }
+}
