7a6bf25e1e7cceabbfad6c53bcae917c77b203cc
James Gunn <james@gunn.io>
2020-06-01 11:19:05 +0100

    Add missing fields to Cosmos Standards update (PTCD-393)

--- src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/Queries/UpsertStandards.cs
+++ src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/Queries/UpsertStandards.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using OneOf.Types;
 
 namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries
@@ -13,7 +14,13 @@ namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries
         public int StandardCode { get; set; }
         public int Version { get; set; }
         public string StandardName { get; set; }
+        public string StandardSectorCode { get; set; }
         public string NotionalEndLevel { get; set; }
+        public DateTime? EffectiveFrom { get; set; }
+        public DateTime? EffectiveTo { get; set; }
+        public string URLLink { get; set; }
+        public decimal? SectorSubjectAreaTier1 { get; set; }
+        public decimal? SectorSubjectAreaTier2 { get; set; }
         public string OtherBodyApprovalRequired { get; set; }
     }
 }
