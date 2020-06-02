14fdf7bc642229f318b12f7a41f23e770b372e3f
MrKevJoy <60096576+MrKevJoy@users.noreply.github.com>
2020-06-02 08:59:28 +0100

    ptcd-325 #cookie details page (#1501)
    * ptcd-325 #add cookie details page.
    
    * typo
    
    * remove view component.
    
    Co-authored-by: MrKevJoy <MrKevinJoy@outlook.com>

---
+++ src/Dfc.CourseDirectory.WebV2/Features/Cookies/CookiesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.Features.Cookies
+{
+    [Route("cookies")]
+    public class CookiesController : Controller
+    {
+        [HttpGet("details")]
+        [AllowAnonymous]
+        public IActionResult CookieDetails()
+        {
+            return View();
+        }
+    }
+}
