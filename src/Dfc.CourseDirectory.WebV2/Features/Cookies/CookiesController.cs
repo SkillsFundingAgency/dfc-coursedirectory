9360d8313336d9be194724d5e28eb7428c7985a8
James Gunn <james@gunn.io>
2020-06-03 15:23:19 +0100

    Add Cookie Settings page (PTCD-322)

--- src/Dfc.CourseDirectory.WebV2/Features/Cookies/CookiesController.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/Cookies/CookiesController.cs
@@ -1,14 +1,23 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dfc.CourseDirectory.WebV2.Features.Cookies
 {
     [Route("cookies")]
+    [AllowAnonymous]
     public class CookiesController : Controller
     {
         private const string CookieName = "CookiePolicy";
+        private readonly IMediator _mediator;
+
+        public CookiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
         [HttpGet("details")]
-        [AllowAnonymous]
         public IActionResult CookieDetails()
         {
             return View();
@@ -23,5 +32,21 @@ namespace Dfc.CourseDirectory.WebV2.Features.Cookies
 
             return RedirectToAction("Index", "Home");
         }
+
+        [HttpGet("settings")]
+        public async Task<IActionResult> Settings() => await _mediator.SendAndMapResponse(
+            new Settings.Query(),
+            command => View(command));
+
+        [HttpPost("settings")]
+        public async Task<IActionResult> Settings(Settings.Command command) => await _mediator.SendAndMapResponse(
+            command,
+            response => response.Match<IActionResult>(
+                errors => this.ViewFromErrors(errors),
+                success =>
+                {
+                    TempData["SavedCookieSettings"] = true;
+                    return RedirectToAction(nameof(Settings));
+                }));
     }
 }
---
+++ src/Dfc.CourseDirectory.WebV2/Features/Cookies/Settings.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.Validation;
+using Dfc.CourseDirectory.WebV2.Cookies;
+using FluentValidation;
+using MediatR;
+using OneOf;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.WebV2.Features.Cookies.Settings
+{
+    public class Query : IRequest<Command>
+    {
+    }
+
+    public class Command : IRequest<OneOf<ModelWithErrors<Command>, Success>>
+    {
+        public bool? AllowAnalyticsCookies { get; set; }
+    }
+
+    public class Handler :
+        IRequestHandler<Query, Command>,
+        IRequestHandler<Command, OneOf<ModelWithErrors<Command>, Success>>
+    {
+        private readonly ICookieSettingsProvider _cookiePreferencesProvider;
+
+        public Handler(ICookieSettingsProvider cookiePreferencesProvider)
+        {
+            _cookiePreferencesProvider = cookiePreferencesProvider;
+        }
+
+        public Task<Command> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var currentUserPreferences = _cookiePreferencesProvider.GetPreferencesForCurrentUser();
+
+            var command = new Command()
+            {
+                AllowAnalyticsCookies = currentUserPreferences?.AllowAnalyticsCookies
+            };
+
+            return Task.FromResult(command);
+        }
+
+        public async Task<OneOf<ModelWithErrors<Command>, Success>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var validator = new CommandValidator();
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (!validationResult.IsValid)
+            {
+                return new ModelWithErrors<Command>(request, validationResult);
+            }
+
+            var preferences = new CookieSettings()
+            {
+                AllowAnalyticsCookies = request.AllowAnalyticsCookies.Value
+            };
+            _cookiePreferencesProvider.SetPreferencesForCurrentUser(preferences);
+
+            return new Success();
+        }
+
+        private class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(c => c.AllowAnalyticsCookies)
+                    .NotNull()
+                    .WithMessage("Select if you want to use cookies that measure website use");
+            }
+        }
+    }
+}
