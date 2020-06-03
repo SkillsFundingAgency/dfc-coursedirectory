4952f392c287dfda38fd2b54861fd39421e43352
James Gunn <james@gunn.io>
2020-06-03 15:54:25 +0100

    Refactor CookieBanner
    - Use ICookieSettingsProvider for determining whether to show banner.
    - Remove excess CSS classes & use tag helpers
    - Add tests

--- src/Dfc.CourseDirectory.WebV2/Features/Cookies/CookieBannerViewComponent.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/Cookies/CookieBannerViewComponent.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dfc.CourseDirectory.WebV2.Cookies;
+using Dfc.CourseDirectory.WebV2.Features.Cookies;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dfc.CourseDirectory.WebV2.SharedViews.Components
 {
     public class CookieBannerViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() => View("~/Features/Cookies/CookieBanner.cshtml");
+        private readonly ICookieSettingsProvider _cookieSettingsProvider;
+
+        public CookieBannerViewComponent(ICookieSettingsProvider cookieSettingsProvider)
+        {
+            _cookieSettingsProvider = cookieSettingsProvider;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var viewModel = new CookieBannerViewModel()
+            {
+                ShowBanner = _cookieSettingsProvider.GetPreferencesForCurrentUser() == null
+            };
+
+            return View("~/Features/Cookies/CookieBanner.cshtml", viewModel);
+        }
     }
 }
