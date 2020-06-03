2af0f04f7e7550ce846d82d455f81bc2b4cf0355
MrKevJoy <MrKevinJoy@outlook.com>
2020-06-03 11:56:02 +0100

    ptcd-111 #added cookie banner.

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/FeatureTests/Cookies/AcceptCookiesTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dfc.CourseDirectory.WebV2.Tests.FeatureTests.Cookies
+{
+    public class AcceptCookiesTest : MvcTestBase
+    {
+        public AcceptCookiesTest(CourseDirectoryApplicationFactory factory)
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Post_ReturnsOk()
+        {
+            // Arrange
+
+            // Act
+            var response = await HttpClient.PostAsync("cookies/acceptcookies", null);
+            var doc = await response.GetDocument();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.Null(doc.GetElementById("cookiesform"));
+        }
+    }
+}
