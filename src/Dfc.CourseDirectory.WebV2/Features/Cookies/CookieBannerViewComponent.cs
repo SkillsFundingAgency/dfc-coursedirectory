96f8afb00a5295517d53dcfd8a88698c26fa1ffa
James Gunn <james@gunn.io>
2020-06-03 15:31:19 +0100

    Move CookieBanner into Cookies Feature namespace

---
+++ src/Dfc.CourseDirectory.WebV2/Features/Cookies/CookieBannerViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.SharedViews.Components
+{
+    public class CookieBannerViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View("~/Features/Cookies/CookieBanner.cshtml");
+    }
+}
