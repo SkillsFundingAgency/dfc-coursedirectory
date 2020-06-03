77aafa2dfabda9917405b3170484c32cce4661bd
MrKevJoy <60096576+MrKevJoy@users.noreply.github.com>
2020-06-03 13:40:30 +0100

    ptcd-111 #added cookie banner. (#1504)
    * ptcd-111 #added cookie banner.
    
    * revert guid
    
    * Upgrade GFA package
    
    * Add methods for looking up elements by data-testid attribute
    
    * Beginnings of testing UI elements by data-testid instead of class
    
    * ptcd-111 #added cookie banner.
    
    * revert guid
    
    Co-authored-by: MrKevJoy <MrKevinJoy@outlook.com>
    Co-authored-by: James Gunn <james@gunn.io>

--- src/Dfc.CourseDirectory.WebV2/Features/Cookies/CookiesController.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/Cookies/CookiesController.cs
@@ -6,11 +6,22 @@ namespace Dfc.CourseDirectory.WebV2.Features.Cookies
     [Route("cookies")]
     public class CookiesController : Controller
     {
+        private const string CookieName = "CookiePolicy";
         [HttpGet("details")]
         [AllowAnonymous]
         public IActionResult CookieDetails()
         {
             return View();
+        }
+
+        [HttpPost("acceptcookies")]
+        [AllowAnonymous]
+        public IActionResult AcceptCookies()
+        {
+            if(Request.Cookies[CookieName] == null)
+                Response.Cookies.Append(CookieName, "true");
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
