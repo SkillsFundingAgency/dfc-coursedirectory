f9da11924d4d74f4ef8aeb6929d6bfb79e19717b
James Gunn <james@gunn.io>
2020-06-03 15:21:36 +0100

    Add abstractions for settings & retrieving a user's cookie preferences

---
+++ src/Dfc.CourseDirectory.WebV2/Cookies/ICookieSettingsProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Dfc.CourseDirectory.WebV2.Cookies
+{
+    public interface ICookieSettingsProvider
+    {
+        CookieSettings GetPreferencesForCurrentUser();
+        void SetPreferencesForCurrentUser(CookieSettings preferences);
+    }
+}
