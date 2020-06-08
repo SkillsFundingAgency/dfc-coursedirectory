95f55c61d5e07a016ded39b9a27ea333964f57a5
James Gunn <james@gunn.io>
2020-06-08 20:17:13 +0100

    Add BackLinkTagHelper

---
+++ src/Dfc.CourseDirectory.WebV2/TagHelpers/BackLinkTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using GovUk.Frontend.AspNetCore;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Dfc.CourseDirectory.WebV2.TagHelpers
+{
+    [HtmlTargetElement("pttcd-back-link")]
+    public class BackLinkTagHelper : TagHelper
+    {
+        private readonly IGovUkHtmlGenerator _generator;
+
+        public BackLinkTagHelper(IGovUkHtmlGenerator generator)
+        {
+            _generator = generator;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var tagBuilder = _generator.GenerateBackLink(
+                href: "#",
+                content: new HtmlString("Back"),
+                attributes: new Dictionary<string, string>()
+                {
+                    { "class", "needs-js pttcd-c-back-link" },
+                    { "aria-hidden", "" },
+                    { "onclick", "window.history.back()" },
+                });
+
+            tagBuilder.Attributes.Remove("href");
+
+            output.TagName = tagBuilder.TagName;
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.MergeAttributes(tagBuilder);
+            output.Content.SetHtmlContent(tagBuilder.InnerHtml);
+        }
+    }
+}
