312045e000d12d1c0c6eb476f042986a499eff6c
James Gunn <james@gunn.io>
2020-06-15 16:55:03 +0100

    Update SQL schema for Providers (PTCD-366)

---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Tables/ProviderContacts.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [Pttcd].[ProviderContacts]
+(
+	[ProviderContactId] BIGINT IDENTITY NOT NULL CONSTRAINT PK_ProviderContacts PRIMARY KEY,
+	[ProviderId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT FK_ProviderContacts_Provider FOREIGN KEY REFERENCES [Pttcd].[Providers] (ProviderId),
+	[ContactType] CHAR,
+	[ContactRole] NVARCHAR(MAX),
+	[AddressSaonDescription] NVARCHAR(MAX),
+	[AddressPaonDescription] NVARCHAR(MAX),
+	[AddressStreetDescription] NVARCHAR(MAX),
+	[AddressLocality] NVARCHAR(MAX),
+	[AddressItems] NVARCHAR(MAX),
+	[AddressPostTown] NVARCHAR(MAX),
+	[AddressPostcode] NVARCHAR(MAX),
+	[PersonalDetailsPersonNameTitle] NVARCHAR(MAX),
+	[PersonalDetailsPersonNameGivenName] NVARCHAR(MAX),
+	[PersonalDetailsPersonNameFamilyName] NVARCHAR(MAX),
+)
