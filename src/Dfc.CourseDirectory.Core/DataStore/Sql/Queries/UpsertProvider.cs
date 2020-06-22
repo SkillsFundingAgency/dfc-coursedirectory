d345e65190f7ea55eabd46038c1ca51b2b3d094d
James Gunn <james@gunn.io>
2020-06-22 16:06:55 +0100

    Sync Provider data to SQL (PTCD-375)

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/Queries/UpsertProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Dfc.CourseDirectory.Core.Models;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.Queries
+{
+    public class UpsertProvider : ISqlQuery<None>
+    {
+        public Guid ProviderId { get; set; }
+        public int Ukprn { get; set; }
+        public ProviderStatus ProviderStatus { get; set; }
+        public ProviderType ProviderType { get; set; }
+        public string ProviderName { get; set; }
+        public string UkrlpProviderStatusDescription { get; set; }
+        public string MarketingInformation { get; set; }
+        public string CourseDirectoryName { get; set; }
+        public string TradingName { get; set; }
+        public string Alias { get; set; }
+        public IEnumerable<UpsertProviderContact> Contacts { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+
+    public class UpsertProviderContact
+    {
+        public string ContactType { get; set; }
+        public string ContactRole { get; set; }
+        public string AddressSaonDescription { get; set; }
+        public string AddressPaonDescription { get; set; }
+        public string AddressStreetDescription { get; set; }
+        public string AddressLocality { get; set; }
+        public string AddressItems { get; set; }
+        public string AddressPostTown { get; set; }
+        public string AddressPostcode { get; set; }
+        public string PersonalDetailsPersonNameTitle { get; set; }
+        public string PersonalDetailsPersonNameGivenName { get; set; }
+        public string PersonalDetailsPersonNameFamilyName { get; set; }
+    }
+}
---
+++ src/Dfc.CourseDirectory.Core/SqlDataSync.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Models;
+using Dfc.CourseDirectory.Core.DataStore.Sql;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dfc.CourseDirectory.Core
+{
+    public class SqlDataSync
+    {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+
+        public SqlDataSync(IServiceScopeFactory serviceScopeFactory)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+        }
+
+        public Task SyncProvider(Provider provider) => WithSqlDispatcher(dispatcher =>
+            dispatcher.ExecuteQuery(new UpsertProvider()
+            {
+                ProviderId = provider.Id,
+                Ukprn = provider.Ukprn,
+                ProviderStatus = provider.Status,
+                ProviderType = provider.ProviderType,
+                ProviderName = provider.ProviderName,
+                UkrlpProviderStatusDescription = provider.ProviderStatus,
+                MarketingInformation = provider.MarketingInformation,
+                CourseDirectoryName = provider.CourseDirectoryName,
+                TradingName = provider.TradingName,
+                Alias = provider.Alias,
+                UpdatedOn = provider.DateUpdated,
+                UpdatedBy = provider.UpdatedBy,
+                Contacts = provider.ProviderContact.Select(c => new UpsertProviderContact()
+                {
+                    ContactType = c.ContactType,
+                    ContactRole = c.ContactRole,
+                    AddressSaonDescription = c.ContactAddress?.SAON?.Description,
+                    AddressPaonDescription = c.ContactAddress?.PAON?.Description,
+                    AddressStreetDescription = c.ContactAddress?.StreetDescription,
+                    AddressLocality = c.ContactAddress?.Locality,
+                    AddressItems = string.Join(" ", c.ContactAddress?.Items ?? Array.Empty<string>()),
+                    AddressPostTown = c.ContactAddress?.PostTown,
+                    AddressPostcode = c.ContactAddress?.PostCode,
+                    PersonalDetailsPersonNameTitle = string.Join(" ", c.ContactPersonalDetails?.PersonNameTitle ?? Array.Empty<string>()),
+                    PersonalDetailsPersonNameGivenName = string.Join(" ", c.ContactPersonalDetails?.PersonGivenName ?? Array.Empty<string>()),
+                    PersonalDetailsPersonNameFamilyName = c.ContactPersonalDetails?.PersonFamilyName
+                })
+            }));
+
+        private async Task WithSqlDispatcher(Func<ISqlQueryDispatcher, Task> action)
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var sqlDispatcher = scope.ServiceProvider.GetRequiredService<ISqlQueryDispatcher>();
+
+                await action(sqlDispatcher);
+
+                sqlDispatcher.Transaction.Commit();
+            }
+        }
+    }
+}
