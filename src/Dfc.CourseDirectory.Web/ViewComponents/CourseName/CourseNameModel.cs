b4f645f4dc9621cefaaa3e86a6141c032e56fc67
Tim Abell <tim@timwise.co.uk>
2020-06-29 15:11:33 +0100

    PTCD-410 - Set course name hint in course details
    * This required using HtmlString to allow raw html in the HintText
      property of the model to be able to include a link.
    * Remove trailing whitespace in code
    
    As per prototype and change document.

--- src/Dfc.CourseDirectory.Web/ViewComponents/CourseName/CourseNameModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewComponents/CourseName/CourseNameModel.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Html;
 
 namespace Dfc.CourseDirectory.Web.ViewComponents.CourseName
 {
     public class CourseNameModel
     {
-             public string CourseName { get; set; }
+        public string CourseName { get; set; }
         public string LabelText { get; set; }
-        public string HintText { get; set; }
+        public HtmlString HintText { get; set; }
         public string AriaDescribedBy { get; set; }
     }
 }
