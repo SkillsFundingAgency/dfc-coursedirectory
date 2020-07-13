520b2c5a68413baeadf65bffc6653cdc78995a35
James Gunn <james@gunn.io>
2020-07-13 12:01:46 +0100

    Add Venues SQL schema (PTCD-374)

---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Tables/Venues.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [Pttcd].[Venues]
+(
+	[VenueId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT [PK_Venues] PRIMARY KEY,
+	[VenueStatus] TINYINT,
+	[CreatedOn] DATETIME,
+	[CreatedBy] NVARCHAR(MAX),
+	[UpdatedOn] DATETIME,
+	[UpdatedBy] NVARCHAR(MAX),
+	[VenueName] NVARCHAR(MAX),
+	[ProviderUkprn] INT,
+	[TribalProviderId] INT,
+	[TribalVenueId] INT,
+	[ProviderVenueRef] NVARCHAR(MAX),
+	[AddressLine1] NVARCHAR(MAX),
+	[AddressLine2] NVARCHAR(MAX),
+	[Town] NVARCHAR(MAX),
+	[County] NVARCHAR(MAX),
+	[Postcode] NVARCHAR(MAX),
+	[Position] GEOGRAPHY,
+	[Telephone] NVARCHAR(MAX),
+	[Email] NVARCHAR(MAX),
+	[Website] NVARCHAR(MAX)
+)
