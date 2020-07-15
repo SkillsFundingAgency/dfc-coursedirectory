96d639865d0bf6419fa9d4338e8d770840088de3
James Law <james.law@alterius.co.uk>
2020-07-15 11:51:07 +0100

    Updated scaffolded Reference for UkrlpService to fix deserialization issues with AddressStructure
    Co-authored-by: Tim Abell <tim@timwise.co.uk>

--- src/Dfc.CourseDirectory.Core/Connected Services/UkrlpService/Reference.cs
+++ src/Dfc.CourseDirectory.Core/Connected Services/UkrlpService/Reference.cs
@@ -785,7 +785,7 @@ namespace UkrlpService
         private string postCodeField;
         
         /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute(Order=0)]
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, Order = 0)]
         public string Address1
         {
             get
@@ -799,7 +799,7 @@ namespace UkrlpService
         }
         
         /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute(Order=1)]
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, Order = 1)]
         public string Address2
         {
             get
@@ -813,7 +813,7 @@ namespace UkrlpService
         }
         
         /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute(Order=2)]
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, Order = 2)]
         public string Address3
         {
             get
@@ -827,7 +827,7 @@ namespace UkrlpService
         }
         
         /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute(Order=3)]
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, Order = 3)]
         public string Address4
         {
             get
@@ -841,7 +841,7 @@ namespace UkrlpService
         }
         
         /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute(Order=4)]
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, Order = 4)]
         public string Town
         {
             get
@@ -855,7 +855,7 @@ namespace UkrlpService
         }
         
         /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute(Order=5)]
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, Order = 5)]
         public string County
         {
             get
@@ -869,7 +869,7 @@ namespace UkrlpService
         }
         
         /// <remarks/>
-        [System.Xml.Serialization.XmlElementAttribute(Order=6)]
+        [System.Xml.Serialization.XmlElementAttribute(Form = System.Xml.Schema.XmlSchemaForm.Unqualified, Order = 6)]
         public string PostCode
         {
             get
