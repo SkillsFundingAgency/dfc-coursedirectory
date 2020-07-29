f70e66dcf65c0e7fe2418510bd65fa7b62b8de57
James Gunn <james@gunn.io>
2020-07-29 15:46:03 +0100

    Add new CsvUtil class with a CountLines method

---
+++ src/Dfc.CourseDirectory.Core/CsvUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Dfc.CourseDirectory.Core
+{
+    public static class CsvUtil
+    {
+        public static int CountLines(Stream stream)
+        {
+            var reader = new StreamReader(stream, leaveOpen: true);
+
+            int count = 0;
+            while (reader.ReadLine() != null)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ tests/Dfc.CourseDirectory.Core.Tests/CsvUtilTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace Dfc.CourseDirectory.Core.Tests
+{
+    public class CsvUtilTests
+    {
+        [Theory]
+        [MemberData(nameof(GetCountLinesData))]
+        public void CountLines(string lines, int expectedResult)
+        {
+            // Arrange
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(lines));
+            stream.Seek(0L, SeekOrigin.Begin);
+
+            // Act
+            var lineCount = CsvUtil.CountLines(stream);
+
+            // Assert
+            Assert.Equal(expectedResult, lineCount);
+        }
+
+        public static IEnumerable<object[]> GetCountLinesData()
+        {
+            yield return new object[]
+            {
+                "",
+                0
+            };
+
+            yield return new object[]
+            {
+                "one line",
+                1
+            };
+
+            yield return new object[]
+            {
+                "first line\nsecond line\nthird line",
+                3
+            };
+
+            yield return new object[]
+            {
+                "first line\nsecond line\nthird line\n",
+                3
+            };
+
+            yield return new object[]
+            {
+                "first line\r\nsecond line\r\nthird line",
+                3
+            };
+        }
+    }
+}
