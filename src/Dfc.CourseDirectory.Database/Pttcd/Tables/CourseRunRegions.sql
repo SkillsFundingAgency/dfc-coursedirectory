928d7ceeb43bbbdc44882faa7cec238a34ab15df
James Gunn <james@gunn.io>
2020-08-06 10:38:03 +0100

    Add CourseRunRegions table

---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Tables/CourseRunRegions.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [Pttcd].[CourseRunRegions]
+(
+	[CourseRunId] UNIQUEIDENTIFIER NOT NULL CONSTRAINT [FK_CourseRunRegions_CourseRun] FOREIGN KEY REFERENCES [Pttcd].[CourseRuns] ([CourseRunId]),
+	[RegionId] VARCHAR(9) NOT NULL,
+	CONSTRAINT [PK_CourseRunRegions] PRIMARY KEY ([CourseRunId], [RegionId])
+)
