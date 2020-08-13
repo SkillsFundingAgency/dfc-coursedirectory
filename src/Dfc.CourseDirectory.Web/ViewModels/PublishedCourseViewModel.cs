b895287f3486aa3d5cb81f4273e5871a9fc4c708
James Law <james.law@alterius.co.uk>
2020-08-13 16:45:54 +0100

    Added Published confirmation page to AddCourse

---
+++ src/Dfc.CourseDirectory.Web/ViewModels/PublishedCourseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Dfc.CourseDirectory.Web.ViewModels
+{
+    public class PublishedCourseViewModel
+    {
+        public Guid CourseId { get; set; }
+
+        public Guid CourseRunId { get; set; }
+
+        public string CourseName { get; set; }
+    }
+}
