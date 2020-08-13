b0e3695af269c3f5215cd6a47dfbc7285eb2c7fc
James Gunn <james@gunn.io>
2020-08-13 11:25:37 +0100

    Sync apprenticeships from SQL (PTCD-378)

---
+++ src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/Queries/ProcessAllApprenticeships.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Models;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries
+{
+    public class ProcessAllApprenticeships : ICosmosDbQuery<None>
+    {
+        public Func<IReadOnlyCollection<Apprenticeship>, Task> ProcessChunk { get; set; }
+    }
+}
