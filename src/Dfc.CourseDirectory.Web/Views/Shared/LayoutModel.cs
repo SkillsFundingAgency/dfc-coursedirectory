d7116b883a2ccaba3fff5d5de23d5ca0707cf188
James Law <james.law@alterius.co.uk>
2020-08-14 16:02:11 +0100

    Updated BackLinkTagHelper and LayoutModel to include customisable href and text + added custom backlink to CourseSummary page.

--- src/Dfc.CourseDirectory.Web/Views/Shared/LayoutModel.cs
+++ src/Dfc.CourseDirectory.Web/Views/Shared/LayoutModel.cs
@@ -3,5 +3,9 @@ namespace Dfc.CourseDirectory.Web.Views.Shared
     public class LayoutModel
     {
         public bool BackLink { get; set; }
+
+        public string BackLinkHref { get; set; }
+
+        public string BackLinkText { get; set; }
     }
 }
