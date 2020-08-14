c2116466616b0ca70f04af7dadd731812d6516b9
James Gunn <james@gunn.io>
2020-08-14 13:19:02 +0100

    Add a decorator implementation of IBackgroundWorkScheduler so we can observe work item scheduling in tests

---
+++ tests/Dfc.CourseDirectory.Testing/SignallingBackgroundWorkScheduler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.BackgroundWorkers;
+
+namespace Dfc.CourseDirectory.Testing
+{
+    public class SignallingBackgroundWorkScheduler : IBackgroundWorkScheduler
+    {
+        private readonly IBackgroundWorkScheduler _innerScheduler;
+        private readonly Action _onScheduled;
+
+        public SignallingBackgroundWorkScheduler(
+            IBackgroundWorkScheduler innerScheduler,
+            Action onScheduled)
+        {
+            _innerScheduler = innerScheduler;
+            _onScheduled = onScheduled;
+        }
+
+        public async Task Schedule(WorkItem workItem, object state = null)
+        {
+            try
+            {
+                await _innerScheduler.Schedule(workItem, state);
+            }
+            finally
+            {
+                _onScheduled();
+            }
+        }
+    }
+}
