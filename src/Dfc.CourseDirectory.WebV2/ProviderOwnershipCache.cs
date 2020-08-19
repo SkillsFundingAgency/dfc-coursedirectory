6461ba90236e8c5941ceb8bd585e51edb62c2fd2
James Gunn <james@gunn.io>
2020-08-19 12:41:07 +0100

    Add course methods to IProviderOwnershipCache

--- src/Dfc.CourseDirectory.WebV2/ProviderOwnershipCache.cs
+++ src/Dfc.CourseDirectory.WebV2/ProviderOwnershipCache.cs
@@ -17,13 +17,43 @@ namespace Dfc.CourseDirectory.WebV2
             _cache = cache;
         }
 
-        public async Task<Guid?> GetProviderForApprenticeship(Guid apprenticeshipId)
+        public async Task<Guid?> GetProviderForCourse(Guid courseId)
         {
-            var cacheKey = GetCacheKey(apprenticeshipId);
+            var cacheKey = GetCourseCacheKey(courseId);
 
             if (!_cache.TryGetValue<Guid?>(cacheKey, out var providerId))
             {
-                // Cosmos document doesn't stored ProviderID on the apprenticeship but UKPRN
+                // Cosmos document doesn't store ProviderID on the course but UKPRN
+                // so we need two lookups here :-/
+
+                var ukprn = await _cosmosDbQueryDispatcher.ExecuteQuery(
+                    new GetProviderUkprnForCourse() { CourseId = courseId });
+
+                if (ukprn.HasValue)
+                {
+                    var provider = await _cosmosDbQueryDispatcher.ExecuteQuery(
+                        new GetProviderByUkprn() { Ukprn = ukprn.Value });
+
+                    providerId = provider.Id;
+                }
+                else
+                {
+                    providerId = null;
+                }
+
+                _cache.Set(cacheKey, providerId);
+            }
+
+            return providerId;
+        }
+
+        public async Task<Guid?> GetProviderForApprenticeship(Guid apprenticeshipId)
+        {
+            var cacheKey = GetApprenticeshipCacheKey(apprenticeshipId);
+
+            if (!_cache.TryGetValue<Guid?>(cacheKey, out var providerId))
+            {
+                // Cosmos document doesn't store ProviderID on the apprenticeship but UKPRN
                 // so we need two lookups here :-/
 
                 var ukprn = await _cosmosDbQueryDispatcher.ExecuteQuery(
@@ -49,10 +79,20 @@ namespace Dfc.CourseDirectory.WebV2
 
         public void OnApprenticeshipDeleted(Guid apprenticeshipId)
         {
-            var cacheKey = GetCacheKey(apprenticeshipId);
+            var cacheKey = GetApprenticeshipCacheKey(apprenticeshipId);
             _cache.Remove(cacheKey);
         }
 
-        private static string GetCacheKey(Guid apprenticeshipId) => $"apprenticeship-ukprns:{apprenticeshipId}";
+        public void OnCourseDeleted(Guid courseId)
+        {
+            var cacheKey = GetCourseCacheKey(courseId);
+            _cache.Remove(cacheKey);
+        }
+
+        private static string GetApprenticeshipCacheKey(Guid apprenticeshipId) =>
+            $"apprenticeship-ukprns:{apprenticeshipId}";
+
+        private static string GetCourseCacheKey(Guid courseId) =>
+            $"course-ukprns:{courseId}";
     }
 }
