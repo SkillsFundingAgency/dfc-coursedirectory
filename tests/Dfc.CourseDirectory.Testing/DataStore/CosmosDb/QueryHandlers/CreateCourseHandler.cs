e2edf8ee657c33f48023760c0883c3e68a72497b
James Gunn <james@gunn.io>
2020-08-20 15:12:00 +0100

    Add a minimal TestData.CreateCourse method
    For now CourseRuns are not configurable but this is sufficient for
    creating a valid course with a single course run.

---
+++ tests/Dfc.CourseDirectory.Testing/DataStore/CosmosDb/QueryHandlers/CreateCourseHandler.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Models;
+using Dfc.CourseDirectory.Testing.DataStore.CosmosDb.Queries;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.Testing.DataStore.CosmosDb.QueryHandlers
+{
+    public class CreateCourseHandler : ICosmosDbQueryHandler<CreateCourse, Success>
+    {
+        public Success Execute(InMemoryDocumentStore inMemoryDocumentStore, CreateCourse request)
+        {
+            var course = new Course()
+            {
+                Id = request.CourseId,
+                ProviderId = request.ProviderId,
+                ProviderUKPRN = request.ProviderUkprn,
+                QualificationCourseTitle = request.QualificationCourseTitle,
+                LearnAimRef = request.LearnAimRef,
+                NotionalNVQLevelv2 = request.NotionalNVQLevelv2,
+                AwardOrgCode = request.AwardOrgCode,
+                QualificationType = request.QualificationType,
+                CourseDescription = request.CourseDescription,
+                EntryRequirements = request.EntryRequirements,
+                WhatYoullLearn = request.WhatYoullLearn,
+                HowYoullLearn = request.HowYoullLearn,
+                WhatYoullNeed = request.WhatYoullNeed,
+                HowYoullBeAssessed = request.HowYoullBeAssessed,
+                WhereNext = request.WhereNext,
+                AdultEducationBudget = request.AdultEducationBudget,
+                AdvancedLearnerLoan = request.AdvancedLearnerLoan,
+                CourseRuns = request.CourseRuns.Select(cr => new CourseRun()
+                {
+                    Id = cr.CourseRunId,
+                    VenueId = cr.VenueId,
+                    CourseName = cr.CourseName,
+                    DeliveryMode = cr.DeliveryMode,
+                    FlexibleStartDate = cr.FlexibleStartDate,
+                    StartDate = cr.StartDate,
+                    CourseURL = cr.CourseUrl,
+                    Cost = cr.Cost,
+                    CostDescription = cr.CostDescription,
+                    DurationUnit = cr.DurationUnit,
+                    DurationValue = cr.DurationValue,
+                    StudyMode = cr.StudyMode,
+                    AttendancePattern = cr.AttendancePattern,
+                    National = cr.National,
+                    Regions = cr.Regions,
+                    RecordStatus = Core.Models.CourseStatus.Live,
+                    CreatedDate = request.CreatedDate,
+                    CreatedBy = request.CreatedByUser.Email,
+                    UpdatedDate = request.CreatedDate,
+                    UpdatedBy = request.CreatedByUser.Email,
+                }),
+                CourseStatus = Core.Models.CourseStatus.Live,
+                CreatedBy = request.CreatedByUser.Email,
+                CreatedDate = request.CreatedDate,
+                UpdatedBy = request.CreatedByUser.Email,
+                UpdatedDate = request.CreatedDate
+            };
+
+            inMemoryDocumentStore.Courses.Save(course);
+
+            return new Success();
+        }
+    }
+}
---
+++ tests/Dfc.CourseDirectory.Testing/TestData/TestData.CreateCourse.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries;
+using Dfc.CourseDirectory.Core.Models;
+using Dfc.CourseDirectory.Testing.DataStore.CosmosDb.Queries;
+
+namespace Dfc.CourseDirectory.Testing
+{
+    public partial class TestData
+    {
+        public async Task<Guid> CreateCourse(
+            Guid providerId,
+            UserInfo createdBy,
+            string qualificationCourseTitle = "Education assessment in Maths",
+            string learnAimRef = "Z0007842",
+            string notionalNVQLevelv2 = "E",  // TODO When we have a LARS data store for testing we should lookup from there
+            string awardOrgCode = "NONE",  // as above
+            string qualificationType = "Other",  // as above
+            string courseDescription = "The description",
+            string entryRequirements = "To be eligible for ESFA government funding, you must be 19 or over. You must be a UK or EU citizen or have indefinite leave to remain in the UK. Learners will be ineligible if they have previously achieved a GCSE grade A* to C in the subject.",
+            string whatYoullLearn = "DIGITAL EMPLOYABILITY SKILLS",
+            string howYoullLearn = "Group coaching and 1-2-1 support through Tutor led learning, written assignments throughout courses, Independent Learning, access to comprehensive online and paper based learning resources.",
+            string whatYoullNeed = "Valid ID (driving licence, passport, birth certificate)",
+            string howYoullBeAssessed = "Paper based or online assessments",
+            string whereNext = "We will actively signpost and support you to achieve the next level of progression for all certificated aims.",
+            bool adultEducationBudget = false,
+            bool advancedLearnerLoan = false,
+            DateTime? createdUtc = null)
+        {
+            var provider = await _cosmosDbQueryDispatcher.ExecuteQuery(new GetProviderById()
+            {
+                ProviderId = providerId
+            });
+
+            if (provider == null)
+            {
+                throw new ArgumentException("Provider does not exist.", nameof(providerId));
+            }
+
+            var courseId = Guid.NewGuid();
+
+            await _cosmosDbQueryDispatcher.ExecuteQuery(
+                new CreateCourse()
+                {
+                    CourseId = courseId,
+                    ProviderId = providerId,
+                    ProviderUkprn = provider.Ukprn,
+                    QualificationCourseTitle = qualificationCourseTitle,
+                    LearnAimRef = learnAimRef,
+                    NotionalNVQLevelv2 = notionalNVQLevelv2,
+                    AwardOrgCode = awardOrgCode,
+                    QualificationType = qualificationType,
+                    CourseDescription = courseDescription,
+                    EntryRequirements = entryRequirements,
+                    WhatYoullLearn = whatYoullLearn,
+                    HowYoullLearn = howYoullLearn,
+                    WhatYoullNeed = whatYoullNeed,
+                    HowYoullBeAssessed = howYoullBeAssessed,
+                    WhereNext = whereNext,
+                    AdultEducationBudget = adultEducationBudget,
+                    AdvancedLearnerLoan = advancedLearnerLoan,
+                    CourseRuns = new[]
+                    {
+                        new CreateCourseCourseRun()
+                        {
+                            CourseRunId = Guid.NewGuid(),
+                            CourseName = qualificationCourseTitle,
+                            DeliveryMode = CourseDeliveryMode.Online,
+                            FlexibleStartDate = true,
+                            Cost = 69,
+                            DurationUnit = CourseDurationUnit.Months,
+                            DurationValue = 6,
+                            AttendancePattern = CourseAttendancePattern.Evening,
+                            National = true
+                        }
+                    },
+                    CreatedDate = createdUtc ?? _clock.UtcNow,
+                    CreatedByUser = createdBy
+                });
+
+            return courseId;
+        }
+    }
+}
