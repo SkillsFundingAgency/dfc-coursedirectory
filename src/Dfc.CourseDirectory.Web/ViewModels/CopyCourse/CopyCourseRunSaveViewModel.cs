f42da034f8c96e3a165369c19b9e74622fcdad1f
James Law <james.law@alterius.co.uk>
2020-08-27 23:29:44 +0100

    PTCD-493 Add Check and Publish and Published pages to CopyCourseRun journey

--- src/Dfc.CourseDirectory.Web/ViewModels/CopyCourse/CopyCourseRunSaveViewModel.cs
+++ src/Dfc.CourseDirectory.Web/ViewModels/CopyCourse/CopyCourseRunSaveViewModel.cs
@@ -1,39 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using Dfc.CourseDirectory.Models.Models;
 using Dfc.CourseDirectory.Models.Models.Courses;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Dfc.CourseDirectory.Web.ViewModels.CopyCourse
 {
     public class CopyCourseRunSaveViewModel
     {
+        public Guid? CourseId { get; set; }
+        public Guid CourseRunId { get; set; }
+        public string LearnAimRefTitle { get; set; }
         public string CourseName { get; set; }
-        public DateTime StartDate { get; set; }
-        public string StartDateType { get; set; }
+        public string CourseProviderReference { get; set; }
+        public DeliveryMode DeliveryMode { get; set; }
+        public StartDateType StartDateType { get; set; }
         public string Day { get; set; }
         public string Month { get; set; }
         public string Year { get; set; }
-        public string DurationLength { get; set; }
-        public string Cost { get; set; }
-        public string CostDescription { get; set; }
-
         public Guid VenueId { get; set; }
         public bool National { get; set; }
         public string[] SelectedRegions { get; set; }
-        public DeliveryMode DeliveryMode { get; set; }
-        public string CourseProviderReference { get; set; }
         public string Url { get; set; }
+        public string Cost { get; set; }
+        public string CostDescription { get; set; }
+        public string DurationLength { get; set; }
         public DurationUnit DurationUnit { get; set; }
-        public StudyMode StudyMode { get; set; }
         public AttendancePattern AttendanceMode { get; set; }
-        public Guid? CourseId { get; set; }
-        public Guid CourseRunId { get; set; }
-
-        public string QualificationType { get; set; }
-
-        public bool FlexibleStartDate { get; set; }
-
+        public StudyMode StudyMode { get; set; }
+        public string RefererAbsolutePath { get; set; }
     }
-
 }
---
+++ src/Dfc.CourseDirectory.Web/ViewModels/CopyCourse/CopyCourseRunSummaryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Dfc.CourseDirectory.Models.Models.Courses;
+
+namespace Dfc.CourseDirectory.Web.ViewModels.CopyCourse
+{
+    public class CopyCourseRunSummaryViewModel
+    {
+        public string LearnAimRefTitle { get; set; }
+        public string CourseName { get; set; }
+        public string CourseProviderReference { get; set; }
+        public DeliveryMode DeliveryMode { get; set; }
+        public string StartDate { get; set; }
+        public IEnumerable<string> Venues { get; set; }
+        public IEnumerable<string> Regions { get; set; }
+        public string Url { get; set; }
+        public string Cost { get; set; }
+        public string CostDescription { get; set; }
+        public string CourseLength { get; set; }
+        public AttendancePattern AttendancePattern { get; set; }
+        public StudyMode StudyMode { get; set; }
+    }
+}
