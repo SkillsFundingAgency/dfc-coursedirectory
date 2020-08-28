7e55062efdcb74e667e74e4aa04de7d235ffb086
James Gunn <james@gunn.io>
2020-08-28 09:09:55 +0100

    Add extension method on IProviderInfoCache for retrieving provider info given a UKPRN

---
+++ src/Dfc.CourseDirectory.WebV2/ProviderInfoCacheExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.WebV2
+{
+    public static class ProviderInfoCacheExtensions
+    {
+        public static async Task<ProviderInfo> GetProviderInfoForUkprn(this IProviderInfoCache providerInfoCache, int ukprn)
+        {
+            if (providerInfoCache == null)
+            {
+                throw new ArgumentNullException(nameof(providerInfoCache));
+            }
+
+            var providerId = await providerInfoCache.GetProviderIdForUkprn(ukprn);
+
+            if (providerId == null)
+            {
+                return null;
+            }
+
+            return await providerInfoCache.GetProviderInfo(providerId.Value);
+        }
+    }
+}
