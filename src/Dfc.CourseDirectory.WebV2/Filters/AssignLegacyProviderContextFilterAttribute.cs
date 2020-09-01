1418c19533f4cef6a4f03d0b928e3fd35deae757
James Gunn <james@gunn.io>
2020-09-01 12:56:54 +0100

    Add a filter attribute for setting the legacy provider context
    When we're transitioning from new pages to old pages, the old
    session-based provider context may not be set. (If it's not set
    correctly then navigating to old pages will either return data for the
    wrong provider or it will navigate to the provider selection UI.)

---
+++ src/Dfc.CourseDirectory.WebV2/Filters/AssignLegacyProviderContextFilterAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Dfc.CourseDirectory.WebV2.Filters
+{
+    public sealed class AssignLegacyProviderContextFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var env = context.HttpContext.RequestServices.GetRequiredService<IHostEnvironment>();
+
+            if (!env.IsTesting())
+            {
+                var providerContextProvider = context.HttpContext.RequestServices.GetRequiredService<IProviderContextProvider>();
+                providerContextProvider.AssignLegacyProviderContext();
+            }
+        }
+    }
+}
