f594ffc9ae9459b28ee7a4690000b5d5a75050a1
James Gunn <james@gunn.io>
2020-09-02 14:21:34 +0100

    Add mechanism to render a back link in the v2 layouts
    Add a helper method for setting all of the ViewData entries used by the
    layout.

---
+++ src/Dfc.CourseDirectory.WebV2/SharedViews/ViewContextExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Dfc.CourseDirectory.WebV2.SharedViews
+{
+    public static class ViewContextExtensions
+    {
+        public static void SetLayoutData(
+            this ViewContext viewContext,
+            string title,
+            bool showBackLink = false)
+        {
+            viewContext.ViewData["Title"] = title;
+            viewContext.ViewData["ShowBackLink"] = showBackLink;
+        }
+    }
+}
