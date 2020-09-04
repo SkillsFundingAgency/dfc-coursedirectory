8ad09075635c4f1c391e01d9da9b790a9adc0eb4
James Gunn <james@gunn.io>
2020-09-04 12:39:02 +0100

    Link to the new Provider Details page and remove the old one

--- tests/Dfc.CourseDirectory.WebV2.Tests/StubControllers/ProviderController.cs
+++ tests/Dfc.CourseDirectory.WebV2.Tests/StubControllers/ProviderController.cs
@@ -4,9 +4,6 @@ namespace Dfc.CourseDirectory.WebV2.Tests.StubControllers
 {
     public class ProviderController : Controller
     {
-        [HttpGet("provider/details")]
-        public IActionResult Details(ProviderInfo providerInfo) => Ok();
-
         [HttpGet("Provider/Dashboard")]
         public IActionResult Dashboard() => Ok();
 		
