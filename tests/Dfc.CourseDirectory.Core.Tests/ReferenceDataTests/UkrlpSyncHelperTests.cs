653ba8042ddd832922d80c165ed0895523d19e67
Tim Abell <tim@timwise.co.uk>
2020-09-07 17:42:42 +0100

    Extend UKRLP tests to cover all address fields

--- tests/Dfc.CourseDirectory.Core.Tests/ReferenceDataTests/UkrlpSyncHelperTests.cs
+++ tests/Dfc.CourseDirectory.Core.Tests/ReferenceDataTests/UkrlpSyncHelperTests.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Models;
 using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries;
 using Dfc.CourseDirectory.Core.Models;
 using Dfc.CourseDirectory.Core.ReferenceData.Ukrlp;
 using Dfc.CourseDirectory.Testing;
+using FluentAssertions;
+using FluentAssertions.Execution;
 using Microsoft.Extensions.Logging;
 using Moq;
 using UkrlpService;
@@ -19,103 +24,91 @@ namespace Dfc.CourseDirectory.Core.Tests.ReferenceDataTests
         }
 
         [Fact]
-        public async Task SyncProviderData_ProviderAlreadyExists_UpdatesProviderInfo()
+        public async Task SyncProviderData_CreatesProvider()
         {
             // Arrange
-            var ukprn = 01234566;
+            const int ukprn = 1234569;
+            var ukrlpData = GenerateUkrlpProviderData(ukprn);
+            var ukrlpContact = ukrlpData.ProviderContact.Single();
+            var ukrlpSyncHelper = SetupUkrlpSyncHelper(ukprn, ukrlpData);
+
+            ICollection<CreateProviderFromUkrlpData> capturedUpdateCommands = new List<CreateProviderFromUkrlpData>();
+            CosmosDbQueryDispatcher.Setup(mock => mock.ExecuteQuery(Capture.In(capturedUpdateCommands)));
+
+            // Act
+            await ukrlpSyncHelper.SyncProviderData(ukprn);
+
+            // Assert
+            var createCommand = capturedUpdateCommands.Should().ContainSingle().Subject;
+            createCommand.Alias.Should().Be(ukrlpData.ProviderAliases.Single().ProviderAlias);
+            createCommand.DateUpdated.Should().Be(Clock.UtcNow);
+            createCommand.ProviderName.Should().Be(ukrlpData.ProviderName);
+            createCommand.ProviderStatus.Should().Be(ukrlpData.ProviderStatus);
+            createCommand.ProviderType.Should().Be(ProviderType.Both);
+            createCommand.Status.Should().Be(ProviderStatus.Registered);
+            createCommand.Ukprn.Should().Be(ukprn);
+            var actualContact = createCommand.Contacts.Should().ContainSingle().Subject;
+            AssertContactMapping(actualContact, ukrlpContact);
+        }
+
+        [Fact]
+        public async Task SyncProviderData_UpdatesExistingProvider()
+        {
+            // Arrange
+            const int ukprn = 1234567;
 
             var providerId = await TestData.CreateProvider(
                 ukprn,
                 providerName: "Test Provider",
                 providerType: ProviderType.Both,
-                providerStatus: "Active",
+                providerStatus: "Provider deactivated, not verified",
                 contacts: new[]
                 {
-                    new CreateProviderContact()
+                    new CreateProviderContact
                     {
                         ContactType = "P",
                         ContactTelephone1 = "0123456789",
-                        ContactWebsiteAddress = "http://www.testing.com",
-                        ContactEmail = "test@test.com",
-                        AddressItems = new[] { "ItemDescription" },
-                        AddressPaonDescription = "PAON Description",
+                        ContactWebsiteAddress = "http://www.example.com",
+                        ContactEmail = "test@example.com",
                         AddressSaonDescription = "SAON Description",
+                        AddressPaonDescription = "PAON Description",
                         AddressStreetDescription = "Street",
-                        AddressPostCode = "Postcode",
                         AddressLocality = "Locality",
+                        AddressItems = new[] {"ItemDescription"},
+                        AddressPostTown = "PostTown",
+                        AddressPostCode = "Postcode",
                         PersonalDetailsFamilyName = "FamilyName",
                         PersonalDetailsGivenName = "GivenName"
                     }
                 });
 
-            var ukrlpProviderData = GenerateUkrlpProviderData(ukprn);
+            var ukrlpData = GenerateUkrlpProviderData(ukprn);
+            var ukrlpContact = ukrlpData.ProviderContact.Single();
 
-            var ukrlpWcfService = new Mock<IUkrlpService>();
-            ukrlpWcfService.Setup(w => w.GetProviderData(ukprn)).ReturnsAsync(ukrlpProviderData);
+            var ukrlpSyncHelper = SetupUkrlpSyncHelper(ukprn, ukrlpData);
 
-            var loggerFactory = new Mock<ILoggerFactory>();
-            loggerFactory.Setup(mock => mock.CreateLogger(It.IsAny<string>())).Returns(Mock.Of<ILogger>());
-
-            var ukrlpSyncHelper = new UkrlpSyncHelper(
-                ukrlpWcfService.Object,
-                CosmosDbQueryDispatcher.Object,
-                Clock,
-                loggerFactory.Object);
+            ICollection<UpdateProviderFromUkrlpData> capturedUpdateCommands = new List<UpdateProviderFromUkrlpData>();
+            CosmosDbQueryDispatcher.Setup(mock => mock.ExecuteQuery(Capture.In(capturedUpdateCommands)));
 
             // Act
             await ukrlpSyncHelper.SyncProviderData(ukprn);
 
             // Assert
-            CosmosDbQueryDispatcher.Verify(mock => mock.ExecuteQuery(It.Is<UpdateProviderFromUkrlpData>(cmd =>
-                cmd.Alias == "ProviderAlias" &&
-                cmd.DateUpdated == Clock.UtcNow &&
-                cmd.ProviderId == providerId &&
-                cmd.ProviderName == ukrlpProviderData.ProviderName &&
-                cmd.ProviderStatus == "Active"
-            )));
-        }
-
-        [Fact]
-        public async Task SyncProviderData_ProviderDoesNotExist_CreatesProviderInfo()
-        {
-            // Arrange
-            var ukprn = 01234566;
-
-            var ukrlpProviderData = GenerateUkrlpProviderData(ukprn);
-
-            var ukrlpWcfService = new Mock<IUkrlpService>();
-            ukrlpWcfService.Setup(w => w.GetProviderData(ukprn)).ReturnsAsync(ukrlpProviderData);
-
-            var loggerFactory = new Mock<ILoggerFactory>();
-            loggerFactory.Setup(mock => mock.CreateLogger(It.IsAny<string>())).Returns(Mock.Of<ILogger>());
-
-            var ukrlpSyncHelper = new UkrlpSyncHelper(
-                ukrlpWcfService.Object,
-                CosmosDbQueryDispatcher.Object,
-                Clock,
-                loggerFactory.Object);
-
-            // Act
-            await ukrlpSyncHelper.SyncProviderData(ukprn);
-
-            // Assert
-            CosmosDbQueryDispatcher.Verify(mock => mock.ExecuteQuery(It.Is<CreateProviderFromUkrlpData>(cmd =>
-                cmd.Alias == "ProviderAlias" &&
-                cmd.DateUpdated == Clock.UtcNow &&
-                cmd.ProviderName == ukrlpProviderData.ProviderName &&
-                cmd.ProviderStatus == "Active" &&
-                cmd.ProviderType == ProviderType.Both &&
-                cmd.Status == ProviderStatus.Registered &&
-                cmd.Ukprn == ukprn
-            )));
+            var updateCommand = capturedUpdateCommands.Should().ContainSingle().Subject;
+            updateCommand.Alias.Should().Be(ukrlpData.ProviderAliases.Single().ProviderAlias);
+            updateCommand.DateUpdated.Should().Be(Clock.UtcNow);
+            updateCommand.ProviderName.Should().Be(ukrlpData.ProviderName);
+            updateCommand.ProviderId.Should().Be(providerId);
+            updateCommand.ProviderStatus.Should().Be(ukrlpData.ProviderStatus);
+            var actualContact = updateCommand.Contacts.Should().ContainSingle().Subject;
+            AssertContactMapping(actualContact, ukrlpContact);
         }
 
         [Fact]
         public void SelectContact_SelectsMostRecentlyUpdatedPTypeContact()
         {
             // Arrange
-
-            var contact1 = new ProviderContactStructure()
+            var contact1 = new ProviderContactStructure
             {
                 ContactType = "P",
                 LastUpdated = new DateTime(2020, 3, 1),
@@ -123,7 +116,7 @@ namespace Dfc.CourseDirectory.Core.Tests.ReferenceDataTests
                 ContactPersonalDetails = new PersonNameStructure()
             };
 
-            var contact2 = new ProviderContactStructure()
+            var contact2 = new ProviderContactStructure
             {
                 ContactType = "P",
                 LastUpdated = new DateTime(2020, 4, 1),
@@ -131,7 +124,7 @@ namespace Dfc.CourseDirectory.Core.Tests.ReferenceDataTests
                 ContactPersonalDetails = new PersonNameStructure()
             };
 
-            var contact3 = new ProviderContactStructure()
+            var contact3 = new ProviderContactStructure
             {
                 ContactType = "L",
                 LastUpdated = new DateTime(2020, 5, 1),
@@ -153,38 +146,81 @@ namespace Dfc.CourseDirectory.Core.Tests.ReferenceDataTests
             Assert.Same(selected, contact2);
         }
 
-        private ProviderRecordStructure GenerateUkrlpProviderData(int ukprn) => new ProviderRecordStructure()
+        private void AssertContactMapping(ProviderContact actualContact, ProviderContactStructure ukrlpContact)
+        {
+            actualContact.ContactType.Should().Be(ukrlpContact.ContactType);
+            actualContact.ContactRole.Should().Be(ukrlpContact.ContactRole);
+            actualContact.ContactTelephone1.Should().Be(ukrlpContact.ContactTelephone1);
+            actualContact.ContactWebsiteAddress.Should().Be(ukrlpContact.ContactWebsiteAddress);
+            actualContact.ContactEmail.Should().Be(ukrlpContact.ContactEmail);
+            actualContact.ContactFax.Should().Be(ukrlpContact.ContactFax);
+            actualContact.LastUpdated.Should().Be(Clock.UtcNow);
+            actualContact.AdditionalData.Should().BeNull();
+            actualContact.ContactPersonalDetails.Should().NotBeNull();
+
+            var actualContactAddress = actualContact.ContactAddress;
+            actualContactAddress.SAON.Description.Should().Be(ukrlpContact.ContactAddress.SAON.Description);
+            actualContactAddress.PAON.Description.Should().Be(ukrlpContact.ContactAddress.PAON.Description);
+            actualContactAddress.StreetDescription.Should().Be(ukrlpContact.ContactAddress.StreetDescription);
+            actualContactAddress.Locality.Should().Be(ukrlpContact.ContactAddress.Locality);
+            actualContactAddress.Items.SingleOrDefault().Should().Be(ukrlpContact.ContactAddress.Items.Single());
+            actualContactAddress.PostTown.Should().BeNull(); // original value is not copied across when address is recreated
+            actualContactAddress.PostCode.Should().Be(ukrlpContact.ContactAddress.PostCode);
+            actualContactAddress.AdditionalData.Should().BeNull();
+
+            var actualPersonalDetails = actualContact.ContactPersonalDetails;
+            actualPersonalDetails.PersonNameTitle.SingleOrDefault().Should().Be(ukrlpContact.ContactPersonalDetails.PersonNameTitle.Single());
+            actualPersonalDetails.PersonGivenName.SingleOrDefault().Should().Be(ukrlpContact.ContactPersonalDetails.PersonGivenName.Single());
+            actualPersonalDetails.PersonFamilyName.Should().Be(ukrlpContact.ContactPersonalDetails.PersonFamilyName);
+            actualPersonalDetails.AdditionalData.Should().BeNull();
+
+        }
+
+        private UkrlpSyncHelper SetupUkrlpSyncHelper(int ukprn, ProviderRecordStructure ukrlpProviderData)
+        {
+            var ukrlpWcfService = new Mock<IUkrlpService>();
+            ukrlpWcfService.Setup(w => w.GetProviderData(ukprn)).ReturnsAsync(ukrlpProviderData);
+
+            var loggerFactory = new Mock<ILoggerFactory>();
+            loggerFactory.Setup(mock => mock.CreateLogger(It.IsAny<string>())).Returns(Mock.Of<ILogger>());
+
+            return new UkrlpSyncHelper(
+                ukrlpWcfService.Object,
+                CosmosDbQueryDispatcher.Object,
+                Clock,
+                loggerFactory.Object);
+        }
+
+        private ProviderRecordStructure GenerateUkrlpProviderData(int ukprn) => new ProviderRecordStructure
         {
             UnitedKingdomProviderReferenceNumber = ukprn.ToString(),
             ProviderName = "Ukrlp Test Provider",
             ProviderStatus = "Active",
-            ProviderAliases = new[]
+            ProviderAliases = new[] { new ProviderAliasesStructure {ProviderAlias = "Ukrlp ProviderAliasHere"} },
+            ProviderContact = new[]
             {
-                new ProviderAliasesStructure() { ProviderAlias = "ProviderAlias" }
-            },
-            ProviderContact = new ProviderContactStructure[]
-            {
-                new ProviderContactStructure()
+                new ProviderContactStructure
                 {
                     ContactType = "P",
-                    ContactAddress = new BSaddressStructure()
+                    ContactAddress = new BSaddressStructure
                     {
-                        SAON = new AONstructure() { Description = "Ukrlp SAON Description" },
-                        PAON = new AONstructure() { Description = "Ukrlp PAON Description" },
+                        SAON = new AONstructure {Description = "Ukrlp SAON Description"},
+                        PAON = new AONstructure {Description = "Ukrlp PAON Description"},
                         StreetDescription = "Ukrlp Street",
                         Locality = "Ukrlp Locality",
-                        Items = new string[] { "Ukrlp Item Description " },
+                        Items = new[] {"Ukrlp Item Description "},
+                        PostTown = "Ukrlp PostTown",
                         PostCode = "Ukrlp PostCode"
                     },
-                    ContactPersonalDetails = new PersonNameStructure()
+                    ContactPersonalDetails = new PersonNameStructure
                     {
-                        PersonNameTitle = new string[] { "Ukrlp Mr" },
-                        PersonGivenName = new string[] { "Ukrlp Given name" },
+                        PersonNameTitle = new[] {"Ukrlp Mr"},
+                        PersonGivenName = new[] {"Ukrlp Given name"},
                         PersonFamilyName = "Ukrlp Family name",
                     },
                     ContactTelephone1 = "Ukrlp Telephone 1",
-                    ContactWebsiteAddress = "http://www.ukrlptest.com",
-                    ContactEmail = "Ukrlptest@test.com",
+                    ContactWebsiteAddress = "http://www.ukrlptest.example.com",
+                    ContactEmail = "Ukrlptest@example.com",
                     LastUpdated = Clock.UtcNow,
                 }
             }
