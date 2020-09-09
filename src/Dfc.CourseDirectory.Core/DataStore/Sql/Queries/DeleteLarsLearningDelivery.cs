59f985057871319a45f69cfb33517b7001a9c8ef
James Law <james.law@alterius.co.uk>
2020-09-09 17:38:10 +0100

    PTCD-670 Added DeleteLarsLearningDelivery query and handler

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/Queries/DeleteLarsLearningDelivery.cs
@@ -0,0 +1,7 @@
+﻿namespace Dfc.CourseDirectory.Core.DataStore.Sql.Queries
+{
+    public class DeleteLarsLearningDelivery : ISqlQuery<string>
+    {
+        public string LearnAimRef { get; set; }
+    }
+}
---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/DeleteLarsLearningDeliveryHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.QueryHandlers
+{
+    public class DeleteLarsLearningDeliveryHandler : ISqlQueryHandler<DeleteLarsLearningDelivery, string>
+    {
+        public async Task<string> Execute(SqlTransaction transaction, DeleteLarsLearningDelivery query)
+        {
+            var sql = @$"
+                DELETE FROM LARS.LearningDelivery
+                OUTPUT deleted.LearnAimRef
+                WHERE LearnAimRef = @{nameof(query.LearnAimRef)}";
+
+            return await transaction.Connection.QuerySingleAsync<string>(sql, query, transaction);
+        }
+    }
+}
