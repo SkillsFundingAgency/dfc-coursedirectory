e38320f2a6b2f5684b1bcf1cba1fde60294563d9
James Gunn <james@gunn.io>
2020-09-14 11:33:09 +0100

    Replace the helper method with a section for rendering a back link

--- src/Dfc.CourseDirectory.WebV2/SharedViews/ViewContextExtensions.cs
+++ src/Dfc.CourseDirectory.WebV2/SharedViews/ViewContextExtensions.cs
@@ -4,13 +4,9 @@ namespace Dfc.CourseDirectory.WebV2.SharedViews
 {
     public static class ViewContextExtensions
     {
-        public static void SetLayoutData(
-            this ViewContext viewContext,
-            string title,
-            bool showBackLink = false)
+        public static void SetLayoutData(this ViewContext viewContext, string title)
         {
             viewContext.ViewData["Title"] = title;
-            viewContext.ViewData["ShowBackLink"] = showBackLink;
         }
     }
 }
