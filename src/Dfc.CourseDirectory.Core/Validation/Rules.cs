a40bcf2ca6eee3584da2a28c70daca7a3c7113e1
James Gunn <james@gunn.io>
2020-09-25 12:08:45 +0100

    Make the general validation helper methods regular static methods
    Having them as extension methods yields naming collisions for methods
    like PhoneNumber(). We want the entity-specific methods to 'win' when
    validators are being defined. This changes the general purpose rules to
    make them regular methods.
    
    Also added an Apply() extension method which allows using these
    fluently.

--- src/Dfc.CourseDirectory.Core/Validation/Rules.cs
+++ src/Dfc.CourseDirectory.Core/Validation/Rules.cs
@@ -4,17 +4,24 @@ using FluentValidation;
 
 namespace Dfc.CourseDirectory.Core.Validation
 {
-    public static class RuleBuilderExtensions
+    public static class Rules
     {
         private static readonly Regex UkPhoneNumber = new Regex(
             @"^((\(?0\d{4}\)?\s?\d{3}\s?\d{3})|(\(?0\d{3}\)?\s?\d{3}\s?\d{4})|(\(?0\d{2}\)?\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$",
             RegexOptions.Compiled);
 
-        public static IRuleBuilderOptions<T, string> PhoneNumber<T>(this IRuleBuilder<T, string> field) =>
+        public static IRuleBuilderOptions<T, R> Apply<T, R>(
+            this IRuleBuilder<T, R> field,
+            Func<IRuleBuilder<T, R>, IRuleBuilderOptions<T, R>> addRule)
+        {
+            return addRule(field);
+        }
+
+        public static IRuleBuilderOptions<T, string> PhoneNumber<T>(IRuleBuilder<T, string> field) =>
             field
                 .Matches(UkPhoneNumber);
 
-        public static IRuleBuilderOptions<T, string> Website<T>(this IRuleBuilder<T, string> field) =>
+        public static IRuleBuilderOptions<T, string> Website<T>(IRuleBuilder<T, string> field) =>
             field
                 .Must(url =>
                 {
