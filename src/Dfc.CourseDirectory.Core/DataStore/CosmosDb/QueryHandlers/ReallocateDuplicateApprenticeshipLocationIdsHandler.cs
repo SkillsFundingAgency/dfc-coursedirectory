b2b76de1ee4cc80a0c92d68176436658b4a074e4
James Gunn <james@gunn.io>
2020-10-06 15:50:23 +0100

    Temporary Azure Function to fix duplicate Apprenticeship Location IDs

---
+++ src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/QueryHandlers/ReallocateDuplicateApprenticeshipLocationIdsHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb.QueryHandlers
+{
+    public class ReallocateDuplicateApprenticeshipLocationIdsHandler :
+        ICosmosDbQueryHandler<ReallocateDuplicateApprenticeshipLocationIds, None>
+    {
+        public async Task<None> Execute(
+            DocumentClient client,
+            Configuration configuration,
+            ReallocateDuplicateApprenticeshipLocationIds request)
+        {
+            var locations = request.Apprenticeship.ApprenticeshipLocations;
+
+            for (var i = 1; i < locations.Count; i++)
+            {
+                var thisLocation = locations[i];
+
+                if (locations.Take(i).Any(l => l.Id == thisLocation.Id))
+                {
+                    thisLocation.Id = Guid.NewGuid();
+                }
+            }
+
+            request.Apprenticeship.UpdatedBy = request.UpdatedBy;
+            request.Apprenticeship.UpdatedDate = request.UpdatedOn;
+
+            var documentUri = UriFactory.CreateDocumentUri(
+                configuration.DatabaseId,
+                configuration.ApprenticeshipCollectionName,
+                request.Apprenticeship.Id.ToString());
+
+            await client.ReplaceDocumentAsync(
+                documentUri,
+                request.Apprenticeship,
+                new RequestOptions() { PartitionKey = new PartitionKey(request.Apprenticeship.ProviderUKPRN) });
+
+            return new None();
+        }
+    }
+}
---
+++ src/Dfc.CourseDirectory.Functions/FixDuplicateApprenticeshipLocationIds.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries;
+using Microsoft.Azure.WebJobs;
+
+namespace Dfc.CourseDirectory.Functions
+{
+    public class FixDuplicateApprenticeshipLocationIds
+    {
+        private readonly ICosmosDbQueryDispatcher _cosmosDbQueryDispatcher;
+        private readonly IClock _clock;
+
+        public FixDuplicateApprenticeshipLocationIds(
+            ICosmosDbQueryDispatcher cosmosDbQueryDispatcher,
+            IClock clock)
+        {
+            _cosmosDbQueryDispatcher = cosmosDbQueryDispatcher;
+            _clock = clock;
+        }
+
+        [FunctionName(nameof(FixDuplicateApprenticeshipLocationIds))]
+        [NoAutomaticTrigger]
+        public Task Execute(string input) => _cosmosDbQueryDispatcher.ExecuteQuery(new ProcessAllApprenticeships()
+        {
+            ProcessChunk = async chunk =>
+            {
+                foreach (var apprenticeship in chunk)
+                {
+                    var hasDuplicates = apprenticeship.ApprenticeshipLocations
+                        .GroupBy(l => l.Id)
+                        .Any(g => g.Count() > 1);
+
+                    if (hasDuplicates)
+                    {
+                        await _cosmosDbQueryDispatcher.ExecuteQuery(
+                            new ReallocateDuplicateApprenticeshipLocationIds()
+                            {
+                                Apprenticeship = apprenticeship,
+                                UpdatedBy = nameof(FixDuplicateApprenticeshipLocationIds),
+                                UpdatedOn = _clock.UtcNow
+                            });
+                    }
+                }
+            }
+        });
+    }
+}
