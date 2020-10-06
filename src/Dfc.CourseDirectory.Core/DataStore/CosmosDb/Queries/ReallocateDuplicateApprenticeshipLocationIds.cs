cda0803a86efa0e1977cd5355f925eb4118aa3d4
James Gunn <james@gunn.io>
2020-10-06 15:50:23 +0100

    Temporary Azure Function to fix duplicate Apprenticeship Location IDs

---
+++ src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/Queries/ReallocateDuplicateApprenticeshipLocationIds.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Models;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries
+{
+    public class ReallocateDuplicateApprenticeshipLocationIds : ICosmosDbQuery<None>
+    {
+        public Apprenticeship Apprenticeship { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
