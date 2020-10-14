962876e2cb8c28fcf6d5db7bbef1193d78745c79
James Gunn <james@gunn.io>
2020-10-14 11:28:47 +0100

    Revise regex used for phone number validation

--- src/Dfc.CourseDirectory.Core/Validation/Rules.cs
+++ src/Dfc.CourseDirectory.Core/Validation/Rules.cs
@@ -7,7 +7,7 @@ namespace Dfc.CourseDirectory.Core.Validation
     public static class Rules
     {
         private static readonly Regex _ukPhoneNumberPattern = new Regex(
-            @"^((\(?0\d{4}\)?\s?\d{3}\s?\d{3})|(\(?0\d{3}\)?\s?\d{3}\s?\d{4})|(\(?0\d{2}\)?\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$",
+            @"^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$",
             RegexOptions.Compiled);
 
         private static readonly Regex _ukPostcodePattern = new Regex(
