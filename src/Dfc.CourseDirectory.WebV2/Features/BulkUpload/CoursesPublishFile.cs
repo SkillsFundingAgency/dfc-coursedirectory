798dbd3d7c674805ef03409aa1d199df4a85aff4
Tim Abell <tim@timwise.co.uk>
2020-10-16 15:07:51 +0100

    Flatten class into namespace
    PR feedback
    
    * Plus organised usings

--- src/Dfc.CourseDirectory.WebV2/Features/BulkUpload/CoursesPublishFile.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/BulkUpload/CoursesPublishFile.cs
@@ -3,47 +3,43 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Dfc.CourseDirectory.Core.DataStore.CosmosDb;
-using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries;
 using Dfc.CourseDirectory.Core.Models;
 using MediatR;
 using CosmosQueries = Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries;
 
-namespace Dfc.CourseDirectory.WebV2.Features.BulkUpload
+namespace Dfc.CourseDirectory.WebV2.Features.BulkUpload.CoursesPublishFile
 {
-    public class CoursesPublishFile
+    public class Query : IRequest<ViewModel>
     {
-        public class Query : IRequest<ViewModel>
+        public Guid ProviderId { get; set; }
+    }
+
+    public class ViewModel
+    {
+        public int NumberOfCourses { get; set; }
+    }
+
+    public class QueryHandler : IRequestHandler<Query, ViewModel>
+    {
+        private readonly ICosmosDbQueryDispatcher _cosmosDbQueryDispatcher;
+
+        public QueryHandler(ICosmosDbQueryDispatcher cosmosDbQueryDispatcher)
         {
-            public Guid ProviderId { get; set; }
+            _cosmosDbQueryDispatcher = cosmosDbQueryDispatcher;
         }
 
-        public class ViewModel
+        public async Task<ViewModel> Handle(Query request, CancellationToken cancellationToken)
         {
-            public int NumberOfCourses { get; set; }
-        }
-
-        public class QueryHandler : IRequestHandler<Query, ViewModel>
-        {
-            private readonly ICosmosDbQueryDispatcher _cosmosDbQueryDispatcher;
-
-            public QueryHandler(ICosmosDbQueryDispatcher cosmosDbQueryDispatcher)
+            var cosmosProvider = await _cosmosDbQueryDispatcher.ExecuteQuery(new CosmosQueries.GetProviderById()
             {
-                _cosmosDbQueryDispatcher = cosmosDbQueryDispatcher;
-            }
-
-            public async Task<ViewModel> Handle(Query request, CancellationToken cancellationToken)
+                ProviderId = request.ProviderId
+            });
+            var results = await _cosmosDbQueryDispatcher.ExecuteQuery(new CosmosQueries.GetAllCoursesForProvider
             {
-                var cosmosProvider = await _cosmosDbQueryDispatcher.ExecuteQuery(new CosmosQueries.GetProviderById()
-                {
-                    ProviderId = request.ProviderId
-                });
-                var results = await _cosmosDbQueryDispatcher.ExecuteQuery(new GetAllCoursesForProvider
-                {
-                    ProviderUkprn = cosmosProvider.Ukprn,
-                    CourseStatuses = CourseStatus.BulkUploadReadyToGoLive,
-                });
-                return new ViewModel {NumberOfCourses = results.SelectMany(c => c.CourseRuns).Count()};
-            }
+                ProviderUkprn = cosmosProvider.Ukprn,
+                CourseStatuses = CourseStatus.BulkUploadReadyToGoLive,
+            });
+            return new ViewModel {NumberOfCourses = results.SelectMany(c => c.CourseRuns).Count()};
         }
     }
 }
