871e5c5f1f65d711438bd4ba8029647fead8086e
James Gunn <james@gunn.io>
2020-10-22 17:46:16 +0100

    Remove AuthService - it's no longer used (#1708)

--- src/Dfc.CourseDirectory.Services/Interfaces/AuthService/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Dfc.CourseDirectory.Services.Interfaces.AuthService
-{
-    public interface IAuthService
-    {
-        Task<string> GetProviderType(string UKPRN, string roleName);
-    }
-}
