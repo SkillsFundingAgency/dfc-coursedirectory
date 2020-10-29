0deec0ee696549f2af538504b24ada9ff9273471
James Gunn <james@gunn.io>
2020-10-29 13:47:49 +0000

    Upgrade to ASP.NET Core 3.1

--- src/Dfc.CourseDirectory.FindACourseApi/Program.cs
+++ src/Dfc.CourseDirectory.FindACourseApi/Program.cs
@@ -1,23 +1,16 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Dfc.CourseDirectory.FindACourseApi
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build()
-                                      .Run();
-        }
+        public static void Main(string[] args) => CreateWebHostBuilder(args).Build().Run();
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                   .UseStartup<Startup>()
-                   .ConfigureLogging(builder =>
-                   {
-                       builder.AddApplicationInsights("2bdf58ed-77ed-4b5c-8c7a-eea63685945a");
-                   });
+                .ConfigureAppConfiguration(config => config.AddApplicationInsightsSettings())
+                .UseStartup<Startup>();
     }
 }
--- src/Dfc.CourseDirectory.FindACourseApi/Startup.cs
+++ src/Dfc.CourseDirectory.FindACourseApi/Startup.cs
@@ -1,36 +1,22 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Dfc.CourseDirectory.FindACourseApi.Helpers;
 using Dfc.CourseDirectory.FindACourseApi.Interfaces;
 using Dfc.CourseDirectory.FindACourseApi.Services;
 using Dfc.CourseDirectory.FindACourseApi.Settings;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Converters;
-using Swashbuckle.AspNetCore.Swagger;
 
 namespace Dfc.CourseDirectory.FindACourseApi
 {
     public class Startup
     {
-        private readonly IHostingEnvironment _env;
-
-        public Startup(IHostingEnvironment env, IConfiguration configuration)
+        public Startup(IConfiguration configuration)
         {
-            _env = env;
-
-            Configuration = //configuration;
-                new ConfigurationBuilder()
-                .SetBasePath(Environment.CurrentDirectory)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{_env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables()
-                .AddUserSecrets(typeof(Startup).Assembly)
-                .AddApplicationInsightsSettings()
-                .Build();
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -40,29 +26,39 @@ namespace Dfc.CourseDirectory.FindACourseApi
         {
             services
                 .AddMvc()
-                .AddJsonOptions(jsonOptions => jsonOptions.SerializerSettings.Converters.Insert(0, new StringEnumConverter()))
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                .AddNewtonsoftJson(jsonOptions => jsonOptions.SerializerSettings.Converters.Insert(0, new StringEnumConverter()));
 
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "Find a Course API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Find a Course API", Version = "v1" });
             });
 
-            services.Configure<CourseServiceSettings>(Configuration.GetSection(nameof(CourseServiceSettings)))
-                    .Configure<SearchServiceSettings>(Configuration.GetSection(nameof(SearchServiceSettings)))
-                    .AddScoped<ICourseService, CoursesService>()
-                    .AddSingleton<SearchServiceWrapper>()
-                    .AddTransient<ISearchServiceSettings>(sp => sp.GetRequiredService<IOptions<SearchServiceSettings>>().Value);
+            services
+                .Configure<CourseServiceSettings>(Configuration.GetSection(nameof(CourseServiceSettings)))
+                .Configure<SearchServiceSettings>(Configuration.GetSection(nameof(SearchServiceSettings)))
+                .AddScoped<ICourseService, CoursesService>()
+                .AddSingleton<SearchServiceWrapper>()
+                .AddTransient<ISearchServiceSettings>(sp => sp.GetRequiredService<IOptions<SearchServiceSettings>>().Value);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
+            app.UseRouting();
+
             app.UseSwagger(c =>
             {
-                c.PreSerializeFilters.Add((swaggerDoc, httpReq) => swaggerDoc.Host = httpReq.Host.Value);
+                c.SerializeAsV2 = true;
+
+                c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+                {
+                    swaggerDoc.Servers = new List<OpenApiServer>
+                    {
+                        new OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}" }
+                    };
+                });
             });
 
             app.UseSwaggerUI(c =>
@@ -70,7 +66,10 @@ namespace Dfc.CourseDirectory.FindACourseApi
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Find a Course API");
             });
 
-            app.UseMvc();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
