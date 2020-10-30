865a84cd2ef12af92517ee7f1def593ae0bc8a13
James Gunn <james@gunn.io>
2020-10-30 11:26:27 +0000

    Add integration test setup

---
+++ tests/Dfc.CourseDirectory.FindACourseApi.Tests/FindACourseApiApplicationFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Dfc.CourseDirectory.FindACourseApi.Tests
+{
+    [CollectionDefinition("FindACourseApi")]
+    public class FindACourseApiCollection : ICollectionFixture<FindACourseApiApplicationFactory>
+    {
+    }
+
+    public class FindACourseApiApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override IWebHostBuilder CreateWebHostBuilder() => base.CreateWebHostBuilder()
+            .UseEnvironment("Testing");
+    }
+}
---
+++ tests/Dfc.CourseDirectory.FindACourseApi.Tests/TestBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Dfc.CourseDirectory.FindACourseApi.Tests
+{
+    [Collection("FindACourseApi")]
+    public abstract class TestBase : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        public TestBase(FindACourseApiApplicationFactory factory)
+        {
+            Factory = factory;
+            HttpClient = factory.CreateClient();
+        }
+
+        public HttpClient HttpClient { get; }
+
+        public FindACourseApiApplicationFactory Factory { get; }
+    }
+}
