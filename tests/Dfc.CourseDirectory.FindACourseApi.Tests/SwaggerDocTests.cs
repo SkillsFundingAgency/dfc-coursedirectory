b434f20565a124fce1f112a4f3cf6b9fffb9e2ff
James Gunn <james@gunn.io>
2020-10-30 11:31:57 +0000

    Add test that swagger doc is served

---
+++ tests/Dfc.CourseDirectory.FindACourseApi.Tests/SwaggerDocTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Dfc.CourseDirectory.FindACourseApi.Tests
+{
+    public class SwaggerDocTests : TestBase
+    {
+        public SwaggerDocTests(FindACourseApiApplicationFactory factory)
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task SwaggerDocIsServed()
+        {
+            // Arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, "/swagger/v1/swagger.json");
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.Headers.ContentType.MediaType.Should().Be("application/json");
+        }
+    }
+}
