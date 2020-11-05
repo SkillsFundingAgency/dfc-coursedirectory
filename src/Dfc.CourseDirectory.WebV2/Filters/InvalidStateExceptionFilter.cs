9ade4e2f0d76954acfe4d8a0d9421761b7dd2d33
James Gunn <james@gunn.io>
2020-11-05 17:34:06 +0000

    Log a warning for invalid state errors

--- src/Dfc.CourseDirectory.WebV2/Filters/InvalidStateExceptionFilter.cs
+++ src/Dfc.CourseDirectory.WebV2/Filters/InvalidStateExceptionFilter.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Dfc.CourseDirectory.WebV2.Filters
 {
@@ -7,8 +9,12 @@ namespace Dfc.CourseDirectory.WebV2.Filters
     {
         public void OnException(ExceptionContext context)
         {
-            if (context.Exception.GetType() == typeof(InvalidStateException))
+            if (context.Exception is InvalidStateException ex)
             {
+                var loggerFactory = context.HttpContext.RequestServices.GetRequiredService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger<InvalidStateExceptionFilter>();
+                logger.LogWarning(ex, ex.Message);
+
                 context.Result = new BadRequestResult();
                 context.ExceptionHandled = true;
             }
