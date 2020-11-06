a5754efa49e9a5a9ca501a860c25ff8dcd2a5847
James Law <james.law@alterius.co.uk>
2020-11-06 13:55:45 +0000

    Added [AllowDeactivatedProvider] and [AllowAnonymous] to /contact route

--- src/Dfc.CourseDirectory.WebV2/Features/Contact/ContactController.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/Contact/ContactController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dfc.CourseDirectory.WebV2.Filters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Dfc.CourseDirectory.WebV2.Features.Contact
 {
     [Route("contact")]
+    [AllowAnonymous]
+    [AllowDeactivatedProvider]
     public class ContactController : Controller
     {
         [HttpGet]
