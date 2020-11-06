7f8356d013a2ba6607ed447ac09f1dc4532f8c72
James Gunn <james@gunn.io>
2020-11-06 10:59:02 +0000

    Use an enum & helper method for setting the active admin nav link

---
+++ src/Dfc.CourseDirectory.WebV2/SharedViews/AdminTopNavSection.cs
@@ -0,0 +1,11 @@
+﻿namespace Dfc.CourseDirectory.WebV2.SharedViews
+{
+    public enum AdminTopNavSection
+    {
+        HelpdeskDashboard,
+        QualityAssurance,
+        SearchProviders,
+        ManageUsers,
+        MigrationReports
+    }
+}
