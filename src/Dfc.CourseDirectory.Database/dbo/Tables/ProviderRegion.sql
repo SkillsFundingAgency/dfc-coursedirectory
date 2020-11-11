92d7ca46a21fba967ddaad04f3b4e71a14bff53f
James Gunn <james@gunn.io>
2020-11-11 12:02:15 +0000

    Remove redundant tables

--- src/Dfc.CourseDirectory.Database/dbo/Tables/ProviderRegion.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[ProviderRegion] (
-    [ProviderRegionId] INT            NOT NULL,
-    [RegionName]       NVARCHAR (100) NOT NULL,
-    CONSTRAINT [PK_ProviderRegion] PRIMARY KEY CLUSTERED ([ProviderRegionId] ASC)
-);
-
--- src/Dfc.CourseDirectory.Database/dbo/Tables/Subregion.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Subregion] (
-    [SubregionId] NVARCHAR (50) NOT NULL,
-    [Lat]         NVARCHAR (50) NULL,
-    [Long]        NVARCHAR (50) NULL
-);
-
