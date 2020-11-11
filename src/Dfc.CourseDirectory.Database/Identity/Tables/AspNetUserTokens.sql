ad59965b1fee2c7fecd5f67d7219c2a41eeef1fa
James Gunn <james@gunn.io>
2020-11-11 11:57:03 +0000

    Remove ASP NET Identity tables - they're not used

--- src/Dfc.CourseDirectory.Database/Identity/Tables/AspNetUserTokens.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [Identity].[AspNetUserTokens] (
-    [UserId]        NVARCHAR (450) NOT NULL,
-    [LoginProvider] NVARCHAR (128) NOT NULL,
-    [Name]          NVARCHAR (128) NOT NULL,
-    [Value]         NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY CLUSTERED ([UserId] ASC, [LoginProvider] ASC, [Name] ASC)
-);
-
