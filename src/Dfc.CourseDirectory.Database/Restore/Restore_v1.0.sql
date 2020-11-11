a1e6967c21316c94ee6613bb4c62b82973af8df4
James Gunn <james@gunn.io>
2020-11-11 12:01:41 +0000

    Remove Tribal database restore job & scripts

--- src/Dfc.CourseDirectory.Database/Restore/Restore_v1.0.sql
+++
@@ -1,9 +0,0 @@
-----------------------------------------------------------------------
-------------------------------------RESTORE-------------------------------------
-DECLARE @databasename as VARCHAR(150)
-SET @databasename = 'https://dfcdevprovstr.blob.core.windows.net/restore/SFA_CourseDirectory_Backup_' + FORMAT (getdate(), 'yyyy_MM_dd') + '.bak'
---NEW
-RESTORE DATABASE SFA_CourseDirectory 
-FROM URL = @databasename
-GO
-
