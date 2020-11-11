d16ad2037163e93b6794204d40db11e2640327aa
James Gunn <james@gunn.io>
2020-11-11 12:02:15 +0000

    Remove redundant tables

--- src/Dfc.CourseDirectory.Database/dbo/Tables/RecordStatus.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[RecordStatus] (
-    [RecordStatusId]   INT          NOT NULL,
-    [RecordStatusName] VARCHAR (50) NULL,
-    [IsPublished]      BIT          NOT NULL,
-    [IsArchived]       BIT          NOT NULL,
-    [IsDeleted]        BIT          NOT NULL,
-    CONSTRAINT [PK_RecordStatus] PRIMARY KEY CLUSTERED ([RecordStatusId] ASC)
-);
-
