35de940ab06d92b223666d5767df008decb87428
James Gunn <james@gunn.io>
2020-11-11 12:01:41 +0000

    Remove Tribal database restore job & scripts

--- src/Dfc.CourseDirectory.Database/Restore/Credentials_v1.0.sql
+++
@@ -1,5 +0,0 @@
-
-DROP CREDENTIAL [https://dfcdevprovstr.blob.core.windows.net/restore]
-CREATE CREDENTIAL [https://dfcdevprovstr.blob.core.windows.net/restore]
-WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
-SECRET = 'xxxxxxxxxxxxxxxx'
