a53d85598e140f958cdaeefc41cea96d54e93541
James Gunn <james@gunn.io>
2020-11-18 11:45:22 +0000

    Add Course search model & query

---
+++ src/Dfc.CourseDirectory.Core/Search/CourseSearchQuery.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Azure.Search.Documents;
+using Dfc.CourseDirectory.Core.Search.AzureSearch;
+using Dfc.CourseDirectory.Core.Search.Models;
+using Azure.Search.Documents.Models;
+
+namespace Dfc.CourseDirectory.Core.Search
+{
+    public class CourseSearchQuery : IAzureSearchQuery<Course>
+    {
+        private const string RegionBoostScoringProfileName = "region-boost";
+
+        public string CourseName { get; set; }
+
+        public IEnumerable<string> Facets { get; set; }
+
+        public IEnumerable<string> Filters { get; set; }
+
+        public string OrderBy { get; set; }
+
+        public int Size { get; set; }
+
+        public int? Skip { get; set; }
+
+        public (string SearchText, SearchOptions Options) GenerateSearchQuery()
+        {
+            var filter = string.Join(" and ", (Filters ?? Array.Empty<string>())
+                .Append($"{nameof(Course.Status)} eq 1"));
+
+            var searchOptions = new SearchOptions()
+            {
+                Filter = filter,
+                IncludeTotalCount = true,
+                ScoringProfile = RegionBoostScoringProfileName,
+                SearchFields =
+                {
+                    nameof(Course.QualificationCourseTitle),
+                    nameof(Course.CourseName)
+                },
+                SearchMode = SearchMode.All,
+                Size = Size,
+                Skip = Skip,
+                QueryType = SearchQueryType.Full
+            };
+
+            if (Facets != null)
+            {
+                foreach (var facet in Facets)
+                {
+                    searchOptions.Facets.Add(facet);
+                }
+            }
+
+            if (OrderBy != null)
+            {
+                searchOptions.OrderBy.Add(OrderBy);
+            }
+
+            return (CourseName, searchOptions);
+        }
+    }
+}
---
+++ src/Dfc.CourseDirectory.Core/Search/Models/Course.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Dfc.CourseDirectory.Core.Models;
+using Microsoft.Spatial;
+
+namespace Dfc.CourseDirectory.Core.Search.Models
+{
+    public class Course
+    {
+        public string id { get; set; }
+        public Guid CourseId { get; set; }
+        public Guid CourseRunId { get; set; }
+        public string QualificationCourseTitle { get; set; }
+        public string LearnAimRef { get; set; }
+        public string NotionalNVQLevelv2 { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public string VenueName { get; set; }
+        public string VenueAddress { get; set; }
+        [JsonConverter(typeof(Azure.Core.Serialization.MicrosoftSpatialGeoJsonConverter))]
+        public GeographyPoint VenueLocation { get; set; }
+        public string VenueAttendancePattern { get; set; }
+        public string VenueAttendancePatternDescription { get; set; }
+        public string ProviderName { get; set; }
+        public string Region { get; set; }
+        public decimal ScoreBoost { get; set; }
+        public int? Status { get; set; }
+        public string VenueStudyMode { get; set; }
+        public string VenueStudyModeDescription { get; set; }
+        public string DeliveryMode { get; set; }
+        public string DeliveryModeDescription { get; set; }
+        public DateTime? StartDate { get; set; }
+        public string VenueTown { get; set; }
+        public int? Cost { get; set; }
+        public string CostDescription { get; set; }
+        public string CourseText { get; set; }
+        public string UKPRN { get; set; }
+        public string CourseDescription { get; set; }
+        public string CourseName { get; set; }
+        public bool? FlexibleStartDate { get; set; }
+        public CourseDurationUnit? DurationUnit { get; set; }
+        public int? DurationValue { get; set; }
+        public bool? National { get; set; }
+    }
+}
