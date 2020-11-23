f56a3c05c6fa33952a2116e060288e85c45324c0
James Gunn <james@gunn.io>
2020-11-23 11:46:01 +0000

    Add Regions table to SQL and populate it with reference data

---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Tables/Regions.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [Pttcd].[Regions]
+(
+	[RegionId] VARCHAR(12) NOT NULL CONSTRAINT [PK_Regions] PRIMARY KEY,
+	[Name] NVARCHAR(100) NOT NULL,
+	[ParentRegionId] VARCHAR(12) CONSTRAINT [FK_Regions_ParentRegion] FOREIGN KEY REFERENCES [Pttcd].[Regions] ([RegionId])
+)
