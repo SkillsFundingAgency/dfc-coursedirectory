5dda70e210b2d78919d2fa76ff1b0d2353f6e28e
James Law <james.law@alterius.co.uk>
2020-11-24 09:42:10 +0000

    Added GetCourseByIdHandler

---
+++ src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/Queries/GetCourseById.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Models;
+
+namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries
+{
+    public class GetCourseById : ICosmosDbQuery<Course>
+    {
+        public Guid CourseId { get; set; }
+    }
+}
---
+++ src/Dfc.CourseDirectory.Core/DataStore/CosmosDb/QueryHandlers/GetCourseByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Models;
+using Dfc.CourseDirectory.Core.DataStore.CosmosDb.Queries;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+
+namespace Dfc.CourseDirectory.Core.DataStore.CosmosDb.QueryHandlers
+{
+    public class GetCourseByIdHandler : ICosmosDbQueryHandler<GetCourseById, Course>
+    {
+        public async Task<Course> Execute(DocumentClient client, Configuration configuration, GetCourseById request)
+        {
+            var collectionUri = UriFactory.CreateDocumentCollectionUri(
+                configuration.DatabaseId,
+                configuration.CoursesCollectionName);
+
+            var response = await client.CreateDocumentQuery<Course>(collectionUri, new FeedOptions() { EnableCrossPartitionQuery = true })
+                .Where(p => p.Id == request.CourseId)
+                .AsDocumentQuery()
+                .ExecuteNextAsync();
+
+            return response.SingleOrDefault();
+        }
+    }
+}
