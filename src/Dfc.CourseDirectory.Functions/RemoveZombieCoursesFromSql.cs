ad3c51b51b4dcbc3d835c3e5fb2ca7be5051e262
James Gunn <james@gunn.io>
2020-11-25 10:47:58 +0000

    Add a temporary cleanup function for removing zombie courses from SQL
    These were added to Cosmos then hard deleted so our sync process didn't
    'see' their removal. This uses the new LastSyncedFromCosmos column for
    figuring out what to delete (which will always be null for these zombie
    courses). A full sync 'SyncCoursesCosmosCollectionToSql' should be run
    before running this to ensure legit entries have the
    LastSyncedFromCosmos column populated.

---
+++ src/Dfc.CourseDirectory.Functions/RemoveZombieCoursesFromSql.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Dapper;
+using Dfc.CourseDirectory.Core.DataStore.Sql;
+using Microsoft.Azure.WebJobs;
+
+namespace Dfc.CourseDirectory.Functions
+{
+    public class RemoveZombieCoursesFromSql
+    {
+        private readonly ISqlQueryDispatcher _sqlQueryDispatcher;
+
+        public RemoveZombieCoursesFromSql(ISqlQueryDispatcher sqlQueryDispatcher)
+        {
+            _sqlQueryDispatcher = sqlQueryDispatcher;
+        }
+
+        [FunctionName(nameof(RemoveZombieCoursesFromSql))]
+        [NoAutomaticTrigger]
+        public Task Run(string input)
+        {
+            var sql = @"
+select CourseId into #CourseIds from Pttcd.Courses where LastSyncedFromCosmos is null
+
+delete Pttcd.CourseRunRegions
+from Pttcd.CourseRunRegions crr
+join Pttcd.CourseRuns cr on crr.CourseRunId = cr.CourseRunId
+join #CourseIds x on x.CourseId = cr.CourseId
+
+delete Pttcd.CourseRuns
+from Pttcd.CourseRuns cr
+join #CourseIds x on x.CourseId = cr.CourseId
+
+delete Pttcd.Courses
+from Pttcd.Courses c
+join #CourseIds x on x.CourseId = c.CourseId
+
+drop table #CourseIds";
+
+            return _sqlQueryDispatcher.Transaction.Connection.ExecuteAsync(
+                sql,
+                transaction: _sqlQueryDispatcher.Transaction);
+        }
+    }
+}
