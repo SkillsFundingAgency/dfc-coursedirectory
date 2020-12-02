449b919abb735e15c95089a23dd6da808e209f0e
James Gunn <james@gunn.io>
2020-12-02 18:19:15 +0000

    Remove data-testid attributes when running in production mode

---
+++ src/Dfc.CourseDirectory.WebV2/TagHelpers/RemoveTestDataAttributesTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Hosting;
+
+namespace Dfc.CourseDirectory.WebV2.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = "data-testid")]
+    public class RemoveTestDataAttributesTagHelper : TagHelper
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public override int Order => 99;
+
+        public RemoveTestDataAttributesTagHelper(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (_environment.IsProduction())
+            {
+                output.Attributes.RemoveAll("data-testid");
+            }
+        }
+    }
+}
