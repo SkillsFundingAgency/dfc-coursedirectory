4223c3c01de2c0a231cc269bfd2c06e98c4d3dff
James Gunn <james@gunn.io>
2020-12-03 09:08:54 +0000

    Add helper method for creating a RouteValues dictionary from the current request's query params

---
+++ src/Dfc.CourseDirectory.WebV2/ViewHelpers/RouteValuesHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace Dfc.CourseDirectory.WebV2.ViewHelpers
+{
+    public class RouteValuesHelper
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public RouteValuesHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IDictionary<string, string> FromQueryString()
+        {
+            var context = _httpContextAccessor.HttpContext;
+
+            return context.Request.Query.ToDictionary(q => q.Key, q => q.Value.ToString());
+        }
+    }
+}
