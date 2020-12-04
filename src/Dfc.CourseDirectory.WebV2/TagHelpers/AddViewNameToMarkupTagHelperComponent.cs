a0f7226be2d813a1f21266e88e2e84f5763a10a9
James Gunn <james@gunn.io>
2020-12-04 14:17:41 +0000

    Add the current view's path to markup for testing

---
+++ src/Dfc.CourseDirectory.WebV2/TagHelpers/AddViewNameToMarkupTagHelperComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Hosting;
+
+namespace Dfc.CourseDirectory.WebV2.TagHelpers
+{
+    [HtmlTargetElement("html")]
+    public class AddViewNameToMarkupTagHelperComponent : TagHelper
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public AddViewNameToMarkupTagHelperComponent(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!_environment.IsProduction())
+            {
+                output.Attributes.Add("data-viewpath", ViewContext.View.Path);
+            }
+        }
+    }
+}
