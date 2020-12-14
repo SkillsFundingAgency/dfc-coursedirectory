0ffe59a8c3d049db72e776a43e173e80b61a11ac
James Gunn <james@gunn.io>
2020-12-14 11:36:37 +0000

    PTCD-738 Delete T Level

---
+++ src/Dfc.CourseDirectory.WebV2/Features/TLevels/DeleteTLevel/DeleteTLevel.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core;
+using Dfc.CourseDirectory.Core.DataStore.Sql;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Models;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+using Dfc.CourseDirectory.Core.Validation;
+using FluentValidation.Results;
+using FormFlow;
+using MediatR;
+using OneOf;
+using OneOf.Types;
+using DeleteTLevelQuery = Dfc.CourseDirectory.Core.DataStore.Sql.Queries.DeleteTLevel;
+
+namespace Dfc.CourseDirectory.WebV2.Features.TLevels.DeleteTLevel
+{
+    [JourneyState]
+    public class JourneyModel
+    {
+        public string TLevelName { get; set; }
+        public Guid ProviderId { get; set; }
+    }
+
+    public class Request : IRequest<ViewModel>
+    {
+        public Guid TLevelId { get; set; }
+    }
+
+    public class ViewModel : Command
+    {
+        public string TLevelName { get; set; }
+        public string YourReference { get; set; }
+        public DateTime StartDate { get; set; }
+        public IReadOnlyCollection<string> LocationVenueNames { get; set; }
+    }
+
+    public class Command : IRequest<OneOf<ModelWithErrors<ViewModel>, Success>>
+    {
+        public Guid TLevelId { get; set; }
+        public bool Confirm { get; set; }
+    }
+
+    public class DeletedQuery : IRequest<DeletedViewModel>
+    {
+    }
+
+    public class DeletedViewModel
+    {
+        public Guid ProviderId { get; set; }
+        public string TLevelName { get; set; }
+        public bool HasOtherTLevels { get; set; }
+    }
+
+    public class Handler :
+        IRequestHandler<Request, ViewModel>,
+        IRequestHandler<Command, OneOf<ModelWithErrors<ViewModel>, Success>>,
+        IRequestHandler<DeletedQuery, DeletedViewModel>
+    {
+        private readonly IProviderOwnershipCache _providerOwnershipCache;
+        private readonly ISqlQueryDispatcher _sqlQueryDispatcher;
+        private readonly JourneyInstance<JourneyModel> _journeyInstance;
+
+        public Handler(
+            IProviderOwnershipCache providerOwnershipCache,
+            ISqlQueryDispatcher sqlQueryDispatcher,
+            JourneyInstance<JourneyModel> journeyInstance)
+        {
+            _providerOwnershipCache = providerOwnershipCache;
+            _sqlQueryDispatcher = sqlQueryDispatcher;
+            _journeyInstance = journeyInstance;
+        }
+
+        public async Task<ViewModel> Handle(Request request, CancellationToken cancellationToken)
+        {
+            _journeyInstance.ThrowIfCompleted();
+
+            var tLevel = await GetTLevel(request.TLevelId);
+            return CreateViewModel(tLevel);
+        }
+
+        public async Task<OneOf<ModelWithErrors<ViewModel>, Success>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            _journeyInstance.ThrowIfCompleted();
+
+            var tLevel = await GetTLevel(request.TLevelId);
+
+            if (!request.Confirm)
+            {
+                var vm = CreateViewModel(tLevel);
+                var validationResult = new ValidationResult(new[]
+                {
+                    new ValidationFailure(nameof(request.Confirm), "Confirm you want to delete the T Level")
+                });
+                return new ModelWithErrors<ViewModel>(vm, validationResult);
+            }
+
+            await _sqlQueryDispatcher.ExecuteQuery(new DeleteTLevelQuery() { TLevelId = request.TLevelId });
+
+            _providerOwnershipCache.OnTLevelDeleted(request.TLevelId);
+
+            // The next page needs this info - stash it in the JourneyModel
+            // since it will no longer able to query for it.
+            _journeyInstance.UpdateState(new JourneyModel()
+            {
+                TLevelName = tLevel.TLevelDefinition.Name,
+                ProviderId = tLevel.ProviderId
+            });
+
+            _journeyInstance.Complete();
+
+            return new Success();
+        }
+
+        public async Task<DeletedViewModel> Handle(DeletedQuery request, CancellationToken cancellationToken)
+        {
+            _journeyInstance.ThrowIfNotCompleted();
+
+            var providerId = _journeyInstance.State.ProviderId;
+
+            var liveTLevels = await _sqlQueryDispatcher.ExecuteQuery(new GetTLevelsForProvider()
+            {
+                ProviderId = providerId
+            });
+
+            return new DeletedViewModel()
+            {
+                TLevelName = _journeyInstance.State.TLevelName,
+                HasOtherTLevels = liveTLevels.Count > 0,
+                ProviderId = providerId
+            };
+        }
+
+        private ViewModel CreateViewModel(TLevel tLevel) => new ViewModel()
+        {
+            TLevelId = tLevel.TLevelId,
+            LocationVenueNames = tLevel.Locations.Select(l => l.VenueName).ToArray(),
+            StartDate = tLevel.StartDate,
+            TLevelName = tLevel.TLevelDefinition.Name,
+            YourReference = tLevel.YourReference
+        };
+
+        private async Task<TLevel> GetTLevel(Guid tLevelId)
+        {
+            return await _sqlQueryDispatcher.ExecuteQuery(new GetTLevel() { TLevelId = tLevelId }) ??
+                throw new ResourceDoesNotExistException(ResourceType.TLevel, tLevelId);
+        }
+    }
+}
---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/FeatureTests/TLevels/DeleteTLevelTests.cs
@@ -0,0 +1,487 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+using Dfc.CourseDirectory.Core.Models;
+using Dfc.CourseDirectory.Testing;
+using Dfc.CourseDirectory.WebV2.Features.TLevels.DeleteTLevel;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using FormFlow;
+using OneOf;
+using OneOf.Types;
+using Xunit;
+
+namespace Dfc.CourseDirectory.WebV2.Tests.FeatureTests.TLevels
+{
+    public class DeleteTLevelTests : MvcTestBase
+    {
+        public DeleteTLevelTests(CourseDirectoryApplicationFactory factory)
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Get_TLevelDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            var tLevelDefinitions = await TestData.CreateInitialTLevelDefinitions();
+
+            var providerId = await TestData.CreateProvider(
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var tLevelId = Guid.NewGuid();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/t-levels/{tLevelId}/delete");
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Theory]
+        [InlineData(TestUserType.ProviderSuperUser)]
+        [InlineData(TestUserType.ProviderUser)]
+        public async Task Get_UserCannotAccessTLevel_ReturnsForbidden(TestUserType userType)
+        {
+            // Arrange
+            var tLevelDefinitions = await TestData.CreateInitialTLevelDefinitions();
+
+            var anotherProviderId = await TestData.CreateProvider(
+                ukprn: 23456,
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: 12345,
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var venueId = (await TestData.CreateVenue(providerId)).Id;
+
+            var tLevel = await TestData.CreateTLevel(
+                providerId,
+                tLevelDefinitions.First().TLevelDefinitionId,
+                locationVenueIds: new[] { venueId },
+                createdBy: User.ToUserInfo());
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/t-levels/{tLevel.TLevelId}/delete");
+
+            await User.AsTestUser(userType, anotherProviderId);
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Get_ValidRequest_RendersExpectedOutput()
+        {
+            // Arrange
+            var tLevelDefinitions = await TestData.CreateInitialTLevelDefinitions();
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: 12345,
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var venueId = (await TestData.CreateVenue(providerId, venueName: "T Level venue")).Id;
+
+            var tLevelDefinition = tLevelDefinitions.First();
+
+            var yourReference = "YOUR-REF";
+            var startDate = new DateTime(2021, 10, 1);
+
+            var tLevel = await TestData.CreateTLevel(
+                providerId,
+                tLevelDefinition.TLevelDefinitionId,
+                locationVenueIds: new[] { venueId },
+                yourReference: yourReference,
+                startDate: startDate,
+                createdBy: User.ToUserInfo());
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/t-levels/{tLevel.TLevelId}/delete");
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var doc = await response.GetDocument();
+
+            using (new AssertionScope())
+            {
+                doc.GetSummaryListValueWithKey("T Level name").Should().Be(tLevelDefinition.Name);
+                doc.GetSummaryListValueWithKey("Your reference").Should().Be(yourReference);
+                doc.GetSummaryListValueWithKey("Start date").Should().Be(startDate.ToString("d MMMM yyyy"));
+                doc.GetAllElementsByTestId("tlevel-location-names").Should().Contain(e => e.InnerHtml == "T Level venue");
+            }
+        }
+
+        [Fact]
+        public async Task Post_CourseDoesNotExist_ReturnsNotFound()
+        {
+            // Arrange
+            var tLevelDefinitions = await TestData.CreateInitialTLevelDefinitions();
+
+            var providerId = await TestData.CreateProvider(
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var tLevelId = Guid.NewGuid();
+
+            var requestContent = new FormUrlEncodedContentBuilder()
+                .Add("Confirm", "true")
+                .ToContent();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"/t-levels/{tLevelId}/delete")
+            {
+                Content = requestContent
+            };
+
+            CreateJourneyInstance(tLevelId);
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Theory]
+        [InlineData(TestUserType.ProviderSuperUser)]
+        [InlineData(TestUserType.ProviderUser)]
+        public async Task Post_UserCannotAccessCourse_ReturnsForbidden(TestUserType userType)
+        {
+            // Arrange
+            var tLevelDefinitions = await TestData.CreateInitialTLevelDefinitions();
+
+            var anotherProviderId = await TestData.CreateProvider(
+                ukprn: 23456,
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: 12345,
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var venueId = (await TestData.CreateVenue(providerId, venueName: "T Level venue")).Id;
+
+            var tLevelDefinition = tLevelDefinitions.First();
+
+            var yourReference = "YOUR-REF";
+            var startDate = new DateTime(2021, 10, 1);
+
+            var tLevel = await TestData.CreateTLevel(
+                providerId,
+                tLevelDefinition.TLevelDefinitionId,
+                locationVenueIds: new[] { venueId },
+                yourReference: yourReference,
+                startDate: startDate,
+                createdBy: User.ToUserInfo());
+
+            var requestContent = new FormUrlEncodedContentBuilder()
+                .Add("Confirm", "true")
+                .ToContent();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"/t-levels/{tLevel.TLevelId}/delete")
+            {
+                Content = requestContent
+            };
+
+            CreateJourneyInstance(tLevel.TLevelId);
+
+            await User.AsTestUser(userType, anotherProviderId);
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+        }
+
+        [Fact]
+        public async Task Post_NotConfirmed_ReturnsError()
+        {
+            // Arrange
+            var tLevelDefinitions = await TestData.CreateInitialTLevelDefinitions();
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: 12345,
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var venueId = (await TestData.CreateVenue(providerId, venueName: "T Level venue")).Id;
+
+            var tLevelDefinition = tLevelDefinitions.First();
+
+            var yourReference = "YOUR-REF";
+            var startDate = new DateTime(2021, 10, 1);
+
+            var tLevel = await TestData.CreateTLevel(
+                providerId,
+                tLevelDefinition.TLevelDefinitionId,
+                locationVenueIds: new[] { venueId },
+                yourReference: yourReference,
+                startDate: startDate,
+                createdBy: User.ToUserInfo());
+
+            var requestContent = new FormUrlEncodedContentBuilder()
+                .ToContent();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"/t-levels/{tLevel.TLevelId}/delete")
+            {
+                Content = requestContent
+            };
+
+            CreateJourneyInstance(tLevel.TLevelId);
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            var doc = await response.GetDocument();
+            doc.AssertHasError("Confirm", "Confirm you want to delete the T Level");
+        }
+
+        [Fact]
+        public async Task Post_ValidRequest_DeletesCourseRunAndRedirects()
+        {
+            // Arrange
+            var tLevelDefinitions = await TestData.CreateInitialTLevelDefinitions();
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: 12345,
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var venueId = (await TestData.CreateVenue(providerId, venueName: "T Level venue")).Id;
+
+            var tLevelDefinition = tLevelDefinitions.First();
+
+            var yourReference = "YOUR-REF";
+            var startDate = new DateTime(2021, 10, 1);
+
+            var tLevel = await TestData.CreateTLevel(
+                providerId,
+                tLevelDefinition.TLevelDefinitionId,
+                locationVenueIds: new[] { venueId },
+                yourReference: yourReference,
+                startDate: startDate,
+                createdBy: User.ToUserInfo());
+
+            var requestContent = new FormUrlEncodedContentBuilder()
+                .Add("Confirm", "true")
+                .ToContent();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, $"/t-levels/{tLevel.TLevelId}/delete")
+            {
+                Content = requestContent
+            };
+
+            CreateJourneyInstance(tLevel.TLevelId);
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Found);
+
+            response.Headers.Location.OriginalString.Should()
+                .Be($"/t-levels/{tLevel.TLevelId}/delete/success?providerId={providerId}");
+
+            SqlQuerySpy.VerifyQuery<DeleteTLevel, OneOf<NotFound, Success>>(q => q.TLevelId == tLevel.TLevelId);
+        }
+
+        [Fact]
+        public async Task GetDeleted_RendersExpectedTLevelName()
+        {
+            // Arrange
+            var tLevelDefinitions = await TestData.CreateInitialTLevelDefinitions();
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: 12345,
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var venueId = (await TestData.CreateVenue(providerId, venueName: "T Level venue")).Id;
+
+            var tLevelDefinition = tLevelDefinitions.First();
+
+            var yourReference = "YOUR-REF";
+            var startDate = new DateTime(2021, 10, 1);
+
+            var tLevel = await TestData.CreateTLevel(
+                providerId,
+                tLevelDefinition.TLevelDefinitionId,
+                locationVenueIds: new[] { venueId },
+                yourReference: yourReference,
+                startDate: startDate,
+                createdBy: User.ToUserInfo());
+
+            await WithSqlQueryDispatcher(
+                dispatcher => dispatcher.ExecuteQuery(new DeleteTLevel() { TLevelId = tLevel.TLevelId }));
+
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"/t-levels/{tLevel.TLevelId}/delete/success?providerId={providerId}");
+
+            await User.AsProviderUser(providerId, ProviderType.FE);
+
+            var journeyInstance = CreateJourneyInstance(
+                tLevel.TLevelId,
+                new JourneyModel()
+                {
+                    TLevelName = tLevelDefinition.Name,
+                    ProviderId = providerId
+                });
+
+            journeyInstance.Complete();
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            var doc = await response.GetDocument();
+            doc.GetElementByTestId("TLevelName").TextContent.Should().Be(tLevelDefinition.Name);
+        }
+
+        [Fact]
+        public async Task GetDeleted_NoOtherLiveTLevels_DoesNotRenderViewEditLink()
+        {
+            // Arrange
+            var tLevelDefinitions = await TestData.CreateInitialTLevelDefinitions();
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: 12345,
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var venueId = (await TestData.CreateVenue(providerId, venueName: "T Level venue")).Id;
+
+            var tLevelDefinition = tLevelDefinitions.First();
+
+            var yourReference = "YOUR-REF";
+            var startDate = new DateTime(2021, 10, 1);
+
+            var tLevel = await TestData.CreateTLevel(
+                providerId,
+                tLevelDefinition.TLevelDefinitionId,
+                locationVenueIds: new[] { venueId },
+                yourReference: yourReference,
+                startDate: startDate,
+                createdBy: User.ToUserInfo());
+
+            await WithSqlQueryDispatcher(
+                dispatcher => dispatcher.ExecuteQuery(new DeleteTLevel() { TLevelId = tLevel.TLevelId }));
+
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"/t-levels/{tLevel.TLevelId}/delete/success?providerId={providerId}");
+
+            await User.AsProviderUser(providerId, ProviderType.FE);
+
+            var journeyInstance = CreateJourneyInstance(
+                tLevel.TLevelId,
+                new JourneyModel()
+                {
+                    TLevelName = tLevelDefinition.Name,
+                    ProviderId = providerId
+                });
+
+            journeyInstance.Complete();
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            var doc = await response.GetDocument();
+            doc.GetElementByTestId("ViewEditLink").Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetDeleted_HasOtherLiveCourseRuns_DoesRenderViewEditCopyLink()
+        {
+            // Arrange
+            var tLevelDefinitions = await TestData.CreateInitialTLevelDefinitions();
+
+            var providerId = await TestData.CreateProvider(
+                ukprn: 12345,
+                providerType: ProviderType.TLevels,
+                tLevelDefinitionIds: tLevelDefinitions.Select(tld => tld.TLevelDefinitionId).ToArray());
+
+            var venueId = (await TestData.CreateVenue(providerId, venueName: "T Level venue")).Id;
+
+            var tLevelDefinition = tLevelDefinitions.First();
+
+            var yourReference = "YOUR-REF";
+            var startDate = new DateTime(2021, 10, 1);
+
+            var tLevel = await TestData.CreateTLevel(
+                providerId,
+                tLevelDefinition.TLevelDefinitionId,
+                locationVenueIds: new[] { venueId },
+                yourReference: yourReference,
+                startDate: startDate,
+                createdBy: User.ToUserInfo());
+
+            await WithSqlQueryDispatcher(
+                dispatcher => dispatcher.ExecuteQuery(new DeleteTLevel() { TLevelId = tLevel.TLevelId }));
+
+            var anotherTLevel = await TestData.CreateTLevel(
+                providerId,
+                tLevelDefinitions.Last().TLevelDefinitionId,
+                locationVenueIds: new[] { venueId },
+                createdBy: User.ToUserInfo());
+
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"/t-levels/{tLevel.TLevelId}/delete/success?providerId={providerId}");
+
+            await User.AsProviderUser(providerId, ProviderType.FE);
+
+            var journeyInstance = CreateJourneyInstance(
+                tLevel.TLevelId,
+                new JourneyModel()
+                {
+                    TLevelName = tLevelDefinition.Name,
+                    ProviderId = providerId
+                });
+
+            journeyInstance.Complete();
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+
+            var doc = await response.GetDocument();
+            doc.GetElementByTestId("ViewEditLink").Should().NotBeNull();
+        }
+
+        private JourneyInstance<JourneyModel> CreateJourneyInstance(
+            Guid tLevelId,
+            JourneyModel journeyModel = null)
+        {
+            return CreateJourneyInstance(
+                "DeleteTLevel",
+                configureKeys: keysBuilder => keysBuilder
+                    .With("tLevelId", tLevelId),
+                journeyModel ?? new JourneyModel());
+        }
+    }
+}
