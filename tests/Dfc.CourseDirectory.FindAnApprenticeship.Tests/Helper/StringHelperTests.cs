b8ba2477055eb5c5a9e84ffe0b026607a1f0676d
Steve O'Hara <github@soharadesign.com>
2021-01-11 13:53:49 +0000

    added test project

---
+++ tests/Dfc.CourseDirectory.FindAnApprenticeship.Tests/Helper/StringHelperTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using Dfc.Providerportal.FindAnApprenticeship.Helper;
+
+namespace Dfc.ProviderPortal.FindAnApprenticeship.UnitTests.Helper
+{
+    public class StringHelperTests : IDisposable
+    {
+        public StringHelperTests()
+        {
+
+        }
+
+        public class EnforceSpacesAfterFullstops
+        {
+            public class WhereSpaceIsNotDetected
+            {
+                string _textToProcess = "Content with no spaces between full stops.This should be spaced correctly. As should this...";
+
+                [Fact]
+                public void SpaceIsCorrectlyEnforced()
+                {
+                    // arrange
+                    var expected = "Content with no spaces between full stops. This should be spaced correctly. As should this...";
+
+                    // act
+                    var actual = _textToProcess.EnforceSpacesAfterFullstops();
+
+                    // assert
+                    Assert.Equal(expected, actual);
+                }
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ tests/Dfc.CourseDirectory.FindAnApprenticeship.Tests/Integration/MockHttpMessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dfc.ProviderPortal.FindAnApprenticeship.UnitTests.Integration
+{
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _send;
+
+        public MockHttpMessageHandler(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> send)
+        {
+            _send = send ?? throw new ArgumentNullException(nameof(send));
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return _send(request, cancellationToken);
+        }
+    }
+}
