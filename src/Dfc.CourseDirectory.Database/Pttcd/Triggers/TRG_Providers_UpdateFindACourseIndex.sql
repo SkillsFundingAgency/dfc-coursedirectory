999be23f86667ebbd985d57a29afa442cd348304
James Gunn <james@gunn.io>
2021-01-12 18:21:39 +0000

    Only refresh FAC entries for changed provider when necessary

--- src/Dfc.CourseDirectory.Database/Pttcd/Triggers/TRG_Providers_UpdateFindACourseIndex.sql
+++ src/Dfc.CourseDirectory.Database/Pttcd/Triggers/TRG_Providers_UpdateFindACourseIndex.sql
@@ -11,7 +11,12 @@ BEGIN
 	SELECT cr.CourseRunId FROM Pttcd.CourseRuns cr
 	JOIN Pttcd.Courses c ON cr.CourseId = c.CourseId
 	JOIN inserted x ON c.ProviderUkprn = x.Ukprn
+	JOIN deleted y ON y.ProviderId = x.ProviderId
 	WHERE cr.CourseRunStatus = 1
+	-- Check that at least one mutable field used by FAC index has actually changed
+	AND (y.Alias <> x.Alias
+		OR y.ProviderName <> x.ProviderName
+		OR y.DisplayNameSource <> x.DisplayNameSource)
 
 	SET @Now = GETUTCDATE()
 
