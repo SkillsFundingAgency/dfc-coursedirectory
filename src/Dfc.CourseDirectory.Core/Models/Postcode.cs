83d29dbb4ea115daf613648f3dde6f5d75588ac7
James Gunn <james@gunn.io>
2021-02-01 11:11:42 +0000

    Add Postcode type for centralizing validation & normalization of postcodes

---
+++ src/Dfc.CourseDirectory.Core/Models/Postcode.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Dfc.CourseDirectory.Core.Models
+{
+    public sealed class Postcode : IEquatable<Postcode>
+    {
+        private static readonly Regex _ukPostcodePattern = new Regex(
+            @"^((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})$",
+            RegexOptions.Compiled);
+
+        private readonly string _value;
+
+        public Postcode(string value)
+            : this(value, validated: false)
+        {
+        }
+
+        private Postcode(string value, bool validated)
+        {
+            if (!validated)
+            {
+                if (!ValidateAndNormalize(value, out value))
+                {
+                    throw new ArgumentException("Input is not a valid UK postcode.", nameof(value));
+                }
+            }
+
+            _value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public static bool TryParse(string input, out Postcode postcode)
+        {
+            if (!ValidateAndNormalize(input, out var normalized))
+            {
+                postcode = default;
+                return false;
+            }
+
+            postcode = new Postcode(normalized, validated: true);
+            return true;
+        }
+
+        private static bool ValidateAndNormalize(string input, out string normalized)
+        {
+            if (input == null)
+            {
+                normalized = default;
+                return false;
+            }
+
+            if (!_ukPostcodePattern.IsMatch(input))
+            {
+                normalized = default;
+                return false;
+            }
+
+            // See https://www.bph-postcodes.co.uk/guidetopc.cgi
+
+            var trimmed = input.Replace(" ", "").Trim().ToUpper();
+            normalized = $"{trimmed[0..(trimmed.Length - 3)]} {trimmed[^3..]}";
+            return true;
+        }
+
+        public static bool operator ==(Postcode a, Postcode b) => a.Equals(b);
+
+        public static bool operator !=(Postcode a, Postcode b) => !(a == b);
+
+        public static implicit operator string(Postcode postcode) => postcode._value;
+
+        public override bool Equals(object obj) => obj is Postcode other && Equals(other);
+
+        public bool Equals(Postcode other) => other?._value == _value;
+
+        public override int GetHashCode() => _value.GetHashCode();
+
+        public override string ToString() => _value;
+    }
+}
---
+++ tests/Dfc.CourseDirectory.Core.Tests/ModelTests/PostcodeTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Dfc.CourseDirectory.Core.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace Dfc.CourseDirectory.Core.Tests.ModelTests
+{
+    public class PostcodeTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("x")]
+        public void TryParse_InvalidPostcode_ReturnsFalse(string input)
+        {
+            // Arrange
+            Postcode postcode;
+
+            // Act
+            var result = Postcode.TryParse(input, out postcode);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("x")]
+        public void Constructor_InvalidPostcode_ThrowsArgumentException(string input)
+        {
+            // Arrange
+
+            // Act
+            var ex = Record.Exception(() => new Postcode(input));
+
+            // Assert
+            ex.Should().NotBeNull();
+            ex.Should().BeOfType<ArgumentException>().Subject.Message.Should().Be("Input is not a valid UK postcode. (Parameter 'value')");
+        }
+
+        // Examples from https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom
+        [Theory]
+        [InlineData("EC1A 1BB", "EC1A 1BB")]
+        [InlineData("EC1A1BB", "EC1A 1BB")]
+        [InlineData("ec1a 1bb", "EC1A 1BB")]
+        [InlineData("ec1a1bb", "EC1A 1BB")]
+        [InlineData("W1A 0AX", "W1A 0AX")]
+        [InlineData("W1A0AX", "W1A 0AX")]
+        [InlineData("w1a 0ax", "W1A 0AX")]
+        [InlineData("w1a0ax", "W1A 0AX")]
+        [InlineData("M1 1AE", "M1 1AE")]
+        [InlineData("M11AE", "M1 1AE")]
+        [InlineData("m1 1ae", "M1 1AE")]
+        [InlineData("m11ae", "M1 1AE")]
+        public void TryParse_ValidPostcode_ReturnsTrueAndOutputsNonNullNormalizedPostcode(
+            string input,
+            string expectedNormalizedPostcode)
+        {
+            // Arrange
+            Postcode postcode;
+
+            // Act
+            var result = Postcode.TryParse(input, out postcode);
+
+            // Assert
+            result.Should().BeTrue();
+            postcode.ToString().Should().Equals(expectedNormalizedPostcode);
+        }
+
+        [Theory]
+        [InlineData("EC1A 1BB", "EC1A 1BB", true)]
+        [InlineData("EC1A 1BB", "ec1a 1bb", true)]
+        [InlineData("EC1A1BB", "ec1a 1bb", true)]
+        [InlineData("EC1A1BB", "M1 1AE", false)]
+        public void EqualityOperator_ReturnsExpectedResult(string left, string right, bool expectedResult)
+        {
+            // Arrange
+            var leftPostcode = new Postcode(left);
+            var rightPostcode = new Postcode(right);
+
+            // Act
+            var result = leftPostcode == rightPostcode;
+
+            // Assert
+            result.Should().Equals(expectedResult);
+        }
+
+        [Theory]
+        [InlineData("EC1A 1BB", "EC1A 1BB", false)]
+        [InlineData("EC1A 1BB", "ec1a 1bb", false)]
+        [InlineData("EC1A1BB", "ec1a 1bb", false)]
+        [InlineData("EC1A1BB", "M1 1AE", true)]
+        public void InequalityOperator_ReturnsExpectedResult(string left, string right, bool expectedResult)
+        {
+            // Arrange
+            var leftPostcode = new Postcode(left);
+            var rightPostcode = new Postcode(right);
+
+            // Act
+            var result = leftPostcode != rightPostcode;
+
+            // Assert
+            result.Should().Equals(expectedResult);
+        }
+
+        [Theory]
+        [MemberData(nameof(Equals_ReturnsExpectedResultData))]
+        public void Equals_ReturnsExpectedResult(Postcode left, object right, bool expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var result = left.Equals(right);
+
+            // Assert
+            result.Should().Equals(expectedResult);
+        }
+
+        public static TheoryData<Postcode, object, bool> Equals_ReturnsExpectedResultData => new TheoryData<Postcode, object, bool>()
+        {
+            { new Postcode("EC1A 1BB"), "EC1A 1BB", true },
+            { new Postcode("EC1A 1BB"), new Postcode("ec1a 1bb"), true },
+            { new Postcode("EC1A1BB"), new Postcode("ec1a 1bb"), true },
+            { new Postcode("EC1A1BB"), new Postcode("M1 1AE"), false },
+            { new Postcode("EC1A1BB"), string.Empty, false },
+            { new Postcode("EC1A1BB"), null, false },
+        };
+    }
+}
