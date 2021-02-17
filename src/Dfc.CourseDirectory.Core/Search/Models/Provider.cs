7f0e9f194c0670bcc82f32afa98e2a3d17f6aac2
Tim Abell <tim@timwise.co.uk>
2021-02-17 15:21:18 +0000

    Remove unused provider search fields
    No references to these in the code.

--- src/Dfc.CourseDirectory.Core/Search/Models/Provider.cs
+++ src/Dfc.CourseDirectory.Core/Search/Models/Provider.cs
@@ -14,18 +14,10 @@ namespace Dfc.CourseDirectory.Core.Search.Models
 
         public string Town { get; set; }
 
-        public string Region { get; set; }
-
         public string UKPRN { get; set; }
 
         public int Status { get; set; }
 
         public string ProviderStatus { get; set; }
-
-        public string CourseDirectoryName { get; set; }
-
-        public string TradingName { get; set; }
-
-        public string ProviderAlias { get; set; }
     }
 }
