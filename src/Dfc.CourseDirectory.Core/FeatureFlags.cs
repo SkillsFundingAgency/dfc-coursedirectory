f934da4ce58a4d47ac9ce60722aab4cf71942544
Steve O'Hara <github@soharadesign.com>
2021-03-02 12:13:03 +0000

    Added Open Data live course provider report

--- src/Dfc.CourseDirectory.Core/FeatureFlags.cs
+++ src/Dfc.CourseDirectory.Core/FeatureFlags.cs
@@ -5,6 +5,7 @@
         public const string DataManagement = "DataManagement";
         public const string CoursesDataManagement = "CoursesDataManagement";
         public const string VenuesDataManagement = "VenuesDataManagement";
+        public const string OpenData = "OpenData";
         public const string ApprenticeshipsDataManagement = "ApprenticeshipsDataManagement";        
     }
 }
---
+++ src/Dfc.CourseDirectory.WebV2/Features/OpenData/Reporting/TLevelsReportsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core;
+using Dfc.CourseDirectory.WebV2.Features.OpenData.Reporting.LiveCourseProvidersReport;
+using Dfc.CourseDirectory.WebV2.Filters;
+using Dfc.CourseDirectory.WebV2.Mvc;
+using Dfc.CourseDirectory.WebV2.Security;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.Features.OpenData.Reporting
+{
+    [RequireFeatureFlag(FeatureFlags.OpenData)]
+    [Authorize(Policy = AuthorizationPolicyNames.Admin)]
+    [Route("opendata/reports")]
+    public class OpenDataReportsController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IClock _clock;
+
+        public OpenDataReportsController(IMediator mediator, IClock clock)
+        {
+            _mediator = mediator;
+            _clock = clock;
+        }
+
+        [HttpGet("live-course-providers")]
+        public async Task<IActionResult> LiveCourseProvidersReport() =>
+            await _mediator.SendAndMapResponse(new LiveCourseProvidersReport.Query(),
+                records => new CsvResult<Csv>($"{nameof(LiveCourseProvidersReport)}-{_clock.UtcNow:yyyyMMddHHmmss}.csv", records));
+    }
+}
