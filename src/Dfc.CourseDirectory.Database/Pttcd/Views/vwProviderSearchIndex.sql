02b3820abb727ff8249abc75d2d8bb764f150056
Tim Abell <tim@timwise.co.uk>
2021-03-04 10:14:44 +0000

    Code formatting for vwProviderSearchIndex

--- src/Dfc.CourseDirectory.Database/Pttcd/Views/vwProviderSearchIndex.sql
+++ src/Dfc.CourseDirectory.Database/Pttcd/Views/vwProviderSearchIndex.sql
@@ -3,23 +3,23 @@ AS
 SELECT      p.ProviderId,
             p.Ukprn,
             p.ProviderName,
-            ISNULL(pc.AddressPostTown, pc.AddressItems) Town,
-            pc.AddressPostcode Postcode,
+            ISNULL(pc.AddressPostTown, pc.AddressItems) AS Town,
+            pc.AddressPostcode AS Postcode,
             p.ProviderStatus,
             p.UkrlpProviderStatusDescription,
             p.Version, -- Timestamp (RowVersion) type, used here for cosmos HighWaterMark
 
             -- additional fields to skew the search index score approximately as per the old index
-            coalesce(p.CourseDirectoryName,p.ProviderName) CourseDirectoryName, -- as per previous cosmos-based search index definition
+            COALESCE(p.CourseDirectoryName, p.ProviderName) AS CourseDirectoryName, -- as per previous cosmos-based search index definition
             p.TradingName,
             p.Alias,
             p.UpdatedOn,
-            cast(null as datetime) as DateOnboarded,
-            cast(null as varchar(255)) as Region
+            CAST(NULL AS DATETIME) AS DateOnboarded,
+            CAST(NULL AS VARCHAR(255)) AS Region
 FROM        [Pttcd].[Providers] p
 OUTER APPLY (
     SELECT TOP 1    AddressItems,
-                    NULLIF(AddressPostTown, '') AddressPostTown,
+                    NULLIF(AddressPostTown, '') AS AddressPostTown,
                     AddressPostcode
     FROM            [Pttcd].[ProviderContacts]
     WHERE           ProviderId = p.ProviderId
