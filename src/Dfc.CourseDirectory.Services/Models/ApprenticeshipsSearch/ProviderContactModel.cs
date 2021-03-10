cc88cf8ce92bc13d515ea066ee2c4162083ca1ed
James Law <james.law@alterius.co.uk>
2021-03-10 10:37:59 +0000

    Remove unused ApprenticeshipSearch models from Services.Models

--- src/Dfc.CourseDirectory.Services/Models/ApprenticeshipsSearch/ProviderContactModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Dfc.CourseDirectory.Services.Models.ApprenticeshipsSearch
-{
-    public class ProviderContactModel
-    {
-        public string phone { get; set; }
-        public string email { get; set; }
-        public string contactUsUrl { get; set; }
-    }
-}
