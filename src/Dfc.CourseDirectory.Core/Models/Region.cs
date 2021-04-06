9d82662347ea41ad6e87c7cf26b2b26abc04c438
James Gunn <james@gunn.io>
2021-04-06 14:28:07 +0100

    Add a method to combine a set of sub-regions into their parents

--- src/Dfc.CourseDirectory.Core/Models/Region.cs
+++ src/Dfc.CourseDirectory.Core/Models/Region.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Dfc.CourseDirectory.Core.Models
 {
@@ -7,5 +9,67 @@ namespace Dfc.CourseDirectory.Core.Models
         public string Id { get; set; }
         public string Name { get; set; }
         public IReadOnlyCollection<Region> SubRegions { get; set; }
+
+        /// <summary>
+        /// Combine sub regions into their parent region(s) when all sub regions for that parent region
+        /// are specified.
+        /// </summary>
+        public static IReadOnlyCollection<Region> Reduce(
+            IReadOnlyCollection<Region> allRegions,
+            IEnumerable<string> regionIds)
+        {
+            if (allRegions is null)
+            {
+                throw new ArgumentNullException(nameof(allRegions));
+            }
+
+            if (regionIds is null)
+            {
+                throw new ArgumentNullException(nameof(regionIds));
+            }
+
+            var regionIdsArray = regionIds.Distinct().ToArray();
+
+            var topLevelRegionsById = allRegions.ToDictionary(r => r.Id, r => r);
+
+            var subRegionsWithParentId = allRegions.SelectMany(r => r.SubRegions.Select(sr => (ParentRegion: r, SubRegion: sr)))
+                .ToDictionary(r => r.SubRegion.Id, r => r);
+
+            var result = new List<Region>();
+
+            foreach (var id in regionIdsArray)
+            {
+                if (topLevelRegionsById.TryGetValue(id, out var region))
+                {
+                    // Top-level region
+                    result.Add(region);
+                }
+                else if (subRegionsWithParentId.TryGetValue(id, out var subRegionWithParent))
+                {
+                    // Sub region
+
+                    if (result.Contains(subRegionWithParent.ParentRegion))
+                    {
+                        // We already have the parent region
+                        continue;
+                    }
+                    else if (subRegionWithParent.ParentRegion.SubRegions.All(sr => regionIdsArray.Contains(sr.Id)))
+                    {
+                        // We have every sub region for this root region specified
+                        result.Add(subRegionWithParent.ParentRegion);
+                    }
+                    else
+                    {
+                        result.Add(subRegionWithParent.SubRegion);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException($"Unknown region: '{id}'.");
+                }
+            }
+
+            return result;
+        }
     }
 }
---
+++ tests/Dfc.CourseDirectory.Core.Tests/ModelTests/RegionTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Dfc.CourseDirectory.Core.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace Dfc.CourseDirectory.Core.Tests.ModelTests
+{
+    public class RegionTests
+    {
+        [Fact]
+        public void Reduce_ReturnsCorrectRegions()
+        {
+            // Arrange
+            var subRegion1a = CreateRegion("1a");
+            var region1 = CreateRegion("1", subRegion1a);
+
+            var subRegion2a = CreateRegion("2a");
+            var subRegion2b = CreateRegion("2b");
+            var region2 = CreateRegion("2", subRegion2a, subRegion2b);
+
+            var subRegion3a = CreateRegion("3a");
+            var subRegion3b = CreateRegion("3b");
+            var region3 = CreateRegion("3", subRegion3a, subRegion3b);
+
+            var subRegion4a = CreateRegion("4a");
+            var subRegion4b = CreateRegion("4b");
+            var region4 = CreateRegion("4", subRegion4a, subRegion4b);
+
+            var subRegion5a = CreateRegion("5a");
+            var subRegion5b = CreateRegion("5b");
+            var region5 = CreateRegion("5", subRegion5a, subRegion5b);
+
+            var allRegions = new[]
+            {
+                region1,
+                region2,
+                region3,
+                region4,
+                region5
+            };
+
+            var regionIds = new[]
+            {
+                "1",  // A top-level region
+                "1a", // A sub-region of an already-specified top-level region
+                "2a", // A sub-region of another top-level region
+                "2b", // A sub-region of another top-level region, completing the set of all sub-regions for the region
+                "3a", // A sub-region of yet another top-level region, but it's the only sub-region for this region
+                "4",
+                "4",  // A duplicate top-level region
+                "5a",
+                "5a", // A duplicate sub-region
+            };
+
+            // Act
+            var result = Region.Reduce(allRegions, regionIds);
+
+            // Assert
+            result.Should().BeEquivalentTo(new[]
+            {
+                region1,
+                region2,
+                subRegion3a,
+                region4,
+                subRegion5a
+            });
+
+            static Region CreateRegion(string id, params Region[] subRegions) => new Region()
+            {
+                Id = id,
+                Name = $"Region {id}",
+                SubRegions = subRegions ?? Array.Empty<Region>()
+            };
+        }
+    }
+}
