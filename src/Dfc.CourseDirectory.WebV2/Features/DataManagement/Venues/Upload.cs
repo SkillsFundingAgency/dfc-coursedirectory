2aad1e607d0b30de3f37cd5bd42fa52f1bbe260b
James Gunn <james@gunn.io>
2021-04-09 14:26:22 +0100

    Move all venue data management into Venues namespace
    Also tweaked a few tests to ensure we're flowing provider context
    correctly.

--- src/Dfc.CourseDirectory.WebV2/Features/DataManagement/Venues/Upload.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/DataManagement/Venues/Upload.cs
@@ -2,41 +2,36 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Dfc.CourseDirectory.Core.Validation;
-using Dfc.CourseDirectory.WebV2.Validation;
 using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using OneOf;
 using OneOf.Types;
 
-namespace Dfc.CourseDirectory.WebV2.Features.DataManagement.Venues
+namespace Dfc.CourseDirectory.WebV2.Features.DataManagement.Venues.Upload
 {
     public class Command : IRequest<OneOf<ModelWithErrors<Command>, Success>>
     {
-        public Guid ProviderId { get; set; }
         public IFormFile File { get; set; }
     }
 
     public class Handler : IRequestHandler<Command, OneOf<ModelWithErrors<Command>, Success>>
     {
-        private readonly CommandValidator _validator;
-
-        public Handler()
-        {
-            _validator = new CommandValidator();
-        }
-
         public async Task<OneOf<ModelWithErrors<Command>, Success>> Handle(
             Command request,
             CancellationToken cancellationToken)
         {
-            var result = await _validator.ValidateAsync(request);
+            var validator = new CommandValidator();
+            var result = await validator.ValidateAsync(request);
 
             if (!result.IsValid)
+            {
                 return new ModelWithErrors<Command>(request, result);
+            }
             else
+            {
                 return new Success();
+            }
         }
     }
 
