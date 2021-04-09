9c8ea3b57b1010eba085f0209e8128f57f7022b9
James Gunn <james@gunn.io>
2021-04-09 10:46:27 +0100

    PTCD-955 Download venues data file

---
+++ src/Dfc.CourseDirectory.WebV2/Features/DataManagement/Venues/Download.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core;
+using Dfc.CourseDirectory.Core.DataManagement.Schemas;
+using Dfc.CourseDirectory.Core.DataStore.Sql;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+using MediatR;
+
+namespace Dfc.CourseDirectory.WebV2.Features.DataManagement.Venues.Download
+{
+    public class Query : IRequest<Response>
+    {
+    }
+
+    public class Response
+    {
+        public string FileName { get; set; }
+        public IReadOnlyCollection<VenueRow> Rows { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Query, Response>
+    {
+        private readonly IProviderContextProvider _providerContextProvider;
+        private readonly ISqlQueryDispatcher _sqlQueryDispatcher;
+        private readonly IClock _clock;
+
+        public Handler(
+            IProviderContextProvider providerContextProvider,
+            ISqlQueryDispatcher sqlQueryDispatcher,
+            IClock clock)
+        {
+            _providerContextProvider = providerContextProvider;
+            _sqlQueryDispatcher = sqlQueryDispatcher;
+            _clock = clock;
+        }
+
+        public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var providerContext = _providerContextProvider.GetProviderContext();
+
+            var venues = await _sqlQueryDispatcher.ExecuteQuery(new GetVenuesForProvider()
+            {
+                ProviderId = providerContext.ProviderInfo.ProviderId
+            });
+
+            var rows = venues
+                .OrderBy(v => v.ProviderVenueRef)
+                .ThenBy(v => v.VenueName)
+                .Select(VenueRow.FromModel)
+                .ToList();
+
+            var fileName = FileNameHelper.SanitizeFileName(
+                $"{providerContext.ProviderInfo.ProviderName}_venues_{_clock.UtcNow:yyyyMMddHHmm}.csv");
+
+            return new Response()
+            {
+                FileName = fileName,
+                Rows = rows
+            };
+        }
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/Features/DataManagement/Venues/VenuesDataManagementController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core;
+using Dfc.CourseDirectory.Core.DataManagement.Schemas;
+using Dfc.CourseDirectory.WebV2.Filters;
+using Dfc.CourseDirectory.WebV2.Mvc;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.Features.DataManagement.Venues
+{
+    [Route("data-upload/venues")]
+    [RequireProviderContext]
+    [RequireFeatureFlag(FeatureFlags.DataManagement)]
+    public class VenuesDataManagementController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public VenuesDataManagementController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("download")]
+        public async Task<IActionResult> Download() => await _mediator.SendAndMapResponse(
+            new Download.Query(),
+            result => new CsvResult<VenueRow>(result.FileName, result.Rows));
+    }
+}
