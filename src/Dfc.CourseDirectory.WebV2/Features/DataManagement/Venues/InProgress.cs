8b5f693c062e748121fa7a78d5cae9c9d00a961c
James Gunn <james@gunn.io>
2021-04-13 14:43:25 +0100

    PTCD-918 Venue upload in progress page

---
+++ src/Dfc.CourseDirectory.WebV2/Features/DataManagement/Venues/InProgress.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataStore.Sql;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+using Dfc.CourseDirectory.Core.Models;
+using MediatR;
+using OneOf;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.WebV2.Features.DataManagement.Venues.InProgress
+{
+    public class Query : IRequest<OneOf<NotFound, UploadStatus>>
+    {
+    }
+
+    public class Handler : IRequestHandler<Query, OneOf<NotFound, UploadStatus>>
+    {
+        private readonly ISqlQueryDispatcher _sqlQueryDispatcher;
+        private readonly IProviderContextProvider _providerContextProvider;
+
+        public Handler(
+            ISqlQueryDispatcher sqlQueryDispatcher,
+            IProviderContextProvider providerContextProvider)
+        {
+            _sqlQueryDispatcher = sqlQueryDispatcher;
+            _providerContextProvider = providerContextProvider;
+        }
+
+        public async Task<OneOf<NotFound, UploadStatus>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var providerId = _providerContextProvider.GetProviderId();
+
+            var venueUpload = await _sqlQueryDispatcher.ExecuteQuery(new GetLatestIncompleteVenueUploadForProvider()
+            {
+                ProviderId = providerId
+            });
+
+            if (venueUpload == null)
+            {
+                return new NotFound();
+            }
+
+            return venueUpload.UploadStatus;
+        }
+    }
+}
