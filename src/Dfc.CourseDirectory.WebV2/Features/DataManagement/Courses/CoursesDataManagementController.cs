d573e9c1cabfd29a3ed6277967305cdbdcfdca2e
anefiox <huss@anefiox.com>
2021-04-19 17:36:15 +0100

    New course upload page. Went of into tasks in other tickets so will strip some of this out.

---
+++ src/Dfc.CourseDirectory.WebV2/Features/DataManagement/Courses/CoursesDataManagementController.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core;
+using Dfc.CourseDirectory.Core.DataManagement.Schemas;
+using Dfc.CourseDirectory.WebV2.Filters;
+using Dfc.CourseDirectory.WebV2.Mvc;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.Features.DataManagement.Courses
+{
+    [Route("data-upload/courses")]
+    [RequireFeatureFlag(FeatureFlags.DataManagement)]
+    public class CoursesDataManagementController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IProviderContextProvider _providerContextProvider;
+
+        public CoursesDataManagementController(IMediator mediator, IProviderContextProvider providerContextProvider)
+        {
+            _mediator = mediator;
+            _providerContextProvider = providerContextProvider;
+        }
+
+        [HttpGet("")]
+        [RequireProviderContext]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("download")]
+        [RequireProviderContext]
+        public async Task<IActionResult> Download() => await _mediator.SendAndMapResponse(
+            new Download.Query(),
+            result => new CsvResult<CourseRow>(result.FileName, result.Rows));
+
+        [HttpPost("upload")]
+        [RequireProviderContext]
+        public async Task<IActionResult> Upload(Upload.Command command)
+        {
+            var file = Request.Form.Files?.GetFile(nameof(command.File));
+
+            return await _mediator.SendAndMapResponse(
+                new Upload.Command()
+                {
+                    File = file
+                },
+                response => response.Match(
+                    errors => RedirectToAction(nameof(Validation))
+                        .WithProviderContext(_providerContextProvider.GetProviderContext()),
+                    success => RedirectToAction(nameof(CheckAndPublish)))
+                        .WithProviderContext(_providerContextProvider.GetProviderContext()));
+        }
+
+        [HttpGet("validation")]
+        [RequireProviderContext]
+        public IActionResult Validation()
+        {
+            return View();
+        }
+
+        [HttpGet("check-publish")]
+        [RequireProviderContext]
+        public IActionResult CheckAndPublish()
+        {
+            return View();
+        }
+
+        [HttpGet("template")]
+        public IActionResult Template() =>
+           new CsvResult<CourseRow>("courses-template.csv", Enumerable.Empty<CourseRow>());
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/Features/DataManagement/Courses/Upload.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.Validation;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using OneOf;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.WebV2.Features.DataManagement.Courses.Upload
+{
+    public class Command : IRequest<OneOf<ModelWithErrors<Command>, Success>>
+    {
+        public IFormFile File { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Command, OneOf<ModelWithErrors<Command>, Success>>
+    {
+        public async Task<OneOf<ModelWithErrors<Command>, Success>> Handle(
+            Command request,
+            CancellationToken cancellationToken)
+        {
+            var validator = new CommandValidator();
+            var result = await validator.ValidateAsync(request);
+
+            if (!result.IsValid)
+            {
+                return new ModelWithErrors<Command>(request, result);
+            }
+            else
+            {
+                return new Success();
+            }
+        }
+    }
+
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
+        {
+            RuleFor(x => x.File).NotNull().WithMessage("Select a CSV");
+            RuleFor(x => x.File.ContentType).Equal("text/csv", StringComparer.OrdinalIgnoreCase).Unless(item => item.File == null).WithMessage("File must be a csv file");
+        }
+    }
+}
