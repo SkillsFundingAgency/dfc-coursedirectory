abe507d56b78d9b48381bc78e23642b007b1078a
anefiox <huss@anefiox.com>
2021-04-21 15:41:56 +0100

    Commit to change repo

---
+++ src/Dfc.CourseDirectory.Core/DataManagement/ICourseUploadProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.Models;
+
+namespace Dfc.CourseDirectory.Core.DataManagement
+{
+    public interface ICourseUploadProcessor
+    {
+        Task<SaveFileResult> SaveFile(Guid providerId, Stream stream, UserInfo uploadedBy);
+    }
+}
