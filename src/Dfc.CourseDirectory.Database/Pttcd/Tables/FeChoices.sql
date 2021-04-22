7989293f6867049640ba8c455149c048b5a80f8e
Kevin Joy <kevin.joy@esynergy-solutions.co.uk>
2021-04-22 15:15:25 +0100

    initial changes

---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Tables/FeChoices.sql
@@ -0,0 +1,12 @@
+﻿ CREATE TABLE [Pttcd].[FeChoices]
+ (
+    ID UniqueIdentifier NOT NULL CONSTRAINT [PK_FeChoices] PRIMARY KEY,
+	Ukprn INT NOT NULL,
+	LearnerSatisfaction DECIMAL(2,1) NULL,
+	EmployerSatisfaction DECIMAL(2,1) NULL,
+    CreatedDateTimeUtc DATETIME NOT NULL,
+    CreatedOn DATETIME NOT NULL,
+    CreatedBy NVARCHAR(MAX) NULL,
+    LastUpdatedBy NVARCHAR(MAX) NULL,
+    LastUpdatedOn DATETIME NULL
+)
