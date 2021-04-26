a6af673e9f2790e9aa7b699ac6b532c2952e7d1f
Kevin Joy <kevin.joy@esynergy-solutions.co.uk>
2021-04-26 10:33:33 +0100

    query spacing rather than tabs

--- src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/GetProviderMissingPrimaryContactReportHandler.cs
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/GetProviderMissingPrimaryContactReportHandler.cs
@@ -17,29 +17,29 @@ SELECT DISTINCT p.Ukprn as ProviderUkprn,
                 p.ProviderStatus
 FROM        Pttcd.Providers p
 LEFT JOIN [Pttcd].[ProviderContacts] pc on (p.ProviderID = pc.ProviderId AND pc.ContactType = 'P')
-LEFT OUTER JOIN (SELECT	COUNT(1) as LiveCourseCount, 
+LEFT OUTER JOIN (SELECT COUNT(1) as LiveCourseCount, 
                  ProviderUkprn
-                 FROM		[Pttcd].[Courses] c
-			     WHERE		c.[CourseStatus] = ${(int)CourseStatus.Live}
-			     GROUP BY	ProviderUkprn) cu on cu.ProviderUkprn = p.Ukprn
-LEFT OUTER JOIN ( SELECT    COUNT(1) AS LiveTLevelCount, 
-							[ProviderId]
-                FROM        Pttcd.TLevels t
-                WHERE       t.TLevelStatus = ${(int)TLevelStatus.Live}
-				GROUP BY    [ProviderId]
-            ) tl on tl.ProviderId = p.ProviderId
-LEFT OUTER JOIN ( SELECT    COUNT(1) AS LiveApprenticeshipCount, 
-							[ProviderId]
-                FROM        Pttcd.Apprenticeships a
-                WHERE       a.ApprenticeshipStatus = ${(int)ApprenticeshipStatus.Live}
-				GROUP BY    [ProviderId]
-            ) ap on ap.ProviderId = p.ProviderId
+                 FROM [Pttcd].[Courses] c
+			     WHERE c.[CourseStatus] = ${(int)CourseStatus.Live}
+			     GROUP BY ProviderUkprn) cu on cu.ProviderUkprn = p.Ukprn
+LEFT OUTER JOIN (SELECT COUNT(1) AS LiveTLevelCount, 
+                 [ProviderId]
+                FROM Pttcd.TLevels t
+                WHERE t.TLevelStatus = ${(int)TLevelStatus.Live}
+                GROUP BY [ProviderId]
+                ) tl on tl.ProviderId = p.ProviderId
+LEFT OUTER JOIN ( SELECT COUNT(1) AS LiveApprenticeshipCount, 
+                        [ProviderId]
+                FROM Pttcd.Apprenticeships a
+                WHERE a.ApprenticeshipStatus = ${(int)ApprenticeshipStatus.Live}
+                GROUP BY [ProviderId]
+                ) ap on ap.ProviderId = p.ProviderId
 WHERE (pc.AddressPostcode IS NULL OR (pc.AddressSaonDescription IS NULL AND pc.AddressPaonDescription IS NULL AND pc.AddressStreetDescription IS NULL))
 AND	  p.ProviderStatus = ${(int)ProviderStatus.Onboarded}
 AND    (
-		cu.LiveCourseCount			  > 0 
+        cu.LiveCourseCount            > 0 
         OR tl.LiveTLevelCount         > 0 
-	    OR ap.LiveApprenticeshipCount > 0
+        OR ap.LiveApprenticeshipCount > 0
 	    )
 ORDER BY p.Ukprn ASC
 ";
