7e1acd3746fa5e6554a1faf0806cf7416b0f1b82
MrKevJoy <60096576+MrKevJoy@users.noreply.github.com>
2021-04-26 11:51:31 +0100

    Ptcd-889 providers with no primary contact details (#2023)
    * Initial commit
    
    * fix test & add missing dashboard link test
    
    * removed unwanted columns
    
    * typo
    
    * removed unwanted property
    
    * added some addional tests & reworked sql handler
    
    * changes
    
    * interim checkin
    
    * updated tests
    
    * Added missing test for query result order & rows being correct
    
    * query spacing rather than tabs
    
    * spaces rather than tabs
    
    * recommended changes

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/Queries/GetProviderMissingPrimaryContactReport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Dfc.CourseDirectory.Core.Models;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.Queries
+{
+    public class GetProviderMissingPrimaryContactReport : ISqlQuery<IAsyncEnumerable<ProviderMissingPrimaryContactReportItem>>
+    {
+    }
+
+    public class ProviderMissingPrimaryContactReportItem
+    {
+        public int ProviderUkprn { get; set; }
+        public string ProviderName { get; set; }
+        public ProviderType ProviderType { get; set; }
+        public ProviderStatus ProviderStatus { get; set; }
+    }
+}
