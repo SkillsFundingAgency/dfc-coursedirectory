5fe9e08ac9e53882729f47ed4d8e4a725f0fe9a0
Kevin Joy <kevin.joy@esynergy-solutions.co.uk>
2021-04-28 11:49:23 +0100

    Added handlers for new table for fechoices in sql

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/Models/FeChoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.Models
+{
+    public class FeChoice
+    {
+        public Guid Id { get; set; }
+        public int UKPRN { get; set; }
+        public decimal? LearnerSatisfaction { get; set; }
+        public decimal? EmployerSatisfaction { get; set; }
+        public DateTime? CreatedDateTimeUtc { get; set; }
+    }
+}
---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Tables/FeChoices.sql
@@ -0,0 +1,8 @@
+﻿ CREATE TABLE [Pttcd].[FeChoices]
+ (
+    ID UNIQUEIDENTIFIER NOT NULL CONSTRAINT [PK_FeChoices] PRIMARY KEY,
+    Ukprn INT NOT NULL,
+    LearnerSatisfaction DECIMAL(18,2) NULL,
+    EmployerSatisfaction DECIMAL(18,2) NULL,
+    CreatedDateTimeUtc DATETIME NOT NULL
+)
