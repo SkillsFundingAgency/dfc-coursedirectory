7935a6bca506ee2654da9a003bc1dd8fda0fcfc1
James Gunn <james@gunn.io>
2021-05-06 12:36:26 +0100

    Add check that venue file is no larger than 1MB

--- src/Dfc.CourseDirectory.Core/DataManagement/Constants.cs
+++ src/Dfc.CourseDirectory.Core/DataManagement/Constants.cs
@@ -4,5 +4,7 @@
     {
         public const string ContainerName = "data-uploads";
         public const string VenuesFolder = "venues";
+        public const int VenueFileMaxSizeBytes = 1048576;
+        public const string VenueFileMaxSizeLabel = "1MB";
     }
 }
