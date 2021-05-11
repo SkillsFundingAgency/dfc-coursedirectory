27832c562258c6bbfe45ac0589b7b1bdd19766ff
James Gunn <james@gunn.io>
2021-05-11 12:34:39 +0100

    Add method for mapping an error code to the field it relates to

---
+++ src/Dfc.CourseDirectory.Core/DataManagement/Errors.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Dfc.CourseDirectory.Core.DataManagement
+{
+    public static class Errors
+    {
+        public static string MapVenueErrorToFieldGroup(string errorCode)
+        {
+            switch (errorCode)
+            {
+                case "VENUE_ADDRESS_LINE1_FORMAT":
+                case "VENUE_ADDRESS_LINE1_MAXLENGTH":
+                case "VENUE_ADDRESS_LINE1_REQUIRED":
+                case "VENUE_ADDRESS_LINE2_FORMAT":
+                case "VENUE_ADDRESS_LINE2_MAXLENGTH":
+                case "VENUE_COUNTY_FORMAT":
+                case "VENUE_COUNTY_MAXLENGTH":
+                case "VENUE_POSTCODE_FORMAT":
+                case "VENUE_POSTCODE_REQUIRED":
+                case "VENUE_TOWN_FORMAT":
+                case "VENUE_TOWN_MAXLENGTH":
+                case "VENUE_TOWN_REQUIRED":
+                    return "Address";
+                case "VENUE_EMAIL_FORMAT":
+                    return "Email";
+                case "VENUE_NAME_MAXLENGTH":
+                case "VENUE_NAME_REQUIRED":
+                case "VENUE_NAME_UNIQUE":
+                    return "Venue name";
+                case "VENUE_PROVIDER_VENUE_REF_MAXLENGTH":
+                case "VENUE_PROVIDER_VENUE_REF_REQUIRED":
+                case "VENUE_PROVIDER_VENUE_REF_UNIQUE":
+                    return "Your venue reference";
+                case "VENUE_TELEPHONE_FORMAT":
+                    return "Telephone";
+                case "VENUE_WEBSITE_FORMAT":
+                    return "Website";
+            }
+
+            throw new ArgumentException($"Unknown error code: '{errorCode}'.", nameof(errorCode));
+        }
+    }
+}
