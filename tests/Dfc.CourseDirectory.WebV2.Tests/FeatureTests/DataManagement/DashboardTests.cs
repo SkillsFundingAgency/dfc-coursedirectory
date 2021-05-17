5b1d94cd6519b549b4c91130f1b0bb91491f8964
Huss <57395585+anefiox@users.noreply.github.com>
2021-05-17 16:15:02 +0100

    Pipeline debugging

--- tests/Dfc.CourseDirectory.WebV2.Tests/FeatureTests/DataManagement/DashboardTests.cs
+++ tests/Dfc.CourseDirectory.WebV2.Tests/FeatureTests/DataManagement/DashboardTests.cs
@@ -80,10 +80,10 @@ namespace Dfc.CourseDirectory.WebV2.Tests.FeatureTests.DataManagement
 
             var venueUpload = await TestData.CreateVenueUpload(providerId: provider.ProviderId, createdBy: User.ToUserInfo(), uploadStatus: UploadStatus.ProcessedWithErrors);
             //Create some venue upload rows to test new data in UI
-            await TestData.CreateVenueUploadRow(venueUpload.VenueUploadId,2);
-            await TestData.CreateVenueUploadRow(venueUpload.VenueUploadId,3);
-            await TestData.CreateVenueUploadRow(venueUpload.VenueUploadId,4);
-            await TestData.CreateVenueUploadRow(venueUpload.VenueUploadId,5);
+            var rowOne = await TestData.CreateVenueUploadRow(venueUpload.VenueUploadId, 2);
+            var rowTwo = await TestData.CreateVenueUploadRow(venueUpload.VenueUploadId, 3);
+            var rowThree = await TestData.CreateVenueUploadRow(venueUpload.VenueUploadId, 4);
+            var rowFour = await TestData.CreateVenueUploadRow(venueUpload.VenueUploadId, 5);
 
             var request = new HttpRequestMessage(HttpMethod.Get, $"/data-upload?providerId={provider.ProviderId}");
 
