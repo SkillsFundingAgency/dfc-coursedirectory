b6033dc19b1e67498679cc0a08d7280cbf7171b2
James Gunn <james@gunn.io>
2021-05-18 14:23:52 +0100

    Move delete course run functionality over to SQL
    Also removed some redundant legacy pages.

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/Queries/DeleteCourseRun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Dfc.CourseDirectory.Core.Models;
+using OneOf;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.Queries
+{
+    public class DeleteCourseRun : ISqlQuery<OneOf<NotFound, Success>>
+    {
+        public Guid CourseId { get; set; }
+        public Guid CourseRunId { get; set; }
+        public UserInfo DeletedBy { get; set; }
+        public DateTime DeletedOn { get; set; }
+    }
+}
