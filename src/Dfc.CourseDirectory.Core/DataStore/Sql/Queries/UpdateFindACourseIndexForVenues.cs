7eefe4efdfe8bc9854ed1739ff786da260b97994
James Gunn <james@gunn.io>
2021-05-25 18:28:55 +0100

    Improve FAC index performance for venue data upload
    The previous trigger-based update was slow due to its row-by-row update.
    This change removes the trigger in favour of a more explicit command
    that passes the amended venue IDs.

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/Queries/UpdateFindACourseIndexForVenues.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.Queries
+{
+    public class UpdateFindACourseIndexForVenues : ISqlQuery<Success>
+    {
+        public IEnumerable<Guid> VenueIds { get; set; }
+        public DateTime Now { get; set; }
+    }
+}
