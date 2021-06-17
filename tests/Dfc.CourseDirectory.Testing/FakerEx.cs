ea7c35a394cc1e9944c71f48a4c974c524e852a7
James Gunn <james@gunn.io>
2021-06-17 11:27:41 +0100

    Fix flaky test caused by invalid address generation
    Also removed a file that should have been deleted.

---
+++ tests/Dfc.CourseDirectory.Testing/FakerEx.cs
@@ -0,0 +1,21 @@
+﻿namespace Dfc.CourseDirectory.Testing
+{
+    public static class FakerEx
+    {
+        public static string StreetAddressSafe()
+        {
+            // Some Address.StreetAddress() results don't validate against our rules;
+            // keep grabbing one until we get a valid result
+
+            string result;
+
+            do
+            {
+                result = Faker.Address.StreetAddress();
+            }
+            while (!Core.Validation.VenueValidation.RuleBuilderExtensions.AddressLinePattern.IsMatch(result));
+
+            return result;
+        }
+    }
+}
