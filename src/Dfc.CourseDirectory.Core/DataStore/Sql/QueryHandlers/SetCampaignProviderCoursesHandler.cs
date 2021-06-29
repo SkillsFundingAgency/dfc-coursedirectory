2c79722a2ee8839cf8594bd56d897541dda74c65
James Gunn <james@gunn.io>
2021-06-29 14:56:30 +0100

    Populate the CampaignCodes field in FAC index

--- src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/SetCampaignProviderCoursesHandler.cs
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/SetCampaignProviderCoursesHandler.cs
@@ -12,11 +12,54 @@ namespace Dfc.CourseDirectory.Core.DataStore.Sql.QueryHandlers
         public async Task<Success> Execute(SqlTransaction transaction, SetCampaignProviderCourses query)
         {
             var sql = @"
+DECLARE @Changes TABLE (
+    Action NVARCHAR(10),
+    AddedProviderUkprn INT,
+    AddedLearnAimRef VARCHAR(50),
+    DeletedProviderUkprn INT,
+    DeletedLearnAimRef VARCHAR(50)
+)
+
 MERGE Pttcd.CampaignProviderCourses AS target
-USING (SELECT @CampaignCode CampaignCode, ProviderUkprn, LearnAimRef FROM @Records) AS source
+USING (SELECT DISTINCT @CampaignCode CampaignCode, ProviderUkprn, LearnAimRef FROM @Records) AS source
 ON target.CampaignCode = source.CampaignCode AND target.ProviderUkprn = source.ProviderUkprn AND target.LearnAimRef = source.LearnAimRef
 WHEN NOT MATCHED THEN INSERT (CampaignCode, ProviderUkprn, LearnAimRef) VALUES (source.CampaignCode, source.ProviderUkprn, source.LearnAimRef)
-WHEN NOT MATCHED BY SOURCE AND target.CampaignCode = @CampaignCode THEN DELETE;";
+WHEN NOT MATCHED BY SOURCE AND target.CampaignCode = @CampaignCode THEN DELETE
+OUTPUT $action, inserted.ProviderUkprn, inserted.LearnAimRef, deleted.ProviderUkprn, deleted.LearnAimRef INTO @Changes
+;
+
+WITH AmendedProviderLearnAimRefs AS (
+    SELECT
+        p.ProviderId,
+        ISNULL(AddedLearnAimRef, DeletedLearnAimRef) AS LearnAimRef
+    FROM @Changes c
+    JOIN Pttcd.Providers p ON ISNULL(c.AddedProviderUkprn, c.DeletedProviderUkprn) = p.Ukprn
+)
+MERGE Pttcd.FindACourseIndexCampaignCodes AS target
+USING (
+    SELECT
+	    x.ProviderId,
+	    x.LearnAimRef,
+        CONCAT('[', STRING_AGG(CASE WHEN cpc.CampaignCode IS NOT NULL THEN CONCAT('""', REPLACE(cpc.CampaignCode, '""', '\""'), '""') ELSE '' END, ','), ']') CampaignCodesJson
+    FROM AmendedProviderLearnAimRefs x
+    LEFT JOIN pttcd.CampaignProviderCourses cpc ON cpc.LearnAimRef = x.LearnAimRef
+    GROUP BY x.ProviderId, x.LearnAimRef
+) AS source
+ON target.ProviderId = source.ProviderId AND target.LearnAimRef = source.LearnAimRef
+WHEN NOT MATCHED THEN INSERT (ProviderId, LearnAimRef, CampaignCodesJson) VALUES (source.ProviderId, source.LearnAimRef, source.CampaignCodesJson)
+WHEN MATCHED THEN UPDATE SET CampaignCodesJson = source.CampaignCodesJson
+;
+
+MERGE Pttcd.FindACourseIndex AS target
+USING (
+    SELECT cc.ProviderId, cc.LearnAimRef, cc.CampaignCodesJson
+    FROM @Changes c
+    JOIN Pttcd.Providers p ON ISNULL(c.AddedProviderUkprn, c.DeletedProviderUkprn) = p.Ukprn
+    JOIN Pttcd.FindACourseIndexCampaignCodes cc ON p.ProviderId = cc.ProviderId AND ISNULL(AddedLearnAimRef, DeletedLearnAimRef) = cc.LearnAimRef
+) AS source
+ON target.ProviderId = source.ProviderId AND target.LearnAimRef = source.LearnAimRef
+WHEN MATCHED THEN UPDATE SET CampaignCodes = source.CampaignCodesJson
+;";
 
             var paramz = new
             {
---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Tables/FindACourseIndexCampaignCodes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Pttcd].[FindACourseIndexCampaignCodes]
+(
+	[FindACourseIndexCampaignCodeId] BIGINT IDENTITY NOT NULL CONSTRAINT [PK_FindACourseIndexCampaignCodes] PRIMARY KEY,
+	[ProviderId] UNIQUEIDENTIFIER NOT NULL,
+	[LearnAimRef] VARCHAR(50) NOT NULL,
+	[CampaignCodesJson] NVARCHAR(MAX) NOT NULL
+)
