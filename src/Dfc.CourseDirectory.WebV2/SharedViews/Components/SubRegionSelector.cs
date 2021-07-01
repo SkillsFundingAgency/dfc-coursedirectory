86177b0b061b6a711078a36b34f4f85133079dbd
James Gunn <james@gunn.io>
2021-07-01 10:19:38 +0100

    Add view component for selecting sub regions

---
+++ src/Dfc.CourseDirectory.WebV2/SharedViews/Components/SubRegionSelector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dfc.CourseDirectory.WebV2.SharedViews.Components
+{
+    public class SubRegionSelectorViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(
+            string id,
+            string name,
+            IEnumerable<string> selectedSubRegionIds)
+        {
+            var vm = new SubRegionSelectorViewModel()
+            {
+                Id = id,
+                Name = name,
+                SelectedSubRegionIds = selectedSubRegionIds?.ToArray() ?? Array.Empty<string>()
+            };
+
+            return View("~/SharedViews/Components/SubRegionSelector.cshtml", vm);
+        }
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/SharedViews/Components/SubRegionSelectorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Dfc.CourseDirectory.WebV2.SharedViews.Components
+{
+    public class SubRegionSelectorViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public IReadOnlyCollection<string> SelectedSubRegionIds { get; set; }
+    }
+}
