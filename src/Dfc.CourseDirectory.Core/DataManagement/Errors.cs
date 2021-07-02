8f5f65be70785b336382e42cd516d59bfc5be27b
Huss <57395585+anefiox@users.noreply.github.com>
2021-07-02 16:11:16 +0100

    Errors list page

--- src/Dfc.CourseDirectory.Core/DataManagement/Errors.cs
+++ src/Dfc.CourseDirectory.Core/DataManagement/Errors.cs
@@ -39,5 +39,81 @@ namespace Dfc.CourseDirectory.Core.DataManagement
 
             throw new ArgumentException($"Unknown error code: '{errorCode}'.", nameof(errorCode));
         }
+
+        public static string MapCourseErrorToFieldGroup(string errorCode)
+        {
+            switch (errorCode)
+            {
+                case "COURSE_ENTRY_REQUIREMENTS_MAXLENGTH":
+                    return "Entry requirements";
+                case "COURSE_HOW_YOU_WILL_BE_ASSESSED_MAXLENGTH":
+                    return "How will you be assessed";
+                case "COURSE_HOW_YOU_WILL_LEARN_MAXLENGTH":
+                    return "How will you learn";
+                case "COURSE_LARS_QAN_INVALID":
+                case "COURSE_LARS_QAN_REQUIRED":
+                    return "Lars";
+                case "COURSE_WHAT_YOU_WILL_LEARN_MAXLENGTH":
+                    return "What you will learn";
+                case "COURSE_WHAT_CAN_DO_NEXT_MAXLENGTH":
+                    return "What can you do next";
+                case "COURSE_WHAT_YOU_WILL_NEED_TO_BRING_MAXLENGTH":
+                    return "What you will need to bring";
+                case "COURSE_WHERE_NEXT_MAXLENGTH":
+                    return "Where next";
+                case "COURSE_WHO_THIS_COURSE_IS_FOR_MAXLENGTH":
+                case "COURSE_WHO_THIS_COURSE_IS_FOR_REQUIRED":
+                    return "Who is this course for";
+                case "COURSERUN_ATTENDANCE_PATTERN_NOT_ALLOWED":
+                case "COURSERUN_ATTENDANCE_PATTERN_REQUIRED":
+                    return "Attendance pattern";
+                case "COURSERUN_COST_DESCRIPTION_MAXLENGTH":
+                case "COURSERUN_COST_INVALID":
+                case "COURSERUN_COST_REQUIRED":
+                    return "Course cost";
+                case "COURSERUN_COURSE_NAME_FORMAT":
+                case "COURSERUN_COURSE_NAME_MAXLENGTH":
+                case "COURSERUN_COURSE_NAME_REQUIRED":
+                    return "Course name";
+                case "COURSERUN_COURSE_WEB_PAGE_FORMAT":
+                case "COURSERUN_COURSE_WEB_PAGE_MAXLENGTH":
+                    return "Web page";
+                case "COURSERUN_DELIVERY_MODE_REQUIRED":
+                    return "Delivery Mode";
+                case "COURSERUN_DURATION_RANGE":
+                case "COURSERUN_DURATION_REQUIRED":
+                case "COURSERUN_DURATION_UNIT_REQUIRED":
+                    return "Duration";
+                case "COURSERUN_FLEXIBLE_START_DATE_REQUIRED":
+                    return "Flexible start date";
+                case "COURSERUN_NATIONAL_DELIVERY_NOT_ALLOWED":
+                case "COURSERUN_NATIONAL_DELIVERY_REQUIRED":
+                    return "National delivery";
+                case "COURSERUN_PROVIDER_COURSE_REF_FORMAT":
+                case "COURSERUN_PROVIDER_COURSE_REF_MAXLENGTH":
+                    return "Course ref";
+                case "COURSERUN_PROVIDER_VENUE_REF_INVALID":
+                case "COURSERUN_PROVIDER_VENUE_REF_NOT_ALLOWED":
+                    return "Venue Ref";
+                case "COURSERUN_START_DATE_INVALID":
+                case "COURSERUN_START_DATE_NOT_ALLOWED":
+                case "COURSERUN_START_DATE_REQUIRED":
+                    return "Start Date";
+                case "COURSERUN_STUDY_MODE_NOT_ALLOWED":
+                case "COURSERUN_STUDY_MODE_REQUIRED":
+                    return "Study Mode";
+                case "COURSERUN_SUBREGIONS_INVALID":
+                case "COURSERUN_SUBREGIONS_NOT_ALLOWED":
+                case "COURSERUN_SUBREGIONS_REQUIRED":
+                    return "Subregions";
+                case "COURSERUN_VENUE_REQUIRED":
+                case "COURSERUN_VENUE_NAME_INVALID":
+                case "COURSERUN_VENUE_NAME_NOT_ALLOWED":
+                case "COURSERUN_VENUE_NAME_NOT_ALLOWED_WITH_REF":
+                    return "Venue name";
+            }
+
+            throw new ArgumentException($"Unknown error code: '{errorCode}'.", nameof(errorCode));
+        }
     }
 }
