f63e95171ffcfe41e3c3458e8d13f240140382eb
Huss <57395585+anefiox@users.noreply.github.com>
2021-07-12 17:07:56 +0100

    rebase

--- src/Dfc.CourseDirectory.Core/DataManagement/Schemas/CsvCourseRow.cs
+++ src/Dfc.CourseDirectory.Core/DataManagement/Schemas/CsvCourseRow.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using CsvHelper.Configuration.Attributes;
-using Dfc.CourseDirectory.Core.Models;
 using Dfc.CourseDirectory.Core.DataStore.Sql.Models;
 
 namespace Dfc.CourseDirectory.Core.DataManagement.Schemas
