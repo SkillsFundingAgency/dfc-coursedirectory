3806ab68a2b16070da05a1136aa0cc3da5ca29c4
James Gunn <james@gunn.io>
2021-07-15 12:46:11 +0100

    Add support for checking a single entry in ModelState is valid
    The RequireValidModelStateAttribute is used for returning a generic
    error page if request parameters are not valid (e.g. an enum bound from
    a query parameter). However, for POSTs we don't want to necessarily
    block all requests if ModelState is not valid; we want them to flow to
    our validators. This commit adds a constructor overload to
    RequireValidModelStateAttribute where a single key can be specified. In
    this case only that ModelStateEntry for the specified key is checked for
    validity.

--- src/Dfc.CourseDirectory.WebV2/Filters/RequireValidModelStateAttribute.cs
+++ src/Dfc.CourseDirectory.WebV2/Filters/RequireValidModelStateAttribute.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Dfc.CourseDirectory.WebV2.Filters
 {
     public class RequireValidModelStateAttribute : ActionFilterAttribute
     {
+        public RequireValidModelStateAttribute()
+        {
+        }
+
+        public RequireValidModelStateAttribute(string forKey)
+        {
+            ForKey = forKey ?? throw new ArgumentNullException(nameof(forKey));
+        }
+
+        public string ForKey { get; }
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            if (!context.ModelState.IsValid)
+            if (ForKey != null)
+            {
+                if (context.ModelState.TryGetValue(ForKey, out var entry) &&
+                    entry.Errors?.Count > 0)
+                {
+                    context.Result = new BadRequestResult();
+                }
+            }
+            else if (!context.ModelState.IsValid)
             {
                 context.Result = new BadRequestResult();
             }
