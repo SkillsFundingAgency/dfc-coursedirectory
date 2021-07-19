9241a62b18e141794885673dfdf31c2901e732ab
James Gunn <james@gunn.io>
2021-07-19 12:18:30 +0100

    Add method for creating a comma-separated list from an IEnumerable<string>

---
+++ src/Dfc.CourseDirectory.Core/EnumerableExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dfc.CourseDirectory.Core
+{
+    public static class EnumerableExtensions
+    {
+        public static string ToCommaSeparatedString(
+            this IEnumerable<string> values,
+            string finalValuesConjunction = "and")
+        {
+            if (values is null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            if (finalValuesConjunction is null)
+            {
+                throw new ArgumentNullException(nameof(finalValuesConjunction));
+            }
+
+            var valuesArray = values.ToArray();
+
+            if (valuesArray.Length == 0)
+            {
+                return string.Empty;
+            }
+            else if (valuesArray.Length == 1)
+            {
+                return valuesArray[0];
+            }
+            else
+            {
+                return string.Join(", ", valuesArray[0..^2].Append(string.Join($" {finalValuesConjunction} ", valuesArray[^2..])));
+            }
+        }
+    }
+}
---
+++ tests/Dfc.CourseDirectory.Core.Tests/EnumerableExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Dfc.CourseDirectory.Core.Tests
+{
+    public class EnumerableExtensionsTests
+    {
+        [Theory]
+        [MemberData(nameof(ToCommaSeparatedStringData))]
+        public void ToCommaSeparatedString(IEnumerable<string> values, string expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var result = values.ToCommaSeparatedString();
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+
+        public static TheoryData<IEnumerable<string>, string> ToCommaSeparatedStringData { get; } = new TheoryData<IEnumerable<string>, string>()
+        {
+            { new string[0], string.Empty },
+            { new[] { "first" }, "first" },
+            { new[] { "first", "second" }, "first and second" },
+            { new[] { "first", "second", "third" }, "first, second and third" },
+            { new[] { "first", "second", "third", "fourth" }, "first, second, third and fourth" },
+        };
+    }
+}
