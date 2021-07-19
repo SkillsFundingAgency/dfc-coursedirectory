fb639ff9305b44289c1d346c39afd636b9fb731d
Kevin Joy <kevin.joy@esynergy-solutions.co.uk>
2021-07-19 10:38:16 +0100

    SDD-849-CheckApprenticeshipsConformsToSchema - check that file upload conforms to schema

--- src/Dfc.CourseDirectory.Core/DataManagement/Schemas/CsvApprenticeshipRow.cs
+++ src/Dfc.CourseDirectory.Core/DataManagement/Schemas/CsvApprenticeshipRow.cs
@@ -15,5 +15,25 @@ namespace Dfc.CourseDirectory.Core.DataManagement.Schemas
         public string StandardVersion { get; set; }
         [Index(2), Name("APPRENTICESHIP_INFORMATION")]
         public string ApprenticeshipInformation { get; set; }
+        [Index(3), Name("APPRENTICESHIP_WEBPAGE")]
+        public string ApprenticeshipWebpage { get; set; }
+        [Index(4), Name("CONTACT_EMAIL")]
+        public string ContactEmail { get; set; }
+        [Index(5), Name("CONTACT_PHONE")]
+        public string ContactPhone { get; set; }
+        [Index(6), Name("CONTACT_URL")]
+        public string ContactUrl { get; set; }
+        [Index(7), Name("DELIVERY_METHOD")]
+        public string DeliveryMode { get; set; }
+        [Index(8), Name("VENUE")]
+        public string Venue { get; set; }
+        [Index(9), Name("YOUR_VENUE_REFERENCE")]
+        public string YourVenueReference { get; set; }
+        [Index(10), Name("RADIUS")]
+        public string Radius { get; set; }
+        [Index(11), Name("DELIVERY_MODE")]
+        public string NationalDelivery { get; set; }
+        [Index(12), Name("SUB_REGION")]
+        public string SubRegion { get; set; }
     }
 }
