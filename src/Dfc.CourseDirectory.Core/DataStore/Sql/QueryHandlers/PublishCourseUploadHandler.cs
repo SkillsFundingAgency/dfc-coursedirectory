d1b7a30d462f212fbd5a109da0eb24bdbd8944ec
James Gunn <james@gunn.io>
2021-07-21 19:53:17 +0100

    SDD-835 Publish Course Upload

--- src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/PublishCourseUploadHandler.cs
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/PublishCourseUploadHandler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Dapper;
 using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
@@ -24,11 +23,163 @@ BEGIN
     RETURN
 END
 
+DECLARE @ProviderUkprn INT
+
+SELECT @ProviderUkprn = Ukprn FROM Pttcd.Providers p
+JOIN Pttcd.CourseUploads cu ON p.ProviderId = cu.ProviderId
+WHERE cu.CourseUploadId = @CourseUploadId
+
+
+-- Archive all existing courses
+
+DECLARE @CourseIds Pttcd.GuidIdTable
+
+INSERT INTO @CourseIds
+SELECT CourseId FROM Pttcd.Courses
+WHERE ProviderUkprn = @ProviderUkprn
+AND (CourseStatus & (~ {(int)CourseStatus.Archived})) <> 0
+
+UPDATE Pttcd.Courses
+SET CourseStatus = {(int)CourseStatus.Archived}
+FROM Pttcd.Courses c
+JOIN @CourseIds x ON c.CourseId = x.Id
+
+UPDATE Pttcd.CourseRuns 
+SET CourseRunStatus = {(int)CourseStatus.Archived}
+FROM Pttcd.CourseRuns cr
+JOIN @CourseIds x ON cr.CourseId = x.Id
+WHERE cr.CourseRunStatus <> {(int)CourseStatus.Archived}
+
+UPDATE Pttcd.FindACourseIndex
+SET Live = 0
+FROM Pttcd.FindACourseIndex i
+JOIN @CourseIds x ON i.CourseId = x.Id
+
+
+-- Create new courses from the Course Upload's rows
+
+;WITH CoursesCte AS (
+    SELECT
+        CourseId,
+        LearnAimRef,
+        WhoThisCourseIsFor,
+        EntryRequirements,
+        WhatYouWillLearn,
+        HowYouWillLearn,
+        WhatYouWillNeedToBring,
+        HowYouWillBeAssessed,
+        WhereNext,
+        ROW_NUMBER() OVER (PARTITION BY CourseId ORDER BY RowNumber) AS GroupRowNumber
+    FROM Pttcd.CourseUploadRows
+    WHERE CourseUploadId = @CourseUploadId
+    AND CourseUploadRowStatus = {(int)UploadRowStatus.Default}
+)
+INSERT INTO Pttcd.Courses (
+    CourseId,
+    CourseStatus,
+    CreatedOn,
+    CreatedBy,
+    UpdatedOn,
+    UpdatedBy,
+    LearnAimRef,
+    ProviderUkprn,
+    CourseDescription,
+    EntryRequirements,
+    WhatYoullLearn,
+    HowYoullLearn,
+    WhatYoullNeed,
+    HowYoullBeAssessed,
+    WhereNext
+)
+SELECT
+    CourseId,
+    {(int)CourseStatus.Live},
+    @PublishedOn,
+    @PublishedByUserId,
+    @PublishedOn,
+    @PublishedByUserId,
+    LearnAimRef,
+    @ProviderUkprn,
+    WhoThisCourseIsFor,
+    EntryRequirements,
+    WhatYouWillLearn,
+    HowYouWillLearn,
+    WhatYouWillNeedToBring,
+    HowYouWillBeAssessed,
+    WhereNext
+FROM CoursesCte
+WHERE GroupRowNumber = 1
+
+INSERT INTO Pttcd.CourseRuns (
+    CourseRunId,
+    CourseId,
+    CourseRunStatus,
+    CreatedOn,
+    CreatedBy,
+    UpdatedOn,
+    UpdatedBy,
+    CourseName,
+    VenueId,
+    ProviderCourseId,
+    DeliveryMode,
+    FlexibleStartDate,
+    StartDate,
+    CourseWebsite,
+    Cost,
+    CostDescription,
+    DurationUnit,
+    DurationValue,
+    StudyMode,
+    AttendancePattern,
+    [National]
+)
+SELECT
+    CourseRunId,
+    CourseId,
+    {(int)CourseStatus.Live},
+    @PublishedOn,
+    @PublishedByUserId,
+    @PublishedOn,
+    @PublishedByUserId,
+    CourseName,
+    VenueId,
+    ProviderCourseRef,
+    ResolvedDeliveryMode,
+    ResolvedFlexibleStartDate,
+    ResolvedStartDate,
+    CourseWebpage,
+    ResolvedCost,
+    CostDescription,
+    ResolvedDurationUnit,
+    ResolvedDuration,
+    ResolvedStudyMode,
+    ResolvedAttendancePattern,
+    ResolvedNationalDelivery
+FROM Pttcd.CourseUploadRows
+WHERE CourseUploadId = @CourseUploadId
+AND CourseUploadRowStatus = {(int)UploadRowStatus.Default}
+
+INSERT INTO Pttcd.CourseRunSubRegions (CourseRunId, RegionId)
+SELECT cur.CourseRunId, sr.RegionId
+FROM Pttcd.CourseUploadRows cur
+JOIN Pttcd.CourseUploadRowSubRegions sr ON cur.RowNumber = sr.RowNumber AND cur.CourseUploadId = sr.CourseUploadId
+WHERE cur.CourseUploadId = @CourseUploadId
+AND cur.CourseUploadRowStatus = {(int)UploadRowStatus.Default}
+
+
+DECLARE @AddedCourseRunIds Pttcd.GuidIdTable
+
+INSERT INTO @AddedCourseRunIds
+SELECT CourseRunId FROM Pttcd.CourseUploadRows
+WHERE CourseUploadId = @CourseUploadId
+AND CourseUploadRowStatus = {(int)UploadRowStatus.Default}
+
+EXEC Pttcd.RefreshFindACourseIndex @CourseRunIds = @AddedCourseRunIds, @Now = @PublishedOn
+
+
 SELECT 1 AS Status
 
-SELECT r.CourseId FROM Pttcd.CourseUploadRows r
-WHERE r.CourseUploadId = @CourseUploadId
-AND r.CourseUploadRowStatus = {(int)UploadRowStatus.Default}
+SELECT COUNT(*) FROM @AddedCourseRunIds
 ";
 
             var paramz = new
@@ -44,11 +195,11 @@ AND r.CourseUploadRowStatus = {(int)UploadRowStatus.Default}
 
             if (status == 1)
             {
-                var publishedCourseIds = (await reader.ReadAsync<Guid>()).AsList();
+                var publishedCourseRunCount = await reader.ReadSingleAsync<int>();
 
                 return new PublishCourseUploadResult()
                 {
-                    PublishedCount = publishedCourseIds.Count
+                    PublishedCount = publishedCourseRunCount
                 };
             }
             else
---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/FeatureTests/DataManagement/Courses/PublishedTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.WebV2.Features.DataManagement.Courses;
+using FluentAssertions;
+using Xunit;
+
+namespace Dfc.CourseDirectory.WebV2.Tests.FeatureTests.DataManagement.Courses
+{
+    public class PublishedTests : MvcTestBase
+    {
+        public PublishedTests(CourseDirectoryApplicationFactory factory)
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Get_ValidRequest_RendersExpectedContent()
+        {
+            // Arrange
+            var provider = await TestData.CreateProvider();
+
+            var journeyInstance = CreateJourneyInstance(
+                "PublishCourseUpload",
+                keys => keys.With("providerId", provider.ProviderId),
+                new PublishJourneyModel() { CoursesPublished = 3 });
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/data-upload/courses/success?providerId={provider.ProviderId}");
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var doc = await response.GetDocument();
+            doc.GetElementByTestId("PublishedCount").TextContent.Should().Be("3");
+        }
+    }
+}
