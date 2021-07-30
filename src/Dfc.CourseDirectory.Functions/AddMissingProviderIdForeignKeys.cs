9b81cb9fa86004b6867fc3d200985255a1e82cf4
James Gunn <james@gunn.io>
2021-07-30 12:38:53 +0100

    Amend AddMissingProviderIdForeignKeys function to batch its updates (#2167)

--- src/Dfc.CourseDirectory.Functions/AddMissingProviderIdForeignKeys.cs
+++ src/Dfc.CourseDirectory.Functions/AddMissingProviderIdForeignKeys.cs
@@ -18,32 +18,52 @@ namespace Dfc.CourseDirectory.Functions
         [NoAutomaticTrigger]
         public async Task Execute(string input)
         {
-            using var dispatcher = _sqlQueryDispatcherFactory.CreateDispatcher();
-
-            var sql = @"
+            await ExecuteBatchedStatement(@"
 UPDATE Pttcd.Courses
 SET ProviderId = p.ProviderId
 FROM Pttcd.Courses c
 JOIN Pttcd.Providers p ON c.ProviderUkprn = p.Ukprn
-WHERE c.ProviderId IS NULL
+WHERE c.ProviderId IS NULL");
 
+            await ExecuteBatchedStatement(@"
 UPDATE Pttcd.Venues
 SET ProviderId = p.ProviderId
 FROM Pttcd.Venues v
 JOIN Pttcd.Providers p ON v.ProviderUkprn = p.Ukprn
-WHERE v.ProviderId IS NULL
+WHERE v.ProviderId IS NULL");
 
+            await ExecuteBatchedStatement(@"
 UPDATE Pttcd.Apprenticeships
 SET ProviderId = p.ProviderId
 FROM Pttcd.Apprenticeships a
 JOIN Pttcd.Providers p ON a.ProviderUkprn = a.ProviderUkprn
 WHERE a.ProviderId IS NULL OR a.ProviderId = '00000000-0000-0000-0000-000000000000' OR NOT EXISTS (
     SELECT 1 FROM Pttcd.Providers x WHERE x.ProviderId = a.ProviderId
-)";
+)");
 
-            await dispatcher.Transaction.Connection.ExecuteAsync(sql, transaction: dispatcher.Transaction);
+            async Task ExecuteBatchedStatement(string sqlStatement)
+            {
+                const int batchSize = 200;
 
-            await dispatcher.Commit();
+                int updated;
+
+                do
+                {
+                    using var dispatcher = _sqlQueryDispatcherFactory.CreateDispatcher();
+
+                    var batchedStatement = $@"
+SET ROWCOUNT {batchSize}
+
+{sqlStatement}
+
+SELECT @@ROWCOUNT";
+
+                    updated = await dispatcher.Transaction.Connection.QuerySingleAsync<int>(batchedStatement, transaction: dispatcher.Transaction);
+
+                    await dispatcher.Commit();
+                }
+                while (updated == batchSize);
+            }
         }
     }
 }
