88c6798ec8314c158cd18a6c8f3d28c73ea21848
Joseph Casson <jocas@netcompany.com>
2022-03-17 15:25:45 +0000

    Initial Changes

--- src/Dfc.CourseDirectory.WebV2/Features/HelpdeskDashboard/HelpdeskDashboardController.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/HelpdeskDashboard/HelpdeskDashboardController.cs
@@ -1,13 +1,23 @@
 ﻿using Dfc.CourseDirectory.WebV2.Security;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using System.Threading.Tasks;
 
 namespace Dfc.CourseDirectory.WebV2.Features.HelpdeskDashboard
 {
     [Authorize(Policy = AuthorizationPolicyNames.Admin)]
     public class HelpdeskDashboardController : Controller
     {
-        [HttpGet("helpdesk-dashboard")]
-        public IActionResult Dashboard() => View();
+        private readonly IMediator _mediator;
+
+        public HelpdeskDashboardController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("HelpdeskDashboard")]
+        public async Task<IActionResult> Index() =>
+            await _mediator.SendAndMapResponse(new Query(), vm => View("Dashboard", vm));
     }
 }
