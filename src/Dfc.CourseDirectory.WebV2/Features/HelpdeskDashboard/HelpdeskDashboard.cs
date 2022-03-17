7ebbb4546a2d9dd9d128caa593dff015b6b71612
Joseph Casson <jocas@netcompany.com>
2022-03-17 15:25:45 +0000

    Initial Changes

---
+++ src/Dfc.CourseDirectory.WebV2/Features/HelpdeskDashboard/HelpdeskDashboard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core;
+using Dfc.CourseDirectory.Core.DataStore;
+using Dfc.CourseDirectory.Core.DataStore.Sql;
+using MediatR;
+
+namespace Dfc.CourseDirectory.WebV2.Features.HelpdeskDashboard
+{
+    public class Query : IRequest<ViewModel>
+    {
+    }
+    public class ViewModel
+    {
+        public int CourseNumber { get; set; }
+        public int OutofDateCourses { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Query, ViewModel>
+    {
+        private readonly IProviderContextProvider _providerContextProvider;
+        private readonly ISqlQueryDispatcher _sqlQueryDispatcher;
+        private readonly IClock _clock;
+        private readonly IRegionCache _regionCache;
+
+        public Handler(
+            IProviderContextProvider providerContextProvider,
+            ISqlQueryDispatcher sqlQueryDispatcher,
+            IClock clock,
+            IRegionCache regionCache)
+        {
+            _providerContextProvider = providerContextProvider;
+            _sqlQueryDispatcher = sqlQueryDispatcher;
+            _clock = clock;
+            _regionCache = regionCache;
+        }
+
+        public async Task<ViewModel> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var results = await _sqlQueryDispatcher.ExecuteQuery(new GetCountCourses()
+            {
+                Today = _clock.UtcNow.Date
+            });
+            return new ViewModel()
+            {
+                CourseNumber = results.Count,
+                OutofDateCourses = 2
+            };
+        }
+    }
+}
