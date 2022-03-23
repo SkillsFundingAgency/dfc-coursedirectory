7cc743b4acba5f5ffb4087aee0072a465f98cbaa
Joseph Casson <jocas@netcompany.com>
2022-03-23 16:58:15 +0000

    Inital Csv generation complete

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/Queries/GetOutofDateCoursesReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.Queries
+{
+    public class GetOutofDateCoursesReport : ISqlQuery<IAsyncEnumerable<OutofDateCourseItem>>
+    {
+    }
+
+    public class OutofDateCourseItem
+    {
+        public int ProviderUkprn { get; set; }
+        
+        public string ProviderName { get; set; }
+
+        public string CourseId { get; set; }
+
+        public string CourseRunId { get; set; }
+
+        public string CourseName { get; set; }
+
+        public DateTime StartDate { get; set; }
+    }
+}
