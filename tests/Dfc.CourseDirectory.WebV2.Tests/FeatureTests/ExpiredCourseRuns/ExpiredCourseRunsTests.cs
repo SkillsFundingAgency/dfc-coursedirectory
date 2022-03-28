c687d6994f73c8548a1684a648879e320ae4403b
Luke Weller <33352024+myselfweller@users.noreply.github.com>
2022-03-28 15:14:00 +0100

    produce working unit test to be revisted later

---
+++ tests/Dfc.CourseDirectory.WebV2.Tests/FeatureTests/ExpiredCourseRuns/ExpiredCourseRunsTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace Dfc.CourseDirectory.WebV2.Tests.FeatureTests.ExpiredCourseRuns
+{
+    public class ExpiredCourseRunsTests : MvcTestBase
+    {
+        public ExpiredCourseRunsTests(CourseDirectoryApplicationFactory factory)
+            : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task Get_ReturnsExpectedContent()
+        {
+            // Arrange
+            var provider = await TestData.CreateProvider();
+
+            var course1 = await TestData.CreateCourse(
+                provider.ProviderId,
+                createdBy: User.ToUserInfo(),
+                configureCourseRuns: builder =>
+                {
+                    builder.WithCourseRun(startDate: Clock.UtcNow.Date.AddDays(-1));  // Expired
+                });
+
+            var course2 = await TestData.CreateCourse(
+                provider.ProviderId,
+                createdBy: User.ToUserInfo(),
+                configureCourseRuns: builder =>
+                {
+                    builder.WithCourseRun(startDate: Clock.UtcNow.Date.AddDays(1));  // Not expired
+                });
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/courses/expired?providerId={provider.ProviderId}");
+
+            // Act
+            var response = await HttpClient.SendAsync(request);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        }
+    }
+}
