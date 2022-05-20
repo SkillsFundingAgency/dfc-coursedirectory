e4814f1354a4cefd4db454552d7bb9b1535a9f60
Samir Sabbagh <samir.sabbagh@netcompany.com>
2022-05-20 23:34:25 +0100

    Handover

---
+++ src/Dfc.CourseDirectory.WebV2/Features/ChooseQualification/CheckAndPublish.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core;
+using Dfc.CourseDirectory.Core.DataStore;
+using Dfc.CourseDirectory.Core.DataStore.Sql;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+using Dfc.CourseDirectory.Core.Models;
+using Dfc.CourseDirectory.Core.Validation;
+using Dfc.CourseDirectory.WebV2.MultiPageTransaction;
+using Dfc.CourseDirectory.WebV2.Security;
+using FluentValidation.Results;
+using FormFlow;
+using GovUk.Frontend.AspNetCore;
+using MediatR;
+using OneOf;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.WebV2.Features.ChooseQualification
+{
+
+    public class CheckAndPublish 
+    {
+        public CourseDeliveryMode DeliveryMode { get; set; }
+        public string CourseName { get; set; }
+        public string ProviderCourseRef { get; set; }
+        public DateInput StartDate { get; set; }
+        public bool? FlexibleStartDate { get; set; }
+        public bool? NationalDelivery { get; set; }
+        public IEnumerable<string> SubRegionIds { get; set; }
+        public string CourseWebPage { get; set; }
+        public string Cost { get; set; }
+        public string CostDescription { get; set; }
+        public int? Duration { get; set; }
+        public CourseDurationUnit? DurationUnit { get; set; }
+        public CourseStudyMode? StudyMode { get; set; }
+        public CourseAttendancePattern? AttendancePattern { get; set; }
+        public Guid? VenueId { get; set; }
+    }
+
+}
--- src/Dfc.CourseDirectory.WebV2/Features/ChooseQualification/ChooseQualificationController.cs
+++ src/Dfc.CourseDirectory.WebV2/Features/ChooseQualification/ChooseQualificationController.cs
@@ -154,7 +154,7 @@ namespace Dfc.CourseDirectory.WebV2.Features.ChooseQualification
                 command,
                 result => result.Match<IActionResult>(
                     errors => this.ViewFromErrors(errors).WithViewData("ReturnUrl", returnUrl),
-                    Success => RedirectToAction(nameof(CheckAndPublish))
+                    Success => RedirectToAction(nameof(CheckAndPublish( command )))
                         .WithMptxInstanceId(Flow.InstanceId)
                         .WithProviderContext(_providerContext)));
         }
@@ -162,8 +162,11 @@ namespace Dfc.CourseDirectory.WebV2.Features.ChooseQualification
         [RequireProviderContext]
         [HttpGet("add/check-and-publish")]
         [MptxAction]
-        public IActionResult CheckAndPublish()
+        public IActionResult CheckAndPublish(CourseRun.Command command)
         {
+            var courseName = command.CourseName;
+            var query = new CheckAndPublish();
+            query.CourseName = courseName;
             return View();
         }
 
