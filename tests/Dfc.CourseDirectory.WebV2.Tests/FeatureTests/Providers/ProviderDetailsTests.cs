6203ef5cd85113c089efc6128223daf83b975a31
Lovre-Schonauer <Lovre.SCHONAUER@EDUCATION.GOV.UK>
2023-02-15 11:34:42 +0000

    Apprenticeships hidden and removed as input from unit tests (attempt 6)

--- tests/Dfc.CourseDirectory.WebV2.Tests/FeatureTests/Providers/ProviderDetailsTests.cs
+++ tests/Dfc.CourseDirectory.WebV2.Tests/FeatureTests/Providers/ProviderDetailsTests.cs
@@ -87,7 +87,7 @@ namespace Dfc.CourseDirectory.WebV2.Tests.FeatureTests.Providers
             Assert.Equal("Active", doc.GetSummaryListValueWithKey("Course directory status"));
             Assert.Equal(provider.Ukprn.ToString(), doc.GetSummaryListValueWithKey("UKPRN"));
             Assert.Equal("My Trading Name", doc.GetSummaryListValueWithKey("Trading name"));
-            Assert.Equal("FE Courses", doc.GetSummaryListValueWithKey("Provider type"));
+            //Assert.Equal("FE Courses", doc.GetSummaryListValueWithKey("Provider type"));
         }
 
         [Theory]
