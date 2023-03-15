e57f101f7f270ccd02c0d7aeb6850c6a495a904b
hnandra <32364605+hnandra@users.noreply.github.com>
2023-03-15 14:17:01 +0000

    deleted findanapprenticeship

--- src/Dfc.CourseDirectory.FindAnApprenticeshipApi/Models/Providers/Verificationdetail.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dfc.CourseDirectory.FindAnApprenticeshipApi.Models.Provider
-{
-    public class Verificationdetail
-    {
-        public string VerificationAuthority { get; set; }
-        public string VerificationID { get; set; }
-    }
-}
