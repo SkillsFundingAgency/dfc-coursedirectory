db69fa17db986285f803675e488623aac181bc23
Amandeep Sandhu <github@purpleduckmedia.com>
2023-06-07 15:14:37 +0100

    NCSLT-5706 Fixed performance issues with course reports (#2429)
    * Updated report handler to use temp table due to performance issues
    
    * Fix for performance issues with course reports
    
    ---------
    
    Co-authored-by: Amandeep Sandhu <purpleduckmedia@gmail.com>

---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Indexes/IX_CourseRuns_CourseRunId_CourseId_VenueId.sql
@@ -0,0 +1,1 @@
+﻿CREATE INDEX IX_CourseRuns_CourseRunId_CourseId_VenueId ON [Pttcd].[CourseRuns] (CourseRunId, CourseId, VenueId)
---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Indexes/IX_Courses_CourseId_ProviderId.sql
@@ -0,0 +1,1 @@
+﻿CREATE INDEX IX_Courses_CourseId_ProviderId ON [Pttcd].[Courses] (CourseId, ProviderId)
