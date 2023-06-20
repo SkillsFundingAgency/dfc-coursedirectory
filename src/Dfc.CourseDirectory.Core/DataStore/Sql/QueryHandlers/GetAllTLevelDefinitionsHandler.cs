1ec68edb84c08beea154c7eced44ca701ddfbed7
Lovre-Schonauer <lovre.schonauer@netcompany.com>
2023-06-20 09:24:07 +0100

    Allows T level providers to add any type of T level

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/GetAllTLevelDefinitionsHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Models;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.QueryHandlers
+{
+    public class GetAllTLevelDefinitionsHandler : ISqlQueryHandler<GetAllTLevelDefinitions, IReadOnlyCollection<TLevelDefinition>>
+    {
+        public async Task<IReadOnlyCollection<TLevelDefinition>> Execute(SqlTransaction transaction, GetAllTLevelDefinitions query)
+        {
+            const string sql = @"
+SELECT      d.TLevelDefinitionId, d.FrameworkCode, d.ProgType, d.QualificationLevel, d.Name
+FROM        Pttcd.TLevelDefinitions d
+ORDER BY    d.Name ASC";
+
+            return (await transaction.Connection.QueryAsync<TLevelDefinition>(sql, new { query }, transaction)).ToList();
+        }
+    }
+}
