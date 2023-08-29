127cfa78b99c79d748a05b9c55ebce599745f271
LarryOG <43647901+LarryOG@users.noreply.github.com>
2023-08-29 11:26:03 +0100

    added structure for the CreateProviderFromUkrlpData sql query handler

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/CreateProviderFromUkrlpDataHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Models;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+using OneOf;
+using OneOf.Types;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.QueryHandlers
+{
+    public class CreateProviderFromUkrlpDataHandler : ISqlQueryHandler<CreateProviderFromUkrlpData, Success>
+    {
+        public async Task<Success> Execute(SqlTransaction transaction, CreateProviderFromUkrlpDataHandler query)
+        {
+            var sql = $@"
+INSERT INTO 
+";
+
+            var paramz = new
+            {
+                query.ProviderId,
+                query.Ukprn,
+                query.ProviderName,
+                query.Alias,
+                query.ProviderStatus,
+                query.ProviderType,
+                query.Status,
+                query.DateUpdated,
+                query.UpdatedBy
+            };
+
+            await transaction.Connection.ExecuteAsync(sql, paramz, transaction);
+
+            return new Success();
+        }
+    }
+}
