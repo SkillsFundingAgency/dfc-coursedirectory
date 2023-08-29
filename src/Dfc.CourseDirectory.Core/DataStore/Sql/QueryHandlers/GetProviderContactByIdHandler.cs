e3cefc7f458efbc2cc5089d1d4d79ee7604a58ec
pradeepkumarvoorukonda <87078845+PradeepVoorukonda@users.noreply.github.com>
2023-08-29 14:49:28 +0100

    ProviderContact query and its handler

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/GetProviderContactByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Models;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.QueryHandlers
+{
+    public class GetProviderContactByIdHandler : ISqlQueryHandler<GetProviderContactById, ProviderContact>
+    {
+        public Task<Provider> Execute(SqlTransaction transaction, GetProviderContactById query)
+        {
+            var sql = @$"
+SELECT  ProviderId,
+        Ukprn,
+        ProviderName,
+        ProviderType,
+        Alias,
+        DisplayNameSource,
+        LearnerSatisfaction,
+        EmployerSatisfaction
+FROM    Pttcd.Providers
+WHERE   {nameof(Provider.ProviderId)} = @{nameof(query.ProviderId)}";
+
+            return transaction.Connection.QuerySingleOrDefaultAsync<Provider>(sql, query, transaction);
+        }
+    }
+}
