a01d31d8f9c0285310ac895563278542f52b0568
pradeepkumarvoorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2023-08-29 15:05:01 +0100

    Changes related to ProviderContact

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/QueryHandlers/GetProviderContactByIdHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Models;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.QueryHandlers
+{
+    public class GetProviderContactByIdHandler : ISqlQueryHandler<GetProviderContactById, ProviderContact>
+    {
+        public Task<ProviderContact> Execute(SqlTransaction transaction, GetProviderContactById query)
+        {
+            var sql = @$"
+            SELECT [ProviderContactId]
+                  ,[ProviderId]
+                  ,[ProviderContactIndex]
+                  ,[ContactType]
+                  ,[ContactRole]
+                  ,[AddressSaonDescription]
+                  ,[AddressPaonDescription]
+                  ,[AddressStreetDescription]
+                  ,[AddressLocality]
+                  ,[AddressItems]
+                  ,[AddressPostTown]
+                  ,[AddressCounty]
+                  ,[AddressPostcode]
+                  ,[PersonalDetailsPersonNameTitle]
+                  ,[PersonalDetailsPersonNameGivenName]
+                  ,[PersonalDetailsPersonNameFamilyName]
+                  ,[Telephone1]
+                  ,[Telephone2]
+                  ,[Fax]
+                  ,[WebsiteAddress]
+                  ,[Email]
+              FROM [Pttcd].[ProviderContacts]
+            WHERE {nameof(Provider.ProviderId)} = @{nameof(query.ProviderId)}";
+
+            return transaction.Connection.QuerySingleOrDefaultAsync<ProviderContact>(sql, query, transaction);
+        }
+    }
+}
