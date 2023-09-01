f36d381805901b3870291909113727785d642357
Zhaoming LU <Zhaoming.LU@EDUCATION.GOV.UK>
2023-09-01 16:18:44 +0100

    Add Validity SQL

---
+++ src/Dfc.CourseDirectory.Database/LARS/Tables/Validity.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [LARS].[Validity](
+	[LearnAimRef] [nvarchar](20) NOT NULL,
+	[ValidityCategory] [nvarchar](50) NULL,
+	[StartDate] [datetime]  NOT NULL,
+	[EndDate] [datetime] NULL,
+	[LastNewStartDate] [datetime] NULL,
+	[Created_On] [nvarchar](50) NOT NULL,
+	[Created_By] [nvarchar](50) NOT NULL,
+	[Modified_By] [nvarchar](50) NULL,
+	[Modified_On] [nvarchar](50) NULL
+);
+
