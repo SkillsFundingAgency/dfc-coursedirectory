015d7faded1e7ea7a71498f79ef46f892522d2c3
Amandeep Sandhu <purpleduckmedia@gmail.com>
2023-09-04 21:00:02 +0100

    Added Course Text reference data to SQL
    Updated project to include CourseText table

---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Tables/CourseText.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [Pttcd].[CourseText]
+(
+	[LearnAimRef]					NVARCHAR (10)  NOT NULL CONSTRAINT [PK_CourseText] PRIMARY KEY,
+	[AwardOrgCode]					NVARCHAR (30)  NULL,
+	[QualificationCourseTitle]		NVARCHAR (MAX),
+	[NotionalNVQLevelv2]			NVARCHAR (MAX),
+	[TypeName]						NVARCHAR (MAX),
+	[CourseDescription]				NVARCHAR (MAX),
+	[EntryRequirements]				NVARCHAR (MAX),
+	[WhatYoullLearn]				NVARCHAR (MAX),
+	[HowYoullLearn]					NVARCHAR (MAX),
+	[WhatYoullNeed]					NVARCHAR (MAX),
+	[HowYoullBeAssessed]			NVARCHAR (MAX),
+	[WhereNext]						NVARCHAR (MAX), 
+    CONSTRAINT [FK_CourseText_LearningDelivery_LearnAimRef] FOREIGN KEY ([LearnAimRef]) REFERENCES [LARS].[LearningDelivery]([LearnAimRef])
+
+
+
+
+
+)
