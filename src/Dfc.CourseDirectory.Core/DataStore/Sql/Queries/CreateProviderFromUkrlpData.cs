c8fb93aa089da61f12047d645b9cd5b22f0f733e
LarryOG <43647901+LarryOG@users.noreply.github.com>
2023-09-12 15:34:25 +0100

    removed unnecessary annotation for nullable element from CreateProvider query

--- src/Dfc.CourseDirectory.Core/DataStore/Sql/Queries/CreateProviderFromUkrlpData.cs
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/Queries/CreateProviderFromUkrlpData.cs
@@ -17,6 +17,6 @@ namespace Dfc.CourseDirectory.Core.DataStore.Sql.Queries
         public ProviderType ProviderType { get; set; }
         public DateTime DateUpdated { get; set; }
         public string UpdatedBy { get; set; }
-        public ProviderContact? Contact { get; set; }
+        public ProviderContact Contact { get; set; }
     }
 }
