9d1263fd5fb40f22ca24689f6ab396f4f9401e03
pradeepkumarvoorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2023-10-05 16:13:37 +0100

    Additional logging to controller end points

--- src/Dfc.CourseDirectory.Api/Controllers/FindACourseController.cs
+++ src/Dfc.CourseDirectory.Api/Controllers/FindACourseController.cs
@@ -27,12 +27,13 @@ namespace Dfc.CourseDirectory.Api.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Search([FromBody] FindACourseApi.Features.Search.Query request)
         {
+            _log.LogInformation($"Start Searching for Courses by the Search Criteria [{request}]");
             var result = await _mediator.Send(request);
 
             return result.Match<IActionResult>(
                 p =>
                 {
-                    _log.LogWarning($"{nameof(Search)} failed. {nameof(p.Title)}: {{{nameof(p.Title)}}}, {nameof(p.Detail)}: {{{nameof(p.Detail)}}}.", p.Title, p.Detail);
+                    _log.LogWarning($"Failed to retrieve courses with given search criteria. {nameof(p.Title)}: {{{nameof(p.Title)}}}, {nameof(p.Detail)}: {{{nameof(p.Detail)}}}.", p.Title, p.Detail);
 
                     return new ObjectResult(p)
                     {
@@ -43,7 +44,11 @@ namespace Dfc.CourseDirectory.Api.Controllers
                         StatusCode = p.Status ?? StatusCodes.Status400BadRequest
                     };
                 },
-                r => Ok(r));
+                r =>
+                {
+                    _log.LogInformation($"Courses found. Returning Courses data in Json format. Response Code [OK]");
+                    return Ok(r);
+                });
         }
 
         [HttpGet("~/public/fac/courserundetail")]
@@ -53,11 +58,17 @@ namespace Dfc.CourseDirectory.Api.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> CourseRunDetail([FromQuery] FindACourseApi.Features.CourseRunDetail.Query request)
         {
+            _log.LogInformation($"Start Getting for CourseRunDetail by the CourseId [{request.CourseId}] and CourseRunId [{request.CourseRunId}]");
             var result = await _mediator.Send(request);
 
             return result.Match<IActionResult>(
-                _ => NotFound(),
-                r => Ok(r));
+                _ => {
+                    _log.LogWarning($"Failed to retrieve CourseRun Details with given search criteria.Response Code [NOT FOUND]"); 
+                    return NotFound();
+                },
+                r => {
+                    _log.LogInformation($"CourseRun Details found. Returning data in Json format. Response Code [OK]");
+                    return Ok(r); });
         }
 
         [HttpGet("~/public/fac/tleveldetail")]
@@ -67,11 +78,18 @@ namespace Dfc.CourseDirectory.Api.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> TLevelDetail([FromQuery] FindACourseApi.Features.TLevels.TLevelDetail.Query request)
         {
+            _log.LogInformation($"Start Getting for TLevel Detail by the TlevelID [{request.TLevelId}]");
             var result = await _mediator.Send(request);
 
             return result.Match<IActionResult>(
-                _ => NotFound(),
-                r => Ok(r));
+                _ => {
+                    _log.LogWarning($"Failed to retrieve TLevel Details with given T-Level ID.Response Code [NOT FOUND]");
+                    return NotFound();
+                },
+                r => {
+                    _log.LogInformation($"T-Level Details found. Returning data in Json format. Response Code [OK]");
+                    return Ok(r);
+                });
         }
     }
 }
