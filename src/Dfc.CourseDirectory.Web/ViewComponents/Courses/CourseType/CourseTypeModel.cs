17ee94a6a5318ed05d5ca294b5216217502f093b
pradeepkumarvoorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2023-11-02 15:14:11 +0000

    CourseType - Front end changes added when adding course data

---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/CourseType/CourseTypeModel.cs
@@ -0,0 +1,14 @@
+﻿using Dfc.CourseDirectory.Core.Models;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.CourseType
+{
+    public class CourseTypeModel
+    {
+        //[Required(ErrorMessage = "Select Delivery Mode")]
+        public CourseType? CourseType { get; set; }
+        public string LabelText { get; set; }
+        public string HintText { get; set; }
+        public string SecondHintText { get; set; }
+        public string AriaDescribedBy { get; set; }
+    }
+}
---
+++ src/Dfc.CourseDirectory.Web/ViewComponents/Courses/CourseType/CourseTypeView.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dfc.CourseDirectory.Web.ViewComponents.Courses.CourseType
+{
+    public class CourseTypeView : ViewComponent
+    {
+        public IViewComponentResult Invoke(CourseTypeModel model)
+        {
+            return View("~/ViewComponents/Courses/CourseType/Default.cshtml", model);
+        }
+    }
+}
