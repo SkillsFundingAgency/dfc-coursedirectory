683f8c010e50cec74266b3e80835d0a488a10b33
Muhammad IMRAN <Muhammad.IMRAN@EDUCATION.GOV.UK>
2023-11-08 14:26:17 +0000

    Task #160951 - Add new table Pttcd.CourseTypeCategorydfc-coursedirectory db

---
+++ src/Dfc.CourseDirectory.Database/Pttcd/Tables/CourseTypeCategory.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [Pttcd].[CourseTypeCategory]
+(
+	[CourseType] INT NOT NULL , 
+    [CategoryRef] NVARCHAR(50) NOT NULL FOREIGN KEY REFERENCES [LARS].[Category] ([CategoryRef]), 
+    PRIMARY KEY ([CourseType], [CategoryRef])
+)
