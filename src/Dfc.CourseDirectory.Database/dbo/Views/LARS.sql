80e44001dd10b9c39e845ef02fb456a5bfb58dad
Zhaoming LU <Zhaoming.LU@EDUCATION.GOV.UK>
2023-11-27 10:49:12 +0000

    Add IsExpired column in Lars View

--- src/Dfc.CourseDirectory.Database/dbo/Views/LARS.sql
+++ src/Dfc.CourseDirectory.Database/dbo/Views/LARS.sql
@@ -5,16 +5,18 @@ CREATE VIEW [dbo].[LARS]
 AS
 SELECT        ld.LearnAimRef, ld.LearnAimRefTitle, lt.LearnAimRefTypeDesc, ld.NotionalNVQLevelv2, ld.AwardOrgCode, ld.AwardOrgAimRef, ld.LearnDirectClassSystemCode1, ld.LearnDirectClassSystemCode2, ld.SectorSubjectAreaTier1, 
                          SSA1.SectorSubjectAreaTier1Desc, ld.SectorSubjectAreaTier2, SSA2.SectorSubjectAreaTier2Desc, ld.GuidedLearningHours, ld.TotalQualificationTime, ld.UnitType, ac.AwardOrgName, ld.Modified_On, ld.EffectiveTo, 
-                         ld.CertificationEndDate, ld.OperationalEndDate
-FROM            LARS.LearningDelivery AS ld INNER JOIN
-                         LARS.AwardOrgCode AS ac ON ac.AwardOrgCode = ld.AwardOrgCode INNER JOIN
-                         LARS.LearnAimRefType AS lt ON lt.LearnAimRefType = ld.LearnAimRefType INNER JOIN
-                         LARS.SectorSubjectAreaTier1 AS SSA1 ON SSA1.SectorSubjectAreaTier1 = ld.SectorSubjectAreaTier1 INNER JOIN
-                         LARS.SectorSubjectAreaTier2 AS SSA2 ON SSA2.SectorSubjectAreaTier2 = ld.SectorSubjectAreaTier2
-Where    ld.LearnAimRef in  
+                         ld.CertificationEndDate, ld.OperationalEndDate,
+Case when ld.LearnAimRef in  
 (
 select LearnAimRef from lars.Validity where LastNewStartDate =''
 UNION ALL
 select LearnAimRef from lars.Validity where LearnAimRef not in (
 select LearnAimRef from lars.Validity where LastNewStartDate ='') group by LearnAimRef HAVING max(convert(varchar, LastNewStartDate ,105))>GETDATE()
-)
+) Then 0
+Else 1
+End as IsExpired
+FROM            LARS.LearningDelivery AS ld INNER JOIN
+                         LARS.AwardOrgCode AS ac ON ac.AwardOrgCode = ld.AwardOrgCode INNER JOIN
+                         LARS.LearnAimRefType AS lt ON lt.LearnAimRefType = ld.LearnAimRefType INNER JOIN
+                         LARS.SectorSubjectAreaTier1 AS SSA1 ON SSA1.SectorSubjectAreaTier1 = ld.SectorSubjectAreaTier1 INNER JOIN
+                         LARS.SectorSubjectAreaTier2 AS SSA2 ON SSA2.SectorSubjectAreaTier2 = ld.SectorSubjectAreaTier2
