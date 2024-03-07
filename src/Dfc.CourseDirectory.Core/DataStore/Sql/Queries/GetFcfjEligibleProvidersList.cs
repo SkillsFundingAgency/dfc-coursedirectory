e3a11eca6dc53ea913f35c32791610ecdb3ef0bf
Muhammad IMRAN <Muhammad.IMRAN@EDUCATION.GOV.UK>
2024-03-07 11:47:49 +0000

    Updated logic to retreive free courses for jobs eligible providers list.

---
+++ src/Dfc.CourseDirectory.Core/DataStore/Sql/Queries/GetFcfjEligibleProvidersList.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dfc.CourseDirectory.Core.DataStore.Sql.Queries
+{
+    public class GetFcfjEligibleProvidersList : ISqlQuery<IReadOnlyCollection<Guid>>
+    {        
+    }
+}
