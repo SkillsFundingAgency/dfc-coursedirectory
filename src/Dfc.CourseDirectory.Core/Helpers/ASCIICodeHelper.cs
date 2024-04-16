0c73f27cd41b60dde0f0c84792b3d6e21c2be001
Lovre-Schonauer <116816530+Lovre-Schonauer@users.noreply.github.com>
2024-04-16 17:01:34 +0100

    Add dictionary which replaces unprocessable hex codes with a processable character. Reports should no longer contain unprocessable characters

--- src/Dfc.CourseDirectory.Core/Helpers/ASCIICodeHelper.cs
+++ src/Dfc.CourseDirectory.Core/Helpers/ASCIICodeHelper.cs
@@ -11,11 +11,28 @@ namespace Dfc.CourseDirectory.Core.Helpers
 {
     public static class ASCIICodeHelper
     {
+        //Please add all newly discovered unprocessable hex codes as a 'key' in this dictionary and their replacement char as the 'value'
+        private static Dictionary<string, string> _unprocessableHexCodesMapping = new Dictionary<string, string>()
+        {
+            { "&#x202F;", " " },
+            { "&#xF020;", "*" },
+            { "&#xF06E;", "*" },
+            { "&#xF0A7;", "*" },
+            { "&#xF0B7;", "*" },
+        };
+
         public static string ReplaceHexCodes(string src)
         {
             string returnstring = string.Empty;
             if (src != null)
             {
+                //Search for hex codes known to not process correctly in the reports and replace them with the key specific in _uprocessableHexCodesMapping
+                foreach (var key in _unprocessableHexCodesMapping.Keys)
+                {
+                    src = src.Replace(key, _unprocessableHexCodesMapping.GetValueOrDefault(key));
+                }
+
+                //Now check for any other hex codes and replace them with their corresponding value
                 var hexCodeRegex = new Regex(@"&#x([0-9A-Fa-f]+);");
 
                 returnstring = hexCodeRegex.Replace(src, match =>
