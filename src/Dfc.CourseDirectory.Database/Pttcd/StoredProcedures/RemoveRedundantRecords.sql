c2c6071727b444d324ec86e311269f25b38be33f
Muhammad IMRAN <Muhammad.IMRAN@EDUCATION.GOV.UK>
2024-04-29 16:20:54 +0100

    Updated RemoveRedundantRecords stored proc

--- src/Dfc.CourseDirectory.Database/Pttcd/StoredProcedures/RemoveRedundantRecords.sql
+++ src/Dfc.CourseDirectory.Database/Pttcd/StoredProcedures/RemoveRedundantRecords.sql
@@ -4,22 +4,59 @@ AS
 	DECLARE @NonLiveStatus int = 0
 	DECLARE @ArchivedStatus int = 4
 	DECLARE @UpdatedBy varchar(100) = 'RemoveRedundantRecordsStoredProc'
+	DECLARE @MaxNoOfRecordsToRemove int = 2
 	
 	DELETE 
+		TOP (@MaxNoOfRecordsToRemove) 
 	FROM 
 		Pttcd.FindACourseIndex 
 	WHERE 
 		Live = @NonLiveStatus 
 		AND LastSynced < @RetentionDate
 
+
 	DELETE 
+		TOP (@MaxNoOfRecordsToRemove) 
+		crr 
+	FROM 
+		Pttcd.CourseRuns cr
+		INNER JOIN Pttcd.CourseRunRegions crr ON crr.CourseRunId = cr.CourseRunId
+	WHERE 
+		cr.CourseRunStatus = @ArchivedStatus 
+		AND UpdatedOn < @RetentionDate
+		
+
+	DELETE 
+		TOP (@MaxNoOfRecordsToRemove) crsr
+	FROM 
+		Pttcd.CourseRuns cr
+		INNER JOIN Pttcd.CourseRunSubRegions crsr ON crsr.CourseRunId = cr.CourseRunId
+	WHERE 
+		CourseRunStatus = @ArchivedStatus 
+		AND UpdatedOn < @RetentionDate
+
+
+	DELETE 
+		TOP (@MaxNoOfRecordsToRemove) 
 	FROM 
 		Pttcd.CourseRuns 
 	WHERE 
 		CourseRunStatus = @ArchivedStatus 
 		AND UpdatedOn < @RetentionDate
+		AND CourseRunId NOT IN (Select Distinct CourseRunId From Pttcd.CourseRunRegions)
+		AND CourseRunId NOT IN (Select Distinct CourseRunId From Pttcd.CourseRunSubRegions)
 
 	DELETE 
+		TOP (@MaxNoOfRecordsToRemove)
+	FROM 
+		Pttcd.Courses 
+	WHERE 
+		CourseStatus = @ArchivedStatus 
+		and UpdatedOn < @RetentionDate
+		and CourseId NOT IN (Select Distinct CourseId From Pttcd.CourseRuns)
+
+	DELETE 
+		TOP (@MaxNoOfRecordsToRemove)
 	FROM 
 		Pttcd.Courses 
 	WHERE 
@@ -35,7 +72,9 @@ AS
 
 
 	-- Some of the records in tables are archived but UpdatedOn fields are not set. Below statements set UpdatedOn field so that these records can also be deleted.
+
 	UPDATE 
+		TOP (@MaxNoOfRecordsToRemove)
 		Pttcd.Courses 
 	SET 
 		UpdatedOn = GETDATE(),
@@ -46,21 +85,13 @@ AS
 
 	
 	UPDATE 
+		TOP (@MaxNoOfRecordsToRemove)
 		Pttcd.CourseRuns 
 	SET 
 		UpdatedOn = GETDATE(),
 		UpdatedBy = @UpdatedBy
 	WHERE 
 		CourseRunStatus = @ArchivedStatus
-		AND UpdatedOn IS NULL
-
-	UPDATE 
-		Pttcd.Venues 
-	SET 
-		UpdatedOn = GETDATE(),
-		UpdatedBy = @UpdatedBy
-	WHERE 
-		VenueStatus = @ArchivedStatus
 		AND UpdatedOn IS NULL
 
 RETURN 1
