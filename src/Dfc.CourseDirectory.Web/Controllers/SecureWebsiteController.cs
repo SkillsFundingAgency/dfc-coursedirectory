611bbed42b9b0bfb4077c62fb92f5f96c356af47
Joe Hudson-Small <Joehs@me.com>
2024-04-30 15:12:23 +0100

    validation working for course journey

--- src/Dfc.CourseDirectory.Web/Controllers/SecureWebsiteController.cs
+++ src/Dfc.CourseDirectory.Web/Controllers/SecureWebsiteController.cs
@@ -4,20 +4,27 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Dfc.CourseDirectory.Core.Validation;
+using Dfc.CourseDirectory.Core.Services;
 
 namespace Dfc.CourseDirectory.Web.Controllers
 {
-    /*
+
     [Route("api/[controller]")]
     [ApiController]
     public class SecureWebsiteController : ControllerBase
     {
+
+        private readonly IWebRiskService _webRiskService;
+        public SecureWebsiteController(IWebRiskService webRiskService)
+        {
+            _webRiskService = webRiskService;
+        }
+
         [Authorize]
         [HttpGet]
-        public async Task<bool> Validate([FromQuery] bool isSecureWebsite)
+        public async Task<bool> Validate([FromQuery] string url)
         {
-            return await Rules.SecureWebsite<bool>(isSecureWebsite);
+            return await _webRiskService.CheckForSecureUri(url);
         }
     }
-    */
 }
