f9b1e5285f688b70c5be6663063e2ccaea67d28c
madhu-pasumarthi <madhu.pasumarthi@netcompany.com>
2024-05-07 11:41:43 +0100

    Replacing GetAddress with OS

--- src/Dfc.CourseDirectory.WebV2/AddressSearch/GetAddressAddressSearchService.cs
+++ src/Dfc.CourseDirectory.WebV2/AddressSearch/GetAddressAddressSearchService.cs
@@ -67,18 +67,10 @@ namespace Dfc.CourseDirectory.WebV2.AddressSearch
 
 
 
-            if (result.FirstOrDefault() == null)
-            {
-                result = results.Results.Where(x => x.Lpi != null && x.Lpi.Uprn == Convert.ToInt64(id));
-                // start number is returning 0 when null _line1 = result.FirstOrDefault()?.Lpi.PAO_START_NUMBER.ToString()  +" " + result.FirstOrDefault()?.Lpi.PAO_TEXT?.ToString();
-                _line1 =  result.FirstOrDefault()?.Lpi.PAO_TEXT?.ToString();
-                _line2 = result.FirstOrDefault()?.Lpi.StreetDescription?.ToString() + " " + result.FirstOrDefault()?.Lpi.SAO_TEXT?.ToString();
-                _postTown = result.FirstOrDefault()?.Lpi.TownName;
-            }
-            else 
+            if (result.FirstOrDefault() != null)
             {
                 _line1 = result.FirstOrDefault()?.Dpa.ORGANISATION_NAME?.ToString() + " " + result.FirstOrDefault()?.Dpa.BUILDING_NAME?.ToString() + " " + result.FirstOrDefault()?.Dpa.SUB_BUILDING_NAME?.ToString();
-                _line2 = (result.FirstOrDefault()?.Dpa.BuildingNumber.ToString() ?? string.Empty) + " " + (result.FirstOrDefault()?.Dpa.ThoroughfareName ?? string.Empty);
+                _line2 =  (result.FirstOrDefault()?.Dpa.ThoroughfareName ?? string.Empty);
                 _postTown = result.FirstOrDefault()?.Dpa.PostTown;
             }
 
@@ -100,37 +92,21 @@ namespace Dfc.CourseDirectory.WebV2.AddressSearch
             }
 
             var result = await FindAddresses(postcode);
-
-            //return result?.Results?.Select(a =>
-            //{
-            //    return new PostcodeSearchResult
-            //    {
-            //        Id = $"{postcode}{CompositeIdDelimiter}{a.Dpa.Uprn}",
-            //        //StreetAddress = string.Join(" ", a.Dpa.Address.Where(l => !string.IsNullOrWhiteSpace(l))).Trim(),
-            //        StreetAddress = string.Join(" ", a.Dpa.Address.Trim()),
-            //        Place = a.Dpa.PostTown
-            //    };
-            //}).ToArray() ?? Array.Empty<PostcodeSearchResult>();
-
             return result?.Results?.Select(resultItem =>
             {
                 var dpa = resultItem.Dpa;
-                var lpi = resultItem.Lpi;
                 string streetAddress = "";
                 string id = "";
                 string place = "";
 
-
-                if (lpi != null)
+            if (dpa != null)
                 {
-                    streetAddress = string.Join(" ", lpi.Address.Trim());
-                    id = $"{postcode}{CompositeIdDelimiter}{lpi.Uprn}";
-                    place = $"{lpi.TownName}";
-                }
-                if (dpa != null)
-                {
-                    streetAddress = string.Join(" ", dpa.Address.Trim());
-                    id = $"{postcode}{CompositeIdDelimiter}{dpa.Uprn}";
+                    if (dpa.ORGANISATION_NAME != null && (dpa.BuildingNumber != 0 || dpa.BUILDING_NAME != null))
+                         streetAddress = string.Join(" ", dpa.Address.Replace(dpa.ORGANISATION_NAME + ", ", "").Replace(", " + dpa.Postcode, "").Trim());
+                    else
+                        streetAddress = string.Join(" ", dpa.Address.Replace(", " + dpa.Postcode, "").Trim());
+                    
+                        id = $"{postcode}{CompositeIdDelimiter}{dpa.Uprn}";
                     place = $"{dpa.PostTown}";
                 }
                 return new PostcodeSearchResult
@@ -147,17 +123,11 @@ namespace Dfc.CourseDirectory.WebV2.AddressSearch
 
         }
 
-
-
-
-
         private async Task<OsGetAddress> FindAddresses(string postcode)
         {
             var url = new Url(string.Format(_options.ApiUrl, postcode))
             .SetQueryParam("key", _options.ApiKey);
-            //.SetQueryParam("expand", "true")
-            //.SetQueryParam("dataset", "LPI");
-          
+           
 
             using (var response = await _httpClient.GetAsync(url))
             {
@@ -224,146 +194,8 @@ namespace Dfc.CourseDirectory.WebV2.AddressSearch
            
             [JsonProperty("DPA", NullValueHandling = NullValueHandling.Ignore)]
             public Dpa Dpa { get; set; }
-            [JsonProperty("LPI", NullValueHandling = NullValueHandling.Ignore)]
-            public Lpi Lpi { get; set; }
-
         }
-        public partial class Lpi
-        {
-            [JsonProperty("UPRN")]
-            [JsonConverter(typeof(ParseStringConverter))]
-            public long Uprn { get; set; }
-
-            [JsonProperty("ADDRESS")]
-            public string Address { get; set; }
-
-            [JsonProperty("USRN")]
-            [JsonConverter(typeof(ParseStringConverter))]
-            public long Usrn { get; set; }
-
-            [JsonProperty("LPI_KEY")]
-            public string LpiKey { get; set; }
-
-            [JsonProperty("PAO_TEXT", NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
-            public string PAO_TEXT { get; set; }       
-
-            [JsonProperty("SAO_TEXT", NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
-            public string SAO_TEXT { get; set; }
-
-            [JsonProperty("PAO_START_NUMBER", NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
        
-            public long PAO_START_NUMBER { get; set; }
-
-            [JsonProperty("STREET_DESCRIPTION")]
-            public string  StreetDescription { get; set; }
-
-            [JsonProperty("TOWN_NAME")]
-            public string TownName { get; set; }
-
-            [JsonProperty("ADMINISTRATIVE_AREA")]
-            public string AdministrativeArea { get; set; }
-
-            //[JsonProperty("POSTCODE_LOCATOR")]
-            //public Postcode PostcodeLocator { get; set; }
-
-            [JsonProperty("RPC")]
-            [JsonConverter(typeof(ParseStringConverter))]
-            public long Rpc { get; set; }
-
-            [JsonProperty("X_COORDINATE")]
-            public double XCoordinate { get; set; }
-
-            [JsonProperty("Y_COORDINATE")]
-            public double YCoordinate { get; set; }
-
-            [JsonProperty("STATUS")]
-            public string Status { get; set; }
-
-            [JsonProperty("LOGICAL_STATUS_CODE")]
-            [JsonConverter(typeof(ParseStringConverter))]
-            public long LogicalStatusCode { get; set; }
-
-            [JsonProperty("CLASSIFICATION_CODE")]
-            public string ClassificationCode { get; set; }
-
-            [JsonProperty("CLASSIFICATION_CODE_DESCRIPTION")]
-            public string ClassificationCodeDescription { get; set; }
-
-            [JsonProperty("LOCAL_CUSTODIAN_CODE")]
-            public long LocalCustodianCode { get; set; }
-
-            [JsonProperty("LOCAL_CUSTODIAN_CODE_DESCRIPTION")]
-            public string LocalCustodianCodeDescription { get; set; }
-
-            [JsonProperty("COUNTRY_CODE")]
-            public string CountryCode { get; set; }
-
-            [JsonProperty("COUNTRY_CODE_DESCRIPTION")]
-            public string CountryCodeDescription { get; set; }
-
-            [JsonProperty("POSTAL_ADDRESS_CODE")]
-            public string PostalAddressCode { get; set; }
-
-            [JsonProperty("POSTAL_ADDRESS_CODE_DESCRIPTION")]
-            public string PostalAddressCodeDescription { get; set; }
-
-            [JsonProperty("BLPU_STATE_CODE")]
-            [JsonConverter(typeof(ParseStringConverter))]
-            public long BlpuStateCode { get; set; }
-
-            [JsonProperty("BLPU_STATE_CODE_DESCRIPTION")]
-            public string BlpuStateCodeDescription { get; set; }
-
-            [JsonProperty("TOPOGRAPHY_LAYER_TOID")]
-            public string TopographyLayerToid { get; set; }
-
-            [JsonProperty("WARD_CODE")]
-            public string WardCode { get; set; }
-
-            [JsonProperty("LAST_UPDATE_DATE")]
-            public string LastUpdateDate { get; set; }
-
-            [JsonProperty("ENTRY_DATE")]
-            public string EntryDate { get; set; }
-
-            [JsonProperty("BLPU_STATE_DATE")]
-            public string BlpuStateDate { get; set; }
-
-            [JsonProperty("STREET_STATE_CODE")]
-            [JsonConverter(typeof(ParseStringConverter))]
-            public long StreetStateCode { get; set; }
-
-            [JsonProperty("STREET_STATE_CODE_DESCRIPTION")]
-            public string StreetStateCodeDescription { get; set; }
-
-            [JsonProperty("STREET_CLASSIFICATION_CODE")]
-            [JsonConverter(typeof(ParseStringConverter))]
-            public long StreetClassificationCode { get; set; }
-
-            [JsonProperty("STREET_CLASSIFICATION_CODE_DESCRIPTION")]
-            public string StreetClassificationCodeDescription { get; set; }
-
-            [JsonProperty("LPI_LOGICAL_STATUS_CODE")]
-            [JsonConverter(typeof(ParseStringConverter))]
-            public long LpiLogicalStatusCode { get; set; }
-
-            [JsonProperty("LPI_LOGICAL_STATUS_CODE_DESCRIPTION")]
-            public string LpiLogicalStatusCodeDescription { get; set; }
-
-            //[JsonProperty("LANGUAGE")]
-            //public Language Language { get; set; }
-
-            [JsonProperty("MATCH")]
-            public long Match { get; set; }
-
-            [JsonProperty("MATCH_DESCRIPTION")]
-            public string MatchDescription { get; set; }
-
-        
-            [JsonProperty("PAO_START_SUFFIX", NullValueHandling = NullValueHandling.Ignore)]
-            public string PaoStartSuffix { get; set; }
-        }
-
         public partial class Dpa
         {
             [JsonProperty("UPRN")]
@@ -521,7 +353,6 @@ namespace Dfc.CourseDirectory.WebV2.AddressSearch
         private class FindAddressResult
         {
             public string PostCode { get; set; }
-
             public IEnumerable<FindAddressResultItem> Addresses { get; set; }
         }
 
