790bc9a9847d259a9882141598692f451e726959
Muhammad IMRAN <Muhammad.IMRAN@EDUCATION.GOV.UK>
2024-09-09 15:54:55 +0100

    Remove GlobalSuppressions.cs file. No need of it.

--- src/Dfc.CourseDirectory.Core/GlobalSuppressions.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "CS8981:The type name only contains lower-cased ascii characters.",
-    Justification = "Generated code.",
-    Scope = "namespaceanddescendants",
-    Target = "UkrlpService")]
