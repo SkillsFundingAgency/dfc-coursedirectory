f202df87f140b729123e72eeb4b4ac6160d57742
Muhammad IMRAN <Muhammad.IMRAN@EDUCATION.GOV.UK>
2024-09-09 11:05:41 +0100

    Add GlobalSuppressions.cs file to suppress compiler warning CS8981

---
+++ src/Dfc.CourseDirectory.Core/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "CS8981:The type name only contains lower-cased ascii characters.",
+    Justification = "Generated code.",
+    Scope = "namespaceanddescendants",
+    Target = "UkrlpService")]
