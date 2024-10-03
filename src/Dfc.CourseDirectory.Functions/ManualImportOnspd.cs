f4509c80c1c843dda1abc9b1ab49281cb0de7554
Joe Hudson-Small <Joehs@me.com>
2024-10-03 10:41:51 +0100

    refactor trigger method

--- src/Dfc.CourseDirectory.Functions/ManualImportOnspd.cs
+++ src/Dfc.CourseDirectory.Functions/ManualImportOnspd.cs
@@ -1,6 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.IO;
+using System.Threading.Tasks;
 using Dfc.CourseDirectory.Core.ReferenceData.Onspd;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
 
 namespace Dfc.CourseDirectory.Functions
 {
@@ -16,6 +21,21 @@ namespace Dfc.CourseDirectory.Functions
         [FunctionName("ManualImportONSPD")]
         [Singleton]
         [NoAutomaticTrigger]
-        public Task Run(string filename) => _onspdDataImporter.ManualDataImport(filename);
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request)
+
+        {
+            string requestBody = await new StreamReader(request.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            string filename = data?.filename;
+
+            if (string.IsNullOrEmpty(filename))
+            {
+                return new BadRequestObjectResult("Bad request: please indicate the target .csv filename using the filename parameter.");
+            }
+
+            await _onspdDataImporter.ManualDataImport(filename);
+
+            return new OkObjectResult($"Manual data import triggered for filename: {filename}");
+        }
     }
 }
