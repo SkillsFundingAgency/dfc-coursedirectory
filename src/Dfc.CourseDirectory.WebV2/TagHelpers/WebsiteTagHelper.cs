978713d9a977101297c2d75bf68c413e0c2d2f47
Muhammad IMRAN <Muhammad.IMRAN@EDUCATION.GOV.UK>
2025-01-13 10:23:08 +0000

    Add new TagHelper to modify Website field error message style in T-Levels and Venues pages.

---
+++ src/Dfc.CourseDirectory.WebV2/TagHelpers/WebsiteTagHelper.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GovUk.Frontend.AspNetCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using static GovUk.Frontend.AspNetCore.ComponentDefaults;
+
+namespace Dfc.CourseDirectory.WebV2.TagHelpers
+{
+    [HtmlTargetElement("website-input")]
+    public class WebsiteTagHelper : TagHelper
+    {
+        private readonly IGovUkHtmlGenerator _generator;
+
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression For { get; set; }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public string Hint { get; set; }
+        public string Type { get; set; } = "text";
+        public string Value { get; set; }        
+        public ModelStateEntry ModelStateEntry { get; set; }
+
+        public WebsiteTagHelper(IGovUkHtmlGenerator generator)
+        {
+            _generator = generator;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // Use the model property name if not explicitly provided
+            var id = Id ?? For?.Name;
+            var name = Name ?? For?.Name;
+
+            // Extract validation attributes from the model
+            var validationAttributes = For?.GetValidationAttributes();
+
+            output.TagName = "div";
+            output.Attributes.Add("class", "govuk-form-group");
+
+            var errorHtml = string.Empty;
+            var errorInputClass = string.Empty;
+            
+            var error = ModelStateEntry?.Errors.Any() ?? false;
+            var errorMessage = string.Empty;
+            var webSecurityRiskError = false;
+            if (error)
+            {
+                errorMessage = ModelStateEntry.Errors[0].ErrorMessage;
+
+                webSecurityRiskError = errorMessage.Contains("security issue.");
+
+                if (webSecurityRiskError)
+                {
+                    int firstFullStopIndex = errorMessage.IndexOf('.');
+                    errorMessage = firstFullStopIndex != -1
+                        ? errorMessage.Substring(0, firstFullStopIndex + 1)
+                        : errorMessage;
+                }
+
+                output.Attributes.SetAttribute("class", "govuk-form-group--error");
+                errorHtml = $@"
+                <span class=""govuk-error-message"" style="""">
+                    <span class=""govuk-visually-hidden"">Error:</span>
+                    {errorMessage}
+                </span>";
+                errorInputClass = "govuk-input--error";
+            }            
+
+            // Generate the label
+            var labelHtml = $@"
+            <label class='govuk-label--m govuk-label' for='{Id}'>
+                {Label}
+            </label>";
+
+            // Generate the hint
+            var hintHtml = string.IsNullOrEmpty(Hint) ? "" : $@"
+            <div id='{Id}-hint' class='govuk-hint'>
+                {Hint}
+            </div>";
+
+            var inputHtml = $@"
+            <input class='govuk-input {errorInputClass}' id='{id}' name='{name}' value='{Value}' type='{Type}' aria-describedby='{id}-hint'>";
+
+            var secureWebsiteHintMessage = string.Empty;
+            if (error && webSecurityRiskError)
+            {
+                secureWebsiteHintMessage = $@"<div secure-website-message-hint-text style=""display:block"">
+            <span class=""govuk-hint govuk-!-margin-top-3"">
+                This URL has been reported by <a href=""https://cloud.google.com/web-risk/docs/advisory"" target=""_blank"">Google Cloud's Web Risk (opens in a new tab)</a> as having a security issue and might be malicious.
+            </span>
+            <span class=""govuk-hint govuk-!-margin-top-3"">
+                Check the URL you have entered is correct. If the URL is correct, you'll need to review the security of your website.
+                You cannot upload this URL to the course directory until it can pass this security check.
+            </span>
+        </div>";
+            }
+
+            var htmlContent = $"{labelHtml}{errorHtml}{hintHtml}{inputHtml}{secureWebsiteHintMessage}";
+
+            // Combine all elements
+            output.Content.SetHtmlContent(htmlContent);
+        }
+    }
+
+    public static class ModelExpressionExtensions
+    {
+        // Extension method to extract validation attributes from the model metadata
+        public static IDictionary<string, string> GetValidationAttributes(this ModelExpression modelExpression)
+        {
+            var attributes = new Dictionary<string, string>();
+
+            if (modelExpression?.Metadata?.IsRequired ?? false)
+            {
+                attributes.Add("data-val", "true");
+                attributes.Add("data-val-required", modelExpression.Metadata.ModelBindingMessageProvider.ValueMustNotBeNullAccessor(modelExpression.Name));
+            }
+
+            return attributes;
+        }
+    }
+}
