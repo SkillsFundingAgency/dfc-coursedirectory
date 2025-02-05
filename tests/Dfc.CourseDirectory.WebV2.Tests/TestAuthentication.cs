128a44c7bff757542dc9c15a02b70db709d62903
amjones99 <59894100+amjones99@users.noreply.github.com>
2025-02-05 16:52:03 +0000

    Resolve some build warnings

--- tests/Dfc.CourseDirectory.WebV2.Tests/TestAuthentication.cs
+++ tests/Dfc.CourseDirectory.WebV2.Tests/TestAuthentication.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Encodings.Web;
+﻿using System;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Logging;
