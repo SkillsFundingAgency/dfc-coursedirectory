f68d868976325ded4bc964c43b6607c5d160dc2a
Gavin COMPTON <Gavin.COMPTON@EDUCATION.GOV.UK>
2025-07-01 15:25:31 +0100

    Updated code - removed the IActionResult return value.

--- src/Dfc.CourseDirectory.Functions/ManualImportOnspd.cs
+++ src/Dfc.CourseDirectory.Functions/ManualImportOnspd.cs
@@ -1,33 +1,38 @@
 ﻿using Dfc.CourseDirectory.Core.ReferenceData.Onspd;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
 
 namespace Dfc.CourseDirectory.Functions
 {
     public class ManualImportONSPD
     {
         private readonly OnspdDataImporter _onspdDataImporter;
+        private readonly ILogger<ManualImportONSPD> _logger;
 
-        public ManualImportONSPD(OnspdDataImporter onspdDataImporter)
+        public ManualImportONSPD(
+            OnspdDataImporter onspdDataImporter,
+            ILogger<ManualImportONSPD> logger)
         {
             _onspdDataImporter = onspdDataImporter;
+            _logger = logger;
         }
 
         [Function("ManualImportONSPD")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request)
+        public async Task Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request)
 
         {
             string filename = request.Query["filename"];
 
             if (string.IsNullOrEmpty(filename))
             {
-                return new BadRequestObjectResult("Bad request: please indicate the target .csv filename using the filename parameter.");
+                _logger.LogError("Bad request: please indicate the target .csv filename using the filename parameter.");
             }
-
-            await _onspdDataImporter.ManualDataImport(filename);
-
-            return new OkObjectResult($"Manual data import triggered for filename: {filename}");
+            else
+            {
+                _logger.LogInformation($"Manual data import triggered for filename: {filename}");
+                await _onspdDataImporter.ManualDataImport(filename);
+            }
         }
     }
 }
