f7b026a099df4066c1be812087b89c1d9743d2f0
hnandra <h_nandra@hotmail.co.uk>
2025-07-03 14:51:15 +0100

    Revert "Updated code - removed the IActionResult return value."
    This reverts commit f68d868976325ded4bc964c43b6607c5d160dc2a.

--- src/Dfc.CourseDirectory.Functions/ManualImportOnspd.cs
+++ src/Dfc.CourseDirectory.Functions/ManualImportOnspd.cs
@@ -1,38 +1,33 @@
 ﻿using Dfc.CourseDirectory.Core.ReferenceData.Onspd;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Logging;
 
 namespace Dfc.CourseDirectory.Functions
 {
     public class ManualImportONSPD
     {
         private readonly OnspdDataImporter _onspdDataImporter;
-        private readonly ILogger<ManualImportONSPD> _logger;
 
-        public ManualImportONSPD(
-            OnspdDataImporter onspdDataImporter,
-            ILogger<ManualImportONSPD> logger)
+        public ManualImportONSPD(OnspdDataImporter onspdDataImporter)
         {
             _onspdDataImporter = onspdDataImporter;
-            _logger = logger;
         }
 
         [Function("ManualImportONSPD")]
-        public async Task Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request)
 
         {
             string filename = request.Query["filename"];
 
             if (string.IsNullOrEmpty(filename))
             {
-                _logger.LogError("Bad request: please indicate the target .csv filename using the filename parameter.");
+                return new BadRequestObjectResult("Bad request: please indicate the target .csv filename using the filename parameter.");
             }
-            else
-            {
-                _logger.LogInformation($"Manual data import triggered for filename: {filename}");
-                await _onspdDataImporter.ManualDataImport(filename);
-            }
+
+            await _onspdDataImporter.ManualDataImport(filename);
+
+            return new OkObjectResult($"Manual data import triggered for filename: {filename}");
         }
     }
 }
