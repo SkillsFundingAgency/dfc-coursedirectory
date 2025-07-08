0f682bfadee9683bc99319498f485a5bd2aedd41
Pradeep Voorukonda <87078845+pvoorukonda-DFE@users.noreply.github.com>
2025-07-08 10:46:43 +0100

    fixing dacpac path

--- Dfc.CourseDirectory.Core/SqlDeployHelper.cs
+++ Dfc.CourseDirectory.Core/SqlDeployHelper.cs
@@ -14,7 +14,7 @@ namespace Dfc.CourseDirectory.Core
 
             var dacpacLocation = Path.GetFullPath(Path.Combine(
                 Environment.CurrentDirectory,
-                "../../../../../../Dfc.CourseDirectory.Database/bin",
+                "../../../Dfc.CourseDirectory.Database/bin",
 #if DEBUG
                 "Debug",
 #else
