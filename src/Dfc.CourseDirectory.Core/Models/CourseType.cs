7336ae77d4ac570a39e418270508c133f89145a8
Siva MURUGANANDHAM <Siva.MURUGANANDHAM@EDUCATION.GOV.UK>
2025-07-17 19:15:12 +0100

    GetDescription changes

--- src/Dfc.CourseDirectory.Core/Models/CourseType.cs
+++ src/Dfc.CourseDirectory.Core/Models/CourseType.cs
@@ -34,6 +34,11 @@ namespace Dfc.CourseDirectory.Core.Models
                 CourseType.FreeCoursesForJobs => "Free Courses for Jobs",                
                 CourseType.Multiply => "Multiply",
                 CourseType.SkillsBootcamp => "Skills Bootcamp",
+                CourseType.GCEASLevel => "AS  Levels",
+                CourseType.GCEALevel => "A  Levels",
+                CourseType.RegulatedQualificationFramework => "Regulated Qualification Framework (RQF)",
+                CourseType.VocationalRegulatedQualifications => "Vocational Regulated Qualifications (VRQ)",
+                CourseType.Degree => "Degree",
                 _ => throw new NotImplementedException($"Unknown value: '{courseType}'.")
             };
         }
