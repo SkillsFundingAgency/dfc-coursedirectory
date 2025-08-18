3a1427ab428d8859dd4761e1ff8f90083baeca35
James <152189499+JamesPrior-07@users.noreply.github.com>
2025-08-18 14:58:35 +0100

    Update ImportLars function to run daily

--- src/Dfc.CourseDirectory.Functions/ImportLarsData.cs
+++ src/Dfc.CourseDirectory.Functions/ImportLarsData.cs
@@ -1,5 +1,4 @@
 using Dfc.CourseDirectory.Core.ReferenceData.Lars;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 
@@ -15,7 +14,7 @@ namespace Dfc.CourseDirectory.Functions
         }
 
         [Function("ImportLarsData")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request)
+        public async Task<IActionResult> Run([TimerTrigger("%ImportLarsDataTriggerTimer%", RunOnStartup = true)] TimerInfo timer)
         {
             await _dataImporter.ImportData();
             return new OkObjectResult("LARS data import triggered");
