e13474bf0bc7a9d2780d653b9e1fa6677c9b19c9
Siva MURUGANANDHAM <Siva.MURUGANANDHAM@EDUCATION.GOV.UK>
2025-09-10 09:28:44 +0100

    WIP

---
+++ src/Dfc.CourseDirectory.Core/DataManagement/FileUploadProcessor.Providers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataManagement.Schemas;
+using Dfc.CourseDirectory.Core.DataStore.Sql;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Models;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+using Dfc.CourseDirectory.Core.Models;
+using Dfc.CourseDirectory.Core.Services;
+using Dfc.CourseDirectory.Core.Validation.VenueValidation;
+using FluentValidation;
+
+namespace Dfc.CourseDirectory.Core.DataManagement
+{
+    public partial class FileUploadProcessor
+    {
+        public async Task<SaveProviderFileResult> SaveProviderFile(Stream stream, UserInfo uploadedBy)
+        {
+            CheckStreamIsProcessable(stream);
+
+            if (await FileIsEmpty(stream))
+            {
+                return SaveProviderFileResult.EmptyFile();
+            }
+
+            if (!await LooksLikeCsv(stream))
+            {
+                return SaveProviderFileResult.InvalidFile();
+            }
+
+            return null;
+
+        }
+
+    }
+}
---
+++ src/Dfc.CourseDirectory.WebV2/Features/DataManagement/Providers/ProvidersDataManagementController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core.DataManagement.Schemas;
+using Dfc.CourseDirectory.Core.Models;
+using Dfc.CourseDirectory.WebV2;
+using Dfc.CourseDirectory.WebV2.Features.DataManagement.Courses;
+using Dfc.CourseDirectory.WebV2.Filters;
+using Dfc.CourseDirectory.WebV2.ModelBinding;
+using Dfc.CourseDirectory.WebV2.Mvc;
+using FormFlow;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.SqlServer.Dac.Model;
+using ErrorsWhatNext = Dfc.CourseDirectory.WebV2.Features.DataManagement.Courses.Errors.WhatNext;
+
+namespace Dfc.CourseDirectory.WebV2.Features.DataManagement.Providers
+{
+    [Route("data-upload/providers")]
+    public class ProvidersDataManagementController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IProviderContextProvider _providerContextProvider;
+
+        public ProvidersDataManagementController(IMediator mediator, IProviderContextProvider providerContextProvider)
+        {
+            _mediator = mediator;
+            _providerContextProvider = providerContextProvider;
+        }
+
+        [HttpPost("upload")]
+        public IActionResult Upload(Upload.Command command)
+        {
+            var file = Request.Form.Files?.GetFile(nameof(command.File));
+
+            if (file != null && !file.FileName.Contains("Active_Providers"))
+            {
+                ModelState.AddModelError(nameof(command.File), "The file name doesn't contain");
+
+            }
+            return View(command);
+        }
+    }
+}
