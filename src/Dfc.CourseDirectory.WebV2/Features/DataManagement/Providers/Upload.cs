a7b9856dadbe2fc98ba2a14481aba6c3d958ecb9
Siva MURUGANANDHAM <Siva.MURUGANANDHAM@EDUCATION.GOV.UK>
2025-09-29 10:00:44 +0100

    Merged validation changes

---
+++ src/Dfc.CourseDirectory.WebV2/Features/DataManagement/Providers/Upload.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dfc.CourseDirectory.Core;
+using Dfc.CourseDirectory.Core.DataManagement;
+using Dfc.CourseDirectory.Core.DataStore.Sql;
+using Dfc.CourseDirectory.Core.DataStore.Sql.Queries;
+using Dfc.CourseDirectory.Core.Models;
+using Dfc.CourseDirectory.Core.Validation;
+using Dfc.CourseDirectory.WebV2.Security;
+using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using OneOf;
+
+namespace Dfc.CourseDirectory.WebV2.Features.DataManagement.Providers.Upload
+{
+    public class Query : IRequest<ViewModel>
+    {
+    }
+
+    public class ViewModel : Command
+    {
+        public int VenueCount { get; set; }
+    }
+
+    public class Command : IRequest<OneOf<UploadFailedResult, UploadSucceededResult>>
+    {
+        public IFormFile File { get; set; }
+    }
+
+    public enum UploadSucceededResult
+    {
+        ProcessingInProgress,
+        ProcessingCompletedWithErrors,
+        ProcessingCompletedSuccessfully
+    }
+
+    public class UploadFailedResult : ModelWithErrors<ViewModel>
+    {
+        public UploadFailedResult(ViewModel model, ValidationResult validationResult)
+            : base(model, validationResult)
+        {
+            MissingHeaders = Array.Empty<string>();
+        }
+
+        public UploadFailedResult(ViewModel model, string fileErrorMessage, IEnumerable<string> missingHeaders = null)
+            : base(model, CreateValidationResult(fileErrorMessage))
+        {
+            MissingHeaders = missingHeaders?.ToArray() ?? Array.Empty<string>();
+        }
+
+        public IReadOnlyCollection<string> MissingHeaders { get; }
+
+        private static ValidationResult CreateValidationResult(string fileErrorMessage) =>
+            new ValidationResult(new[]
+            {
+                new ValidationFailure(nameof(Command.File), fileErrorMessage)
+            });
+    }
+
+    public class Handler :
+        IRequestHandler<Query, ViewModel>,
+        IRequestHandler<Command, OneOf<UploadFailedResult, UploadSucceededResult>>
+    {
+        private readonly ISqlQueryDispatcher _sqlQueryDispatcher;
+        private readonly IFileUploadProcessor _fileUploadProcessor;
+        private readonly IProviderContextProvider _providerContextProvider;
+        private readonly ICurrentUserProvider _currentUserProvider;
+        private readonly IOptions<DataManagementOptions> _optionsAccessor;
+
+        public Handler(
+            ISqlQueryDispatcher sqlQueryDispatcher,
+            IFileUploadProcessor fileUploadProcessor,
+            IProviderContextProvider providerContextProvider,
+            ICurrentUserProvider currentUserProvider,
+            IOptions<DataManagementOptions> optionsAccessor)
+        {
+            _sqlQueryDispatcher = sqlQueryDispatcher;
+            _fileUploadProcessor = fileUploadProcessor;
+            _providerContextProvider = providerContextProvider;
+            _currentUserProvider = currentUserProvider;
+            _optionsAccessor = optionsAccessor;
+        }
+
+        public Task<ViewModel> Handle(Query request, CancellationToken cancellationToken) => CreateViewModel();
+
+        public async Task<OneOf<UploadFailedResult, UploadSucceededResult>> Handle(
+            Command request,
+            CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<ViewModel> CreateViewModel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
+        {
+            RuleFor(x => x.File)
+                .NotNull()
+                    .WithMessage("Select a CSV")
+                .Must(file => file == null || file.Length <= Constants.VenueFileMaxSizeBytes)
+                    .WithMessage($"The selected file must be smaller than {Constants.VenueFileMaxSizeLabel}");
+        }
+    }
+}
