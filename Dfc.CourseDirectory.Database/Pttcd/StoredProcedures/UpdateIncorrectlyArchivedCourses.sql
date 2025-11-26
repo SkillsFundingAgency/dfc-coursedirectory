bf5cd33a6a167b32c6621bc30dd53e18fa30a30f
mlarkowski-DFE <178817151+mlarkowski-DFE@users.noreply.github.com>
2025-11-26 10:21:36 +0000

    added a stored procedure to update incorrectly archived courses

---
+++ Dfc.CourseDirectory.Database/Pttcd/StoredProcedures/UpdateIncorrectlyArchivedCourses.sql
@@ -0,0 +1,34 @@
+﻿CREATE PROCEDURE [dbo].[UpdateIncorrectlyArchivedCourses]
+AS
+BEGIN
+
+	DECLARE @ArchivedCourseStatus INT = 4;
+	DECLARE @LiveCourseStatus INT = 1;
+	DECLARE @UpdatedOn DATETIME = GETUTCDATE(); 
+	DECLARE @UpdatedBy VARCHAR(100) = 'Stored Procedure - UpdateIncorrectlyArchivedCourses';	
+
+	
+
+	SELECT 
+		CourseId,
+		CourseRunId
+	INTO #TempTable
+	FROM 
+		[Pttcd].[FindACourseIndex]
+	WHERE 
+		CourseId IN ( SELECT CourseId FROM [Pttcd].Courses WHERE CourseStatus = @ArchivedCourseStatus) AND Live = @LiveCourseStatus
+		
+
+	UPDATE 
+		Pttcd.Courses
+	SET
+		CourseStatus = @LiveCourseStatus,
+		UpdatedOn = @UpdatedOn,
+		UpdatedBy = @UpdatedBy
+	WHERE 
+		CourseId IN (SELECT CourseId FROM #TempTable)
+
+
+	DROP TABLE #TempTable
+
+END
