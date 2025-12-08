9ce6852bf9dbc17b574be813541a41ecdd6b7d54
mpasumarthi <madhu.pasumarthi@education.gov.uk>
2025-12-08 16:01:06 +0000

    Disallow HTML tags

--- Dfc.CourseDirectory.WebV2/Validation/RegexPatterns.cs
+++ Dfc.CourseDirectory.WebV2/Validation/RegexPatterns.cs
@@ -7,8 +7,10 @@
         public const string AllowValidCharactersRelaxed =
             @"^[a-zA-Z0-9/\n/\r/\\u/\¬\!\£\$\%\^\&\*\\é\\è\\ﬁ\(\)_\+\-\=\{\}\[\]\;\:\@\'\#\~\,\<\>\.\?\/\|\`\•\·\●\\’\‘\“\”\—\-\–\‐\‐\…\:/\°\®\\â\\ç\\ñ\\ü\\ø\♦\™\\t/\s\¼\¾\½\" +"\"" + "\\\\]+$";
             
-        public const string DisallowHTMLTags = @"^(\<.*?\>).*?(\<\/.*?\>)"; //Not tested.
+        public const string DisallowHTMLTags = @"^(\&lt;.*?\&gt;).*?(\&lt;\/.*?\&gt;)"; //Not tested.
 
         public const string Base64Encoded = @"^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$";
+
+        public const string BlackListHTMLTags = @"^(?!.*<(\/?\s*(script|style|iframe|embed|object|applet|link|meta|base|form|input|label|select|option|textarea|img|a)\b[^>]*>)).*$";
     }
 }
