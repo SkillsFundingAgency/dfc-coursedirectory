db75b6977af4640bfe980672e55eb7794ad0d1a1
mpasumarthi <madhu.pasumarthi@education.gov.uk>
2025-12-09 09:57:13 +0000

    Removing unnecessary comments

--- Dfc.CourseDirectory.WebV2/Validation/RegexPatterns.cs
+++ Dfc.CourseDirectory.WebV2/Validation/RegexPatterns.cs
@@ -7,7 +7,7 @@
         public const string AllowValidCharactersRelaxed =
             @"^[a-zA-Z0-9/\n/\r/\\u/\¬\!\£\$\%\^\&\*\\é\\è\\ﬁ\(\)_\+\-\=\{\}\[\]\;\:\@\'\#\~\,\<\>\.\?\/\|\`\•\·\●\\’\‘\“\”\—\-\–\‐\‐\…\:/\°\®\\â\\ç\\ñ\\ü\\ø\♦\™\\t/\s\¼\¾\½\" +"\"" + "\\\\]+$";
             
-        public const string DisallowHTMLTags = @"^(\&lt;.*?\&gt;).*?(\&lt;\/.*?\&gt;)"; //Not tested.
+        public const string DisallowHTMLTags = @"^(\&lt;.*?\&gt;).*?(\&lt;\/.*?\&gt;)"; 
 
         public const string Base64Encoded = @"^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$";
 
