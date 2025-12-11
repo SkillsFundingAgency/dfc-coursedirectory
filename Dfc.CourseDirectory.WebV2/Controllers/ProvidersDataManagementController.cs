e54cb4cfc91b1b838fcccc1b0072b84a694bffb7
Siva MURUGANANDHAM <Siva.MURUGANANDHAM@EDUCATION.GOV.UK>
2025-12-11 00:21:35 +0000

    Home page changes

--- Dfc.CourseDirectory.WebV2/Controllers/ProvidersDataManagementController.cs
+++ Dfc.CourseDirectory.WebV2/Controllers/ProvidersDataManagementController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Dfc.CourseDirectory.Core;
@@ -8,17 +9,21 @@ using Dfc.CourseDirectory.Core.Extensions;
 using Dfc.CourseDirectory.Core.Filters;
 using Dfc.CourseDirectory.Core.Middleware;
 using Dfc.CourseDirectory.Core.Models;
+using Dfc.CourseDirectory.Core.Security;
 using Dfc.CourseDirectory.WebV2.ModelBinding;
 using Dfc.CourseDirectory.WebV2.Mvc;
-using Upload = Dfc.CourseDirectory.WebV2.ViewModels.DataManagement.Providers.Upload;
 using FormFlow;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ErrorsWhatNext = Dfc.CourseDirectory.WebV2.ViewModels.DataManagement.Courses.Errors.WhatNext;
+using Home = Dfc.CourseDirectory.WebV2.ViewModels.DataManagement.Providers.Home;
+using Upload = Dfc.CourseDirectory.WebV2.ViewModels.DataManagement.Providers.Upload;
 
 namespace Dfc.CourseDirectory.WebV2.Controllers
 {
     [Route("data-upload/providers")]
+    [Authorize(Policy = AuthorizationPolicyNames.Admin)]
     public class ProvidersDataManagementController : Controller
     {
         private readonly IMediator _mediator;
@@ -31,8 +36,25 @@ namespace Dfc.CourseDirectory.WebV2.Controllers
         }
 
         [HttpGet("")]
-        public async Task<IActionResult> Index() =>
-            await _mediator.SendAndMapResponse(new Upload.Query(), vm => View("Upload", vm));
+        public async Task<IActionResult> Index()
+        {
+            return View(new Home.ViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Home.ViewModel vm)
+        {
+            switch(vm.ProviderUploadType) {
+                case Home.ProviderUploadType.Inactive:
+                    return RedirectToAction("InactiveProviders");
+                default:
+                    return RedirectToAction("ActiveProviders");
+            }
+        }
+
+        [HttpGet("active")]
+        public async Task<IActionResult> ActiveProviders() =>
+           await _mediator.SendAndMapResponse(new Upload.Query(), vm => View("Upload", vm));
 
         [HttpGet("inactive")]
         public async Task<IActionResult> InactiveProviders() =>
