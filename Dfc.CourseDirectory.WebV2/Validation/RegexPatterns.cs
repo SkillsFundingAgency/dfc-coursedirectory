738706eee9539fe8a5bd9d10db6e52d72ecd7884
mpasumarthi <madhu.pasumarthi@education.gov.uk>
2026-01-08 20:09:42 +0000

    Picking Error from Resx file for WhoCoursefor

--- Dfc.CourseDirectory.WebV2/Validation/RegexPatterns.cs
+++ Dfc.CourseDirectory.WebV2/Validation/RegexPatterns.cs
@@ -7,7 +7,8 @@
         public const string AllowValidCharactersRelaxed =
             @"^[a-zA-Z0-9/\n/\r/\\u/\¬\!\£\$\%\^\&\*\\é\\è\\ﬁ\(\)_\+\-\=\{\}\[\]\;\:\@\'\#\~\,\<\>\.\?\/\|\`\•\·\●\\’\‘\“\”\—\-\–\‐\‐\…\:/\°\®\\â\\ç\\ñ\\ü\\ø\♦\™\\t/\s\¼\¾\½\" +"\"" + "\\\\]+$";
             
-        public const string DisallowHTMLTags = @"^(?!.*(?:</?\s*[A-Za-z][A-Za-z0-9-]*(?:\s+[^<>]*?)?\s*>|&lt;/?\s*[A-Za-z][A-Za-z0-9-]*(?:\s+[^&]*?)?\s*&gt;)).+$";
+        //public const string DisallowHTMLTags = @"^(?!.*(?:</?\s*[A-Za-z][A-Za-z0-9-]*(?:\s+[^<>]*?)?\s*>|&lt;/?\s*[A-Za-z][A-Za-z0-9-]*(?:\s+[^&]*?)?\s*&gt;)).+$";
+        public const string DisallowHTMLTags = @"^(<>%`)";
 
         public const string Base64Encoded = @"^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$";
 
