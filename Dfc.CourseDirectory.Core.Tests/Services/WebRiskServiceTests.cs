3009f114a8b33feba3371a073899b558c54351ac
mpasumarthi <madhu.pasumarthi@education.gov.uk>
2026-02-26 11:08:34 +0000

    Webrisk API check for PerformanceTesting

--- Dfc.CourseDirectory.Core.Tests/Services/WebRiskServiceTests.cs
+++ Dfc.CourseDirectory.Core.Tests/Services/WebRiskServiceTests.cs
@@ -16,7 +16,7 @@ namespace Dfc.CourseDirectory.Core.Tests.Services
         public async Task CheckForSecureUri_WithKnownThreat_FailsValidation()
         {
             // Arrange
-            var options = Options.Create(new GoogleWebRiskSettings { ApiKey = "X" });
+            var options = Options.Create(new GoogleWebRiskSettings { ApiKey = "X", PerformanceTesting=false });
 
             var expectedData = "threat";
             var httpClientFactoryMock = new Mock<IHttpClientFactory>();
