836ea08b944a6e9b70a15e457496568306a8a540
Pradeep Voorukonda <87078845+pvoorukonda-DFE@users.noreply.github.com>
2026-02-26 16:36:22 +0000

    moved rollback data script predeployment so that temp table won't get removed

--- Dfc.CourseDirectory.Database/Scripts/Script.PostDeploy.sql
+++ Dfc.CourseDirectory.Database/Scripts/Script.PostDeploy.sql
@@ -72,32 +72,3 @@ BEGIN CATCH
 	ROLLBACK;
 END CATCH
 
--- AD-255086: ROLLBACK HTML TAGS REMOVED FROM COURSE DATA FIELDS SUCH AS COURSE DESCRIPTION, ENTRY REQUIREMENTS, 
--- WHAT YOU'LL LEARN, HOW YOU'LL LEARN, WHAT YOU'LL NEED, HOW YOU'LL BE ASSESSED AND WHERE NEXT FIELDS IN PTTCD.COURSES TABLE
-
-BEGIN TRY
-	IF EXISTS(SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'temp_courses')
-	BEGIN
-		BEGIN TRANSACTION
-
-		UPDATE C
-		   SET C.[CourseDescription] = TC.CourseDescription
-			  ,C.[EntryRequirements] = TC.EntryRequirements
-			  ,C.[WhatYoullLearn] = TC.WhatYoullLearn
-			  ,C.[HowYoullLearn] = TC.HowYoullLearn
-			  ,C.[WhatYoullNeed] = TC.WhatYoullNeed
-			  ,C.[HowYoullBeAssessed] = TC.HowYoullBeAssessed
-			  ,C.[WhereNext] = TC.WhereNext
-			  ,C.UpdatedOn = GETUTCDATE()
-			  ,C.UpdatedBy = 'PostDeploymentScript - Rollback Release'
-		FROM [Pttcd].[Courses] AS C
-		JOIN [Pttcd].temp_courses AS TC ON C.CourseId = TC.CourseId AND C.ProviderId =  TC.ProviderId
-
-		DROP TABLE Pttcd.temp_courses;
-
-		COMMIT TRANSACTION;
-	END
-END TRY
-BEGIN CATCH
-	ROLLBACK TRANSACTION;
-END CATCH
